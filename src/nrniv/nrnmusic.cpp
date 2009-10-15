fc6542061bf5077b1c8e7a5d73c486d3865317d8
Michael Hines <michael.hines@yale.edu>
2009-10-15 11:00:45 -0400

    testing two cell. hangs in switch to run phase

--- src/nrniv/nrnmusic.cpp
+++ src/nrniv/nrnmusic.cpp
@@ -97,6 +97,7 @@ void nrnmusic_injectlist(void* vp, double tt) {
 }
 
 void nrnmusic_inject(void* vport, int gi, double tt) {
+	printf("nrnmusic_inject %lx %d %g\n", (long)vport, gi, tt);
 	((MUSIC::EventOutputPort*)vport)->
 	  insertEvent(tt, (MUSIC::GlobalIndex)gi);
 }
@@ -114,6 +115,7 @@ void NrnMusicEventHandler::filltable(NRNMUSIC::EventInputPort* port, int cnt) {
 void NrnMusicEventHandler::operator () (double t, MUSIC::LocalIndex id) {
 	PreSyn* ps = table[id];
 	ps->send(t, net_cvode_instance, nrn_threads);
+printf("event handler t=%g id=%d\n ps=%lx\n", t, int(id), (long)ps);
 }
 
 NRNMUSIC::EventOutputPort* NRNMUSIC::publishEventOutput (std::string id) {
@@ -176,6 +178,7 @@ PyObject* NRNMUSIC::EventInputPort::index2target(int gi, PyObject* ptarget) {
 	Object* o = hoc_new_object(netcon_sym_, nc);
 	nc->obj_ = o;
 	PyObject* po = nrnpy_ho2po(o);
+printf("index2target %d %s\n", gi, hoc_object_name(target));
 	return po;
 }
 
@@ -223,6 +226,7 @@ static void nrnmusic_runtime_phase() {
 		int cnt = 0;
 		for (TableIterator(Gi2PreSynTable) j(*pst); j.more(); j.next()) {
 			int gi = j.cur_key();
+printf("input port eip=%lx gi=%d\n", (long)eip, gi);
 			gindices.push_back (gi);
 			++cnt;
 		}
@@ -253,7 +257,9 @@ printf("output port eop=%lx gi = %d\n", (long)eop, gi);
 	delete music_output_ports;
 
 	//switch to the runtime phase
+printf("usable_mindelay = %g\n", usable_mindelay_);
 	nrnmusic_runtime = new MUSIC::Runtime(nrnmusic_setup, usable_mindelay_);
+printf("two\n");
 	npme = new NetParMusicEvent();
 	npme->send(0, net_cvode_instance, nrn_threads);
 }
