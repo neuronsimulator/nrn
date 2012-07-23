d204f40a628dc432fb6defbb8bb3021f71b4714f
Michael Hines <michael.hines@yale.edu>
2012-07-23 10:46:05 -0400

    Botched changeset 709:faa3318bb455. Now correct.

--- src/ivoc/graph.cpp
+++ src/ivoc/graph.cpp
@@ -476,10 +476,11 @@ IFGUI
 			expr = gargstr(1);
 			if (hoc_is_str_arg(2)) {
 				pd = hoc_val_pointer(gargstr(2));
+				ioff += 1;
 			}else if (hoc_is_pdouble_arg(2)) {
 				pd = hoc_pgetarg(2);
+				ioff += 1;
 			}
-			ioff += 1;
 		}else if (hoc_is_str_arg(2)) { // 1 label, 2 expression
 			lab = gargstr(1);
 			expr = gargstr(2);
