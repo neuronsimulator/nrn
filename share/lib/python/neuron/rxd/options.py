9a5fc40ffbbae588fb704759b0ad04c04b55d78d
Robert A. McDougal <unknown>
2013-05-06 09:27:25 -0400

    added version check of scipy; memory leak issue exists if version < 0.10.1; use different method in that case

--- share/lib/python/neuron/rxd/options.py
+++ share/lib/python/neuron/rxd/options.py
@@ -1,2 +1,16 @@
-use_reaction_contribution_to_jacobian = True
-"""Should we use the reaction contribution to the Jacobian? probably yes if cvode"""
+try:
+    import scipy
+except:
+    raise Exception("NEURON's rxd module requires scipy")
+
+import warnings
+
+scipy_version = [int(v) for v in scipy.__version__.split('.')]
+
+if scipy_version[0] > 0 or scipy_version[1] > 10 or (scipy_version[1] == 10 and scipy_version[2] >= 1):
+    use_reaction_contribution_to_jacobian = True
+    """Should we use the reaction contribution to the Jacobian? probably yes if cvode"""
+else:
+    warnings.warn('scipy < 0.10.1 found; setting rxd.options.use_reaction_contribution_to_jacobian = False to avoid a memory leak in scipy.sparse.linalg.factorized')
+    use_reaction_contribution_to_jacobian = False
+
