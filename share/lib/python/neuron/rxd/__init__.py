5c4165709fa67c7fa5e070009c9c400c0408deb8
Robert A. McDougal <unknown>
2013-06-30 22:20:53 -0400

    First ModelView support for RxD

--- share/lib/python/neuron/rxd/__init__.py
+++ share/lib/python/neuron/rxd/__init__.py
@@ -23,3 +23,16 @@ from geometry import membrane, inside, Shell, FractionalVolume, FixedCrossSectio
 # from geometry import ConstantArea, ConstantVolume
 Parameter = Species
 State = Species
+
+def _model_view(tree):
+    import species
+    from neuron import h
+    species_dict = species._get_all_species()
+    if 'TreeViewItem' not in dir(h): return
+    if species_dict:
+        rxd_head = h.TreeViewItem(None, 'Reaction Diffusion Items')
+        # TODO: do the species disappear if they go out of scope? or does this overcount?
+        rxd_species = h.TreeViewItem(rxd_head, '%d Species/State/Parameter' % len(species_dict))
+        species_children = [h.TreeViewItem(rxd_species, str(name)) for name in species_dict]
+        rxd_reactions = h.TreeViewItem(rxd_head, '%d Reaction/Rate/MultiCompartmentReaction' % len([r for r in rxd._all_reactions if r() is not None]))        
+        tree.append(rxd_head)
