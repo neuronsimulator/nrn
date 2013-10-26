1920254cc301555b3d21357b0cdf7aef4084866c
Michael Hines <michael.hines@yale.edu>
2013-10-26 18:29:28 -0400

    g++ -pedantic -I. -Inrnoc -Inrniv -c main.cpp -o/dev/null Now time to link and make available the missing functions.
    CoreNEURON Repo SHA: BlueBrain/CoreNeuron@8cf0562eb460a5ffd4ec4049530186f8c690f839

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <nrnconf.h>
+#include <nrnmpi.h>
+
+extern "C" {
+  extern void mk_netcvode();
+  extern void nrn_setup();
+  extern void BBS_netpar_solve(double);
+  extern void output_spikes();
+}
+
+int main(int argc, char** argv, char** env) {
+  nrnmpi_init(1, &argc, &argv);
+  mk_netcvode();
+  nrn_setup();
+  BBS_netpar_solve(100.);
+  output_spikes();
+  return 0;
+}
