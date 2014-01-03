cvode jacobian fix

share/lib/python/neuron/rxd/rxd.py
@@ -182,6 +182,10 @@ def _ode_solve(dt, t, b, y):
     full_b = numpy.zeros(n)
     full_b[_nonzero_volume_indices] = b[lo : hi]
     b[lo : hi] = _react_matrix_solver(_diffusion_matrix_solve(dt, full_b))[_nonzero_volume_indices]
+    # the following version computes the reaction matrix each time
+    #full_y = numpy.zeros(n)
+    #full_y[_nonzero_volume_indices] = y[lo : hi]
+    #b[lo : hi] = _reaction_matrix_solve(dt, full_y, _diffusion_matrix_solve(dt, full_b))[_nonzero_volume_indices]
     # this line doesn't include the reaction contributions to the Jacobian
     #b[lo : hi] = _diffusion_matrix_solve(dt, full_b)[_nonzero_volume_indices]
 
@@ -306,13 +310,11 @@ def _diffusion_matrix_solve(dt, rhs):
                    _diffusion_p_ptr, ctypes.c_int(n))
     return result
 
-def _reaction_matrix_solve(dt, states, rhs):
-    if not options.use_reaction_contribution_to_jacobian:
-        return rhs
+def _get_jac(dt, states):
     # now handle the reaction contribution to the Jacobian
     # this works as long as (I - dt(Jdiff + Jreact)) \approx (I - dtJreact)(I - dtJdiff)
     count = 0
-    n = len(rhs)
+    n = len(states)
     rows = range(n)
     cols = range(n)
     data = [1] * n
@@ -326,7 +328,24 @@ def _reaction_matrix_solve(dt, states, rhs):
             count += 1
             
     if count > 0 and n > 0:
-        jac = scipy.sparse.coo_matrix((data, (rows, cols)), shape=(n, n)).tocsr()
+        return scipy.sparse.coo_matrix((data, (rows, cols)), shape=(n, n))
+    return None
+
+def _reaction_matrix_solve(dt, states, rhs):
+    if not options.use_reaction_contribution_to_jacobian:
+        return rhs
+    jac = _get_jac(dt, states)
+    if jac is not None:
+        jac = jac.tocsr()
+        """
+        print 'states:', list(states)
+        print 'jacobian (_solve):'
+        m = jac.todense()
+        for i in xrange(m.shape[0]):
+            for j in xrange(m.shape[1]):
+                print ('%15g' % m[i, j]),
+            print    
+        """
         #result, info = scipy.sparse.linalg.bicgstab(jac, rhs)
         #assert(info == 0)
         result = scipy.sparse.linalg.spsolve(jac, rhs)
@@ -336,33 +355,25 @@ def _reaction_matrix_solve(dt, states, rhs):
     return result
 
 _react_matrix_solver = None    
-def _reaction_matrix_setup(dt, unexpanded_rhs):
+def _reaction_matrix_setup(dt, unexpanded_states):
     global _react_matrix_solver
     if not options.use_reaction_contribution_to_jacobian:
         _react_matrix_solver = lambda x: x
         return
 
-    # now handle the reaction contribution to the Jacobian
-    # this works as long as (I - dt(Jdiff + Jreact)) \approx (I - dtJreact)(I - dtJdiff)
-    count = 0
-    rhs = numpy.array(node._get_states())
-    rhs[_nonzero_volume_indices] = unexpanded_rhs    
-    n = len(rhs)
-    rows = range(n)
-    cols = range(n)
-    data = [1] * n
-    for rptr in _all_reactions:
-        r = rptr()
-        if r:
-            r_rows, r_cols, r_data = r._jacobian_entries(rhs, multiply=-dt)
-            rows += r_rows
-            cols += r_cols
-            data += r_data
-            count += 1
-            
-    if count > 0:
-        
-        jac = scipy.sparse.coo_matrix((data, (rows, cols)), shape=(n, n)).tocsc()
+    states = numpy.zeros(len(node._get_states()))
+    states[_nonzero_volume_indices] = unexpanded_states    
+    jac = _get_jac(dt, states)
+    if jac is not None:
+        jac = jac.tocsc()
+        """
+        print 'jacobian (_reaction_matrix_setup):'
+        m = jac.todense()
+        for i in xrange(m.shape[0]):
+            for j in xrange(m.shape[1]):
+                print ('%15g' % m[i, j]),
+            print
+        """
         #result, info = scipy.sparse.linalg.bicgstab(jac, rhs)
         #assert(info == 0)
         _react_matrix_solver = scipy.sparse.linalg.factorized(jac)
@@ -378,7 +389,9 @@ def _conductance(d):
     
 def _ode_jacobian(dt, t, ypred, fpred):
     #print '_ode_jacobian: dt = %g, last_dt = %r' % (dt, _last_dt)
-    _reaction_matrix_setup(dt, ypred)
+    lo = _rxd_offset
+    hi = lo + len(_nonzero_volume_indices)    
+    _reaction_matrix_setup(dt, ypred[lo : hi])
 
 _callbacks = [_setup, None, _fixed_step_currents, _conductance, _fixed_step_solve,
               _ode_count, _ode_reinit, _ode_fun, _ode_solve, _ode_jacobian, None]