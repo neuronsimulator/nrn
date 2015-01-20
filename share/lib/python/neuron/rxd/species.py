95e0a7c0750fad5f8266b53a0f477cf9f9a7430e
Robert A McDougal <unknown>
2015-01-20 14:30:24 -0500

    rxd.SpeciesOnRegion.nodes now ensures initialization

--- share/lib/python/neuron/rxd/species.py
+++ share/lib/python/neuron/rxd/species.py
@@ -176,6 +176,7 @@ class SpeciesOnRegion(_SpeciesMathable):
         constructs all Node objects belonging to the Species ca and then culls the list to only include those also
         belonging to the Region cyt.
         """
+        initializer._do_init()
         return nodelist.NodeList(itertools.chain.from_iterable([s.nodes for s in self._species()._secs if s._region == self._region()]))
     
     @property
