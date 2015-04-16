97c21bb8e84e627b643109ff31dfa26be3e9afee
Robert A. McDougal <devnull@localhost>
2015-04-15 21:35:40 -0400

    changed copyright from 2015 to 2010-2015 to indicate initial version from Tom McTavish

--- conf.py
+++ conf.py
@@ -48,7 +48,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'NEURON + Python Tutorials'
-copyright = u'2015, Tom McTavish, Robert A McDougal, Michael L Hines'
+copyright = u'2010-2015, Tom McTavish, Robert A McDougal, Michael L Hines'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -169,7 +169,7 @@ html_static_path = ['_static']
 #html_show_sphinx = True
 
 # If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
-#html_show_copyright = True
+html_show_copyright = True
 
 # If true, an OpenSearch description file will be output, and all pages will
 # contain a <link> tag referring to it.  The value of this option must be the
