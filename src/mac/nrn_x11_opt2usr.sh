53ec02d73d3d1fd6d559ce096820cba2b9fd9e98
Michael Hines <michael.hines@yale.edu>
2016-01-21 08:53:14 -0500

    OS X version installer package is signed by an identified developer. When making a package, change all the dynamic library links that begin with /opt/X11 to /usr/X11 (i.e. Mavericks already has X11 installed in /usr/X11) Installer will change .bashrc if that exists, otherwise .bash_profile

---
+++ src/mac/nrn_x11_opt2usr.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#Work around for the problem that
+#I cannot get the linker to use /usr/X11/lib instead of /opt/X11/lib
+#The issue is with Mavericks (10.7) that has an apple version of X11 already
+#installed in /usr/X11 instead of xquartz which installs in /opt/X11 and has
+#a symbolic link to /usr/X11
+
+#Change all links of /opt/X11/lib/*.dyld to /usr/X11/lib/*.dyld
+
+prefix=$1
+cpu=$2
+
+function change() {
+  a="`otool -L $1 |sed -n 's,.*/opt/X11/lib/\([^ ]*\) .*,\1,p'`"
+  if test "$a" != "" ; then
+    for name in $a ; do
+      echo change $name in $i
+      install_name_tool -change /opt/X11/lib/$name /usr/X11/lib/$name $i
+    done
+  fi
+}
+
+cd $prefix
+echo $prefix
+echo $cpu
+
+for i in `find . -name \*.so` ; do
+  change $i
+done
+for i in `find . -name \*.dylib` ; do
+  change $i
+done
+cd $cpu/bin
+for i in * ; do
+  change $i
+done
+
+cd $prefix
+cd ../iv/$cpu
+cd bin
+for i in * ; do
+  change $i
+done
+cd ../lib
+for i in * ; do
+  change $i
+done
