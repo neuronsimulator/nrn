f69d5d7644058962ee3dd184cdbdbec6ef1c7610
Michael Hines <michael.hines@yale.edu>
2016-06-17 11:54:43 +0200

    Quadratic order performance problem for (double*, Observer) management. Became unusable when creating models involving creating and destroying large numbers of intermediate vectors. A tentative solution involving standard template library maps to replace linear lookup lists seems to resolve the issue. This commit transforms taht solution into a template class in order to handle in the future, the lesser problem of (void*, Observer).

--- src/ivoc/ivoc.cpp
+++ src/ivoc/ivoc.cpp
@@ -7,10 +7,13 @@
 #include <nrnmutdec.h>
 #include "oc2iv.h"
 #include "ocfunc.h"
+
 #if HAVE_IV
 #include "utility.h"
 #include "ivoc.h"
 #endif
+
+#include "bimap.hpp"
 
 #if USE_PTHREAD
 static MUTDEC
@@ -31,7 +34,14 @@ public:
 declareList(PList, PObserver);
 implementList(PList, PObserver);
 static PList* p_list;
-static PList* pd_list;
+
+static nrn::tool::bimap<double*,Observer*>* pdob;
+
+// fast insert, find, and remove of (double*, Observer*) using either as
+// a key. Use pair of multimap since there can be many observers of the
+// same double. And perhaps one Observer is watching several double*. Also
+// the double* being watched is removed when the array (pd*, size) it is
+// a part of is freed. So the upper_bound property is needed
 
 int nrn_err_dialog_active_;
 
@@ -58,32 +68,26 @@ void nrn_notify_when_void_freed(void* p, Observer* ob) {
 
 void nrn_notify_when_double_freed(double* p, Observer* ob) {
 	MUTLOCK
-	if (!pd_list) {
-		pd_list = new PList(10);
+	if (!pdob) {
+		pdob = new nrn::tool::bimap<double*,Observer*>();
 	}
-	PObserver pob;
-	pob.p = (void*)p;
-	pob.observer = ob;
-	pd_list->append(pob);
+	pdob->insert(p, ob);
 	MUTUNLOCK
 }
 
 void nrn_notify_pointer_disconnect(Observer* ob) {
-	PList* pl = p_list;
 	MUTLOCK
-   for(pl = p_list;;pl = pd_list) {
-	if (pl) {
-		long i, n = pl->count() - 1;
+	if (p_list) {
+		long i, n = p_list->count() - 1;
 		for (i = n; i >= 0; --i) {
-			if (pl->item(i).observer == ob) {
-				pl->remove(i);
+			if (p_list->item(i).observer == ob) {
+				p_list->remove(i);
 			}
 		}
 	}
-	if (pl == pd_list) {
-		break;
-	}
-   }
+	if (pdob) {
+		pdob->obremove(ob);
+	}	
 	MUTUNLOCK
 }
 
@@ -123,23 +127,14 @@ void notify_freed_val_array(double* p, size_t size) {
 			(*f_list->item(i))((void*)p, size);
 		}
 	}
-	if (pd_list) {
-	    bool removed = true;
-	    while (removed) {
-		removed = false;
-		long i, n = pd_list->count() - 1;
-		for (i = n; i >= 0; --i) {
-			long j = (double*)pd_list->item(i).p - p;
+	if (pdob) {
+		double* pp;
+		Observer* ob;
+		while(pdob->find(p, size, pp, ob)) {
 //printf("notify_freed_val_array %d %ld\n", size, j);
-			if (j >= 0 && j < size) {
-				Observer* obs = pd_list->item(i).observer;
-				pd_list->remove(i);
-				obs->update(NULL);
-				removed = true;
-				break;
-			}
+			ob->update(NULL);
+			pdob->remove(pp, ob);
 		}
-	    }
 	}
 }
 } // end extern "C"
