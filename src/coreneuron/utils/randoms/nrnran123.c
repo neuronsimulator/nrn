b6900932ddb0cdaba313577bef4e6c289caf5d4c
Pramod Kumbhar <pramod.kumbhar@epfl.ch>
2016-07-18 12:46:22 +0200

    netstim.mod is updated to be compatible with NEURON Also added nrnran123_newstream3 method in nrnran123.c (from sandbox/hines/order-dup-gap)
    Change-Id: I0c42963b6ea7b20c9df1cdaa8f9a8e412f4fcf83
    
    CoreNEURON Repo SHA: BlueBrain/CoreNeuron@f814ef74c13f4759d9e76778d96d10766c13870b

--- src/coreneuron/utils/randoms/nrnran123.c
+++ src/coreneuron/utils/randoms/nrnran123.c
@@ -74,6 +74,18 @@ nrnran123_State* nrnran123_newstream(uint32_t id1, uint32_t id2) {
 	return s;
 }
 
+nrnran123_State* nrnran123_newstream3(uint32_t id1, uint32_t id2, uint32_t id3) {
+    nrnran123_State* s = (nrnran123_State*)ecalloc(sizeof(nrnran123_State), 1);
+    s->c.v[1] = id3;
+    s->c.v[2] = id1;
+    s->c.v[3] = id2;
+    nrnran123_setseq(s, 0, 0);
+    MUTLOCK
+    ++instance_count_;
+    MUTUNLOCK
+    return s;
+}
+
 void nrnran123_deletestream(nrnran123_State* s) {
 	MUTLOCK
 	--instance_count_;
