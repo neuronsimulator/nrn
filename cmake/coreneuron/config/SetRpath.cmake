8d19dc7391c510c442728880be9996e2a3223d18
Pramod Kumbhar <pramod.kumbhar@epfl.ch>
2016-11-01 14:28:55 +0100

    XLC on BG-Q doesnt link RPATH settings with static library. Disable RPATH for BG-Q and with static build.
    Change-Id: Ibbe3ff3f40588f0c5161b7b58dd217aef2ccd9ca
    
    CoreNEURON Repo SHA: BlueBrain/CoreNeuron@8a5462827325010ba384c28a1b946c982ad728cf

--- cmake/coreneuron/config/SetRpath.cmake
+++ cmake/coreneuron/config/SetRpath.cmake
@@ -1,18 +1,23 @@
 # enable @rpath in the install name for any shared library being built
 set(CMAKE_MACOSX_RPATH 1)
 
-# use, i.e. don't skip the full RPATH for the build tree
-SET(CMAKE_SKIP_BUILD_RPATH  FALSE)
+# on bg-q we do static linking and xlc doesnt like RPATH settings in this case
+IF( NOT BLUEGENE)
 
-# when building, don't use the install RPATH already # (but later on when installing)
-SET(CMAKE_BUILD_WITH_INSTALL_RPATH FALSE)
+    # use, i.e. don't skip the full RPATH for the build tree
+    SET(CMAKE_SKIP_BUILD_RPATH  FALSE)
 
-# add the automatically determined parts of the RPATH
-# which point to directories outside the build tree to the install RPATH
-SET(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
+    # when building, don't use the install RPATH already # (but later on when installing)
+    SET(CMAKE_BUILD_WITH_INSTALL_RPATH FALSE)
 
-# the RPATH to be used when installing, but only if it's not a system directory
-LIST(FIND CMAKE_PLATFORM_IMPLICIT_LINK_DIRECTORIES "${CMAKE_INSTALL_PREFIX}/lib" isSystemDir)
-IF("${isSystemDir}" STREQUAL "-1")
-   SET(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}/lib")
-ENDIF("${isSystemDir}" STREQUAL "-1")
+    # add the automatically determined parts of the RPATH
+    # which point to directories outside the build tree to the install RPATH
+    SET(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
+
+    # the RPATH to be used when installing, but only if it's not a system directory
+    LIST(FIND CMAKE_PLATFORM_IMPLICIT_LINK_DIRECTORIES "${CMAKE_INSTALL_PREFIX}/lib" isSystemDir)
+    IF("${isSystemDir}" STREQUAL "-1")
+       SET(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}/lib")
+    ENDIF("${isSystemDir}" STREQUAL "-1")
+
+ENDIF()
