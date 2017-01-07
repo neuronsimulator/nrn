35e40fb6b059905e1090d6caec8a6c4b536614fb
Michael Hines <michael.hines@yale.edu>
2017-01-07 17:16:04 -0500

    works for both mercurial and git

--- nrnversion.sh
+++ nrnversion.sh
@@ -8,21 +8,30 @@ fi
 
 if test -f $NSRC/src/nrnoc/nrnversion.h ; then
 	VERHFILE=`cat $NSRC/src/nrnoc/nrnversion.h`
-else
+elif test -d $NSRC/.git ; then
+	VERHFILE=`sh $NSRC/git2nrnversion_h.sh`
+elif test -d $NSRC/.hg ; then
 	VERHFILE=`sh $NSRC/hg2nrnversion_h.sh`
 fi
 
 ver=`sed -n '/^AC_INIT/s/^.*nrn\],\[\(.*\)\].*/\1/p' < $NSRC/configure.ac`
 
-type=`echo "$VERHFILE" | sed -n '/HG_BRANCH/s/.*\(".*"\).*/\1/p' | sed '
+HG=HG
+global=`echo "$VERHFILE" | sed -n '/HG_CHANGESET/s/.*"\(.*\)".*/\1/p'`
+if test "$global" = "" ; then
+  global=`echo "$VERHFILE" | sed -n '/GIT_CHANGESET/s/.*"\(.*\)".*/\1/p'`
+  HG=GIT
+fi
+
+type=`echo "$VERHFILE" | sed -n /${HG}'_BRANCH/s/.*\(".*"\).*/\1/p' | sed '
 s/"trunk"/alpha/
 s/"Release.*"/rel/
 s/"\(.*\)"/\1/
 '`
-local=`echo "$VERHFILE" | sed -n '/HG_LOCAL/s/.*"\(.*\)".*/\1/p'`
-global=`echo "$VERHFILE" | sed -n '/HG_CHANGESET/s/.*"\(.*\)".*/\1/p'`
+local=`echo "$VERHFILE" | sed -n /${HG}'_LOCAL/s/.*"\(.*\)".*/\1/p'`
 base=`echo $global | sed 's/\+//'`
-commit=`echo "$VERHFILE" | sed -n '/HG_LOCAL/s/.*"\(.*\)".*/\1/p'`
+commit=`echo "$VERHFILE" | sed -n /${HG}'_LOCAL/s/.*"\(.*\)".*/\1/p'`
+date=`echo "$VERHFILE" | sed -n /${HG}'_DATE/s/.*"\(.*\)".*/\1/p'`
 
 case $1 in
 	2) echo $ver.$type-$commit ;;
@@ -32,6 +41,7 @@ case $1 in
 	"local") echo $local ;;
 	"base") echo $base ;;
 	"global") echo $global ;;
+	"date") echo $date ;;
 	*) echo $ver ;;
 esac
 
