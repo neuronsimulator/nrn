d2a566d71a55d6d811addbb8fb9937d2b826cff6
Pramod S Kumbhar <pramod.kumbhar@epfl.ch>
2017-10-06 10:42:14 +0200

    Bootstrapping project structure for NOCMODL
    Change-Id: I6403d39bb3d5e9aff627099887895fce7d3f30c7
    
    NMODL Repo SHA: BlueBrain/nmodl@15a732e56f5528235adb60139c751d27be53421b

---
+++ README.md
@@ -0,0 +1,12 @@
+PROJECT_SOURCE_DIR## NOCMODL : Code Generation Framework
+
+This is prototype implementation of code generation framework for NMODL.
+
+### How To Build?
+
+```
+mkdir build
+cd build
+cmake ..
+make
+```
---
+++ cmake/nmodl/GitRevision.cmake
@@ -0,0 +1,30 @@
+# For now use simple approach to get version information as
+# git is always avaialble on the machine where we are building
+
+find_package(Git)
+
+if(GIT_FOUND)
+
+    # get last commit sha1
+    execute_process(COMMAND
+            ${GIT_EXECUTABLE} log -1 --format=%h
+            WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+            OUTPUT_VARIABLE GIT_REVISION_SHA1
+            ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+    # get last commit date
+    execute_process(COMMAND
+        ${GIT_EXECUTABLE} log -1 --format=%ad --date=format:"%d-%m-%Y %H:%M"
+        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+        OUTPUT_VARIABLE GIT_REVISION_DATE
+        ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+    # remove extra double quotes
+    string(REGEX REPLACE "\"" "" GIT_REVISION_DATE ${GIT_REVISION_DATE})
+    set(GIT_REVISION "${GIT_REVISION_SHA1} ${GIT_REVISION_DATE}")
+
+else()
+
+    set(GIT_REVISION "unknown")
+
+endif()
