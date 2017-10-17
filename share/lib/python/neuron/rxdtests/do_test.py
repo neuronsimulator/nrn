0a0657fe1ce3771bf136a887965044b20aa71244
adam newton <adam.newton@yale.edu>
2017-10-17 11:06:17 -0400

    Added rxdtests and replaced the correct_data with the current output which differs slightly from the previous python output.

---
+++ share/lib/python/neuron/rxdtests/do_test.py
@@ -0,0 +1,78 @@
+"""
+do_test.py -- runs a specific test; does not evaluate it
+
+Robert A McDougal
+June 23, 2014
+"""
+
+def exec_test(the_file, global_var, local_var):
+    if sys.version_info.major < 3:
+        execfile(the_file, global_var, local_var)
+    else:
+        exec(compile(open(the_file).read(), the_file, 'exec'), global_var, local_var)
+
+def do_test(test_to_run, results_location, num_record=10):
+    import os
+    # switch to the directory
+    initial_path = os.getcwd() + '/'
+    the_dir, the_file = os.path.split(test_to_run)
+    os.chdir(the_dir)
+
+    from neuron import h, rxd
+    import itertools
+    
+
+    data = {'record_count': 0, 'data': []}
+    do_test.data=data
+    record_count = 0
+
+    def collect_data():
+        """grabs the membrane potential data, h.t, and the rxd state values"""
+        data['record_count'] += 1
+        if data['record_count'] > num_record:
+            save_and_cleanup()
+        
+        all_potentials = [seg.v for seg in itertools.chain.from_iterable(h.allsec())]
+        all_rxd = list(rxd.node._states)
+        local_data = [h.t] + all_potentials + all_rxd
+
+        # remove data before t=0
+        if h.t==0:
+            data['data']=[]
+            data['record_count'] = 1
+        # remove previous record if h.t is the same
+        if data['record_count']>1 and h.t==data['data'][-len(local_data)]:
+            data['record_count'] -= 1
+            del data['data'][-len(local_data):]
+        # add new data record
+        data['data'].extend(local_data)
+        # print correct record length
+        if data['record_count']==2:
+            outstr = "<BAS_RL %i BAS_RL> %s %s" % (len(local_data), repr(h.t), data['record_count'])
+            print(outstr)
+    
+    
+    def save_and_cleanup():
+        import array
+        os.chdir(initial_path)
+        # save the data
+        with open(results_location, 'wb') as f:
+            array.array('d', data['data']).tofile(f)
+        
+        import sys
+        sys.exit(0)
+
+    h.CVode().extra_scatter_gather(0, collect_data)
+    
+    # now run it
+    exec_test(the_file,globals(),globals())
+    
+    # should only get here if very few steps
+    save_and_cleanup()
+    
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) != 3:
+        print('needs two arguments: relative path to python test to run, relative path for results')
+        sys.exit()
+    do_test(sys.argv[1], sys.argv[2])
---
+++ share/lib/python/neuron/rxdtests/run_all.py
@@ -0,0 +1,127 @@
+def test(files):
+    import os
+    import sys
+    import filecmp
+    import subprocess
+    import re, array, numpy
+    from scipy import interpolate
+    
+    tol = 1e-10
+    dt_eps = 1e-20
+    
+    try:
+        os.makedirs('test_data')
+    except OSError:
+        pass
+
+    passed_list = []
+    failed_list = []
+    incomplete_list = []
+    for f in files:
+        base_name = f[: -3]
+        print('%s: ' % base_name)
+
+        output_file = os.path.join('test_data', base_name + '.dat')
+        # remove any old test results
+        try:
+            os.remove(output_file)
+        except OSError:
+            pass
+        
+#        os.system('python do_test.py %s %s' % (os.path.join('tests', f), output_file))
+
+        
+        try:
+            outp = subprocess.check_output(['python%i' % sys.version_info.major, 'do_test.py', os.path.join('tests', f), output_file] )
+            sobj = re.search( r'<BAS_RL (\d*) BAS_RL>', outp.decode('utf-8'), re.M)
+            rlen =  int(sobj.group(1))
+            success = False
+            corr_dat = numpy.fromfile(os.path.join('correct_data', base_name + '.dat')).reshape(-1, rlen)
+            tst_dat = numpy.fromfile(output_file).reshape(-1, rlen)
+            t1 = corr_dat[:,0]
+            t2 = tst_dat[:,0]
+            # remove any initial t that are greter than the next t (removes times before 0) in correct data
+            c= 0
+            while (c<len(t1)-1 and t1[c]>t1[c+1]):
+                c=c+1
+            t1 = numpy.delete(t1, range( c))
+            corr_dat = numpy.delete(corr_dat, range(c),0)
+            # remove any initial t that are greter than the next t (removes times before 0) in test data
+            c= 0
+            while (c<len(t2)-1 and t2[c]>t2[c+1]):
+                c=c+1
+            t2 = numpy.delete(t2, range( c))
+            tst_dat = numpy.delete(tst_dat, range(c),0)
+            # get rid of repeating t in correct data (otherwise interpolation fails)    
+            c = 0;            
+            while (c<len(t1)-1):
+                c1=c+1
+                while(c1<len(t1) and abs(t1[c]-t1[c1])<dt_eps):
+                    c1 = c1+1
+                t1 = numpy.delete(t1, range(c, c1-1))
+                corr_dat = numpy.delete(corr_dat, range(c, c1-1),0)
+                c = c+1
+            # get rid of the test data outside of the correct data time interval
+            t2_n = len(t2)
+            t2_0 = 0
+            while t2[t2_n-1]>t1[-1]:
+                t2_n = t2_n -1
+            while t2[t2_0]<t1[0]:
+                t2_0 = t2_0 + 1
+            #interpolate and compare
+            corr_int = interpolate.interp1d(t1, corr_dat[:,1:].T)
+            corr_vals = corr_int(t2[t2_0:t2_n])
+            max_err = numpy.amax(abs(corr_vals.T - tst_dat[t2_0:t2_n, 1:]))
+#            max_err = numpy.amax(abs(corr1 - tst1))
+            
+            if(max_err<tol): success = True
+
+            if success:
+                passed_list.append(base_name)
+                print('passed')
+            else:
+                print('failed')
+
+                failed_list.append(base_name)
+        except:
+            print('incomplete')
+            incomplete_list.append(base_name)
+
+    print('')
+    print('---')
+    print('Passed:     ', ', '.join(passed_list))
+    print('Failed:     ', ', '.join(failed_list))
+    print('Incomplete: ', ', '.join(incomplete_list))
+    print('---')
+    print('Ran %d tests:' % len(files))
+    print('Passed:     %d' % len(passed_list))
+    print('Failed:     %d' % len(failed_list))
+    print('Incomplete: %d' % len(incomplete_list))
+    if len(passed_list) < len(files):
+        return -1
+    return 0
+
+
+
+if __name__ == '__main__':
+    import os
+    import sys
+    
+    abspath = os.path.abspath(__file__)
+    dname = os.path.dirname(abspath)
+    os.chdir(dname)
+    
+    files = [f for f in os.listdir('tests') if f[-3:].lower() == '.py']
+    dirs = [name for name in os.listdir('tests') if os.path.isdir(os.path.join('tests', name))]
+    for dr in dirs:
+        fname = os.path.join('tests', dr, 'torun.txt')
+        if os.path.isfile(fname):
+            # compile any mod files
+            os.chdir(os.path.join('tests', dr))
+            os.system('nrnivmodl')
+            os.chdir(os.path.join('..', '..'))
+            with open(fname) as f:
+                files += [os.path.join(dr, s) for s in f.read().splitlines()]
+    
+    sys.exit(test(files))
+
