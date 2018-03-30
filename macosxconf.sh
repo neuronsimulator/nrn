8f56e0a70e74d02cdee4d128fa735d9166fb6f05
Michael Hines <michael.hines@yale.edu>
2018-03-30 09:19:07 -0400

    Some information about building a signed pkg distribution on High Sierra

--- macosxconf.sh
+++ macosxconf.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Note the current bldnrnmac.sh build script I use to prepare a pkg
+# distribution is included at the end of this file
+
 # assume InterViews sources in $HOME/neuron/iv
 # assume NEURON sources in $HOME/neuron/nrn
 # assume that you are using a Terminal window
@@ -71,3 +74,40 @@ defaults write org.macosforge.xquartz.X11 wm_ffm -bool true
 #in $SOFT/nrn
 ../nrn/configure --prefix=$IDIR/nrn --with-iv=$IDIR/iv --with-nrnpython=dynamic \
   CFLAGS='-arch x86_64' CXXFLAGS='-arch x86_64' --host=x86_64-apple-darwin10.8.0
+
+##################
+#this is a copy of my current build script: bldnrnmac.sh
+
+#!/bin/bash
+set -e
+# distribution built with
+# sh bldnrnmac.sh python3 /Volumes/HinesWD/mac/anaconda3/bin/python python
+#sh bldnrnmac.sh python3 /Volumes/HinesWD/mac/anaconda3/bin/python /Volumes/HinesWD/mac/anaconda2/bin/python
+
+INST=/Applications/NEURON-7.5/nrn
+cd $HOME/neuron/nrnobj
+
+export MACOSX_DEPLOYMENT_TARGET=10.7
+
+bld () {
+rm -r -f src/nrnpython/build
+../nrn/configure --prefix=$INST --with-paranrn=dynamic \
+  --with-nrnpython=dynamic --with-pyexe=$1 $2 CYTHON=/Volumes/HinesWD/mac/anaconda2/bin/cython
+make -j 2 install
+}
+
+bld $1 ""
+shift
+for i in $* ; do
+  bld $i "--with-nrnpython-only"
+done
+
+make after_install
+#/Applications/Packages.app from
+# http://s.sudre.free.fr/Software/Packages/about.html
+# For mac to do a productsign, need my developerID_installer.cer
+# and Neurondev.p12 file. To add to the keychain, double click each
+# of those files. By default, I added my cerificate to the login keychain.
+make pkg
+make alphadist
+
