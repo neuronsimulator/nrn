380cdaa238357c7455085fdf6c10fec3ed882afe
Pramod S Kumbhar <pramod.kumbhar@epfl.ch>
2018-04-02 16:43:08 +0200

    Draft implementation of C lexer and parser for parsing verbatim blocks
    Change-Id: Iafe6ef9626ac7ceb4a2b53c1325c88fc00f712ba

---
+++ src/lexer/main_c.cpp
@@ -0,0 +1,56 @@
+#include <fstream>
+#include <iostream>
+
+#include "lexer/c11_lexer.hpp"
+#include "parser/c11_driver.hpp"
+#include "tclap/CmdLine.h"
+
+/**
+ * Standlone lexer program for C. This demonstrate basic
+ * usage of scanner and driver class.
+ */
+
+int main(int argc, const char* argv[]) {
+    try {
+        TCLAP::CmdLine cmd("C Lexer: Standalone lexer program for C");
+        TCLAP::ValueArg<std::string> filearg("", "file", "C input file path", false,
+                                             "../test/input/channel.c", "string");
+
+        cmd.add(filearg);
+        cmd.parse(argc, argv);
+
+        std::string filename = filearg.getValue();
+        std::ifstream file(filename);
+
+        if (!file) {
+            throw std::runtime_error("Could not open file " + filename);
+        }
+
+        std::cout << "\n C Lexer : Processing file : " << filename << std::endl;
+
+        std::istream& in(file);
+        c11::Driver driver;
+        c11::Lexer scanner(driver, &in);
+
+        using Token = c11::Parser::token;
+        using TokenType = c11::Parser::token_type;
+        using SymbolType = c11::Parser::symbol_type;
+
+        /// parse C file untile EOF, print each token
+        while (true) {
+            SymbolType sym = scanner.next_token();
+            TokenType token = sym.token();
+
+            /// end of file
+            if (token == Token::END) {
+                break;
+            }
+            std::cout << sym.value.as<std::string>();
+        }
+    } catch (TCLAP::ArgException& e) {
+        std::cout << std::endl << "Argument Error: " << e.error() << " for arg " << e.argId();
+        return 1;
+    }
+
+    return 0;
+}
---
+++ src/parser/c11_driver.cpp
@@ -0,0 +1,55 @@
+#include <fstream>
+#include <sstream>
+
+#include "lexer/c11_lexer.hpp"
+#include "parser/c11_driver.hpp"
+
+namespace c11 {
+
+    Driver::Driver(bool strace, bool ptrace) : trace_scanner(strace), trace_parser(ptrace) {
+    }
+
+    /// parse c file provided as istream
+    bool Driver::parse_stream(std::istream& in) {
+        Lexer scanner(*this, &in);
+        Parser parser(scanner, *this);
+
+        this->lexer = &scanner;
+        this->parser = &parser;
+
+        scanner.set_debug(trace_scanner);
+        parser.set_debug_level(trace_parser);
+        return (parser.parse() == 0);
+    }
+
+    //// parse c file
+    bool Driver::parse_file(const std::string& filename) {
+        std::ifstream in(filename.c_str());
+        streamname = filename;
+
+        if (!in.good()) {
+            return false;
+        }
+        return parse_stream(in);
+    }
+
+    /// parser c provided as string (used for testing)
+    bool Driver::parse_string(const std::string& input) {
+        std::istringstream iss(input);
+        return parse_stream(iss);
+    }
+
+    void Driver::error(const std::string& m, const class location& l) {
+        std::cerr << l << " : " << m << std::endl;
+    }
+
+    void Driver::error(const std::string& m) {
+        std::cerr << m << std::endl;
+    }
+
+    void Driver::process(std::string text) {
+        // here we will query and look into symbol table or register callback
+        // std::cout << text;
+    }
+
+}  // namespace c11
