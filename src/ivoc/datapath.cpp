neuronsimulator/nrn

commit 76e0330ea42475c28d6ed88c97ab1fc1150b2cf4

Sections created in Python can have names that allow GUI sessions across launches.
The syntax in hoc is _pysec.namesec
namesec is the nrn.Section.name() of a section created by python with
h.Section(name='sname') or h.Section(name='sname, cell=self)
For hoc to understand the names, the sname component must have valid
hoc syntax and may have a trailing [int] where int is a literal integer.
If there is a cell name component, it comes from str(cell) and that also
has to have valid hoc scalar or single dimension syntax.

Note that PlotWhat has a new group called "Python Sections" that alows
navigation to range variable instances of any Python section that satisifies
the above name convention.

src/nrniv/pysecname2sec.cpp
@@ -1,0 +1,258 @@
+#include <../../nrnconf.h>
+
+#include <stdio.h>
+#include <hocparse.h>
+
+extern "C" {
+#include <parse.h>
+}
+
+#include <nrnoc2iv.h>
+
+#include "nrnsymdiritem.h"
+
+#include <string>
+#include <map>
+
+static bool activated = false; // true on first use of hoc __pysec.
+
+enum CorStype {CELLTYPE, SECTYPE, OVERLOADCOUNT, NONETYPE};
+typedef std::pair<CorStype, void*> CellorSec;
+typedef std::map<const std::string, CellorSec > Name2CellorSec;
+static Name2CellorSec n2cs;
+
+#define hoc_acterror(a,b) printf("%s %s\n", a, b)
+
+static void activate() {
+  if (!activated) {
+    //printf("first activation\n");
+    activated = true;
+    hoc_Item* qsec;
+    ForAllSections(sec)
+      if (sec->prop && sec->prop->dparam[PROP_PY_INDEX]._pvoid) {
+        nrnpy_pysecname2sec_add(sec);
+      }
+    }
+  }
+}
+
+Section* nrnpy_pysecname2sec(const char* name) { // could be Cell part or Sec
+  activate();
+  std::string n(name);
+  if (nrn_parsing_pysec_ == (void*)1) {
+    Name2CellorSec::iterator search = n2cs.find(n);
+    if (search != n2cs.end()) {
+      if (search->second.first == SECTYPE) {
+          nrn_parsing_pysec_ = NULL; /* done */
+          return (Section*)search->second.second;
+      }else if (search->second.first == CELLTYPE) {
+          nrn_parsing_pysec_ = search->second.second;
+      }else if (search->second.first == OVERLOADCOUNT) {
+          nrn_parsing_pysec_ = NULL;
+          hoc_acterror(name, " is an overloaded first part name for multiple sections created in python");
+      }
+    } else {
+      nrn_parsing_pysec_ = NULL;
+      hoc_acterror(name, " is not a valid first part name for section created in python");
+    }
+  } else {
+    Name2CellorSec* n2s = (Name2CellorSec*)nrn_parsing_pysec_;
+    Name2CellorSec::iterator search = n2s->find(n);
+    if (search != n2s->end()) {
+      if (search->second.first == OVERLOADCOUNT) {
+        nrn_parsing_pysec_ = NULL;
+        hoc_acterror(name, " is an overloaded second part name for multiple sections created in python");
+      }
+      nrn_parsing_pysec_ = NULL; /* done */
+      assert(search->second.first == SECTYPE);
+      return (Section*)search->second.second;
+    } else {
+      nrn_parsing_pysec_ = NULL;
+      hoc_acterror(name, " is not a valid last part name for section created in python");
+    }
+  }
+  return NULL;
+}
+
+// Want to add a Section to a cell but one may already be there or
+// already be overloaded.
+static void n2s_add(Name2CellorSec& n2s, std::string n, Section* sec) {
+    Name2CellorSec::iterator search = n2s.find(n);
+    if (search == n2s.end()) {
+      n2s[n] = CellorSec(SECTYPE, sec);
+    }else{
+      CellorSec& cs = search->second;
+      if (cs.first == SECTYPE) {
+        cs.first = OVERLOADCOUNT;
+        cs.second = (void*)2;
+      }else if (cs.first == OVERLOADCOUNT) {
+        cs.second = (void*)((size_t)cs.second + 1);
+      }
+    }
+}
+
+// Want to add a Section to top level but name may already exist. If so
+// already a Section -- overload 2
+// already overloaded section -- overload increase by 1
+// already a cell -- no longer usable, becomes NONETYPE
+// already NONETYPE -- nothing to do
+static void n2cs_add_sec(Name2CellorSec& n2cs, std::string sname, Section* sec) {
+    //printf("n2cs_add_sec %s section=%p\n", sname.c_str(), sec);
+    Name2CellorSec::iterator search = n2cs.find(sname);
+    if (search == n2cs.end()) {
+      n2cs[sname] = CellorSec(SECTYPE, sec);
+    }else {
+      CellorSec& cs = search->second;
+      if (cs.first == CELLTYPE) { // conflict so destroy
+        Name2CellorSec* n2s = (Name2CellorSec*)cs.second;
+        delete n2s;
+        cs.first = NONETYPE;
+        cs.second = NULL;
+      }else if (cs.first == SECTYPE) { // overload
+          cs.first = OVERLOADCOUNT;
+          cs.second = (void*)2;
+      }else if (cs.first == OVERLOADCOUNT) { // increase overload
+          cs.second = (void*)((size_t)cs.second + 1);
+      }
+    }
+}
+
+// Want to add a Cell to top level but name may already exist. If so
+// already a Cell -- nothing to do
+// already a overloaded section or section -- no longer usable, becomes NONETYPE
+// already NONETYPE -- nothing to do
+static Name2CellorSec* n2cs_add_cell(Name2CellorSec& n2cs, std::string cname) {
+    Name2CellorSec::iterator search = n2cs.find(cname);
+    Name2CellorSec* n2s = NULL; // not usable if it stays NULL
+    if (search == n2cs.end()) { // not found add  cname, Name2Section*
+      n2s = new Name2CellorSec();
+      n2cs[cname] = CellorSec(CELLTYPE, n2s);
+    } else {
+      CellorSec& cs = search->second;
+      if (cs.first == SECTYPE || cs.first == OVERLOADCOUNT) { // conflict so destroy
+        cs.first = NONETYPE;
+        cs.second = NULL;
+      } else if (cs.first == CELLTYPE) { // exists, ready to use.
+        n2s = (Name2CellorSec*)cs.second;
+      }
+    }
+    return n2s;
+}
+
+void n2cs_add(Name2CellorSec& n2cs, std::string cname, std::string sname, Section* sec) {
+    Name2CellorSec* n2s = n2cs_add_cell(n2cs, cname);
+    //printf("n2cs_add %s %s n2s=%p section=%p\n", cname.c_str(), sname.c_str(), n2s, sec);
+    if (n2s) {
+      n2s_add(*n2s, sname, sec);
+    }
+}
+ 
+void nrnpy_pysecname2sec_add(Section* sec) {
+  if (!activated) { return; }
+  std::string n(secname(sec));
+  if (n.find("__nrnsec_0x", 0) == 0) { return; }
+  if (n.find("<", 0)  != n.npos) { return; }
+  //printf("nrnpy_pysecname2sec_add %s\n", secname(sec));
+  size_t dot = n.find('.', 1);
+  if (dot != n.npos) { // cell.sec
+    std::string cname = n.substr(0, dot);
+    std::string sname = n.substr(dot+1);
+    n2cs_add(n2cs, cname, sname, sec);
+  } else { // sec
+    n2cs_add_sec(n2cs, n, sec);
+  }
+}
+
+static bool decrement(CellorSec& cs) {
+    cs.second = (void*)((size_t)cs.second - 1);
+    return cs.second ? false : true;
+}
+
+void nrnpy_pysecname2sec_remove(Section* sec) {
+  if (!activated) { return; }
+  std::string name(secname(sec));
+  //printf("remove %s\n", name.c_str());
+  if (name[0] == '<') {return;}
+  
+  size_t dot = name.find('.', 1);
+  if (dot != name.npos) { // cell.sec
+    std::string cname = name.substr(0, dot);
+    std::string sname = name.substr(dot+1);
+    Name2CellorSec::iterator it = n2cs.find(cname);
+    assert(it != n2cs.end());
+    // must be CELLTYPE or NONETYPE
+    CellorSec& cs = it->second;
+    if (cs.first == CELLTYPE) {
+      Name2CellorSec* n2s = (Name2CellorSec*)cs.second;
+      Name2CellorSec::iterator its = n2s->find(sname);
+      assert(its != n2s->end())
+      // must be SECTYPE or OVERLOADCOUNT
+      CellorSec& css = its->second;
+      if (css.first == SECTYPE) {
+        n2s->erase(its);
+        if (n2s->empty()) {
+          delete n2s;
+          n2cs.erase(it);
+        }
+      }else {
+        assert(css.first == OVERLOADCOUNT);
+        if (decrement(css)) {
+          n2s->erase(its);
+          if (n2s->empty()) {
+            delete n2s;
+            n2cs.erase(it);
+          }
+        }
+      }
+    }else{
+      assert(cs.first == NONETYPE);
+    }
+  }else{ // sec
+    Name2CellorSec::iterator it = n2cs.find(name);
+    assert(it != n2cs.end());
+    // must be SECTYPE, NONETYPE, or OVERLOADCOUNT
+    CellorSec& cs = it->second;
+    if (cs.first == SECTYPE) {
+      n2cs.erase(it);
+    }else if (cs.first == OVERLOADCOUNT) {
+      if (decrement(cs)) {
+        n2cs.erase(it);
+      }
+    }else{
+      assert(cs.first == NONETYPE);
+    }
+  }
+#if 0
+  if (n2cs.empty()) {
+    printf("pysecname2sec is empty\n");
+  }
+#endif
+}
+
+void nrn_symdir_load_pysec(SymbolList& sl, void* v) {
+  activate();
+  if (!v) {
+    // top level items are any of the four types
+    for (Name2CellorSec::iterator it = n2cs.begin(); it != n2cs.end(); ++it) {
+      CellorSec& cs = it->second;
+      if (cs.first != NONETYPE && cs.first != OVERLOADCOUNT) {
+        SymbolItem* si = new SymbolItem(it->first.c_str(), 0);
+	si->pysec_type_ = cs.first == CELLTYPE ? PYSECOBJ : PYSECNAME;
+        si->pysec_ = (Section*)cs.second;
+        sl.append(si);
+      }
+    }
+  }else{
+    // in cell items are either OVERLOADCOUNT or SECTYPE
+    Name2CellorSec* n2s = (Name2CellorSec*)v;
+    for (Name2CellorSec::iterator it = n2s->begin(); it != n2s->end(); ++it) {
+      CellorSec& cs = it->second;
+      if (cs.first == SECTYPE) {
+        SymbolItem* si = new SymbolItem(it->first.c_str(), 0);
+        si->pysec_type_ = PYSECNAME;
+        si->pysec_ = (Section*)cs.second;
+        sl.append(si);
+      }
+    }
+  }
+}

src/nrnoc/cabcode.c
@@ -771,6 +771,16 @@ void* hoc_sec_internal_name2ptr(const char* s, int eflag) {
 	return vp;
 }
 
+void* hoc_pysec_name2ptr(const char* s, int eflag) {
+	/*
+	  syntax is _pysec.<name>  where <name> is the name of a python
+	  nrn.Section from (*nrnpy_pysec_name_p_)(sec) 
+	  
+	*/
+	Section* sec = nrnpy_pysecname2sec(s);
+	return (void*)sec;
+}
+
 /* in an object syntax a section may either be last or next to last
 in either case it is pushed when it is seen in hoc_object_component
 and section_object_seen is set.
@@ -1785,6 +1795,18 @@ const char* secname(Section* sec) /* name of section (for use in error messages)
 	return name;
 }
 
+const char* nrn_sec2pysecname(Section* sec) {
+  static char buf[256];
+  const char* name = secname(sec);
+  if (sec && sec->prop->dparam[PROP_PY_INDEX]._pvoid
+    && strncmp(name, "__nrnsec_0x", 11) != 0) {
+    sprintf(buf, "__pysec.%s", name);
+  }else{
+    strcpy(buf, name);
+  }
+  return buf;
+}
+
 void section_owner(void) {
 	Section* sec;
 	Object* ob;
@@ -1819,7 +1841,7 @@ char* hoc_section_pathname(Section* sec)
 			Sprintf(name, "%s%s", s->name, hoc_araystr(s, indx, hoc_objectdata));
 		}
 	}else if (sec && sec->prop && sec->prop->dparam[PROP_PY_INDEX]._pvoid) {
-		sprintf(name, "__nrnsec_%p", sec);
+		strcpy(name, nrn_sec2pysecname(sec));
 	}else{
 		name[0] = '\0';
 	}
@@ -2231,8 +2253,13 @@ void sectionname(void) {
 
 void hoc_secname(void) {
 	static char* buf = (char*)0;
+	Section* sec = chk_access();
 	if (!buf) { buf = (char*)emalloc(256*sizeof(char)); }
-	strcpy(buf,secname(chk_access()));
+	if (ifarg(1) && chkarg(1, 0., 1.) == 1.) {
+		strcpy(buf, nrn_sec2pysecname(sec));
+	}else{
+		strcpy(buf, secname(sec));
+	}
 	hoc_ret();
 	hoc_pushstr(&buf);
 }

src/ivoc/Makefile.am
@@ -69,7 +69,8 @@ noinst_HEADERS = apwindow.h axis.h bndedval.h cbwidget.h checkpnt.h \
 	ocpicker.h ocpointer.h random1.h rect.h rubband.h scenepic.h \
 	scenevie.h symchoos.h symdir.h utility.h ivocvect.h xmenu.h \
 	nrngsl.h nrngsl_hc_radix2.c nrngsl_real_radix2.c \
-	grglyph.h nrnmutdec.h ocnotify.h ocptrvector.h bimap.hpp
+	grglyph.h nrnmutdec.h ocnotify.h ocptrvector.h bimap.hpp \
+	nrnsymdiritem.h
 
 ivoc_SOURCES = nrnmain.cpp ivocmain.cpp $(nrniv_iv_sources)
 

src/ivoc/nrnsymdiritem.h
@@ -1,0 +1,40 @@
+#ifndef nrnsymdiritem_h
+#define nrnsymdiritem_h
+
+// allow communication between src/ivoc/symdir.cpp and src/nrniv/pysecname.cpp
+
+#include <OS/list.h>
+#include <OS/string.h>
+
+class SymbolItem {
+public:
+	SymbolItem(const char*, int whole_array = 0);
+	SymbolItem(Symbol*, Objectdata*, int index = 0, int whole_array=0);
+	SymbolItem(Object*);
+	~SymbolItem();
+	Symbol* symbol() const {return symbol_;}
+	Object* object() const {return ob_;}
+	void no_object();
+	const String& name() const { return name_;}
+	bool is_directory() const;
+	int array_index() const { return index_;}
+	int whole_vector();
+#if CABLE
+	int pysec_type_; /* PYSECOBJ (cell prefix) or PYSECNAME (Section) */
+	void* pysec_;   /* Name2Section* or Section* */
+#endif
+private:
+	CopyString name_;
+	Symbol* symbol_;
+	int index_;
+	Object* ob_;
+	int whole_array_;
+};
+
+declarePtrList(SymbolList, SymbolItem);
+
+#if CABLE
+  void nrn_symdir_load_pysec(SymbolList& sl, void*);
+#endif
+
+#endif

src/ivoc/symchoos.cpp
@@ -96,6 +96,7 @@ private:
     void show_objid();
     void show_objref();
     void show_sec();
+    void show_pysec();
     double* selected_var();
     int selected_vector_count();
     FieldEditor* add_filter(
@@ -470,6 +471,11 @@ Menu* SymChooserImpl::makeshowmenu() {
 #if SHOW_SECTION
 	mi->state()->set(TelltaleState::is_chosen, true);
 #endif
+	mi = K::radio_menu_item(ttg, "Python Sections");
+	mi->action(new ActionCallback(SymChooserImpl)(
+		this, &SymChooserImpl::show_pysec
+	));
+	mp->append_item(mi);
 
 	return mb;
 }
@@ -495,6 +501,9 @@ void SymChooserImpl::show_objid() {
 }
 void SymChooserImpl::show_sec() {
 	show(SECTION);
+}
+void SymChooserImpl::show_pysec() {
+	show(PYSEC);
 }
 
 void SymChooserImpl::clear(int bindex) {
@@ -711,18 +720,21 @@ bool SymChooserImpl::chdir(int bindex, int index) {
 if (dir_[bindex]->obj(index)) {
 		d = new SymDirectory(dir_[bindex]->obj(index));
 		bi = bindex;
+}else if (dir_[bindex]->is_pysec(index)) {
+		d = dir_[bindex]->newsymdir(index);
+		bi = bindex + 1;
 }else{
 		d = new SymDirectory(
 			dir_[bindex]->path(),
 			dir_[bindex]->object(),
 			dir_[bindex]->symbol(index),
 			dir_[bindex]->array_index(index));
-			bi = bindex + 1;
-			if (bi > nbrowser_-1) {
-				bi = nbrowser_-1;
-				// rotate
-			}
+		bi = bindex + 1;
 }
+		if (bi > nbrowser_-1) {
+			bi = nbrowser_-1;
+			// rotate
+		}
 		Resource::ref(d);
 		browser_index_ = bi;
 		Resource::unref(dir_[bi]);

src/ivoc/symdir.cpp
@@ -27,6 +27,9 @@ extern int hoc_array_index(Symbol*, Objectdata*);
 #include "string.h"
 #include "symdir.h"
 
+#include "nrnsymdiritem.h"
+implementPtrList(SymbolList, SymbolItem);
+
 const char* concat(const char* s1, const char* s2) {
 	static char* tmp = 0;
 	int l1 = strlen(s1);
@@ -38,30 +41,6 @@ const char* concat(const char* s1, const char* s2) {
 	sprintf(tmp, "%s%s", s1, s2);
 	return (const char*)tmp;
 }
-
-class SymbolItem {
-public:
-	SymbolItem(const char*, int whole_array = 0);
-	SymbolItem(Symbol*, Objectdata*, int index = 0, int whole_array=0);
-	SymbolItem(Object*);
-	~SymbolItem();
-	Symbol* symbol() const {return symbol_;}
-	Object* object() const {return ob_;}
-	void no_object();
-	const String& name() const { return name_;}
-	bool is_directory() const;
-	int array_index() const { return index_;}
-	int whole_vector();
-private:
-	CopyString name_;
-	Symbol* symbol_;
-	int index_;
-	Object* ob_;
-	int whole_array_;
-};
-
-declarePtrList(SymbolList, SymbolItem);
-implementPtrList(SymbolList, SymbolItem);
 
 class SymDirectoryImpl : public Observer {
 public:
@@ -195,6 +174,41 @@ SymDirectory::SymDirectory(Object* ob) {
 	ObjObservable::Attach(impl_->obj_, impl_);
 	impl_->load_object();
 	impl_->sort();
+}
+
+bool SymDirectory::is_pysec(int index) const {
+	SymbolItem* si = impl_->symbol_list_.item(index);
+#if CABLE
+	return si->pysec_ ? true : false;
+#else
+	return false;
+#endif
+}
+SymDirectory* SymDirectory::newsymdir(int index) {
+	SymbolItem* si = impl_->symbol_list_.item(index);
+	SymDirectory* d = new SymDirectory();
+#if CABLE
+	if (si->pysec_type_ == PYSECOBJ) {
+		nrn_symdir_load_pysec(d->impl_->symbol_list_, si->pysec_);
+	}else{
+		d->impl_->sec_ = (Section*)si->pysec_;
+		section_ref(d->impl_->sec_);
+		d->impl_->load_section();
+	}
+	d->impl_->path_ = concat(path().string(), si->name().string());
+	d->impl_->path_ = concat(d->impl_->path_.string(), ".");
+	d->impl_->sort();
+#endif
+	return d;
+}
+
+SymDirectory::SymDirectory() {
+	impl_ = new SymDirectoryImpl();
+#if CABLE
+	impl_->sec_ = NULL;
+#endif
+	impl_->obj_ = NULL;
+	impl_->t_ = NULL;
 }
 
 SymDirectory::SymDirectory(int type) {
@@ -363,7 +377,10 @@ SymbolItem::SymbolItem(const char* n, int whole_array) {
 	ob_ = NULL;
 	name_ = n;
 	whole_array_ = whole_array;
-		
+#if CABLE
+	pysec_type_ = 0;
+	pysec_ = NULL;
+#endif
 }
 SymbolItem::SymbolItem(Symbol* sym, Objectdata* od, int index, int whole_array) {
 	symbol_ = sym;
@@ -385,6 +402,10 @@ SymbolItem::SymbolItem(Symbol* sym, Objectdata* od, int index, int whole_array) 
 		name_ = sym->name;
 	}
 	index_ = index;
+#if CABLE
+	pysec_type_ = 0;
+	pysec_ = NULL;
+#endif
 }
 
 int SymbolItem::whole_vector() {
@@ -398,6 +419,10 @@ SymbolItem::SymbolItem(Object* ob) {
 	char buf[10];
 	sprintf(buf, "%d", ob->index);
 	name_ = buf;
+#if CABLE
+	pysec_type_ = 0;
+	pysec_ = NULL;
+#endif
 }
 
 void SymbolItem::no_object() {
@@ -420,6 +445,11 @@ bool SymbolItem::is_directory() const {
 	if (ob_) {
 		return true;
 	}
+#if CABLE
+	if (pysec_) {
+		return true;
+	}
+#endif
 	return false;
 }
 
@@ -440,6 +470,10 @@ void SymDirectoryImpl::load(int type) {
 #if CABLE
 	case RANGEVAR:
 		load(type, hoc_built_in_symlist);
+		break;
+	case PYSEC:
+		path_ = "_pysec.";
+		nrn_symdir_load_pysec(symbol_list_, NULL);
 		break;
 #endif
 	default:

src/ivoc/symdir.h
@@ -33,6 +33,7 @@ public:
 		Symbol*, int array_index=0, int node_index=0);
 	SymDirectory(Object*);
 	SymDirectory(int type);
+	SymDirectory();
 	virtual ~SymDirectory();
 
 	virtual const String& path() const;
@@ -49,6 +50,8 @@ public:
 	int array_index(int index) const;
 	Object* object() const; // the parent_object
 	Object* obj(int index); // non-NULL if SymbolItem is an object
+	virtual bool is_pysec(int index) const;
+	SymDirectory* newsymdir(int index);
 private:
 	SymDirectoryImpl* impl_;
 };

src/nrniv/Makefile.am
@@ -83,7 +83,7 @@ libnrniv_la_SOURCES = nrnmenu.cpp shape.cpp classreg.cpp rotate3d.cpp \
 	nvector_nrnthread_ld.c nvector_nrnserial_ld.c \
 	$(PARSRC1) bgpmeminfo.c \
 	netpar.cpp partrans.cpp splitcell.cpp multisplit.cpp \
-	bbsavestate.cpp nrnbbcore_write.cpp \
+	bbsavestate.cpp nrnbbcore_write.cpp pysecname2sec.cpp \
 	nrndae.cpp matrixmap.cpp geometry3d.cpp
 
 libnrniv_la_LIBADD = @MUSIC_LIBLA@

commit 6819979820ecb26394f2c1930eeb5fdd43cb7856

When configured without python, changeset 76e0330 caused secname() problems. i.e wrongly prefixed _pysec. to object.name Make sure PROP_PY_INDEX is not used unless #if USE_PYTHON ...

src/ivoc/datapath.cpp
@@ -5,6 +5,7 @@
 #include <OS/table.h>
 #include <OS/list.h>
 #include "hoclist.h"
+#include <nrnpython_config.h>
 #if HAVE_IV
 #include "graph.h"
 #endif
@@ -444,6 +445,7 @@ void HocDataPathImpl::search_vectors() {
 #if CABLE
 
 void HocDataPathImpl::search_pysec() {
+#if USE_PYTHON
   CopyString cs("");
   hoc_Item* qsec;
   ForAllSections(sec)
@@ -454,6 +456,7 @@ void HocDataPathImpl::search_pysec() {
       strlist_.remove(strlist_.count()-1);
     }
   }
+#endif
 }
 
 void HocDataPathImpl::search(Section* sec) {

src/nrniv/pysecname2sec.cpp
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 #include <hocparse.h>
+#include <nrnpython_config.h>
 
 extern "C" {
 #include <parse.h>
@@ -24,6 +25,7 @@ static Name2CellorSec n2cs;
 #define hoc_acterror(a,b) printf("%s %s\n", a, b)
 
 static void activate() {
+#if USE_PYTHON
   if (!activated) {
     //printf("first activation\n");
     activated = true;
@@ -34,6 +36,7 @@ static void activate() {
       }
     }
   }
+#endif
 }
 
 Section* nrnpy_pysecname2sec(const char* name) { // could be Cell part or Sec

src/nrnoc/cabcode.c
@@ -1796,6 +1796,7 @@ const char* secname(Section* sec) /* name of section (for use in error messages)
 }
 
 const char* nrn_sec2pysecname(Section* sec) {
+#if USE_PYTHON
   static char buf[256];
   const char* name = secname(sec);
   if (sec && sec->prop->dparam[PROP_PY_INDEX]._pvoid
@@ -1805,6 +1806,9 @@ const char* nrn_sec2pysecname(Section* sec) {
     strcpy(buf, name);
   }
   return buf;
+#else
+  return secname(sec);
+#endif
 }
 
 void section_owner(void) {
@@ -1840,8 +1844,10 @@ char* hoc_section_pathname(Section* sec)
 		}else{
 			Sprintf(name, "%s%s", s->name, hoc_araystr(s, indx, hoc_objectdata));
 		}
+#if USE_PYTHON
 	}else if (sec && sec->prop && sec->prop->dparam[PROP_PY_INDEX]._pvoid) {
 		strcpy(name, nrn_sec2pysecname(sec));
+#endif
 	}else{
 		name[0] = '\0';
 	}