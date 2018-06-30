e9bc9eaa996d28b9c168a15851c1538217483b55
adam newton <adam.newton@yale.edu>
2018-06-30 14:47:59 -0400

    Replaced correct_data based on current C implementation. 	modified:   correct_data/cabuf.dat 	modified:   correct_data/cabuf_fixed_step.dat 	modified:   correct_data/multicompartment_reactions.dat 	modified:   correct_data/pure_diffusion.dat 	modified:   correct_data/pure_diffusion_cvode.dat 	modified:   tests/multicompartment_reactions.py

--- share/lib/python/neuron/rxdtests/tests/multicompartment_reactions.py
+++ share/lib/python/neuron/rxdtests/tests/multicompartment_reactions.py
@@ -2,20 +2,20 @@
 #http://www.neuron.yale.edu/neuron/static/docs/rxd/index.html
 from neuron import rxd, h, gui
 from matplotlib import pyplot
-
+import numpy
+pyplot.ion()
 sec = h.Section()
 sec.L=100
 sec.diam=1
 sec.nseg=100
 
-#h.CVode().active(1)
-
-#h.CVode().re_init()
+h.CVode().active(1)
 
 
-caDiff = 0.016
+
+caDiff =  0.016
 ip3Diff = 0.283
-cac_init = 1.e-4
+cac_init = 1.0e-4
 ip3_init = 0.1
 gip3r = 12040
 gserca = 0.3913
@@ -23,13 +23,13 @@ gleak = 6.020
 kserca = 0.1
 kip3 = 0.15
 kact = 0.4
-ip3rtau = 2000
+ip3rtau = 2000.0
 
 #These parameters where missing in the tutorial so arbitrary values were chosen
 #any resemblance to experimental values is purely coincidental.
 fc = 0.7
 fe = 0.3
-caCYT_init=1.0 
+caCYT_init=0.1 
 
 
 cyt = rxd.Region(h.allsec(), name='cyt', nrn_region='i', geometry=rxd.FractionalVolume(fc,surface_fraction=1))
@@ -38,11 +38,11 @@ er= rxd.Region(h.allsec(), name='er', geometry=rxd.FractionalVolume(fe/2.))
 
 cyt_er_membrane = rxd.Region(h.allsec(), name='mem', geometry = rxd.ScalableBorder(1, on_cell_surface=False))
 
-ca  = rxd.Species([cyt, er], d=caDiff, name="ca", charge=2, initial=caCYT_init)
+ca  = rxd.Species({cyt, er}, d=caDiff, name="ca", charge=2, initial=caCYT_init)
 
 
 ip3 = rxd.Species(cyt, d=ip3Diff, name="ip3", initial=ip3_init)
-ip3r_gate_state = rxd.State(cyt_er_membrane, name="gate", initial=0.8)
+ip3r_gate_state = rxd.Species(cyt_er_membrane, name="gate", initial=0.8)
 h_gate = ip3r_gate_state[cyt_er_membrane]
 minf = ip3[cyt] * 1000. * ca[cyt] / (ip3[cyt] + kip3) / (1000. * ca[cyt] + kact)
 k = gip3r * (minf * h_gate) ** 3
@@ -80,12 +80,11 @@ for node in ip3.nodes:
         node.concentration = 2
 
 
+h.CVode().re_init()
+h.continuerun(1000)
 
-h.frecord_init()
-#h.continuerun(100)
-
-#pyplot.plot(times,cacyt_trace,label="ca[cyt]")
-#pyplot.plot(times,caer_trace,label="ca[er]")
-#pyplot.plot(times,ip3_trace,label="ip3")
-#pyplot.legend()
-#pyplot.show()
+pyplot.plot(times,cacyt_trace,label="ca[cyt]")
+pyplot.plot(times,caer_trace,label="ca[er]")
+pyplot.plot(times,ip3_trace,label="ip3")
+pyplot.legend()
+pyplot.show()
