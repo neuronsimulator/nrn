ba0e0431b849e2a02e5b4bfee558eba126db6b3f
adam newton <adam.newton@yale.edu>
2018-09-28 10:54:21 -0400

    Removed 'rxdmath.h' as the declarations have been moved to a python string.

--- src/nrnpython/rxdmath.h
+++
@@ -1,8 +0,0 @@
-/*Some functions supported by numpy that aren't included in math.h
- * names and arguments match the wrappers used in rxdmath.py
- */
-
-double factorial(const double);
-double degrees(const double);
-void radians(const double, double*);
-double log1p(const double);
