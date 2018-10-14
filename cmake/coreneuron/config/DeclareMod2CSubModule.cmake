4fde1773b0d8ea44097eec8b042413c421699000
Pramod Kumbhar <pramod.s.kumbhar@gmail.com>
2018-10-14 23:30:21 +0200

    Add cmake option to set external MOD2C (BlueBrain/CoreNeuron#123)
    Remove CMAKE_CACHE_ARGS to avoid long warnings like:
    
    CMake Warning at /usr/local/Cellar/cmake/3.12.3/share/cmake/Modules/ExternalProject.cmake:1702 (message):
      Line 'CMAKE_C_COMPILER=gcc' does not match regex.  Ignoring.
    Call Stack (most recent call first):
      /usr/local/Cellar/cmake/3.12.3/share/cmake/Modules/ExternalProject.cmake:2766 (_ep_command_line_to_initial_cache)
      /usr/local/Cellar/cmake/3.12.3/share/cmake/Modules/ExternalProject.cmake:2840 (_ep_extract_configure_command)
    
    The drawback of this approach is that cmake shows different compiler that make for external project.
    
    CoreNEURON Repo SHA: BlueBrain/CoreNeuron@41ef177847333bbbcb224f7d39f902bd090a6a48

--- cmake/coreneuron/config/DeclareMod2CSubModule.cmake
+++ cmake/coreneuron/config/DeclareMod2CSubModule.cmake
@@ -28,21 +28,22 @@ include(FindPkgConfig)
 
 find_path(MOD2C_PROJ NAMES CMakeLists.txt PATHS "${PROJECT_SOURCE_DIR}/external/mod2c")
 find_package_handle_standard_args(MOD2C REQUIRED_VARS MOD2C_PROJ)
+
 if (NOT MOD2C_FOUND)
   message (FATAL_ERROR "missing mod2c submodule :run on top directory of your sources (git > 1.8.2 required):
       git submodule update --init --remote
       ")
 endif()
+
 set(ExternalProjectCMakeArgs
      -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
      -DCMAKE_INSTALL_PREFIX=${PROJECT_BINARY_DIR}/external/mod2c
      -DCMAKE_C_COMPILER=${FRONTEND_C_COMPILER} -DCMAKE_CXX_COMPILER=${FRONTEND_CXX_COMPILER}
-   )
+)
 
 ExternalProject_Add(mod2c
   SOURCE_DIR ${PROJECT_SOURCE_DIR}/external/mod2c
   GIT_SUBMODULES
   CMAKE_ARGS ${ExternalProjectCMakeArgs}
-  CMAKE_CACHE_ARGS ${ExternalProjectCMakeArgs}
-  )
+)
 
