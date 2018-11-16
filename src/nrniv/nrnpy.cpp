neuronsimulator/nrn

commit b85e8ff9ce4d354802c3ad7c20b8448652894811

Squashed commit of the following:
commit 741dcd8dad714a010b826b7f988428169a680c66
Author: Michael Hines <michael.hines@yale.edu>
Date:   Thu Nov 15 13:58:14 2018 -0500

    hoc_back2forward changes in place so need to strdup first.

commit a67828873962e985274b57eede79895a90913335
Author: Robert A McDougal <robert.mcdougal@yale.edu>
Date:   Wed Nov 14 18:15:18 2018 -0500

    allows nrniv to work in Windows 10 with the Linux Subsystem enabled and with Git command line tools

src/nrniv/nrnpy.cpp
@@ -23,6 +23,7 @@ extern void (*p_nrnpython_start)(int);
 void nrnpython();
 static void (*p_nrnpython_real)();
 static void (*p_nrnpython_reg_real)();
+char* hoc_back2forward(char* s);
 }
 
 // following is undefined or else has the value of sys.api_version
@@ -129,9 +130,21 @@ static void set_nrnpylib() {
   if (nrnpy_pyhome) { nrnpy_pyhome = strdup(nrnpy_pyhome); }
 
   if (nrnmpi_myid_world == 0) {
-    char line[1024];
+    #ifdef MINGW
+    char* line = new char[1024 + 3*strlen(neuron_home)];
+    char* bfnrnhome = strdup(neuron_home);
+    hoc_back2forward(neuron_home);
+    snprintf(line, 1024, "%s\\mingw\\usr\\bin\\bash %s/bin/nrnpyenv.sh %s --NEURON_HOME=%s",
+      neuron_home, 
+      bfnrnhome,
+      (nrnpy_pyexe && strlen(nrnpy_pyexe) > 0) ? nrnpy_pyexe : "",
+      bfnrnhome);
+    delete [] bfnrnhome;
+    #else
+    char* line = new char[1024 + 3*strlen(neuron_home)];
     snprintf(line, 1024, "bash nrnpyenv.sh %s",
       (nrnpy_pyexe && strlen(nrnpy_pyexe) > 0) ? nrnpy_pyexe : "");
+   #endif
     FILE* p = popen(line, "r");
     if (!p) {
       printf("could not popen '%s'\n", line);
@@ -153,6 +166,7 @@ static void set_nrnpylib() {
       }
       pclose(p);
     }
+    delete [] line;
   }
 #if NRNMPI
   if (nrnmpi_numprocs_world > 1) { // 0 broadcasts to everyone else.

bin/nrnpyenv.sh
@@ -36,6 +36,17 @@ export originalPYTHONPATH="$PYTHONPATH"
 export originalPYTHONHOME="$PYTHONHOME"
 export originalLDLIBRARYPATH="$LD_LIBRARY_PATH"
 
+# get the last argument
+for last; do true; done
+
+# if the last argument begins with --NEURON_HOME=
+if [[ $last == "--NEURON_HOME="* ]] ; then
+last=${last#"--NEURON_HOME="}
+export PATH=/cygdrive/${last//:/}/mingw/usr/bin:/cygdrive/${last//:/}/mingw/mingw64/bin:$PATH
+# remove the last argument
+set -- "${@:1:$(($#-1))}"
+fi
+
 if test "$PYTHONHOME" != "" ; then
   echo "# Ignoring existing PYTHONHOME=$PYTHONHOME."
   unset PYTHONHOME

mingw_files/nrnmingwenv.sh
@@ -23,7 +23,7 @@ cp $HOME/.inputrc $N/etc/inputrc
 #cp /msys2_shell.cmd $N
 
 binprog="basename bash cat cp dirname echo find grep ls make mintty
-  mkdir mv rebase rm sed sh sort unzip which cygpath cygcheck"
+  mkdir mv rebase rm sed sh sort unzip which cygpath cygcheck uname"
 for i in $binprog ; do
   echo $i
   cp /usr/bin/$i.exe $N/usr/bin/$i.exe
@@ -84,8 +84,6 @@ copy mingw64/x86_64-w64-mingw32/include '
 _mingw.h
 _mingw_mac.h
 _mingw_off_t.h
-_mingw_print_pop.h
-_mingw_print_push.h
 _mingw_secapi.h
 assert.h
 corecrt_startup.h

commit 613c14946d8071487da3468487af98c5f40da1be

fix bug introduced by b85e8ff

src/nrniv/nrnpy.cpp
@@ -130,26 +130,28 @@ static void set_nrnpylib() {
   if (nrnpy_pyhome) { nrnpy_pyhome = strdup(nrnpy_pyhome); }
 
   if (nrnmpi_myid_world == 0) {
+    int linesz = 1024 + (nrnpy_pyexe ? strlen(nrnpy_pyexe) : 0);
     #ifdef MINGW
-    char* line = new char[1024 + 3*strlen(neuron_home)];
+    linesz += 3*strlen(neuron_home);
+    char* line = new char[linesz+1];
     char* bfnrnhome = strdup(neuron_home);
-    hoc_back2forward(neuron_home);
-    snprintf(line, 1024, "%s\\mingw\\usr\\bin\\bash %s/bin/nrnpyenv.sh %s --NEURON_HOME=%s",
+    hoc_back2forward(bfnrnhome);
+    sprintf(line, "%s\\mingw\\usr\\bin\\bash %s/bin/nrnpyenv.sh %s --NEURON_HOME=%s",
       neuron_home, 
       bfnrnhome,
       (nrnpy_pyexe && strlen(nrnpy_pyexe) > 0) ? nrnpy_pyexe : "",
       bfnrnhome);
-    delete [] bfnrnhome;
+    free(bfnrnhome);
     #else
-    char* line = new char[1024 + 3*strlen(neuron_home)];
-    snprintf(line, 1024, "bash nrnpyenv.sh %s",
+    char* line = new char[linesz+1];
+    sprintf(line, "bash nrnpyenv.sh %s",
       (nrnpy_pyexe && strlen(nrnpy_pyexe) > 0) ? nrnpy_pyexe : "");
    #endif
     FILE* p = popen(line, "r");
     if (!p) {
       printf("could not popen '%s'\n", line);
     }else{
-      while(fgets(line, 1024, p)) {
+      while(fgets(line, linesz, p)) {
         char* cp;
         // must get rid of beginning '"' and trailing '"\n'
         if (!nrnpy_pyhome && (cp = strstr(line, "export PYTHONHOME="))) {