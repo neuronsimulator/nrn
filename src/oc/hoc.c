neuronsimulator/nrn

commit 31493b9c55c29769348b63b7599730bdf8a4f539

Built in NEURON methods may now return ints and bools to Python (in addition to doubles) (#67)
Example (previously all the returns were doubles):

>>> v = h.Vector([2,62,15.1])
>>> v.x[2]
15.1
>>> v.size()
3
>>> v.contains(142)
False
>>> v.contains(62)
True
>>> pc = h.ParallelContext()
>>> pc.nhost()
1
>>> pc.id()
0
>>> s = h.Section()
>>> h.define_shape()
1.0
>>> h.x3d(1)
50.0

src/oc/hoc_oop.c
@@ -46,6 +46,8 @@ static void free_objectdata(Objectdata*, Template*);
 
 int hoc_print_first_instance = 1;
 
+int hoc_max_builtin_class_id = -1;
+
 static Symbol* hoc_obj_;
 
 void hoc_install_hoc_obj(void) {
@@ -1512,6 +1514,9 @@ void class2oc(
 	tsym->subtype = CPLUSOBJECT;
 	hoc_begintemplate(tsym);
 	t = tsym->u.template;
+	if (t->id > hoc_max_builtin_class_id) {
+		hoc_max_builtin_class_id = t->id;
+	}
 	t->constructor = cons;
 	t->destructor = destruct;
 	t->steer = 0;

src/ivoc/graph.cpp
@@ -6,6 +6,8 @@
 #include <math.h>
 #include <assert.h>
 
+extern "C" int hoc_return_type_code;
+
 #if HAVE_IV
 #include <InterViews/glyph.h>
 #include <InterViews/hit.h>
@@ -990,6 +992,7 @@ ENDGUI
 }
 
 static double gr_view_count(void* v) {
+	hoc_return_type_code = 1; // integer
 #if HAVE_IV
 	int n = 0;
 IFGUI

src/ivoc/ivocvect.cpp
@@ -138,6 +138,8 @@ extern "C" {
         extern int nrn_mlh_gsort (double* vec, int *base_ptr, int total_elems, doubleComparator cmp);
 };
 
+extern "C" int hoc_return_type_code;
+
 IvocVect::IvocVect(Object* o) : ParentVect(){obj_ = o; label_ = NULL; MUTCONSTRUCT(0)}
 IvocVect::IvocVect(int l, Object* o) : ParentVect(l){obj_ = o; label_ = NULL; MUTCONSTRUCT(0)}
 IvocVect::IvocVect(int l, double fill_value, Object* o) : ParentVect(l, fill_value){obj_ = o; label_ = NULL; MUTCONSTRUCT(0)}
@@ -394,6 +396,7 @@ static double v_fwrite(void* v) {
 	int x_max = vp->capacity()-1;
 	int start = 0;
 	int end = x_max;
+	hoc_return_type_code = 1; // integer
 	if (ifarg(2)) {
 	  start = int(chkarg(2,0,x_max));
 	  end = int(chkarg(3,0,x_max));
@@ -418,6 +421,7 @@ static double v_fread(void* v) {
 	void* s = (void*)(vp->vec());
 
         Object* ob = *hoc_objgetarg(1);
+
 	check_obj_type(ob, "File");
 	OcFile* f = (OcFile*)(ob->u.this_pointer);
 
@@ -761,6 +765,7 @@ static double v_printf(void *v) {
 	   }
            if(extra_newline) {printf("\n");}
 	 }
+	hoc_return_type_code = 1; // integer
 	return double(end-start+1);
 }
 
@@ -774,6 +779,8 @@ static double v_scanf(void *v) {
         Object* ob = *hoc_objgetarg(1);
     	check_obj_type(ob, "File");
         OcFile* f = (OcFile*)(ob->u.this_pointer);
+
+    hoc_return_type_code = 1; // integer
 
 	if (ifarg(4)) {
 	  n = int(*getarg(2));
@@ -831,6 +838,7 @@ static double v_scantil(void *v) {
     	check_obj_type(ob, "File");
         OcFile* f = (OcFile*)(ob->u.this_pointer);
 
+    hoc_return_type_code = 1; // integer
 	til = *getarg(2);
 	if (ifarg(4)) {
 	  c = int(*getarg(3));
@@ -1252,6 +1260,7 @@ static Object** v_ind(void* v) {
 
 static double v_size(void* v) {
 	Vect* x = (Vect*)v;
+	hoc_return_type_code = 1;
 	return double(x->capacity());
 }
 
@@ -1261,6 +1270,7 @@ static double v_buffer_size(void* v) {
 		int n = (int)chkarg(1, (double)x->capacity(), dmaxint_);
 		x->buffer_size(n);
 	}
+	hoc_return_type_code = 1;
 	return x->buffer_size();
 }
 
@@ -1386,6 +1396,7 @@ static Object** v_remove(void* v) {
 static double v_contains(void* v) {
         Vect* x = (Vect*)v;
         double g = *getarg(1);
+        hoc_return_type_code = 2;
         for (int i=0;i<x->capacity();i++) {
            if (Math::equal(x->elem(i), g, hoc_epsilon)) return 1.;
         }
@@ -1728,7 +1739,7 @@ static double v_indwhere(void* v) {
   int i, iarg, m=0;
   char* op;
   double value,value2;
-
+  hoc_return_type_code = 1;
     op = gargstr(1);
     value = *getarg(2);
     iarg = 3;
@@ -2065,6 +2076,7 @@ static double v_min_ind(void* v)
 {
   Vect* x = (Vect*)v;
   int x_max = x->capacity()-1;
+  hoc_return_type_code = 1; // integer
   if (ifarg(1)) {
     int start = int(chkarg(1,0,x_max));
     int end = int(chkarg(2,0,x_max));
@@ -2091,6 +2103,7 @@ static double v_max_ind(void* v)
 {
   Vect* x = (Vect*)v;
   int x_max = x->capacity()-1;
+  hoc_return_type_code = 1; // integer
   if (ifarg(1)) {
     int start = int(chkarg(1,0,x_max));
     int end = int(chkarg(2,0,x_max));

src/ivoc/matrix.cpp
@@ -11,6 +11,8 @@
 #define EPS hoc_epsilon
 static Symbol* smat_;
 
+extern "C" int hoc_return_type_code;
+
 extern "C" {
 	extern double hoc_scan(FILE*);
 	extern FILE* hoc_obj_file_arg(int i);
@@ -63,11 +65,13 @@ Object** Matrix::temp_objvar() {
 }
 
 static double m_nrow(void* v) {
+	hoc_return_type_code = 1; // integer
 	Matrix* m = (Matrix*)v;
 	return (double)m->nrow();
 }
 
 static double m_ncol(void* v) {
+	hoc_return_type_code = 1; // integer
 	Matrix* m = (Matrix*)v;
 	return (double)m->ncol();
 }
@@ -95,6 +99,7 @@ static double m_getval(void* v) {
 static double m_sprowlen(void* v) {
 	Matrix* m = (Matrix*)v;
 	int i;
+	hoc_return_type_code = 1; // integer
 	i = (int)chkarg(1, 0, m->nrow()-1);
 	return double(m->sprowlen(i));
 }

src/ivoc/mlinedit.cpp
@@ -1,4 +1,7 @@
 #include <../../nrnconf.h>
+
+extern "C" int hoc_return_type_code;
+
 #if HAVE_IV // to end of file
 
 #include <stdio.h>
@@ -46,6 +49,7 @@ static double map(void* v) {
 
 static double readonly(void* v) {
 	OcMLineEditor* e = (OcMLineEditor*)v;
+	hoc_return_type_code = 2; // boolean
 	if (ifarg(1)) {
 		e->txt_->readOnly(int(chkarg(1, 0, 1)));
 	}

src/ivoc/mymath.cpp
@@ -6,6 +6,8 @@
 #include <math.h>
 #include <stdio.h>
 
+extern "C" int hoc_return_type_code;
+
 static double distance_to_line(void*) {
 	return MyMath::distance_to_line(*getarg(1), *getarg(2),
 		*getarg(3), *getarg(4),	*getarg(5), *getarg(6));
@@ -17,6 +19,7 @@ static double distance_to_line_segment(void*) {
 }
 
 static double inside(void*) {
+	hoc_return_type_code = 2; // boolean
 	return MyMath::inside(*getarg(1), *getarg(2), *getarg(3), *getarg(4),
 		*getarg(5), *getarg(6));
 }
@@ -54,6 +57,7 @@ int nrn_feround(int mode) {
 
 static double feround(void*) {
 	int arg = 0;
+	hoc_return_type_code = 1; // integer
 	if (ifarg(1)) {
 		arg = (int)chkarg(1, 0, 4);
 	}

src/ivoc/ocbox.cpp
@@ -22,6 +22,8 @@
 #include "oc2iv.h"
 #include "classreg.h"
 
+extern "C" int hoc_return_type_code;
+
 #if HAVE_IV
 
 class NrnFixedLayout : public Layout {
@@ -250,6 +252,7 @@ ENDGUI
 }
 
 static double ismapped(void* v) {
+	hoc_return_type_code = 2;
 #if HAVE_IV
 	bool b = false;
 IFGUI

src/ivoc/ocfile.cpp
@@ -9,6 +9,8 @@
 #include <unistd.h>
 #endif
 
+extern "C" int hoc_return_type_code;
+
 #ifdef WIN32
 #include <errno.h>
 #include <io.h>
@@ -257,6 +259,7 @@ static double f_seek(void* v){
 
 static double f_tell(void* v){
 	OcFile* f = (OcFile*)v;
+	hoc_return_type_code = 1;
 	BinaryMode(f)
 	return (double)ftell(f->file());
 }

commit f837b5cf79708723b32da2a2d0cd5efffbca137c

Use of nrn_load_dll(...) after creating a hoc template could cause problems. This bug was introduced in changeset 31493b9c .

src/oc/hoc.c
@@ -784,12 +784,13 @@ int hoc_pid(void) { return (int)getpid();} /* useful for making unique temporary
 /* readline should be avoided if stdin is not a terminal */
 int nrn_istty_;
 
+int hoc_main1_inited_;
+
 /* has got to be called first. oc can only be event driven after this returns */
 void hoc_main1_init(const char* pname, const char** envp)
 {
 	extern NrnFILEWrap *frin;
 	extern FILE	*fout;
-	static int inited = 0;
 
 	if (!hoc_xopen_file_) {
 		hoc_xopen_file_size_ = 200;
@@ -800,10 +801,9 @@ void hoc_main1_init(const char* pname, const char** envp)
 	hoc_promptstr = "oc>";
 	yystart = 1;
 	lineno = 0;
-	if (inited) {
+	if (hoc_main1_inited_) {
 		return;
 	}
-	inited = 1;
 
 	/* could have been forced with the -isatty option */
 	if (nrn_istty_ == 0) { /* if not set then */
@@ -843,6 +843,7 @@ void hoc_main1_init(const char* pname, const char** envp)
 #if defined(__GO32__)
 	setcbrk(0);
 #endif
+	hoc_main1_inited_ = 1;
 }
 
 HocStr* hocstr_create(size_t size) {

src/oc/hoc_oop.c
@@ -1503,6 +1503,7 @@ void class2oc(
 	Member_ret_obj_func* mobjret,
 	Member_ret_str_func* strret
 ){
+	extern int hoc_main1_inited_;
 	Symbol* tsym, *s;
 	Template* t;
 	int i;
@@ -1514,7 +1515,7 @@ void class2oc(
 	tsym->subtype = CPLUSOBJECT;
 	hoc_begintemplate(tsym);
 	t = tsym->u.template;
-	if (t->id > hoc_max_builtin_class_id) {
+	if (!hoc_main1_inited_ && t->id > hoc_max_builtin_class_id) {
 		hoc_max_builtin_class_id = t->id;
 	}
 	t->constructor = cons;