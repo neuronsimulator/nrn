aa8d28a9dd6a03f0ddfbed251654bc1b7ce72810
Pramod Kumbhar <pramod.s.kumbhar@gmail.com>
2019-03-06 21:51:04 +0100

    Fix issues in standalone lexer (#36)
      - lexer now returns POD data structures but standalone
        lexer was not updated
      - add command line option `--text` to lex/parse nmodl as text
      - update README

--- src/parser/main_nmodl.cpp
+++ src/parser/main_nmodl.cpp
@@ -18,18 +18,26 @@
 int main(int argc, const char* argv[]) {
     CLI::App app{"NMODL-Parser : Standalone Parser for NMODL"};
 
-    std::vector<std::string> files;
-    app.add_option("file", files, "One or more MOD files to process")
-        ->required()
-        ->check(CLI::ExistingFile);
+    std::vector<std::string> mod_files;
+    std::vector<std::string> mod_texts;
+
+    app.add_option("file", mod_files, "One or more NMODL files")->check(CLI::ExistingFile);
+    app.add_option("--text", mod_texts, "One or more NMODL constructs as text");
 
     CLI11_PARSE(app, argc, argv);
 
-    for (const auto& f: files) {
-        nmodl::logger->info("Processing {}", f);
-        nmodl::parser::NmodlDriver driver;
-        driver.set_verbose(true);
+    nmodl::parser::NmodlDriver driver;
+    driver.set_verbose(true);
+
+    for (const auto& f: mod_files) {
+        nmodl::logger->info("Processing file : {}", f);
         driver.parse_file(f);
     }
+
+    for (const auto& text: mod_texts) {
+        nmodl::logger->info("Processing text : {}", text);
+        driver.parse_string(text);
+    }
+
     return 0;
 }
