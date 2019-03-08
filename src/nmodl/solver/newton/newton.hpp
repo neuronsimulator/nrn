a3941230bf1fc2f57e628fbc1a4a04625821fcc6
Liam Keegan <liam@keegan.ch>
2019-03-08 20:24:15 +0100

    Add Newton solver (BlueBrain/nmodl#38)
      - newton solver, header only,  implementation using Eigen matrix library (will be used by the derivimplicit integrator)
      - duplicates functionality of newton_thread from scopmath_core of coreneuron
      - should be more performant for small matrices (<=4)
      - added tests for non-linear systems of size : 1, 2, 3, 4, 5, 10 (for examples of use see test/newton)
      - add eigen as submodule for tests
    
    NMODL Repo SHA: BlueBrain/nmodl@748c3496cf3907778b0c18888a630d0875593474

---
+++ src/nmodl/solver/newton/newton.hpp
@@ -0,0 +1,119 @@
+/*************************************************************************
+ * Copyright (C) 2018-2019 Blue Brain Project
+ *
+ * This file is part of NMODL distributed under the terms of the GNU
+ * Lesser General Public License. See top-level LICENSE file for details.
+ *************************************************************************/
+
+#pragma once
+
+// Implementation of Newton method for solving
+// system of non-linear equations using Eigen
+
+#include <iostream>
+
+#include "Eigen/LU"
+
+namespace nmodl {
+namespace newton {
+
+constexpr int MAX_ITER = 1e3;
+constexpr double EPS = 1e-12;
+
+// Newton method
+// given initial vector X
+// and a functor that calculates F(X), J(X)
+// solves for F(X) = 0,
+// starting with initial value of X
+// where J(X) is the Jacobian of F(X)
+// by iterating:
+// X_{n+1} = X_n - J(X_n)^{-1} F(X_n)
+//
+// when |F|^2 < eps^2, solution has converged
+// returns number of iterations (-1 if failed to converge)
+template <int N, typename FUNC>
+int newton_solver(Eigen::Matrix<double, N, 1>& X,
+                  FUNC functor,
+                  double eps = EPS,
+                  int max_iter = MAX_ITER) {
+    // Vector to store result of function F(X):
+    Eigen::Matrix<double, N, 1> F;
+    // Matrix to store jacobian of F(X):
+    Eigen::Matrix<double, N, N> J;
+    // Solver iteration count:
+    int iter = -1;
+
+    while (++iter < max_iter) {
+        // calculate F, J from X using user-supplied functor
+        functor(X, F, J);
+        // get error norm: here we use sqrt(|F|^2)
+        double error = F.norm();
+        // std::cout.precision(15);
+        // std::cout << "[newton_solver] iter: " << iter << "\terror: " << error << "\n";
+        // std::cout << "\n" << X << std::endl;
+        if (error < eps) {
+            // we have converged: return iteration count
+            return iter;
+        }
+        // update X
+        // use in-place LU decomposition of J with partial pivoting
+        // (suitable for any N, but less efficient than .inverse() for N <=4)
+        X -= Eigen::PartialPivLU<Eigen::Ref<Eigen::Matrix<double, N, N>>>(J).solve(F);
+    }
+    // If we fail to converge after max_iter iterations, return -1
+    return -1;
+}
+
+template <typename FUNC, int N>
+int solver(Eigen::Matrix<double, N, 1>& X, FUNC functor, double eps, int max_iter) {
+    Eigen::Matrix<double, N, 1> F;
+    Eigen::Matrix<double, N, N> J;
+    int iter = -1;
+    while (++iter < max_iter) {
+        functor(X, F, J);
+        double error = F.norm();
+        if (error < eps) {
+            return iter;
+        }
+        X -= J.inverse() * F;
+    }
+    return -1;
+}
+
+// template specializations for N <= 4
+// use explicit inverse of F instead of LU decomposition
+// (more efficient for small matrices - not safe for large matrices)
+template <typename FUNC>
+int newton_solver(Eigen::Matrix<double, 1, 1>& X,
+                  FUNC functor,
+                  double eps = EPS,
+                  int max_iter = MAX_ITER) {
+    return solver<FUNC, 1>(X, functor, eps, max_iter);
+}
+
+template <typename FUNC>
+int newton_solver(Eigen::Matrix<double, 2, 1>& X,
+                  FUNC functor,
+                  double eps = EPS,
+                  int max_iter = MAX_ITER) {
+    return solver<FUNC, 2>(X, functor, eps, max_iter);
+}
+
+template <typename FUNC>
+int newton_solver(Eigen::Matrix<double, 3, 1>& X,
+                  FUNC functor,
+                  double eps = EPS,
+                  int max_iter = MAX_ITER) {
+    return solver<FUNC, 3>(X, functor, eps, max_iter);
+}
+
+template <typename FUNC>
+int newton_solver(Eigen::Matrix<double, 4, 1>& X,
+                  FUNC functor,
+                  double eps = EPS,
+                  int max_iter = MAX_ITER) {
+    return solver<FUNC, 4>(X, functor, eps, max_iter);
+}
+
+}  // namespace newton
+}  // namespace nmodl
