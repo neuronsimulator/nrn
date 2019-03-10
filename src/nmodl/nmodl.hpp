ad9c2efaaee02c8f476c668705d3475bb8510628
Pramod Kumbhar <pramod.s.kumbhar@gmail.com>
2019-03-10 13:19:33 +0100

    Newton solver CUDA compatibility with Eigen (#45)
      - mark newton solver routines with EIGEN_DEVICE_FUNC
        so that the solver can be used from cpu or gpu kernels
      - usable with latest master of eigen
      - update eigen submodule to latest master
      - add header nmodl.hpp which includes common solver header
         and set common settings for eigen
    
    Resolves #41 #47

---
+++ src/nmodl/nmodl.hpp
@@ -0,0 +1,19 @@
+/*************************************************************************
+ * Copyright (C) 2018-2019 Blue Brain Project
+ *
+ * This file is part of NMODL distributed under the terms of the GNU
+ * Lesser General Public License. See top-level LICENSE file for details.
+ *************************************************************************/
+
+#pragma once
+
+
+/// no parallelization using openmp
+#define EIGEN_DONT_PARALLELIZE
+
+/// keep host and CUDA code compatible
+#ifndef EIGEN_DEFAULT_DENSE_INDEX_TYPE
+#define EIGEN_DEFAULT_DENSE_INDEX_TYPE int
+#endif
+
+#include "newton/newton.hpp"
