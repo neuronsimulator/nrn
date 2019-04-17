ae8b8292bae69b34bebc90ec8041e438943f5435
ramcdougal <robert.mcdougal@yale.edu>
2019-04-17 09:40:11 -0400

    units definitions
    Allows writing, e.g.
    
    ca = rxd.Species(cyt, name='ca', charge=2, initial = 100 * uM)
    
    T1degradation = rxd.Rate(T1, -0.01 * nM / hour * T1)

---
+++ share/lib/python/neuron/units.py
@@ -0,0 +1,23 @@
+''' unit definitions '''
+
+# NEURON's default units
+mM = 1
+ms = 1
+mV = 1
+
+# concentration
+uM = 1e-3 * mM
+nM = 1e-6 * mM
+M = 1e3 * mM
+
+# time
+us = 1e-3 * ms
+sec = 1e3 * ms
+minute = 60 * sec
+hour = 60 * minute
+day = 24 * hour
+
+# voltage
+uV = 1e-3 * mV
+V = 1e3 * mV
+
