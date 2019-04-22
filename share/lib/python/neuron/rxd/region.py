91496e89377bdb46bb7495c401799e683e954cd6
Robert A. McDougal <robert.mcdougal@yale.edu>
2019-04-22 17:54:34 -0400

    bugfix: rxd: dx in repr of region in both crxd and rxd

--- share/lib/python/neuron/rxd/region.py
+++ share/lib/python/neuron/rxd/region.py
@@ -37,7 +37,7 @@ class Region(object):
         # Note: this used to print out dimension, but that's now on a per-segment basis
         # TODO: remove the note when that is fully true
         #self._dx = None
-        return 'Region(..., nrn_region=%r, geometry=%r, dx=%r, name=%r)' % (self.nrn_region, self._geometry, self._dx, self._name)
+        return 'Region(..., nrn_region=%r, geometry=%r, dx=%r, name=%r)' % (self.nrn_region, self._geometry, self.dx, self._name)
 
     def _short_repr(self):
         if self._name is not None:
