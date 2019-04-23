867354667c3bf8de4a79844a8e7a1df9bbfa33da
Pramod Kumbhar <pramod.s.kumbhar@gmail.com>
2019-04-23 13:03:56 +0200

    Update processing of Jinja template files (#147)
      - move templates to sub-directories ast, visitors and pybind
      - remove hardcoded directory names
      - copy .clang-format to tempfile to get generated templates formated
      - trim block set to false to allow empty lines
      - pass clang-format options from cmake

--- src/language/code_generator.py
+++ src/language/code_generator.py
@@ -9,11 +9,10 @@ import argparse
 import filecmp
 import logging
 import os
-from pathlib import Path
+from pathlib import Path, PurePath
 import shutil
 import subprocess
 import tempfile
-
 import jinja2
 
 from parser import LanguageParser
@@ -24,8 +23,9 @@ import utils
 logging.basicConfig(level=logging.INFO, format='%(message)s')
 parser = argparse.ArgumentParser()
 parser.add_argument('--clang-format', help="Path to clang-format executable")
-parser.add_argument('--clang-format-opts', help="Path to clang-format executable", nargs='+')
+parser.add_argument('--clang-format-opts', help="clang-format options", nargs='+')
 parser.add_argument('--base-dir')
+
 args = parser.parse_args()
 clang_format = args.clang_format
 if clang_format:
@@ -33,54 +33,59 @@ if clang_format:
     if args.clang_format_opts:
         clang_format += args.clang_format_opts
 
-# parse nmodl definition file and get list of abstract nodes
+# parse NMODL and codegen definition file to get AST nodes
 nmodl_nodes = LanguageParser("nmodl.yaml").parse_file()
 codegen_nodes = LanguageParser("codegen.yaml").parse_file()
 
-# combine ast nodes from NMODL specification and codegen specification
+# combine NMODL and codegen nodes for whole AST
 nodes = nmodl_nodes
 nodes.extend(x for x in codegen_nodes if x not in nodes)
 
-templates = Path(__file__).parent / 'templates'
+# directory containing all templates
+templates_dir = Path(__file__).parent / 'templates'
 
-basedir = Path(args.base_dir) or Path(__file__).resolve().parent.parent
-for subdir in ['pybind', 'visitors', 'ast']:
-    (basedir / subdir).mkdir(parents=True, exist_ok=True)
+# destination directory to render templates
+destination_dir = Path(args.base_dir) or Path(__file__).resolve().parent.parent
 
-env = jinja2.Environment(loader=jinja2.FileSystemLoader(str(templates)),
-                         trim_blocks=True,
+# templates will be created and clang-formated in tempfile.
+# copy .clang-format file for correct formating
+clang_format_file = Path(Path(__file__).resolve().parent.parent.parent / ".clang-format")
+if clang_format_file.exists():
+    temp_dir = tempfile.gettempdir()
+    shutil.copy2(clang_format_file, temp_dir)
+
+env = jinja2.Environment(loader=jinja2.FileSystemLoader(str(templates_dir)),
+                         trim_blocks=False,
                          lstrip_blocks=True)
 env.filters['snake_case'] = utils.to_snake_case
 
 updated_files = []
 
-for fn in templates.glob('*.[ch]pp'):
-    if fn.name.startswith('py'):
-        filename = basedir / 'pybind' / fn.name
-    elif 'visitor' in fn.name:
-        filename = basedir / 'visitors' / fn.name
-    else:
-        filename = basedir / 'ast' / fn.name
-    template = env.get_template(fn.name)
-    content = template.render(nodes=nodes, node_info=node_info)
-    if filename.exists():
-        # render template in temporary file
-        # and update target file ONLY if different
-        # to save a lot of build time
-        fd, tmp_path = tempfile.mkstemp()
-        os.write(fd, content.encode('utf-8'))
-        os.close(fd)
-        if clang_format:
-            subprocess.check_call(clang_format + ['-i', tmp_path])
-        if not filecmp.cmp(str(filename), tmp_path):
-            shutil.move(tmp_path, filename)
-            updated_files.append(str(fn.name))
-    else:
-        with filename.open('w') as fd:
-            fd.write(content)
-            updated_files.append(str(fn.name))
-        if clang_format:
-            subprocess.check_call(clang_format + ['-i', filename])
+for path in templates_dir.iterdir():
+    sub_dir = PurePath(path).name
+    (destination_dir / sub_dir).mkdir(parents=True, exist_ok=True)
+    for filepath in path.glob('*.[ch]pp'):
+        source_file = os.path.join(sub_dir, filepath.name)
+        destination_file = destination_dir / sub_dir / filepath.name
+        template = env.get_template(source_file)
+        content = template.render(nodes=nodes, node_info=node_info)
+        if destination_file.exists():
+            # render template in temporary file and update target file
+            # ONLY if different (to save a lot of build time)
+            fd, tmp_path = tempfile.mkstemp()
+            os.write(fd, content.encode('utf-8'))
+            os.close(fd)
+            if clang_format:
+                subprocess.check_call(clang_format + ['-i', tmp_path])
+            if not filecmp.cmp(str(destination_file), tmp_path):
+                shutil.move(tmp_path, destination_file)
+                updated_files.append(str(filepath.name))
+        else:
+            with destination_file.open('w') as fd:
+                fd.write(content)
+                updated_files.append(str(filepath.name))
+            if clang_format:
+                subprocess.check_call(clang_format + ['-i', destination_file])
 
 if updated_files:
     logging.info('       Updating out of date template files : %s', ' '.join(updated_files))
