6df826b21854434253a1b169f6b8f1b7b0f44e2b
Jorge Blanco Alonso <41900536+jorblancoa@users.noreply.github.com>
2019-05-07 17:37:58 +0200

    Improve documentation (BlueBrain/nmodl#189)
    - Remove some documentation generation warnings
     - Put doxygen documentation on a separate folder
     - Fix README.md link
     - Exclude notebook README from sphinx
    
    NMODL Repo SHA: BlueBrain/nmodl@f37828549d40c71720e4e22e25c3b5b2d18ffb23

--- docs/nmodl/transpiler/conf.py
+++ docs/nmodl/transpiler/conf.py
@@ -29,9 +29,6 @@ import nmodl  # isort:skip
 # Run doxygen
 subprocess.call('doxygen Doxyfile', shell=True)
 
-# Copy doxygen output to the sphinx output directory
-html_static_path = ['_static']
-
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
@@ -106,7 +103,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "**.ipynb_checkpoints"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "**.ipynb_checkpoints", "**.md"]
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = "sphinx"
@@ -131,7 +128,13 @@ html_theme = "sphinx_rtd_theme"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+#html_static_path = ["_static"]
+
+# A list of paths that contain extra files not directly related to the 
+# documentation, such as robots.txt or .htaccess. Relative paths are taken 
+# as relative to the configuration directory. They are copied to the output 
+# directory. They will overwrite any existing file of the same name.
+html_extra_path = ["sphinx_doxygen"]
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
