298320ac7a3ca265d867cf02d83af4e673135da1
adam newton <adam.newton@yale.edu>
2019-05-11 16:53:06 -0400

    Update species.nodes when nseg is changed.

---
+++ share/lib/python/neuron/rxdtests/tests/crxd/nodes_update.py
@@ -0,0 +1,15 @@
+from neuron import h, crxd as rxd
+h.load_file('stdrun.hoc')
+soma = h.Section(name='soma')
+r = rxd.Region([soma])
+soma.nseg = 5
+c = rxd.Species(r, initial=1.0)
+assert(len(c.nodes) == 5)
+soma.nseg = 3
+assert(len(c.nodes) == 3)
+soma.nseg = 7
+assert(len(c[r].nodes) == 7)
+h.tstop = 100
+h.run()
+
+
