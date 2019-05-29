0c61fee314e8d340dbb00ab041b87bb75262658f
Francesco <francesco.cremonesi0@gmail.com>
2019-05-29 22:42:06 +0200

    Fix mofidication of const ion variables (BlueBrain/nmodl#210)
      - read_ion_var should never be marked as const because they
         store copy of corresponding neuron variable value (in which
         case copy is required)
    
    Resolves BlueBrain/nmodl#209.
    
    NMODL Repo SHA: BlueBrain/nmodl@892b87acce2b197d929b3bcd7178a174d2488acb

--- src/nmodl/codegen/codegen_c_visitor.cpp
+++ src/nmodl/codegen/codegen_c_visitor.cpp
@@ -729,9 +729,6 @@ bool CodegenCVisitor::is_constant_variable(std::string name) {
     auto symbol = program_symtab->lookup_in_scope(name);
     bool is_constant = false;
     if (symbol != nullptr) {
-        if (symbol->has_any_property(NmodlType::read_ion_var)) {
-            is_constant = true;
-        }
         if (symbol->has_any_property(NmodlType::param_assign) && symbol->get_write_count() == 0) {
             is_constant = true;
         }
