2a1bdcdb9a6a8947ea5eda4eafeb2808a9686a37
Ioannis Magkanaris <iomagkanaris@gmail.com>
2019-06-19 15:39:47 +0200

    Changed pinned parallel version of neuron to generic parallel version (BlueBrain/CoreNeuron#181)
     * Fix for selecting the first version of neuron with `/parallel` found
     * Added check for finding a compatible neuron version
     * Print error message to stderr
    
    CoreNEURON Repo SHA: BlueBrain/CoreNeuron@2829bb81e118248486dab408aba6a582e160fd9a

--- tests/jenkins/nrnivmodl.sh
+++ tests/jenkins/nrnivmodl.sh
@@ -6,7 +6,17 @@ set +x
 TEST_DIR="$1"
 
 . /gpfs/bbp.cscs.ch/apps/hpc/jenkins/config/modules.sh
-module load neuron/2018-10/python3/parallel intel
+module load intel
+
+neuron_version=$(module av neuron 2>&1 | grep -o -m 1 '^neuron.*/parallel$' | awk -F' ' '{print $1}')
+if [[ $neuron_version ]]; then
+    module load $neuron_version
+    module list
+else
+    echo "Error: no compatible neuron version found." >&2
+    module list
+    exit 1
+fi
 
 unset $(env|awk -F= '/^(PMI|SLURM)_/ {if ($1 != "SLURM_ACCOUNT") print $1}')
 
