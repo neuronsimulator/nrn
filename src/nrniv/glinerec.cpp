7de61816b0021fcef96f7d5be59cd90a00f437fb
Michael Hines <michael.hines@yale.edu>
2019-06-27 07:45:07 -0400

    CoreNEURON direct gui trajectory code must be wrapped with HAVE_IV

--- src/nrniv/glinerec.cpp
+++ src/nrniv/glinerec.cpp
@@ -16,14 +16,15 @@ extern "C" {
 }
 
 #include <OS/list.h>
-#include "glinerec.h"
+#include "nrnoc2iv.h"
+#include "vrecitem.h"
 #include "netcvode.h"
 #include "cvodeobj.h"
-#include "ocjump.h"
-
 
 #if HAVE_IV // to end of file
 #include "graph.h"
+#include "glinerec.h"
+#include "ocjump.h"
 
 extern "C" {
 extern NetCvode* net_cvode_instance;
@@ -250,9 +251,6 @@ void GLineRecord::plot(int vecsz, double tstop) {
     assert(0);
   }
 }
-#else
+#else // do not HAVE_IV
 void NetCvode::simgraph_remove() {}
-void GLineRecord::record_init() {}
-void GLineRecord::continuous(double) {}
-void GLineREcord::plot(int, double) {}
-#endif
+#endif // HAVE_IV
