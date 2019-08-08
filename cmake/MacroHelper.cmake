127167cb93b3a4f5e70015d6d16ebaf6d1e7c0a0
Pramod Kumbhar <pramod.kumbhar@epfl.ch>
2019-08-08 13:24:33 -0700

    Various to make NEURON buildable under windows:   - make cmake option consistent for shared library   - replace find command with CMake's find(GLOB_RECURSE..     so that it becomes windows compatiable (find.exe has different     options than *nix)   - link stnadard libc and libc++ in static mode for mingw/windows build   - use setup.cfg to tell distutils to use mingw toolchain instead of     visual studio   - setup.py is updated to translate C:/msys64 to C:\\msys64 so that     windows python understand paths correctly   - adjust python install --home argument for windows

--- cmake/MacroHelper.cmake
+++ cmake/MacroHelper.cmake
@@ -187,15 +187,10 @@ endmacro()
 # =============================================================================
 macro(nrn_find_project_files list_name)
   foreach(name ${ARGN})
-    execute_process(
-      COMMAND find ${PROJECT_SOURCE_DIR}/src -name ${name}
-      RESULTS_VARIABLE result
-      OUTPUT_VARIABLE filepath
-    )
-    if ( (NOT result EQUAL 0) OR filepath STREQUAL "")
+    file(GLOB_RECURSE filepath "${PROJECT_SOURCE_DIR}/src/*${name}")
+    if (filepath STREQUAL "")
       message(FATAL_ERROR " ${name} not found in ${PROJECT_SOURCE_DIR}/src")
     else()
-      string(REGEX REPLACE "\n$" "" filepath "${filepath}")
       list(APPEND ${list_name} ${filepath})
     endif()
   endforeach(name)
