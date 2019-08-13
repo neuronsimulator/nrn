dc26098f478a05b5657799745ae695be62b00f3c
Jorge Blanco Alonso <41900536+jorblancoa@users.noreply.github.com>
2019-08-13 16:54:04 +0200

    Fix bug when spikes vector is empty (BlueBrain/CoreNeuron#192)
    CoreNEURON Repo SHA: BlueBrain/CoreNeuron@b122751834a07c68cdb0514bee20f37ab58285eb

--- src/coreneuron/nrniv/output_spikes.cpp
+++ src/coreneuron/nrniv/output_spikes.cpp
@@ -95,8 +95,12 @@ void local_spikevec_sort(std::vector<double>& isvect,
 #if NRNMPI
 
 void sort_spikes(std::vector<double>& spikevec_time, std::vector<int>& spikevec_gid) {
-    double lmin_time = *(std::min_element(spikevec_time.begin(), spikevec_time.end()));
-    double lmax_time = *(std::max_element(spikevec_time.begin(), spikevec_time.end()));
+    double lmin_time = std::numeric_limits<double>::max();
+    double lmax_time = std::numeric_limits<double>::min();
+    if(!spikevec_time.empty()) {
+        lmin_time = *(std::min_element(spikevec_time.begin(), spikevec_time.end()));
+        lmax_time = *(std::max_element(spikevec_time.begin(), spikevec_time.end()));
+    }	
     double min_time = nrnmpi_dbl_allmin(lmin_time);
     double max_time = nrnmpi_dbl_allmax(lmax_time);
 
