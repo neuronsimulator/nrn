1068de1f4bc018f5783a7502320cfbac909e9439
Pramod Kumbhar <pramod.kumbhar@epfl.ch>
2019-09-03 09:44:54 +0200

    Handle code generation for INITIAL block under NET_RECEIVE (BlueBrain/nmodl#240)
      - variables under INITIAL block were not correctly handle
      - bug fix: if all parameters were not used correctly, variable
        from weight index was not correctly used
      - added unit tests for VarUsageVisitor
    
    Co-Authored-By: Ioannis Magkanaris <iomagkanaris@gmail.com>
    
    NMODL Repo SHA: BlueBrain/nmodl@f526c9b6ed91c5ec432b800848d4e5846d355952

--- src/nmodl/codegen/codegen_c_visitor.cpp
+++ src/nmodl/codegen/codegen_c_visitor.cpp
@@ -3372,7 +3372,7 @@ void CodegenCVisitor::print_net_receive_common_code(Block* node, bool need_mech_
     printer->add_line("int tid = pnt->_tid;");
     printer->add_line("int id = pnt->_i_instance;");
     printer->add_line("double v = 0;");
-    if (info.artificial_cell) {
+    if (info.artificial_cell || node->is_initial_block()) {
         printer->add_line("NrnThread* nt = nrn_threads + tid;");
         printer->add_line("Memb_list* ml = nt->_ml_list[pnt->_type];");
     }
@@ -3393,14 +3393,14 @@ void CodegenCVisitor::print_net_receive_common_code(Block* node, bool need_mech_
         printer->add_newline();
         for (auto& parameter: parameters) {
             auto name = parameter->get_node_name();
-            VarUsageVisitor vu;
-            auto var_used = vu.variable_used(node, "(*" + name + ")");
+            bool var_used = VarUsageVisitor().variable_used(node, "(*" + name + ")");
             if (var_used) {
-                auto statement = "double* {} = weights + weight_index + {};"_format(name, i++);
+                auto statement = "double* {} = weights + weight_index + {};"_format(name, i);
                 printer->add_line(statement);
                 RenameVisitor vr(name, "*" + name);
                 node->visit_children(&vr);
             }
+            i++;
         }
     }
 }
@@ -3481,12 +3481,52 @@ void CodegenCVisitor::print_net_event_call(FunctionCall* node) {
     printer->add_text(")");
 }
 
+/**
+ * Rename arguments to NET_RECEIVE block with corresponding pointer variable
+ *
+ * Arguments to NET_RECEIVE block are packed and passed via weight vector. These
+ * variables need to be replaced with corresponding pointer variable. For example,
+ * if mod file is like
+ *
+ * \code{.mod}
+ *      NET_RECEIVE (weight, R){
+ *          INITIAL {
+ *              R=1
+ *          }
+ *      }
+ * \endcode
+ *
+ * then generated code for initial block should be:
+ *
+ * \code{.cpp}
+ *      double* R = weights + weight_index + 0;
+ *      (*R) = 1.0;
+ * \endcode
+ *
+ * So, the `R` in AST needs to be renamed with `(*R)`.
+ */
+static void rename_net_receive_arguments(ast::NetReceiveBlock* net_receive_node, ast::Node* node) {
+    auto parameters = net_receive_node->get_parameters();
+    for (auto& parameter: parameters) {
+        auto name = parameter->get_node_name();
+        auto var_used = VarUsageVisitor().variable_used(node, name);
+        if (var_used) {
+            RenameVisitor vr(name, "(*" + name + ")");
+            node->get_statement_block()->visit_children(&vr);
+        }
+    }
+}
+
 
 void CodegenCVisitor::print_net_init() {
     auto node = info.net_receive_initial_node;
     if (node == nullptr) {
         return;
     }
+
+    // rename net_receive arguments used in the initial block of net_receive
+    rename_net_receive_arguments(info.net_receive_node, node);
+
     codegen = true;
     auto args = "Point_process* pnt, int weight_index, double flag";
     printer->add_newline(2);
@@ -3633,16 +3673,8 @@ void CodegenCVisitor::print_net_receive_kernel() {
     printing_net_receive = true;
     auto node = info.net_receive_node;
 
-    // rename arguments if same name is used
-    auto parameters = node->get_parameters();
-    for (auto& parameter: parameters) {
-        auto name = parameter->get_node_name();
-        auto var_used = VarUsageVisitor().variable_used(node, name);
-        if (var_used) {
-            RenameVisitor vr(name, "(*" + name + ")");
-            node->get_statement_block()->visit_children(&vr);
-        }
-    }
+    // rename net_receive arguments used in the block itself
+    rename_net_receive_arguments(info.net_receive_node, node);
 
     std::string name;
     auto params = ParamVector();
---
+++ test/nmodl/transpiler/visitor/var_usage.cpp
@@ -0,0 +1,95 @@
+/*************************************************************************
+ * Copyright (C) 2018-2019 Blue Brain Project
+ *
+ * This file is part of NMODL distributed under the terms of the GNU
+ * Lesser General Public License. See top-level LICENSE file for details.
+ *************************************************************************/
+
+#include "catch/catch.hpp"
+
+#include "parser/nmodl_driver.hpp"
+#include "test/utils/test_utils.hpp"
+#include "visitors/var_usage_visitor.hpp"
+
+using namespace nmodl;
+using namespace visitor;
+
+//=============================================================================
+// Variable usage visitor tests
+//=============================================================================
+
+bool run_var_usage_visitor(std::shared_ptr<ast::Node> node, const std::string& variable) {
+    return VarUsageVisitor().variable_used(node.get(), variable);
+}
+
+SCENARIO("Searching for variable name using VarUsageVisitor", "[visitor][var_usage]") {
+    auto to_ast = [](const std::string& text) {
+        parser::NmodlDriver driver;
+        return driver.parse_string(text);
+    };
+
+    GIVEN("A simple NMODL block") {
+        std::string nmodl_text = R"(
+            DERIVATIVE states {
+                tau = 11.1
+                exp(tau)
+            {
+                h' = h + 2 + n
+            }
+            }
+        )";
+
+        auto ast = to_ast(nmodl_text);
+        auto node = ast->blocks[0];
+
+        WHEN("Looking for existing variable") {
+            THEN("Can find variables") {
+                REQUIRE(run_var_usage_visitor(node, "tau"));
+                REQUIRE(run_var_usage_visitor(node, "h"));
+                REQUIRE(run_var_usage_visitor(node, "n"));
+            }
+        }
+
+        WHEN("Looking for missing variable") {
+            THEN("Can not find variable") {
+                REQUIRE_FALSE(run_var_usage_visitor(node, "tauu"));
+                REQUIRE_FALSE(run_var_usage_visitor(node, "my_var"));
+            }
+        }
+    }
+
+    GIVEN("A nested NMODL block") {
+        std::string nmodl_text = R"(
+            NET_RECEIVE (weight,weight_AMPA, weight_NMDA, R){
+                LOCAL result
+                weight_AMPA = weight
+                weight_NMDA = weight * NMDA_ratio
+                INITIAL {
+                    R = 1
+                    u = u0
+                    {
+                        tsyn = t
+                    }
+                }
+            }
+        )";
+
+        auto ast = to_ast(nmodl_text);
+        auto node = ast->blocks[0];
+
+        WHEN("Looking for existing variable in outer block") {
+            THEN("Can find variables") {
+                REQUIRE(run_var_usage_visitor(node, "weight"));
+                REQUIRE(run_var_usage_visitor(node, "NMDA_ratio"));
+            }
+        }
+
+        WHEN("Looking for existing variable in inner block") {
+            THEN("Can find variables") {
+                REQUIRE(run_var_usage_visitor(node, "R"));
+                REQUIRE(run_var_usage_visitor(node, "u0"));
+                REQUIRE(run_var_usage_visitor(node, "tsyn"));
+            }
+        }
+    }
+}
