18b2f2a3cfba6ef5281ad68fbd2279f229692cac
Robert A McDougal <robert.mcdougal@yale.edu>
2019-10-12 23:18:17 -0400

    ipynb version of the parallel ball-and-stick

--- ballandstick.py
+++ ballandstick.py
@@ -1,93 +1,82 @@
-# -*- coding: utf-8 -*-
-r"""
-Ball and stick model. This is a two-section cell: A soma with active channels 
-and a dendrite with passive properties.
+from neuron import h, gui
+from neuron.units import ms, mV
+h.load_file('stdrun.hoc')
 
-AUTHORS:
-- THOMAS MCTAVISH (2010-11-04): initial version. Modified version of the
-        project by Hines and Carnevale. (Hines M.L. and Carnevale N.T, 
-        Translating network models to parallel hardware in NEURON,
-        Journal of Neuroscience Methods 169 (2008) 425-455).
+class Cell:
+    def __init__(self, gid, x, y, z, theta):
+        self._gid = gid
+        self._setup_morphology()
+        self.all = self.soma.wholetree()
+        self._setup_biophysics()
+        self.x = self.y = self.z = 0
+        h.define_shape()
+        self._rotate_z(theta)
+        self._set_position(x, y, z)
+        
+        # everything below here in this method is NEW
+        self._spike_detector = h.NetCon(self.soma(0.5)._ref_v, None, sec=self.soma)
+        self.spike_times = h.Vector()
+        self._spike_detector.record(self.spike_times)
+        
+        self._ncs = []
+        
+        self.soma_v = h.Vector().record(self.soma(0.5)._ref_v)
 
-modified to use h instead of nrn -- 2015-04-21
-
-modified to use sec= instead of pushing and popping sections by Robert A McDougal 2018-04-20
-"""
-from cell import *
-from neuron import h
-
-class BallAndStick(Cell):  #### Inherits from Cell
-    """Two-section cell: A soma with active channels and
-    a dendrite with passive properties."""
+        
+    def __repr__(self):
+        return '{}[{}]'.format(self.name, self._gid)
     
-    #### __init__ is gone and handled in Cell.
-    #### We can override __init__ completely, or do some of
-    #### our own initialization first, and then let Cell do its
-    #### thing, and then do a bit more ourselves with "super".
-    ####
-    #### def __init__(self):
-    ####     # Do some stuff
-    ####     super(Cell, self).__init__()
-    ####     # Do some more stuff
-             
-    def create_sections(self):
-        """Create the sections of the cell."""
+    def _set_position(self, x, y, z):
+        for sec in self.all:
+            for i in range(sec.n3d()):
+                sec.pt3dchange(i,
+                               x - self.x + sec.x3d(i),
+                               y - self.y + sec.y3d(i),
+                               z - self.z + sec.z3d(i),
+                              sec.diam3d(i))
+        self.x, self.y, self.z = x, y, z
+        
+    def _rotate_z(self, theta):
+        """Rotate the cell about the Z axis."""
+        for sec in self.all:
+            for i in range(sec.n3d()):
+                x = sec.x3d(i)
+                y = sec.y3d(i)
+                c = h.cos(theta)
+                s = h.sin(theta)
+                xprime = x * c - y * s
+                yprime = x * s + y * c
+                sec.pt3dchange(i, xprime, yprime, sec.z3d(i), sec.diam3d(i))
+
+
+class BallAndStick(Cell):
+    name = 'BallAndStick'
+    
+    def _setup_morphology(self):
         self.soma = h.Section(name='soma', cell=self)
         self.dend = h.Section(name='dend', cell=self)
-        
-    def build_topology(self):
-        """Connect the sections of the cell to build a tree."""
-        self.dend.connect(self.soma(1))
-        
-    def define_geometry(self):
-        """Set the 3D geometry of the cell."""
-        self.soma.L = self.soma.diam = 12.6157 # microns
-        self.dend.L = 200                      # microns
-        self.dend.diam = 1                     # microns
-        self.dend.nseg = 5
-        self.shape_3D()
+        self.dend.connect(self.soma)
+        self.soma.L = self.soma.diam = 12.6157
+        self.dend.L = 200
+        self.dend.diam = 1
 
-    def define_biophysics(self):
-        """Assign the membrane properties across the cell."""
-        for sec in self.all: # 'all' exists in parent object.
-            sec.Ra = 100     # Axial resistance in Ohm * cm
-            sec.cm = 1       # Membrane capacitance in micro Farads / cm^2
-        
-            # Insert active Hodgkin-Huxley current in the soma
-            self.soma.insert('hh')
-            for seg in self.soma:
-                seg.hh.gnabar = 0.12  # Sodium conductance in S/cm2
-                seg.hh.gkbar = 0.036  # Potassium conductance in S/cm2
-                seg.hh.gl = 0.0003    # Leak conductance in S/cm2
-                seg.hh.el = -54.3     # Reversal potential in mV
-                
-            # Insert passive current in the dendrite
-            self.dend.insert('pas')
-            for seg in self.dend:
-                seg.pas.g = 0.001  # Passive conductance in S/cm2
-                seg.pas.e = -65    # Leak reversal potential mV 
-    
-    def shape_3D(self):
-        """
-        Set the default shape of the cell in 3D coordinates.
-        Set soma(0) to the origin (0,0,0) and dend extending along
-        the X-axis.
-        """
-        len1 = self.soma.L
-        h.pt3dclear(sec=self.soma)
-        h.pt3dadd(0, 0, 0, self.soma.diam, sec=self.soma)
-        h.pt3dadd(len1, 0, 0, self.soma.diam, sec=self.soma)
-        
-        len2 = self.dend.L
-        h.pt3dclear(sec=self.dend)
-        h.pt3dadd(len1, 0, 0, self.dend.diam, sec=self.dend)
-        h.pt3dadd(len1 + len2, 0, 0, self.dend.diam, sec=self.dend)
-        
-    #### build_subsets, rotateZ, and set_location are now in cell object. ####
-    
-    #### NEW STUFF ####
-    
-    def create_synapses(self):
-        syn = h.ExpSyn(self.dend(0.5))
-        syn.tau = 2
-        self.synlist.append(syn) # synlist is defined in Cell
+    def _setup_biophysics(self):
+        for sec in self.all:
+            sec.Ra = 100    # Axial resistance in Ohm * cm
+            sec.cm = 1      # Membrane capacitance in micro Farads / cm^2
+        self.soma.insert('hh')                                          
+        for seg in self.soma:
+            seg.hh.gnabar = 0.12  # Sodium conductance in S/cm2
+            seg.hh.gkbar = 0.036  # Potassium conductance in S/cm2
+            seg.hh.gl = 0.0003    # Leak conductance in S/cm2
+            seg.hh.el = -54.3     # Reversal potential in mV
+        # Insert passive current in the dendrite
+        self.dend.insert('pas')                 
+        for seg in self.dend:
+            seg.pas.g = 0.001  # Passive conductance in S/cm2
+            seg.pas.e = -65    # Leak reversal potential mV
+
+        # NEW: the synapse
+        self.syn = h.ExpSyn(self.dend(0.5))
+        self.syn.tau = 2 * ms
+
--- ring.py
+++ ring.py
@@ -1,162 +1,62 @@
 from neuron import h
-import ballandstick
-import numpy
-from itertools import izip
-from neuronpy.util import spiketrain
+from ballandstick import BallAndStick
+
+### MPI must be initialized before we create a ParallelContext object
+h.nrnmpi_init()
+pc = h.ParallelContext()
 
 class Ring:
     """A network of *N* ball-and-stick cells where cell n makes an
     excitatory synapse onto cell n + 1 and the last, Nth cell in the
     network projects to the first cell.
     """
-    def __init__(self, N=5, stim_w=0.004, stim_spike_num=1, syn_w=0.01,
-            syn_delay=5):
+    def __init__(self, N=5, stim_w=0.04, stim_t=9, stim_delay=1, syn_w=0.01, syn_delay=5, r=50):
         """
         :param N: Number of cells.
         :param stim_w: Weight of the stimulus
-        :param stim_spike_num: Number of spikes generated in the stimulus
+        :param stim_t: time of the stimulus (in ms)
+        :param stim_delay: delay of the stimulus (in ms)
         :param syn_w: Synaptic weight
         :param syn_delay: Delay of the synapse
-        """
-        self._N = N              # Total number of cells in the net
-        self.cells = []          # Cells on this host
-        self.nclist = []         # NetCon list on this host
-        self.gidlist = []        # List of global identifiers on this host
-        self.stim = None         # Stimulator
-        self.stim_w = stim_w     # Weight of stim
-        self.stim_spike_num = stim_spike_num  # Number of stim spikes
-        self.syn_w = syn_w       # Synaptic weight
-        self.syn_delay = syn_delay  # Synaptic delay
-        self.t_vec = h.Vector()   # Spike time of all cells on this host
-        self.id_vec = h.Vector()  # Ids of spike times on this host
-
-        #### Make a new ParallelContext object
-        self.pc = h.ParallelContext()
-
-        self.set_numcells(N)  # Actually build the net -- at least the portion
-                              # of cells on this host.
-
-    def set_numcells(self, N, radius=50):
-        """Create, layout, and connect N cells."""
+        :param r: radius of the network
+        """ 
         self._N = N
-        self.set_gids() #### Used when creating and connecting cells
-        self.create_cells()
-        self.connect_cells()
-        self.connect_stim()
-
+        self.set_gids()                   ### assign gids to processors
+        self._syn_w = syn_w
+        self._syn_delay = syn_delay
+        self._create_cells(r)             ### changed to use self._N instead of passing in N
+        self._connect_cells()
+        ### the 0th cell only exists on one process... that's the only one that gets a netstim
+        if pc.gid_exists(0):
+            self._netstim = h.NetStim()
+            self._netstim.number = 1
+            self._netstim.start = stim_t
+            self._nc = h.NetCon(self._netstim, pc.gid2cell(0).syn)   ### grab cell with gid==0 wherever it exists
+            self._nc.delay = stim_delay
+            self._nc.weight[0] = stim_w
+    
     def set_gids(self):
         """Set the gidlist on this host."""
-        self.gidlist = []
         #### Round-robin counting.
-        #### Each host as an id from 0 to pc.nhost() - 1.
-        for i in range(int(self.pc.id()), self._N, int(self.pc.nhost())):
-            self.gidlist.append(i)
-
-    def create_cells(self):
-        """Create cell objects on this host and set their location."""
+        #### Each host has an id from 0 to pc.nhost() - 1.
+        self.gidlist = list(range(pc.id(), self._N, pc.nhost()))
+        for gid in self.gidlist:
+            pc.set_gid2node(gid, pc.id())
+    
+    def _create_cells(self, r):
         self.cells = []
-        N = self._N
-        r = 50 # Radius of cell locations from origin (0,0,0) in microns
+        for i in self.gidlist:    ### only create the cells that exist on this host
+            theta = i * 2 * h.PI / self._N
+            self.cells.append(BallAndStick(i, h.cos(theta) * r, h.sin(theta) * r, 0, theta))
+        ### associate the cell with this host and gid
+        for cell in self.cells:
+            pc.cell(cell._gid, cell._spike_detector)
 
-        for i in self.gidlist:
-            cell = ballandstick.BallAndStick()
-            # When cells are created, the soma location is at (0,0,0) and
-            # the dendrite extends along the X-axis.
-            # First, at the origin, rotate about Z.
-            cell.rotateZ(i*2*numpy.pi/N)
-
-            # Then reposition
-            x_loc = float(numpy.sin(i*2*numpy.pi/N))*r
-            y_loc = float(numpy.cos(i*2*numpy.pi/N))*r
-            cell.set_position(x_loc, y_loc, 0)
-
-            self.cells.append(cell)
-
-            #### Tell this host it has this gid
-            #### gids can be any integer, they just need to be unique.
-            #### In this simple case, we set the gid to i.
-            self.pc.set_gid2node(i, int(self.pc.id()))
-
-            #### Means to tell the ParallelContext that this cell is
-            #### a source for all other hosts. NetCon is temporary.
-            nc = cell.connect2target(None)
-            self.pc.cell(i, nc) # Associate the cell with this host and gid
-
-            #### Record spikes of this cell
-            self.pc.spike_record(i, self.t_vec, self.id_vec)
-
-# OLD WAY
-#    def connect_cells(self):
-#        self.nclist = []
-#        N = self._N
-#        for i in range(N):
-#            src = self.cells[i]
-#            tgt_syn = self.cells[(i+1)%N].synlist[0]
-#            nc = src.connect2target(tgt_syn)
-#            nc.weight[0] = self.syn_w
-#            nc.delay = self.syn_delay
-#            nc.record(self.t_vec, self.id_vec, i)
-#            self.nclist.append(nc)
-
-    def connect_cells(self):
-        """Connect cell n to cell n + 1."""
-        self.nclist = []
-        N = self._N
-        for i in self.gidlist:
-            src_gid = (i-1+N) % N
-            tgt_gid = i
-            if self.pc.gid_exists(tgt_gid):
-                target = self.pc.gid2cell(tgt_gid)
-                syn = target.synlist[0]
-                nc = self.pc.gid_connect(src_gid, syn)
-                nc.weight[0] = self.syn_w
-                nc.delay = self.syn_delay
-                self.nclist.append(nc)
-
-    def connect_stim(self):
-        """Connect a spike generator on the first cell in the network."""
-        #### If the first cell is not on this host, return
-        if not self.pc.gid_exists(0):
-            return
-        self.stim = h.NetStim()
-        self.stim.number = self.stim_spike_num
-        self.stim.start = 9
-        self.ncstim = h.NetCon(self.stim, self.cells[0].synlist[0])
-        self.ncstim.delay = 1
-        self.ncstim.weight[0] = self.stim_w # NetCon weight is a vector.
-
-    def get_spikes(self):
-        """Get the spikes as a list of lists."""
-        return spiketrain.netconvecs_to_listoflists(self.t_vec, self.id_vec)
-
-    def write_spikes(self, file_name='out.spk'):
-        """Append the spike output file with spikes on this host. The output
-        format is the timestamp followed by a tab then the gid of the source
-        followed by a newline.
-
-        :param file_name: is the full or relative path to a spike output file.
-
-        .. note::
-
-            When parallelized, each process will write to the same file so it
-            is opened in append mode. The order in which the processes write is
-            arbitrary so while the spikes within the process may be ordered by
-            time, the output file will be unsorted. A quick way to sort a file
-            is with the bash command sort, which can be called after all
-            processes have written the file with the following format::
-
-                exec_cmd = 'sort -k 1n,1n -k 2n,2n ' + file_name + \
-                        ' > ' + 'sorted_' + file_name
-                os.system(exec_cmd)
-        """
-        for i in range(int(self.pc.nhost())):
-            self.pc.barrier() # Sync all processes at this point
-            if i == int(self.pc.id()):
-                if i == 0:
-                    mode = 'w' # write
-                else:
-                    mode = 'a' # append
-                with open(file_name, mode) as spk_file: # Append
-                    for (t, id) in izip(self.t_vec, self.id_vec):
-                        spk_file.write('%.3f\t%d\n' %(t, id)) # timestamp, id
-        self.pc.barrier()
+    def _connect_cells(self):
+        ### this method is different because we now must use ids instead of objects
+        for target in self.cells:
+            source_gid = (target._gid - 1 + self._N) % self._N
+            nc = pc.gid_connect(source_gid, target.syn)
+            nc.weight[0] = self._syn_w
+            nc.delay = self._syn_delay
+            target._ncs.append(nc)        
