cbef7ed81d231e6e424e5c9d419f6e53a5a02119
Ioannis Magkanaris <iomagkanaris@gmail.com>
2019-11-11 18:26:34 +0100

    Fixed fast_imem test

--- test/unit_test.cpp
+++ test/unit_test.cpp
@@ -9,11 +9,8 @@ extern "C" {
 #include "../src/nrnoc/neuron.h"
 #include "../src/nrnoc/multicore.h"
 
-#define CACHELINE_ALLOC(name, type, size) name = (type*)nrn_cacheline_alloc((void**)&name, size*sizeof(type))
-
-void nrn_threads_create(int, int);
+extern void nrn_threads_create(int, int);
 extern void nrn_fast_imem_alloc();
-extern void nrn_matrix_node_alloc(void);
 extern void nrn_calc_fast_imem(NrnThread *);
 }
 
@@ -27,24 +24,18 @@ void modl_reg() {
 
 }
 
-TEST_CASE("Test hoc interpreter", "[Neuron][hoc_interpeter]") {
+TEST_CASE("Test hoc interpreter", "[Neuron][hoc_interpreter]") {
     hoc_init_space();
     hoc_pushx(4.0);
     hoc_pushx(5.0);
     hoc_add();
-    REQUIRE( hoc_xpop() == 9.0);
+    REQUIRE( hoc_xpop() == 9.0 );
 }
 
 TEST_CASE("Test fast_imem calculation", "[Neuron][fast_imem]") {
-    INFO("Test starting");
-    //CACHELINE_ALLOC(nrn_threads, NrnThread, nthreads);
-    INFO("nrn_nthread = " << nrn_nthread);
     nrn_threads_create(1, 0);
-    INFO("nrn_nthread = " << nrn_nthread);
-    INFO("nrn_threads = " << nrn_threads);
-    INFO("nrn_threads + nrn_nthread = " << nrn_threads + nrn_nthread);
-    for(NrnThread* nt = nrn_threads; nt < nrn_threads + nrn_nthread; ++nt) {
-        INFO("nt = " << nt);
+    for(int it = 0; it < nrn_nthread; ++it) {
+        NrnThread* nt = &nrn_threads[it];
         nt->end = 1;
         nt->_actual_rhs = (double*)ecalloc(nt->end, sizeof(double));
         nt->_actual_d = (double*)ecalloc(nt->end, sizeof(double));
@@ -55,17 +46,14 @@ TEST_CASE("Test fast_imem calculation", "[Neuron][fast_imem]") {
             nt->_actual_area[i] = 0.1;
         }
     }
-    INFO("NrnThread setup done");
     nrn_fast_imem_alloc();
-    INFO("nrn_fast_imem_alloc setup done");
-    for(NrnThread* nt = nrn_threads; nt < nrn_threads + nrn_nthread; ++nt) {
-        INFO("nt = " << nt);
-        nrn_calc_fast_imem(nt);
+    for(int it = 0; it < nrn_nthread; ++it) {
+        NrnThread* nt = &nrn_threads[it];
         for(int i = 0; i < nt->end; i++) {
-            INFO("i = " << i);
-            INFO("nt->_nrn_fast_imem->_nrn_sav_d[i] = " << nt->_nrn_fast_imem->_nrn_sav_d[i]);
-            CHECK(nt->_nrn_fast_imem->_nrn_sav_d[i] != 0.1);
+            nt->_nrn_fast_imem->_nrn_sav_rhs[i] = 0.1;
+            nt->_nrn_fast_imem->_nrn_sav_d[i] = 0.1;
         }
+        nrn_calc_fast_imem(nt);
     }
-    REQUIRE( 0 == 1 );
+    REQUIRE( nrn_threads[0]._nrn_fast_imem->_nrn_sav_rhs[0] == Approx( 0.00011 ) );
 }
