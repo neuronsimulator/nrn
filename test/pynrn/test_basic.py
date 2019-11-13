4f17cda63c1e71a2f0dedbf9b2ffb10ebfb0ec2f
Awile Omar <omar.awile@epfl.ch>
2019-11-13 17:23:20 -0500

    Fixed CMakeLists.txt to set the proper environment
    (and fixed a forgotten diff in the python tests)

--- test/pynrn/test_basic.py
+++ test/pynrn/test_basic.py
@@ -1,11 +1,6 @@
 import pytest
-<<<<<<< Updated upstream
-from neuron import h, gui
-import sys
-=======
 import neuron
-from neuron import h, gui
->>>>>>> Stashed changes
+from neuron import h
 
 def func(x):
     return x + 1
@@ -17,6 +12,7 @@ def test_answer():
 
 def test_neuron():
     h('''create soma''')
+    h.load_file("stdrun.hoc")
     h.soma.L=5.6419
     h.soma.diam=5.6419
     h.soma.insert("hh")
@@ -25,23 +21,9 @@ def test_neuron():
     ic.dur = 0.1
     ic.amp = 0.3
 
-<<<<<<< Updated upstream
-    h.cvode.use_fast_imem(1)
-    h.cvode.cache_efficient(1)
-
-    v = h.Vector()
-    v.record(h.soma(.5)._ref_v, sec = h.soma)
-    i_mem = h.Vector()
-    i_mem.record(h.soma(.5)._ref_i_membrane_, sec = h.soma)
-    tv = h.Vector()
-    tv.record(h._ref_t, sec=h.soma)
-    h.run()
-    print(v[1])
-=======
     v = h.Vector()
     v.record(h.soma(.5)._ref_v, sec = h.soma)
     tv = h.Vector()
     tv.record(h._ref_t, sec=h.soma)
     h.run()
     assert(v[0] == -65.0)
->>>>>>> Stashed changes
