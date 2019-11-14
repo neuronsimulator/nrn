5229c1378eba5e121aa65b065fda469b2e8a2820
Fernando Pereira <fernando.pereira@epfl.ch>
2019-11-14 11:29:39 -0500

    Use Relative rpaths

--- cmake/RpathHelper.cmake
+++ cmake/RpathHelper.cmake
@@ -8,14 +8,22 @@ set(CMAKE_SKIP_BUILD_RPATH FALSE)
 # when building, don't use the install RPATH already (but later on when installing)
 set(CMAKE_BUILD_WITH_INSTALL_RPATH FALSE)
 
-set(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}/lib")
+if (${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
+    set(LOADER_PATH_FLAG "\\@loader_path")
+elseif(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
+    set(LOADER_PATH_FLAG "\\\$ORIGIN")
+else()
+    set(LOADER_PATH_FLAG "")
+endif()
+
+set(CMAKE_INSTALL_RPATH "${LOADER_PATH_FLAG}/../lib")
 
 # add the automatically determined parts of the RPATH which point to directories outside the build
 # tree to the install RPATH
 set(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
 
 # the RPATH to be used when installing, but only if it's not a system directory
-list(FIND CMAKE_PLATFORM_IMPLICIT_LINK_DIRECTORIES "${CMAKE_INSTALL_PREFIX}/lib" isSystemDir)
-if("${isSystemDir}" STREQUAL "-1")
-  set(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}/lib")
-endif()
+#list(FIND CMAKE_PLATFORM_IMPLICIT_LINK_DIRECTORIES "${CMAKE_INSTALL_PREFIX}/lib" isSystemDir)
+#if("${isSystemDir}" STREQUAL "-1")
+#  set(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}/lib")
+#endif()
