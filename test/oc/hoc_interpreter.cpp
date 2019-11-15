5390de42e485f16e840455cc1850ee15e2b66c6d
Ioannis Magkanaris <iomagkanaris@gmail.com>
2019-11-15 16:16:00 -0500

    Added seperate catch test

---
+++ test/oc/hoc_interpreter.cpp
@@ -0,0 +1,15 @@
+#include <catch2/catch.hpp>
+
+extern "C" {
+#include <hocdec.h>
+#include <ocfunc.h>
+#include <code.h>
+}
+
+TEST_CASE("Test hoc interpreter", "[Neuron][hoc_interpreter]") {
+    hoc_init_space();
+    hoc_pushx(4.0);
+    hoc_pushx(5.0);
+    hoc_add();
+    REQUIRE( hoc_xpop() == 9.0 );
+}
