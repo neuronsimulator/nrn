bb4f6949242b8e79180acdf03f76a4c54789999f
Pramod Kumbhar <pramod.kumbhar@epfl.ch>
2019-11-30 01:17:07 +0100

    set master doc

--- docs/conf.py
+++ docs/conf.py
@@ -49,6 +49,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'python/venv']
 #
 html_theme = 'sphinx_rtd_theme'
 
+# Sphinx expects the master doc to be contents
+master_doc = 'index'
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
