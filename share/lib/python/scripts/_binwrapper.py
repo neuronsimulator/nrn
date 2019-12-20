f48416fb15594e2f2f3bbff1833124ef9ba82329
Fernando Pereira <fernando.pereira@epfl.ch>
2019-12-20 17:07:11 +0000

    Wrapper to avoid pkg_resources and set NEURONHOME
    Binary wrapper must not use pkg_resource which forces an import
    and ends up initializing neuron - then too late to set NEURONHOME
    Therefore we compute the path ourselves from site, which is much faster

--- share/lib/python/scripts/_binwrapper.py
+++ share/lib/python/scripts/_binwrapper.py
@@ -4,15 +4,17 @@ A generic wrapper to access nrn binaries from a python installation
 Plese create a softlink with the binary name to be called.
 """
 import os
+import site
 import subprocess as sp
-import pkg_resources
 import sys
 
 
 def _launch_command(exe_name):
-    exe_path = pkg_resources.resource_filename('neuron', '.data/bin/' + exe_name)
-    sp.call([exe_path] + sys.argv[1:])
+    NRN_PREFIX = os.path.join(site.getsitepackages()[0], 'neuron', '.data')
+    os.environ["NEURONHOME"] = os.path.join(NRN_PREFIX, 'share/nrn')
+    exe_path = os.path.join(NRN_PREFIX, 'bin', exe_name)
+    return sp.call([exe_path] + sys.argv[1:])
 
 
 if __name__ == '__main__':
-    _launch_command(os.path.basename(sys.argv[0]))
+    sys.exit(_launch_command(os.path.basename(sys.argv[0])))
