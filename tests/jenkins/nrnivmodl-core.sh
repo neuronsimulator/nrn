b86a8c45c93704fde0d29ccf2c6adda334b748ce
Fernando Pereira <ferdonline@gmail.com>
2020-01-24 16:57:27 +0100

    Small fixes in nrnivmodl regarding MODS_LOCATION (BlueBrain/CoreNeuron#235)
    Small fixes in nrnivmodl regarding MODS_LOCATION
     - MODS_LOCATION was always being overwriten from CLI. Now it can be
    loaded from the save file.
    
    Co-authored-by: Pramod Kumbhar <pramod.s.kumbhar@gmail.com>
    
    CoreNEURON Repo SHA: BlueBrain/CoreNeuron@439cfab870ce01638109cfc1f1a2f5e92f4b0ed5

--- tests/jenkins/nrnivmodl-core.sh
+++ tests/jenkins/nrnivmodl-core.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-set -e
+set -xe
 
 . /gpfs/bbp.cscs.ch/apps/hpc/jenkins/config/modules.sh
 module load intel hpe-mpi
@@ -13,11 +13,15 @@ mkdir -p ${CORENRN_TYPE}
 
 pushd ${CORENRN_TYPE}
 
+set +x
 if [ "${TEST_DIR}" = "ringtest" ]; then
-    $WORKSPACE/install_${CORENRN_TYPE}/bin/nrnivmodl-core ../mod-ext
+    echo "Running install_${CORENRN_TYPE}/bin/nrnivmodl-core ."
+    $WORKSPACE/install_${CORENRN_TYPE}/bin/nrnivmodl-core .
 else
+    echo "Running install_${CORENRN_TYPE}/bin/nrnivmodl-core ../mod"
     $WORKSPACE/install_${CORENRN_TYPE}/bin/nrnivmodl-core ../mod
 fi
+set -x
 
 # rpath $ORIGIN should make it relocatable
 find * -mindepth 1 -maxdepth 1 -type f -exec mv "{}" ./ \;
