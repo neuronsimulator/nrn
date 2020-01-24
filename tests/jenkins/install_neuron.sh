6757088251267cc63a130f8d01e572e45e72381b
Ioannis Magkanaris <iomagkanaris@gmail.com>
2020-01-24 14:37:27 +0100

    Add branch selection for Neuron and Spack  (BlueBrain/CoreNeuron#237)
    - Added parameters for SPACK_BRANCH, NEURON_BRANCH and CORENEURON_BRANCH
    - Added logic to patch neuron spack recipe with the branch we want to test
    - Install +debug variant of NEURON
    - Export to the user the variable CORENEURON_BRANCH instead of sha1
    - Also added this variable in the branches to build (needs testing)
    
    Co-authored-by: Pramod Kumbhar <pramod.s.kumbhar@gmail.com>
    
    CoreNEURON Repo SHA: BlueBrain/CoreNeuron@d8976adad79237b5ef856b3728bcb815ae4a51c3

--- tests/jenkins/install_neuron.sh
+++ tests/jenkins/install_neuron.sh
@@ -1,15 +1,36 @@
 #!/usr/bin/bash
 
+sed_apply() (
+    f=$1
+    sedexp=$2
+    echo "PATCHING $f with '$sedexp'"
+    (cd $(dirname $f) && git checkout "$(basename $f)") && sed -i "$sedexp" "$f"
+    grep 'version(' "$f"
+)
+
+patch_neuron() (
+    if [ "$NEURON_BRANCH" ]; then
+        pkg_file="${SPACK_ROOT}/var/spack/repos/builtin/packages/neuron/package.py"
+        sedexp="/version.*tag=/d"  # Drop tags
+        sedexp="$sedexp; /version.*preferred=/d"  # Drop preferred version
+        sedexp="$sedexp; s#branch=[^)]*)#branch='$NEURON_BRANCH', preferred=True)#g"  # replace branch
+        sed_apply "$pkg_file" "$sedexp"
+    fi
+)
+
 set -x
 set -e
 
+export SPACK_ROOT="$WORKSPACE/BUILD_HOME/spack/"
 export SPACK_INSTALL_PREFIX="${SPACK_INSTALL_PREFIX:-${WORKSPACE}/INSTALL_HOME}"
-source $WORKSPACE/BUILD_HOME/spack/share/spack/setup-env.sh
+source ${SPACK_ROOT}/share/spack/setup-env.sh
 source /gpfs/bbp.cscs.ch/apps/hpc/jenkins/config/modules.sh
 export PATH=$WORKSPACE/BUILD_HOME/spack/bin:/usr/bin:$PATH
 export MODULEPATH=$SPACK_INSTALL_PREFIX/modules/tcl/$(spack arch):$MODULEPATH
 
 unset $(env|awk -F= '/^(PMI|SLURM)_/ {if ($1 != "SLURM_ACCOUNT") print $1}')
 
-spack install neuron@develop
+patch_neuron
+
+spack install neuron+debug@develop
 module av neuron
