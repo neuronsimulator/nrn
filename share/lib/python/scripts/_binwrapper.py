21af5339ef90b8af778ae0218a70e5f4dd38aef4
Fernando Pereira <fernando.pereira@epfl.ch>
2020-03-27 11:51:39 +0100

    bin wrapper sets default compiler

--- share/lib/python/scripts/_binwrapper.py
+++ share/lib/python/scripts/_binwrapper.py
@@ -7,6 +7,13 @@ import os
 import site
 import subprocess as sp
 import sys
+import sysconfig
+
+
+def _set_default_compiler():
+    """Set (dont overwrite) CC/CXX so that apps dont use the build-time ones"""
+    os.environ.setdefault("CC", sysconfig.get_config_var("CC"))
+    os.environ.setdefault("CXX", sysconfig.get_config_var("CXX"))
 
 
 def _launch_command(exe_name):
@@ -14,6 +21,8 @@ def _launch_command(exe_name):
     os.environ["NEURONHOME"] = os.path.join(NRN_PREFIX, 'share/nrn')
     os.environ["NRNHOME"] = NRN_PREFIX
     exe_path = os.path.join(NRN_PREFIX, 'bin', exe_name)
+    _set_default_compiler()
+
     return sp.call([exe_path] + sys.argv[1:])
 
 
