d632ab4fdb9f413d339b01188fba024f3c31362b
Robert A. McDougal <robert.mcdougal@yale.edu>
2020-03-29 21:13:20 -0400

    separated the gui-redirect scripts into their own file so they are available when no interviews

--- src/ivoc/ivoc.h
+++ src/ivoc/ivoc.h
@@ -7,72 +7,9 @@
 #include <OS/string.h>
 #include <stdio.h>
 #include <ivstream.h>
-
+#include "gui-redirect.h"
 extern int nrn_err_dialog_active_;
 
-#define TRY_GUI_REDIRECT_OBJ(name, obj) {\
-    Object** ngh_result;\
-    if (nrnpy_gui_helper_) {\
-        ngh_result = nrnpy_gui_helper_(name, (Object*) obj);\
-        if (ngh_result) {\
-            return (void*) *ngh_result;\
-        }\
-    }\
-}
-
-#define TRY_GUI_REDIRECT_NO_RETURN(name, obj) {\
-    Object** ngh_result;\
-    if (nrnpy_gui_helper_) {\
-        ngh_result = nrnpy_gui_helper_(name, (Object*) obj);\
-        if (ngh_result) {\
-            return;\
-        }\
-    }\
-}
-
-#define TRY_GUI_REDIRECT_DOUBLE(name, obj) {\
-    Object** ngh_result;\
-    if (nrnpy_gui_helper_) {\
-        ngh_result = nrnpy_gui_helper_(name, (Object*) obj);\
-        if (ngh_result) {\
-			hoc_ret();\
-			hoc_pushx(nrnpy_object_to_double_(*ngh_result));\
-            return;\
-        }\
-    }\
-}
-
-#define TRY_GUI_REDIRECT_ACTUAL_DOUBLE(name, obj) {\
-    Object** ngh_result;\
-    if (nrnpy_gui_helper_) {\
-        ngh_result = nrnpy_gui_helper_(name, (Object*) obj);\
-        if (ngh_result) {\
-			return(nrnpy_object_to_double_(*ngh_result));\
-        }\
-    }\
-}
-
-#define TRY_GUI_REDIRECT_ACTUAL_OBJ(name, obj) {\
-    Object** ngh_result;\
-    if (nrnpy_gui_helper_) {\
-        ngh_result = nrnpy_gui_helper_(name, (Object*) obj);\
-        if (ngh_result) {\
-            return ngh_result;\
-        }\
-    }\
-}
-
-#define TRY_GUI_REDIRECT_DOUBLE_SEND_STRREF(name, obj) {\
-    Object** ngh_result;\
-    if (nrnpy_gui_helper_) {\
-        ngh_result = nrnpy_gui_helper3_(name, (Object*) obj, 1);\
-        if (ngh_result) {\
-			hoc_ret();\
-			hoc_pushx(nrnpy_object_to_double_(*ngh_result));\
-            return;\
-        }\
-    }\
-}
 
 #if defined(MINGW)
 extern "C" {
