bbf9dd61b9386468fd76f81afc1bfc293ccbd6eb
Ioannis Magkanaris <iomagkanaris@gmail.com>
2020-04-21 18:41:44 +0200

    Added optional argument --expected-hosts to test0.py and fail test if it doesn't match to nhost

--- src/parallel/test0.py
+++ src/parallel/test0.py
@@ -1,9 +1,20 @@
+import sys
 from neuron import h
+
+if "--expected-hosts" in sys.argv:
+    expected_host_index = sys.argv.index("--expected-hosts") + 1
+    expected_hosts = int(sys.argv[expected_host_index])
+else:
+    expected_hosts = 0
+
 h.nrnmpi_init()
 pc = h.ParallelContext()
 
 id = int(pc.id())
 nhost = int(pc.nhost())
+
+if expected_hosts & (nhost != expected_hosts):
+    sys.exit("Number of ranks ({}) different that the expected ({})".format(nhost, expected_hosts))
 
 print ('I am %d of %d'%(id, nhost))
 
