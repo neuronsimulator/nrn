580760080eaddc1abfab5ff6c0406f45e9e7ccf6
Pramod Kumbhar <pramod.s.kumbhar@gmail.com>
2020-04-26 19:12:31 +0200

    Use sudo with update-alternatives

--- packaging/python/test_wheels.sh
+++ packaging/python/test_wheels.sh
@@ -76,9 +76,9 @@ run_parallel_test() {
 
     # Travis Linux
     elif [ "$TRAVIS_OS_NAME" == "linux" ]; then
-      update-alternatives --set mpi /usr/include/mpich
+      sudo update-alternatives --set mpi /usr/include/mpich
       run_mpi_test "mpirun" "MPICH" ""
-      update-alternatives --set mpi /usr/lib/x86_64-linux-gnu/openmpi/include
+      sudo update-alternatives --set mpi /usr/lib/x86_64-linux-gnu/openmpi/include
       run_mpi_test "mpirun" "OpenMPI" ""
 
     # BB5 with multiple MPI libraries
