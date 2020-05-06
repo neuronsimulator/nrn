0b55f6335f9e0a95979489975a71c1062c64a9c2
Pramod Kumbhar <pramod.kumbhar@epfl.ch>
2020-05-06 23:58:43 +0200

    Support for building without libpython (BlueBrain/nmodl#316)
    * Support for building without libpython
      - add extra flag -DLINK_AGAINST_PYTHON=OFF to disable linking
         with libpython libary
      - update travis to use older bison on OSX
      - Support for NMODLHOME env variable to locate nrnunits.lib
    * Copy missing examples
    * Cache hombrew/pip directories
    
    fixes  BlueBrain/nmodl#314 BlueBrain/nmodl#315
    
    NMODL Repo SHA: BlueBrain/nmodl@f4f93a40f735b590c058f050e541c11db5e49cb8

---
+++ cmake/nmodl/PythonLinkHelper.cmake
@@ -0,0 +1,27 @@
+# =============================================================================
+# Support for building NMODL without python library
+# =============================================================================
+# ~~~
+# When NMODL is built under environment  (e.g. manylinux) without python-dev
+# package i.e. libpython.so, we have to build extension and binaries by
+# ignoring undefned symbols. The python library will be loaded later on target
+# system.
+# ~~~
+
+# use same variable name as NEURON as it won't be user option
+set(LINK_AGAINST_PYTHON
+    TRUE
+    CACHE BOOL "Disable linking to python library")
+
+mark_as_advanced(LINK_AGAINST_PYTHON)
+
+# Flags for ignoring undefined symbols for wheel
+if(CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+  set(UNDEFINED_SYMBOLS_IGNORE_FLAG "-Wl,-undefined,dynamic_lookup")
+else()
+  set(UNDEFINED_SYMBOLS_IGNORE_FLAG "-Wl,--unresolved-symbols=ignore-all")
+endif()
+
+if(NOT LINK_AGAINST_PYTHON)
+  string(APPEND CMAKE_EXE_LINKER_FLAGS " ${UNDEFINED_SYMBOLS_IGNORE_FLAG}")
+endif()
