3b4edeca15a6a5aa77659705079631b4cecfd5ec
adamjhn <adam.newton@yale.edu>
2020-05-13 17:18:15 -0400

    bugfix: Correct the volumes for diffusion in 1D/3D hybrid models. (#524)
    * bugfix: Correct the volumes for diffusion in 1D/3D hybrid models.
    Additionally, stops species._ensure_extracellular overwriting the setup and init functions pointers.
    
    * Removed superfluous grid id checks in the Intracellular and Extracellular species destructors, following the review by @ramcdougal.
    
    Co-authored-by: nrnhines <michael.hines@yale.edu>

---
+++ test/rxd/hybrid/test_pure_diffusion_hybrid.py
@@ -0,0 +1,72 @@
+import pytest
+import numpy
+from testutils import compare_data, tol
+
+
+@pytest.fixture
+def ics_diffusion_hybrid(neuron_instance):
+    """A model using intracellular diffusion in a 1D and 3D sections"""
+
+    h, rxd, data = neuron_instance
+    dend1 = h.Section(name='dend1')
+    dend1.diam = 2
+    dend1.nseg = 11
+    dend1.L = 10
+
+    dend2 = h.Section(name='dend2')
+    dend2.diam = 2
+    dend2.nseg = 11
+    dend2.L = 10
+
+    dend3 = h.Section(name='dend3')
+    dend3.diam = 2
+    dend3.nseg = 11
+    dend3.L = 10
+
+    dend2.connect(dend1)
+    dend3.connect(dend2)
+
+
+    diff_constant = 1
+
+    r = rxd.Region(h.allsec(),dx=0.75)
+
+    rxd.set_solve_type([dend2], dimension=3)
+
+    ca = rxd.Species(r, d=diff_constant,
+                     initial=lambda node: 1 if 
+                        (0.8 < node.x and node.segment in dend1) or 
+                        (node.x < 0.2 and node.segment in dend2) else 0)
+    model = ([dend1, dend2, dend3], r, ca)
+    yield (neuron_instance, model)
+
+def test_pure_diffusion_hybrid(ics_diffusion_hybrid):
+    """Test ics_diffusion_hybrid with fixed step methods"""
+
+    neuron_instance, model = ics_diffusion_hybrid
+    h, rxd, data = neuron_instance
+    dend, r, ca = model
+    h.dt *= 50
+    h.finitialize(-65)
+    loss = -(numpy.array(ca.nodes.concentration) * numpy.array(ca.nodes.volume)).sum()
+    h.continuerun(125)
+    loss += (numpy.array(ca.nodes.concentration) * numpy.array(ca.nodes.volume)).sum()
+    assert loss < tol
+    max_err = compare_data(data)
+    assert max_err < tol
+
+
+def test_pure_diffusion_hybrid_cvode(ics_diffusion_hybrid):
+    """Test ics_diffusion_hybrid with variable step methods"""
+
+    neuron_instance, model = ics_diffusion_hybrid
+    h, rxd, data = neuron_instance
+    dend, r, ca = model
+    h.CVode().active(True)
+    h.finitialize(-65)
+    loss = -(numpy.array(ca.nodes.concentration) * numpy.array(ca.nodes.volume)).sum()
+    h.continuerun(125)
+    loss += (numpy.array(ca.nodes.concentration) * numpy.array(ca.nodes.volume)).sum()
+    assert loss < tol
+    max_err = compare_data(data)
+    assert max_err < tol
