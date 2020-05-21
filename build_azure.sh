5f59741b0b194b933ec5d492251c61cc4dba4f21
Pramod Kumbhar <pramod.s.kumbhar@gmail.com>
2020-05-22 01:34:22 +0200

    Get rid off all debug steps - start clean

--- build_azure.sh
+++ build_azure.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-set -x
+set -ex
 
 export MSYSTEM_CHOST=x86_64-w64-mingw32
 export MSYSTEM=MINGW64
@@ -11,17 +10,12 @@ export PATH=/mingw64/bin:$PATH
 
 sed -i "s/elif msc_ver == '1600':/elif msc_ver == '1900':/g" /c/python354/lib/distutils/cygwinccompiler.py
 sed -i "s/elif msc_ver == '1600':/elif msc_ver == '1916':/g" /c/python368/lib/distutils/cygwinccompiler.py
-#sed -i "s/elif msc_ver == '1600':/elif msc_ver == '1900':/g" /c/python377/lib/distutils/cygwinccompiler.py
-#sed -i "s/elif msc_ver == '1600':/elif msc_ver == '1916':/g" /c/python382/lib/distutils/cygwinccompiler.py
+sed -i "s/elif msc_ver == '1600':/elif msc_ver == '1900':/g" /c/python377/lib/distutils/cygwinccompiler.py
+sed -i "s/elif msc_ver == '1600':/elif msc_ver == '1916':/g" /c/python382/lib/distutils/cygwinccompiler.py
 sed -i "s/return \['msvcr100'\]/return \['msvcrt'\]/g" /c/python3*/lib/distutils/cygwinccompiler.py
 
-ls
-
 cd $BUILD_SOURCESDIRECTORY
-ls
-rm -rf build
-mkdir -p build
-cd build
+mkdir -p build && cd build
 
 /mingw64/bin/cmake .. \
 	-G 'Unix Makefiles'  \
@@ -33,18 +27,11 @@ cd build
 	-DNRN_ENABLE_RX3D=ON  \
 	-DPYTHON_EXECUTABLE=/c/python354/python.exe \
 	-DNRN_ENABLE_PYTHON_DYNAMIC=ON  \
-	-DNRN_PYTHON_DYNAMIC='c:/python354/python.exe;c:/python368/python.exe'  \
+	-DNRN_PYTHON_DYNAMIC='c:/python354/python.exe;c:/python368/python.exe;c:/python377/python.exe;c:/python382/python.exe'  \
 	-DCMAKE_INSTALL_PREFIX='/c/nrn-install' \
 	-DMPI_CXX_LIB_NAMES:STRING=msmpi \
 	-DMPI_C_LIB_NAMES:STRING=msmpi \
 	-DMPI_msmpi_LIBRARY:FILEPATH=c:/ms-mpi/lib/x64/msmpi.lib
-
-make -j VERBOSE=1
-
-echo "-----------------------"
-ls share/lib/python/neuron/rxd/geometry3d
-ls lib/python/neuron/rxd/geometry3d/
-echo "-----------------------"
-
+make -j
 make install
 make setup_exe
