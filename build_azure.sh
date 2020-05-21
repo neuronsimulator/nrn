40ea08152747743b02ce7518ab6874ba05949fba
Pramod Kumbhar <pramod.s.kumbhar@gmail.com>
2020-05-21 11:18:24 +0200

    multiple MPIs with adjusting distutils/cygwinccompiler.py

--- build_azure.sh
+++ build_azure.sh
@@ -9,6 +9,12 @@ export MINGW_CHOST=x86_64-w64-mingw32
 export MSYSTEM_PREFIX=/mingw64
 export PATH=/mingw64/bin:$PATH
 
+sed -i "s/elif msc_ver == '1600':/elif msc_ver == '1900':/g" /c/python354/lib/distutils/cygwinccompiler.py
+sed -i "s/elif msc_ver == '1600':/elif msc_ver == '1916':/g" /c/python368/lib/distutils/cygwinccompiler.py
+#sed -i "s/elif msc_ver == '1600':/elif msc_ver == '1900':/g" /c/python377/lib/distutils/cygwinccompiler.py
+#sed -i "s/elif msc_ver == '1600':/elif msc_ver == '1916':/g" /c/python382/lib/distutils/cygwinccompiler.py
+sed -i "s/return \['msvcr100'\]/return \['msvcrt'\]/g" /c/python3*/lib/distutils/cygwinccompiler.py
+
 ls
 
 cd $BUILD_SOURCESDIRECTORY
@@ -27,7 +33,7 @@ cd build
 	-DNRN_ENABLE_RX3D=ON  \
 	-DPYTHON_EXECUTABLE=/c/python354/python.exe \
 	-DNRN_ENABLE_PYTHON_DYNAMIC=ON  \
-	-DNRN_PYTHON_DYNAMIC='c:/python354/python.exe'  \
+	-DNRN_PYTHON_DYNAMIC='c:/python354/python.exe;c:/python368/python.exe'  \
 	-DCMAKE_INSTALL_PREFIX='/c/nrn-install' \
 	-DMPI_CXX_LIB_NAMES:STRING=msmpi \
 	-DMPI_C_LIB_NAMES:STRING=msmpi \
