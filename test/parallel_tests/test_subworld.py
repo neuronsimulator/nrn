neuronsimulator/nrn

commit 94068fee1a84e61918c2052a626fc30b2007b91e

Squashed commit of the following:
commit ecb1b2f218608390d6cc6c6fdbb8322106c52970
Author: Michael Hines <michael.hines@yale.edu>
Date:   Sun Feb 24 19:08:43 2019 -0500

    Fix tag upperbound limitation for ParallelContext  Bulletin Board.

    Some MPI implementations limit tags to be less than full MPI_INT domain,
    so pack tag if > 20 in second slot (now reserved) of todo message
    buffer and use 20 as the tag. Messages generated by submit and context
    reserve the second slot (MPI_INT) for the working_id_ value.

commit 17f0991efa468babeb4f60c85d8ec1b697105ec1
Author: Michael Hines <michael.hines@yale.edu>
Date:   Sun Feb 17 16:05:38 2019 -0500

    Eliminate legacy PVM implementation.

commit 2bde7f50b5b6a0753e3cfe2e9b8c1395d40f5506
Author: Michael Hines <michael.hines@yale.edu>
Date:   Sun Feb 17 09:15:15 2019 -0500

    Assert all MPI calls return MPI_SUCCESS

src/parallel/ocbbs.cpp
@@ -124,6 +124,7 @@ static int submit_help(OcBBS* bbs) {
 	}else{
 		bbs->pkint((id = --bbs->next_local_));
 	}
+        bbs->pkint(0); // space for working_id
 	if (ifarg(i+1)) {
 #if 1
 		int argtypes = 0;
@@ -1292,6 +1293,7 @@ void BBSImpl::return_args(int id) {
 	char* s;
 //printf("BBSImpl::return_args(%d):\n", id);
 	i = upkint(); // userid
+	int wid = upkint();
 	int style = upkint();
 //printf("message userid=%d style=%d\n", i, style);
 	switch (style) {

src/nrniv/bbsdirect.cpp
@@ -1,6 +1,4 @@
 #include <nrnmpiuse.h>
 #if NRNMPI
 #include "../parallel/bbsdirectmpi.cpp"
-#else
-#include "../parallel/bbsdirect.cpp"
 #endif

src/nrniv/bbslsrv2.cpp
@@ -1,6 +1,4 @@
 #include <nrnmpiuse.h>
 #if NRNMPI
 #include "../parallel/bbssrv2mpi.cpp"
-#else
-#include "../parallel/bbslsrv2.cpp"
 #endif

src/nrniv/bbsrcli.cpp
@@ -1,6 +1,4 @@
 #include <nrnmpiuse.h>
 #if NRNMPI
 #include "../parallel/bbsclimpi.cpp"
-#else
-#include "../parallel/bbsrcli.cpp"
 #endif

src/nrniv/bbssrv.cpp
@@ -1,6 +1,4 @@
 #include <nrnmpiuse.h>
 #if NRNMPI
 #include "../parallel/bbssrvmpi.cpp"
-#else
-#include "../parallel/bbssrv.cpp"
 #endif

src/nrnmpi/bbsmpipack.c
@@ -22,6 +22,12 @@
 #include <nrnmpi_impl.h>
 #include <hocdec.h>
 
+#if 0
+#define guard(f) assert(f == MPI_SUCCESS)
+#else
+#define guard(f) {int _i = f; if (_i != MPI_SUCCESS) {printf("%s %d\n", #f, _i); assert(0);}}
+#endif
+
 #define nrnmpidebugleak 0
 #define debug 0
 
@@ -54,7 +60,7 @@ printf("%d unpack upkpos=%d pkposition=%d keypos=%d size=%d\n",
   nrnmpi_myid_bbs, r->upkpos, r->pkposition, r->keypos, r->size);
 #endif
 assert(r->upkpos >= 0 && r->size >= r->upkpos);
-	MPI_Unpack(r->buf, r->size, &r->upkpos, type, 2, MPI_INT, nrn_bbs_comm); 
+	guard(MPI_Unpack(r->buf, r->size, &r->upkpos, type, 2, MPI_INT, nrn_bbs_comm)); 
 #if debug
 printf("%d unpack r=%p size=%d upkpos=%d type[0]=%d datatype=%d  type[1]=%d  count=%d\n", nrnmpi_myid_bbs, r, r->size, r->upkpos, type[0], my_datatype, type[1], count);
 #endif
@@ -63,7 +69,7 @@ printf("%d unpack size=%d upkpos=%d type[0]=%d   datatype=%d  type[1]=%d  count=
 }
 	assert(type[0] == my_datatype);
 	assert(type[1] == count);
-	MPI_Unpack(r->buf, r->size, &r->upkpos, buf, count, mytypes[my_datatype], nrn_bbs_comm);
+	guard(MPI_Unpack(r->buf, r->size, &r->upkpos, buf, count, mytypes[my_datatype], nrn_bbs_comm));
 }
 
 void nrnmpi_upkbegin(bbsmpibuf* r) {
@@ -77,13 +83,13 @@ assert(r && r->buf && r->size > 0);
 		hoc_execerror("subworld process with nhost > 0 cannot use", "the bulletin board");
 	}
 	r->upkpos = 0;
-	MPI_Unpack(r->buf, r->size, &r->upkpos,
-		&p, 1, MPI_INT, nrn_bbs_comm);
+	guard(MPI_Unpack(r->buf, r->size, &r->upkpos,
+		&p, 1, MPI_INT, nrn_bbs_comm));
 if (p > r->size) {
 printf("\n %d nrnmpi_upkbegin keypos=%d size=%d\n", nrnmpi_myid_bbs, p, r->size);
 }
 assert(p <= r->size);
-	MPI_Unpack(r->buf, r->size, &p, &type, 1, MPI_INT, nrn_bbs_comm);
+	guard(MPI_Unpack(r->buf, r->size, &p, &type, 1, MPI_INT, nrn_bbs_comm));
 #if debug
 printf("%d nrnmpi_upkbegin type=%d keypos=%d\n", nrnmpi_myid_bbs, type, p);
 #endif
@@ -195,7 +201,7 @@ void nrnmpi_pkbegin(bbsmpibuf* r) {
 #if debug
 printf("%d nrnmpi_pkbegin %p size=%d pkposition=%d\n", nrnmpi_myid_bbs, r, r->size, r->pkposition);
 #endif
-	MPI_Pack(&type, 1, MPI_INT, r->buf, r->size, &r->pkposition, nrn_bbs_comm);
+	guard(MPI_Pack(&type, 1, MPI_INT, r->buf, r->size, &r->pkposition, nrn_bbs_comm));
 }
 
 void nrnmpi_enddata(bbsmpibuf* r) {
@@ -205,7 +211,7 @@ void nrnmpi_enddata(bbsmpibuf* r) {
 #if debug
 printf("%d nrnmpi_enddata %p size=%d pkposition=%d\n", nrnmpi_myid_bbs, r, r->size, p);
 #endif
-	MPI_Pack_size(1, MPI_INT, nrn_bbs_comm, &isize);
+	guard(MPI_Pack_size(1, MPI_INT, nrn_bbs_comm, &isize));
 oldsize = r->size;
 	resize(r, r->pkposition + isize);
 #if debug
@@ -213,11 +219,11 @@ if (oldsize < r->pkposition + isize) {
 	printf("%d %p need %d more. end up with total of %d\n", nrnmpi_myid_bbs, r, isize, r->size);
 }
 #endif
-	MPI_Pack(&type, 1, MPI_INT, r->buf, r->size, &r->pkposition, nrn_bbs_comm);
+	guard(MPI_Pack(&type, 1, MPI_INT, r->buf, r->size, &r->pkposition, nrn_bbs_comm));
 #if debug
 printf("%d nrnmpi_enddata buf=%p size=%d pkposition=%d\n", nrnmpi_myid_bbs, r->buf, r->size, r->pkposition);
 #endif
-	MPI_Pack(&p, 1, MPI_INT, r->buf, r->size, &type, nrn_bbs_comm);
+	guard(MPI_Pack(&p, 1, MPI_INT, r->buf, r->size, &type, nrn_bbs_comm));
 #if debug
 printf("%d after nrnmpi_enddata, %d was packed at beginning and 0 was packed before %d\n", nrnmpi_myid_bbs, p, r->pkposition);
 #endif
@@ -229,8 +235,8 @@ static void pack(void* inbuf, int incount, int my_datatype, bbsmpibuf* r, const 
 #if debug
 printf("%d pack %p count=%d type=%d outbuf-%p pkposition=%d %s\n", nrnmpi_myid_bbs, r, incount, my_datatype, r->buf, r->pkposition, e);
 #endif
-	MPI_Pack_size(incount, mytypes[my_datatype], nrn_bbs_comm, &dsize);
-	MPI_Pack_size(2, MPI_INT, nrn_bbs_comm, &isize);
+	guard(MPI_Pack_size(incount, mytypes[my_datatype], nrn_bbs_comm, &dsize));
+	guard(MPI_Pack_size(2, MPI_INT, nrn_bbs_comm, &isize));
 oldsize = r->size;
 	resize(r, r->pkposition + dsize + isize);
 #if debug
@@ -239,8 +245,8 @@ if (oldsize < r->pkposition + dsize + isize) {
 }
 #endif
 	type[0] = my_datatype;  type[1] = incount;
-	MPI_Pack(type, 2, MPI_INT, r->buf, r->size, &r->pkposition, nrn_bbs_comm);
-	MPI_Pack(inbuf, incount, mytypes[my_datatype], r->buf, r->size, &r->pkposition, nrn_bbs_comm);
+	guard(MPI_Pack(type, 2, MPI_INT, r->buf, r->size, &r->pkposition, nrn_bbs_comm));
+	guard(MPI_Pack(inbuf, incount, mytypes[my_datatype], r->buf, r->size, &r->pkposition, nrn_bbs_comm));
 #if debug
 printf("%d pack done pkposition=%d\n", nrnmpi_myid_bbs, r->pkposition);
 #endif
@@ -279,11 +285,27 @@ void nrnmpi_bbssend(int dest, int tag, bbsmpibuf* r) {
 #if debug
 printf("%d nrnmpi_bbssend %p dest=%d tag=%d size=%d\n", nrnmpi_myid_bbs, r, dest, tag, (r)?r->size:0);
 #endif
+
+	/* Some MPI implementations limit tags to be less than full MPI_INT domain
+	   so pack tag if > 20 in second slot (now reserved) of buffer and use 20 as the tag.
+	*/
+	if (tag > 20) {
+		int save_position = r->pkposition;
+		int save_upkpos = r->upkpos;
+		nrnmpi_upkbegin(r);
+		nrnmpi_upkint(r);
+		r->pkposition = r->upkpos;
+		nrnmpi_pkint(tag, r);
+		r->pkposition = save_position;
+		r->upkpos = save_upkpos;
+		tag = 20;
+	}
+
 	if (r) {
 		assert( r->buf && r->keypos <= r->size);
-		MPI_Send(r->buf, r->size, MPI_PACKED, dest, tag, nrn_bbs_comm);
+		guard(MPI_Send(r->buf, r->size, MPI_PACKED, dest, tag, nrn_bbs_comm));
 	}else{
-		MPI_Send(NULL, 0, MPI_PACKED, dest, tag, nrn_bbs_comm);
+		guard(MPI_Send(NULL, 0, MPI_PACKED, dest, tag, nrn_bbs_comm));
 	}
 	errno = 0;
 #if debug
@@ -300,14 +322,29 @@ int nrnmpi_bbsrecv(int source, bbsmpibuf* r) {
 #if debug
 printf("%d nrnmpi_bbsrecv %p\n", nrnmpi_myid_bbs, r);
 #endif
-	MPI_Probe(source, MPI_ANY_TAG, nrn_bbs_comm, &status);
-	MPI_Get_count(&status, MPI_PACKED, &size);
+	guard(MPI_Probe(source, MPI_ANY_TAG, nrn_bbs_comm, &status));
+	guard(MPI_Get_count(&status, MPI_PACKED, &size));
 #if debug
 printf("%d nrnmpi_bbsrecv probe size=%d source=%d tag=%d\n", nrnmpi_myid_bbs, size, status.MPI_SOURCE, status.MPI_TAG);
 #endif
 	resize(r, size);
-	MPI_Recv(r->buf, r->size, MPI_PACKED, source, MPI_ANY_TAG, nrn_bbs_comm, &status);
+	guard(MPI_Recv(r->buf, r->size, MPI_PACKED, source, MPI_ANY_TAG, nrn_bbs_comm, &status));
 	errno = 0;
+	/* Some MPI implementations limit tags to be less than full MPI_INT domain
+	   In the past we allowed  TODO mesages to have tags > 20 (FIRSTID of src/parallel/bbssrv.h)
+	   To fix the bug we no longer send or receive such tags and instead
+	   copy the tag into the second pkint of the r->buf and send with
+	   a tag of 20.
+	*/
+	if (status.MPI_TAG == 20) {
+		int tag;
+		int save_upkpos = r->upkpos; /* possibly not needed */
+		nrnmpi_upkbegin(r);
+		nrnmpi_upkint(r);
+		tag = nrnmpi_upkint(r);
+		r->upkpos = save_upkpos;
+		return tag;
+	}
 	return status.MPI_TAG;
 }
 
@@ -330,11 +367,11 @@ printf("%d nrnmpi_bbssendrecv nothing available so send\n", nrnmpi_myid_bbs);
 int nrnmpi_iprobe(int* size, int* tag, int* source) {
 	int flag = 0;
 	MPI_Status status;
-	MPI_Iprobe(MPI_ANY_SOURCE, MPI_ANY_TAG, nrn_bbs_comm, &flag, &status);
+	guard(MPI_Iprobe(MPI_ANY_SOURCE, MPI_ANY_TAG, nrn_bbs_comm, &flag, &status));
 	if (flag) {
 		if (source) *source = status.MPI_SOURCE;
 		if (tag) *tag = status.MPI_TAG;
-		if (size) MPI_Get_count(&status, MPI_PACKED, size);
+		if (size) guard(MPI_Get_count(&status, MPI_PACKED, size));
 	}
 	return flag;
 }
@@ -342,10 +379,10 @@ int nrnmpi_iprobe(int* size, int* tag, int* source) {
 void nrnmpi_probe(int* size, int* tag, int* source) {
 	int flag = 0;
 	MPI_Status status;
-	MPI_Probe(MPI_ANY_SOURCE, MPI_ANY_TAG, nrn_bbs_comm, &status);
+	guard(MPI_Probe(MPI_ANY_SOURCE, MPI_ANY_TAG, nrn_bbs_comm, &status));
 	if (source) *source = status.MPI_SOURCE;
 	if (tag) *tag = status.MPI_TAG;
-	if (size) MPI_Get_count(&status, MPI_PACKED, size);
+	if (size) guard(MPI_Get_count(&status, MPI_PACKED, size));
 }
 
 bbsmpibuf* nrnmpi_newbuf(int size) {

src/parallel/Makefile.am
@@ -1,10 +1,10 @@
 # This makefile does nothing except to copy some .c and .h files into
 # the distribution tar file.
 
-EXTRA_DIST = bbs.cpp bbs.h bbsdirect.cpp bbsdirect.h bbsimpl.h \
+EXTRA_DIST = bbs.cpp bbs.h bbsdirect.h bbsimpl.h \
 	bbslocal.cpp bbslocal.h bbslsrv.cpp bbslsrv.h \
-	bbslsrv2.cpp bbslsrv2.h bbsrcli.cpp bbsrcli.h \
-	bbssrv.cpp bbssrv.h ocbbs.cpp \
+	bbslsrv2.h bbsrcli.h \
+	ocbbs.cpp \
 	bbsdirectmpi.cpp bbssrvmpi.cpp bbssrv2mpi.cpp bbsclimpi.cpp \
 	bbssrv2mpi.h subworld.cpp \
 	test0.hoc test1.hoc test2.hoc test3.hoc test4.hoc test5.hoc \

src/parallel/bbs.cpp
@@ -7,7 +7,7 @@
 #include "oc2iv.h"
 #include "bbs.h"
 #include "bbslocal.h"
-#if defined(HAVE_PVM3_H) || defined(NRNMPI)
+#if defined(NRNMPI)
 #include "bbsdirect.h"
 #include "bbsrcli.h"
 #endif
@@ -34,7 +34,6 @@ bool BBSImpl::master_works_ = true;
 #define debug BBSImpl::debug_
 
 int BBSImpl::debug_ = 0;
-bool BBSImpl::use_pvm_;
 int BBSImpl::mytid_;
 
 static int etaskcnt;
@@ -49,50 +48,14 @@ BBS::BBS(int n) {
 	init(n);
 }
 
-#ifdef HAVE_PVM3_H
-void BBS::init(int n) {
-//printf("BBS::init\n");
-//fflush(stdout);
-	if (!BBSImpl::started_) {
-		if (nrnmpi_numprocs_bbs < 2) { // master first time call
-			nrnmpi_numprocs_bbs = 1; // the minimum
-			if (n == 0) { // demand local
-				BBSImpl::use_pvm_ = false;
-			}else{
-				BBSDirect::check_pvm();
-			}
-			BBSImpl::is_master_ = true;
-		}else{
-			BBSImpl::use_pvm_ = true;
-		}
-	}
-	if (BBSImpl::use_pvm_ == false) {
-//printf("new BBSLocal\n");
-//fflush(stdout);
-		impl_ = new BBSLocal();
-	}else{
-		if (BBSImpl::is_master_) {
-//printf("new BBSDirect\n");
-//fflush(stdout);
-			impl_ = new BBSDirect();
-		}else{
-//printf("new BBSClient\n");
-//fflush(stdout);
-			impl_ = new BBSClient();
-		}
-	}
-}
-#else //!HAVE_PVM3_H
 #if NRNMPI
 void BBS::init(int) {
 	if (nrnmpi_use == 0) {
 		BBSImpl::is_master_ = true;
-		BBSImpl::use_pvm_ = false;
 		impl_ = new BBSLocal();
 		return;
 	}
 	if (!BBSImpl::started_) {
-		BBSImpl::use_pvm_ = false;
 		BBSImpl::is_master_ = (nrnmpi_myid_bbs == 0) ? true : false;
 		BBSImpl::master_works_ = true;
 //printf("%d BBS::init is_master=%d\n", nrnmpi_myid_bbs, BBSImpl::is_master_);
@@ -121,14 +84,12 @@ void BBS::init(int) {
 #else // !NRNMPI
 void BBS::init(int) {
 	if (!BBSImpl::started_) {
-		BBSImpl::use_pvm_ = false;
 		BBSImpl::is_master_ = true;
 		BBSImpl::master_works_ = true;
 	}
 	impl_ = new BBSLocal();
 }
 #endif // !NRNMPI
-#endif // !HAVE_PVM3_H
 
 BBSImpl::BBSImpl() {
 	runworker_called_ = 0;
@@ -338,6 +299,7 @@ void BBSImpl::execute(int id) { // assumes a "_todo" message in receive buffer
 printf("execute begin %g: working_id_=%d\n",st, working_id_);
 		}
 		userid = upkint();
+		int wid = upkint();
 		hoc_ac_ = double(id);
 		rs = execute_helper(&n, id); //builds and execute hoc statement
 		et = time() - st;
@@ -348,6 +310,7 @@ et, working_id_, hoc_ac_);
 		}
 		pkbegin();
 		pkint(userid);
+		pkint(wid);
 		pkint(rs?1:0);
 		if (!rs) {
 			pkdouble(hoc_ac_);
@@ -424,6 +387,7 @@ bool BBSImpl::working(int& id, double& x, int& userid) {
 		}
 		if (id != 0) {
 			userid = upkint();
+			int wid = upkint();
 			rtype = upkint();
 			if (rtype == 0) {
 				x = upkdouble();

commit 95f5c22a1ce340efc38a1e257305e0881cd55594

nhost_world() does not have to be a multiple of the subworld size.
Give pc.context a better chance to complete when another pc.context
is issued.

Add a test for pc.context.

Fix pc.context bug introduced by 94068fee1

src/nrnmpi/nrnmpi.c
@@ -67,7 +67,7 @@ void nrnmpi_init(int nrnmpi_under_nrncontrol, int* pargc, char*** pargv) {
 {int i;
 printf("nrnmpi_init: argc=%d\n", *pargc);
 for (i=0; i < *pargc; ++i) {
-	printf("%d |%s|\n", i, (*pargv)[i]);
+        printf("%d |%s|\n", i, (*pargv)[i]);
 }
 }
 #endif
@@ -155,7 +155,7 @@ for (i=0; i < *pargc; ++i) {
 {int i;
 printf("nrnmpi_init: argc=%d\n", *pargc);
 for (i=0; i < *pargc; ++i) {
-	printf("%d |%s|\n", i, (*pargv)[i]);
+        printf("%d |%s|\n", i, (*pargv)[i]);
 }
 }
 #endif
@@ -181,7 +181,7 @@ void nrnmpi_terminate() {
 #if NRNMPI
     if (nrnmpi_use) {
 #if 0
-		printf("%d nrnmpi_terminate\n", nrnmpi_myid_world);
+                printf("%d nrnmpi_terminate\n", nrnmpi_myid_world);
 #endif
 #if USE_HPM
         hpmTerminate(nrnmpi_myid_world);
@@ -218,21 +218,30 @@ void nrnmpi_abort(int errcode) {
 
 #if NRNMPI
 void nrnmpi_subworld_size(int n) {
-    /* n is the size of a subworld, nrnmpi_numprocs (pc.nhost) */
+    /* n is the (desired) size of a subworld (pc.nhost) */
+    /* A subworld (net) is contiguous */
+    /* In case pc.nhost_world/n is not an integer, there are
+       pc.nhost_world/n + 1 subworlds and the last subworld
+       has pc.nhost_world%n ranks. All the other subworlds have n ranks.
+    */
     if (nrnmpi_use != 1) {
         return;
     }
     if (nrnmpi_comm != MPI_COMM_NULL) {
         asrt(MPI_Comm_free(&nrnmpi_comm));
+        nrnmpi_comm = MPI_COMM_NULL;
     }
     if (nrn_bbs_comm != MPI_COMM_NULL) {
         asrt(MPI_Comm_free(&nrn_bbs_comm));
+        nrn_bbs_comm = MPI_COMM_NULL;
     }
     if (grp_bbs != MPI_GROUP_NULL) {
         asrt(MPI_Group_free(&grp_bbs));
+        grp_bbs = MPI_GROUP_NULL;
     }
     if (grp_net != MPI_GROUP_NULL) {
         asrt(MPI_Group_free(&grp_net));
+        grp_net = MPI_GROUP_NULL;
     }
     MPI_Group wg;
     asrt(MPI_Comm_group(nrnmpi_world_comm, &wg));
@@ -262,28 +271,43 @@ void nrnmpi_subworld_size(int n) {
     } else {
         int nw = nrnmpi_numprocs_world;
         int nb = nw / n; /* nrnmpi_numprocs_bbs */
+        int ib;
         int range[3];
-        /* net is contiguous */
-        range[0] = r / n;
-        range[0] *= n;               /* first */
-        range[1] = range[0] + n - 1; /* last */
+        if (nw%n) {
+                nb += 1; /* and the last will have pc.nhost = nw%n */
+        }
+        /* A subworld (net) has contiguous ranks. */
+        /* Every rank is in a specific nrnmpi_comm communicator */
+        ib = r / n;
+        range[0] = ib*n;               /* first rank in group */
+        range[1] = range[0] + n - 1; /* last rank in group */
+        if (range[1] >= nw) {
+                range[1] = nw - 1;
+        }
         range[2] = 1;                /* stride */
         asrt(MPI_Group_range_incl(wg, 1, &range, &grp_net));
         asrt(MPI_Comm_create(nrnmpi_world_comm, grp_net, &nrnmpi_comm));
         asrt(MPI_Comm_rank(nrnmpi_comm, &nrnmpi_myid));
         asrt(MPI_Comm_size(nrnmpi_comm, &nrnmpi_numprocs));
 
-        range[0] = 0;      /* first */
-        range[1] = nw - n; /* last */
+        /* nrn_bbs_com ranks stride is nrnmpi_numprocs */
+        /* only rank 0 of each subworld participates in nrn_bbs_comm */
+        range[0] = 0;      /* first world rank in nrn_bbs_comm */
+        range[1] = (nb - 1)*n; /* last world rank in nrn_bbs_comm */
         range[2] = n;      /* stride */
         asrt(MPI_Group_range_incl(wg, 1, &range, &grp_bbs));
         asrt(MPI_Comm_create(nrnmpi_world_comm, grp_bbs, &nrn_bbs_comm));
-        if (r % n == 0) { /* only rank 0 of the subworlds */
+        if (r % n == 0) { /* only rank 0 participates in nrn_bbs_comm */
             asrt(MPI_Comm_rank(nrn_bbs_comm, &nrnmpi_myid_bbs));
             asrt(MPI_Comm_size(nrn_bbs_comm, &nrnmpi_numprocs_bbs));
         } else {
+#if 1
             nrnmpi_myid_bbs = -1;
             nrnmpi_numprocs_bbs = -1;
+#else
+            nrnmpi_myid_bbs = r/n;
+            nrnmpi_numprocs_bbs = nb;
+#endif
         }
     }
     asrt(MPI_Group_free(&wg));

src/parallel/bbsdirectmpi.cpp
@@ -77,6 +77,7 @@ void BBSDirect::context() {
 		nrnmpi_copy(recvbuf_, sendbuf_);
 		nrnmpi_upkbegin(recvbuf_);
 		nrnmpi_upkint(recvbuf_);
+		nrnmpi_upkint(recvbuf_); // slot reserved for tag
 		execute_helper(&n, -1, false);
 		nrnmpi_unref(recvbuf_);
 		recvbuf_ = rsav;

src/parallel/bbssrv2mpi.cpp
@@ -8,6 +8,7 @@
 #include "bbssrv2mpi.h"
 #include "bbssrv.h"
 #include "bbsimpl.h"
+#include "hocdec.h" //Printf
 
 extern "C" {
 	void nrnbbs_context_wait();
@@ -282,8 +283,15 @@ void  BBSDirectServer::context(bbsmpibuf* send) {
 #if debug
 printf("numprocs_bbs=%d\n", nrnmpi_numprocs_bbs);
 #endif
+	// Previous context may complete after allowing more activity
+	for (j=0; j < 1000; ++j) {
+		if (remaining_context_cnt_ == 0) {
+			break;
+		}
+		handle();
+	}
 	if (remaining_context_cnt_ > 0) {
-		printf("some workers did not receive previous context\n");
+		Printf("some workers did not receive previous context\n");
 		send_context_->erase(send_context_->begin(), send_context_->end());
 		nrnmpi_unref(context_buf_);
 		context_buf_ = nil;

src/parallel/ocbbs.cpp
@@ -260,9 +260,6 @@ static double rank_bbs(void* v) {
 
 static double subworlds(void* v) {
 	int n = int(chkarg(1, 1, nrnmpi_numprocs_world));
-	if (nrnmpi_numprocs_world%n) {
-		hoc_execerror("nhost_world must be an integer multiple of subworld size", 0);
-	}
 #if NRNMPI
 	nrnmpi_subworld_size(n);
 #endif

test/CMakeLists.txt
@@ -87,6 +87,7 @@ list(APPEND TESTS connect_dend)
 if(NRN_ENABLE_PYTHON AND PYTEST_FOUND)
   add_test(NAME basic_tests COMMAND ${PYTHON_EXECUTABLE} -m pytest ${PROJECT_SOURCE_DIR}/test/pynrn
            WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}/test/pynrn)
+  list(APPEND TESTS basic_tests)
   add_custom_target(
     pynrn_mod ALL
     COMMAND
@@ -102,18 +103,25 @@ if(NRN_ENABLE_PYTHON AND PYTEST_FOUND)
     WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}/test/rxd)
   add_dependencies(rxdmod nrniv)
 
-  add_test(NAME rxd_tests COMMAND ${PYTHON_EXECUTABLE} -m pytest ${PROJECT_SOURCE_DIR}/test/rxd)
-  list(APPEND TESTS basic_tests rxd_tests)
-  if(NRN_ENABLE_MPI)
-    find_python_module(mpi4py)
-    if(mpi4py_FOUND)
-      add_test(
-        NAME rxd_mpi_tests
-        COMMAND
-          ${MPIEXEC} -np 1 ${PYTHON_EXECUTABLE} -m pytest ${PROJECT_SOURCE_DIR}/test/rxd --mpi)
-      list(APPEND TESTS rxd_mpi_tests)
+  if(NRN_ENABLE_RX3D)
+    add_test(NAME rxd_tests COMMAND ${PYTHON_EXECUTABLE} -m pytest ${PROJECT_SOURCE_DIR}/test/rxd)
+    list(APPEND TESTS basic_tests)
+    if(NRN_ENABLE_MPI)
+      find_python_module(mpi4py)
+      if(mpi4py_FOUND)
+        add_test(
+          NAME rxd_mpi_tests
+          COMMAND
+            ${MPIEXEC} -np 1 ${PYTHON_EXECUTABLE} -m pytest ${PROJECT_SOURCE_DIR}/test/rxd --mpi)
+        list(APPEND TESTS rxd_mpi_tests)
+      endif()
     endif()
   endif()
+  add_test(
+    NAME parallel_tests
+    COMMAND
+      ${MPIEXEC} -np 6 ${CMAKE_BINARY_DIR}/bin/nrniv -mpi -python ${PROJECT_SOURCE_DIR}/test/parallel_tests/test_subworld.py)
+  list(APPEND TESTS parallel_tests)
 
   if(NRN_ENABLE_CORENEURON)
     file(COPY ${PROJECT_SOURCE_DIR}/test/coreneuron/mod DESTINATION ${PROJECT_BINARY_DIR}/test/)

test/parallel_tests/test_subworld.py
@@ -1,0 +1,62 @@
+
+from neuron import h
+pc = h.ParallelContext()
+
+# each subworld has 3 ranks if nhost_world is multiple of subsize
+# (if not, last will have nworld%subsize ranks)
+subsize = 3
+pc.subworlds(subsize)
+
+ibbs = pc.id_world()//subsize
+nbbs = pc.nhost_world()//subsize
+x = 0 if pc.nhost_world() % subsize == 0 else 1
+
+if pc.id_world() == 0:
+  print("id_world nhost_world id_bbs nhost_bbs ibbs nbbs  id  nhost")
+pc.barrier()
+print("%5d %9d %8d %8d %7d %3d %5d %4d" % (pc.id_world(), pc.nhost_world(), pc.id_bbs(), pc.nhost_bbs(), ibbs, nbbs+x, pc.id(), pc.nhost()))
+
+assert(pc.nhost() == (subsize if ibbs < nbbs else (pc.nhost_world() - subsize*nbbs)))
+assert(pc.id() == pc.id_world()%subsize)
+# id_bbs and nhost_bbs for non-zero id not -1.
+assert(pc.nhost_bbs() == ((nbbs + x) if pc.id() == 0 else -1))
+assert(pc.id_bbs() == (ibbs if pc.id() == 0 else -1))
+
+def f(arg):
+  print("f(%s) id_world=%d id=%d" % (str(arg), pc.id_world(), pc.id()))
+  return (arg, pc.id_world(), pc.id())
+
+x=0
+def gcntxt(arg):
+  global x
+  x = int(arg)
+  #print("gcntxt(%s) id_world=%d" % (str(int(arg)), pc.id_world()))
+
+def gtest(arg):
+  if x != int(arg):
+    h.execerror("gtest %d != %d" % (x, int(arg)))
+    pass
+  #print("gtest x=%d id_world=%d" % (x, pc.id_world()))
+
+pc.runworker()
+
+for i in range(5):
+  pc.submit(f, "submit %d"%i)
+hs = h.ref('')
+while (pc.working()):
+  pc.upkstr(hs)
+  print("working returned %s (submit passed %s)"%(str(pc.pyret()), hs[0]))
+
+pc.context(gcntxt, 6)
+
+for i in range(3*pc.nhost_world()):
+  pc.submit(gtest, 6)
+
+
+x = 6
+while(pc.working()):
+  pass;
+
+pc.done()
+
+h.quit()