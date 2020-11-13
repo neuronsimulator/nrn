c28fffa18103b7c74f8b0e8d6699509500a5946e
Pramod Kumbhar <pramod.kumbhar@epfl.ch>
2020-11-13 18:47:02 +0100

    Add NMODL_ENABLE_PYTHON_BINDINGS option to disable python bindings (#428)
    * Add NMODL_ENABLE_PYTHON_BINDINGS option to disable python bindings
      - NMODL_ENABLE_PYTHON_BINDINGS=ON by default
      - if python bindings are not enabled, show warning at the import
      - do not add dependency with _nmodl target
      - use imp module to test existance of package
    * Address review comments and fix build error
     - fix typo
     - avoid oputput expression with add_custom_command
     - change one of the CI with -DNMODL_ENABLE_PYTHON_BINDINGS=OFF
     - add Pybind test if NMODL_ENABLE_PYTHON_BINDINGS=ON

--- nmodl/__init__.py
+++ nmodl/__init__.py
@@ -1,4 +1,10 @@
-from ._nmodl import NmodlDriver, to_json, to_nmodl  # noqa
-from ._nmodl import __version__
+import imp
+try:
+    # check if nmodl binds are built before importing
+    imp.find_module('_nmodl')
+    from ._nmodl import NmodlDriver, to_json, to_nmodl  # noqa
+    from ._nmodl import __version__
+    __all__ = ["NmodlDriver", "to_json", "to_nmodl"]
+except ImportError:
+    print("[NMODL] [warning] :: Python bindings are not available")
 
-__all__ = ["NmodlDriver", "to_json", "to_nmodl"]
