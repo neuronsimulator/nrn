460659e06e6067ed255e371bcadd3b26ccc7e6ab
Michael Hines <michael.hines@yale.edu>
2020-11-29 07:59:55 -0500

    windows has different signature for mkdir

--- src/nmodl/io.c
+++ src/nmodl/io.c
@@ -491,7 +491,11 @@ int mkdir_p(const char *path)
             /* temporarily truncate */
             *p = '\0';
     
+#if defined(MINGW)
+            if (_mkdir(mypath) != 0) {
+#else
             if (mkdir(mypath, S_IRWXU) != 0) {
+#endif
                 if (errno != EEXIST)
                     return -1;   
             }
