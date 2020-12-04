f69c10834f8eff7f18bcf292a99d3e9eee5b0003
Michael Hines <michael.hines@yale.edu>
2020-12-04 15:07:16 -0500

    Use atof to interpret units string as double

--- src/modlunit/units.c
+++ src/modlunit/units.c
@@ -1014,6 +1014,82 @@ redef:
 	goto l0;
 }
 
+#if NRN_DYNAMIC_UNITS
+static double modern_getflt() {
+	register int c, i, dp;
+	double d, e;
+	int f;
+	char str[100];
+	char* cp;
+	double d_modern;
+
+	assert(table == dynam[0].table);
+
+	d = 0.;
+	dp = 0;
+	cp = str;
+	do
+		c = get();
+	while(c == ' ' || c == '\t');
+
+l1:
+	if(c >= '0' && c <= '9') {
+		*cp++ = c;
+		d = d*10. + c-'0';
+		if(dp)
+			dp++;
+		c = get();
+		goto l1;
+	}
+	if(c == '.') {
+		*cp++ = c;
+		dp++;
+		c = get();
+		goto l1;
+	}
+	if(dp)
+		dp--;
+	if(c == '+' || c == '-') {
+		*cp++ = 'e';
+		*cp++ = c;
+		f = 0;
+		if(c == '-')
+			f++;
+		i = 0;
+		c = get();
+		while(c >= '0' && c <= '9') {
+			*cp++ = c;
+			i = i*10 + c-'0';
+			c = get();
+		}
+		if(f)
+			i = -i;
+		dp -= i;
+	}
+	*cp = '\0';
+	d_modern = atof(str);
+	e = 1.;
+	i = dp;
+	if(i < 0)
+		i = -i;
+	while(i--)
+		e *= 10.;
+	if(dp < 0)
+		d *= e; else
+		d /= e;
+	if (d_modern != d) {
+printf("getflt d_modern = %.18g %a  d=%.18g %a\nstr=\"%s\"\n", d_modern, d_modern, d, d, str);
+	}
+	if(c == '|') {
+		d /= modern_getflt();
+		d_modern /= modern_getflt();
+		return d_modern;
+	}
+	peekc = c;
+	return(d_modern);
+}
+#endif /* NRN_DYNAMIC_UNITS */
+
 static double
 getflt()
 {
@@ -1021,6 +1097,11 @@ getflt()
 	double d, e;
 	int f;
 
+#if NRN_DYNAMIC_UNITS
+	if (table == dynam[0].table) {
+		return modern_getflt();
+	}
+#endif /* NRN_DYNAMIC_UNITS */
 	d = 0.;
 	dp = 0;
 	do
