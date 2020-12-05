8ac7f68c585221e6b0fb5338674e37fb8ca86863
Nicolas Cornu <nicolas.cornu@epfl.ch>
2020-12-05 13:23:14 +0100

    Use cmake helper to initialise submodules if not cloned with --recursive (BlueBrain/nmodl#438)
    * Added cmake/ExternalProjectHelper.cmake to auto-initialise submodules
    * Check and initialize all submodules before building
    * Don't include fmt in all and set -fPIC
    * Add cli11 in the same include_directories
    
    fixes BlueBrain/nmodl#88
    
    Co-authored-by: Pramod Kumbhar <pramod.s.kumbhar@gmail.com>
    
    
    NMODL Repo SHA: BlueBrain/nmodl@46f691ee9af8fb10d6606945476c66842c5d004d

---
+++ cmake/nmodl/ExternalProjectHelper.cmake
@@ -0,0 +1,38 @@
+find_package(Git QUIET)
+
+set(THIRD_PARTY_DIRECTORY
+    "${NMODL_PROJECT_SOURCE_DIR}/3rdparty"
+    CACHE PATH "The path were all the 3rd party projects can be found")
+
+# initialize submodule with given path
+function(initialize_submodule path)
+  if(NOT ${GIT_FOUND})
+    message(
+      FATAL_ERROR "git not found and ${path} sub-module not cloned (use git clone --recursive)")
+  endif()
+  message(STATUS "Sub-module : missing ${path}: running git submodule update --init --recursive")
+  execute_process(
+    COMMAND
+      git submodule update --init --recursive -- ${path}
+    WORKING_DIRECTORY ${NMODL_PROJECT_SOURCE_DIR})
+endfunction()
+
+# check for external project and initialize submodule if it is missing
+function(add_external_project name)
+  find_path(
+    ${name}_PATH
+    NAMES CMakeLists.txt
+    PATHS "${THIRD_PARTY_DIRECTORY}/${name}")
+  if(NOT EXISTS ${${name}_PATH})
+    initialize_submodule("${THIRD_PARTY_DIRECTORY}/${name}")
+  else()
+    message(STATUS "Sub-project : using ${name} from \"${THIRD_PARTY_DIRECTORY}/${name}\"")
+  endif()
+  if(${ARGC} GREATER 1)
+    if(${ARGV2})
+      add_subdirectory("${THIRD_PARTY_DIRECTORY}/${name}")
+    endif()
+  else()
+    add_subdirectory("${THIRD_PARTY_DIRECTORY}/${name}")
+  endif()
+endfunction()
