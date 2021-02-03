4ef131522650ab2e7a4e3edb9cbaf610bc136444
Olli Lupton <oliver.lupton@epfl.ch>
2021-02-03 09:15:01 +0100

    Fix compilation if git signatures shown by default (BlueBrain/CoreNeuron#488)
    Without this change then the presence of:
      [log]
        showSignature = True
    
    in the local ~/.gitconfig file breaks compilation of CoreNEURON.
    
    CoreNEURON Repo SHA: BlueBrain/CoreNeuron@5a93d9b2b256a12acbaf83085044aba2cc56746f

--- cmake/coreneuron/GitRevision.cmake
+++ cmake/coreneuron/GitRevision.cmake
@@ -14,13 +14,13 @@ find_package(Git)
 if(GIT_FOUND)
   # get last commit sha1
   execute_process(
-    COMMAND ${GIT_EXECUTABLE} log -1 --format=%h
+    COMMAND ${GIT_EXECUTABLE} -c log.showSignature=false log -1 --format=%h
     WORKING_DIRECTORY ${CORENEURON_PROJECT_SOURCE_DIR}
     OUTPUT_VARIABLE GIT_REVISION_SHA1
     ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE)
   # get last commit date
   execute_process(
-    COMMAND ${GIT_EXECUTABLE} show -s --format=%ci
+    COMMAND ${GIT_EXECUTABLE} -c log.showSignature=false show -s --format=%ci
     WORKING_DIRECTORY ${CORENEURON_PROJECT_SOURCE_DIR}
     OUTPUT_VARIABLE GIT_REVISION_DATE
     ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE)
