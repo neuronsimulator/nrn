2035afd7033597ec13f900636e855fdff33e7040
adam newton <adam.newton@yale.edu>
2021-03-16 13:22:56 -0400

    bugfix: Stop rxd.Region keeping sections alive when they go out of scope.

---
+++ test/rxd/test_region_sections.py
@@ -0,0 +1,17 @@
+import pytest
+
+def test_region_sections(neuron_instance):
+    """A test that regions do not keep sections alive"""
+
+    h, rxd, data, save_path = neuron_instance
+    soma = h.Section(name='soma')
+    cyt = rxd.Region(soma.wholetree(), name='cyt')
+    ca = rxd.Species(cyt, name='ca', charge=2)
+    h.finitialize(-65)
+    soma = ca = None
+
+    for sec in h.allsec():
+        assert(False)
+
+    
+    
