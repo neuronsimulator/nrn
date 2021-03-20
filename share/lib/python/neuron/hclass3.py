d0876c74d146036b7ca79961b3a71b436896a4b1
Robin De Schepper <robin.deschepper93gmail.com>
2021-03-20 11:03:13 +0100

    Removed the MetaHocObject metaclass and added `nonlocal_hclass` factory

--- share/lib/python/neuron/hclass3.py
+++ share/lib/python/neuron/hclass3.py
@@ -6,35 +6,42 @@
 # ------------------------------------------------------------------------------
 
 from neuron import h, hoc
-import nrn
+import sys, nrn
 
-#avoid syntax error if compiled by python 2
+def assert_not_hoc_composite(cls):
+    hoc_bases = [b for b in cls.__bases__ if issubclass(b, hoc.HocObject)]
+    if len(hoc_bases) > 1:
+        bases = ", ".join(b.__name__)
+        cname = cls.__name__
+        raise TypeError(f"Composition of {bases} HocObjects not allowed in {cname}")
 
-class MetaHocObject(type):
-  """Provides Exception for Inheritance of multiple HocObject"""
-  def __new__(cls, name, bases, attrs):
-    #print cls, name, bases
-    m = []
-    for b in bases:
-      if issubclass(b, hoc.HocObject):
-        m.append(b.__name__)
-    if (len(m) > 1):
-      raise TypeError('Multiple Inheritance of HocObject in %s' % name
-        + ' through %s not allowed' % ','.join(m))
-      #note that join(b.__name__ for b in m) is not valid for Python 2.3
-
-    return type.__new__(cls, name, bases, attrs)
-
-def hclass(c):
+def hclass(hoc_template):
     """Class factory for subclassing h.anyclass. E.g. class MyList(hclass(h.List)):..."""
-    if c == h.Section :
+    if hoc_template == h.Section:
         return nrn.Section
-    class hc(hoc.HocObject, metaclass=MetaHocObject):
-    #class hc(hoc.HocObject):
+
+    class hc(hoc.HocObject):
+        htype = hoc_template
+
+        def __init_subclass__(cls, **kwargs):
+            assert_not_hoc_composite(cls)
+            super().__init_subclass__(**kwargs)
+
         def __new__(cls, *args, **kwds):
             kwds2 = {'hocbase': cls.htype}
             if 'sec' in kwds:
                 kwds2['sec'] = kwds['sec']
             return hoc.HocObject.__new__(cls, *args, **kwds2)
-    setattr(hc, 'htype', c)
+
     return hc
+
+def nonlocal_hclass(hoc_template, module_name, name=None):
+    module = sys.modules[module_name]
+    if name is None:
+        name = hoc_template.hname()[:-2]
+    local_hclass = hclass(hoc_template)
+    setattr(module, name, local_hclass)
+    local_hclass.__module__ = module.__name__
+    local_hclass.__name__ = name
+    local_hclass.__qualname__ = name
+    return local_hclass
