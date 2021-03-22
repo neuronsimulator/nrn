937f7919ea4095a36c464a3a6252c5b9c6480763
adamjhn <adam.newton@yale.edu>
2021-03-22 09:19:03 -0400

    bugfix: Check species id before deleting atolscale. (#1106)

---
+++ test/rxd/test_species_del.py
@@ -0,0 +1,13 @@
+def test_species_del(neuron_instance):
+    """ Test deleting an uninitialized species does not raise any error or
+         exceptions.
+    """
+
+    h, rxd, data, save_path = neuron_instance
+    soma = h.Section(name="soma")
+    cyt = rxd.Region([soma])
+    c = rxd.Species(cyt)
+    try:
+        c.__del__()
+    except:
+        assert(False)
