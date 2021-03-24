3512221803521ebcef7a99a13e9e2beabeab814a
Robin De Schepper <robin.deschepper93@gmail.com>
2021-03-24 10:23:27 +0100

    propagate `cython` error during `FindCython`.

--- cmake/modules/FindCython.cmake
+++ cmake/modules/FindCython.cmake
@@ -43,6 +43,9 @@ if(NOT CYTHON_EXECUTABLE STREQUAL "CYTHON_EXECUTABLE-NOTFOUND")
   if(RESULT EQUAL 0)
     string(REGEX REPLACE ".* ([0-9]+\\.[0-9]+(\\.[0-9]+)?).*" "\\1" CYTHON_VERSION
                          "${CYTHON_VAR_OUTPUT}")
+  else()
+    message(STATUS "Cython found: ${CYTHON_EXECUTABLE}")
+    message(FATAL "Cython error [${RESULT}]: ${CYTHON_VAR_OUTPUT}.")
   endif()
 endif()
 
