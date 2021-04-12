3d46a776deab5c3a81d431ad8d76f3d722425b77
Robert A McDougal <robert.mcdougal@yale.edu>
2021-04-12 11:59:52 -0400

    instantiate fix, also some style changes

--- test/rxd/3d/test_soma_outlines.py
+++ test/rxd/3d/test_soma_outlines.py
@@ -2,6 +2,7 @@ import os
 import pytest
 from testutils import compare_data, tol
 
+
 @pytest.fixture
 def cell_model(neuron_instance):
     """Provide a Cell class that use the simple.asc Neurolucida morphology file."""
@@ -10,28 +11,32 @@ def cell_model(neuron_instance):
 
     class Cell:
         """Create a model cell based on the simple.asc Neurolucida file.
-    
+
         Args:
             shift (triplet, optional) displacement in  x, y, z of the cell,
                 default (0,0,0).
         """
-        def __init__(self, shift=(0,0,0)):
+
+        def __init__(self, shift=(0, 0, 0)):
             self.cell = h.Import3d_Neurolucida3()
             path = os.path.dirname(os.path.abspath(__file__))
-            self.cell.input(os.path.join(path,"simple.asc"))
+            self.cell.input(os.path.join(path, "simple.asc"))
             i3d = h.Import3d_GUI(self.cell, False)
             i3d.instantiate(self)
             for sec in self.all:
                 sec.nseg = int(sec.L / 40) * 2 + 1
             sx, sy, sz = shift
             for sec in self.soma:
-                pts = [(sec.x3d(i), sec.y3d(i), sec.z3d(i), sec.diam3d(i))
-                        for i in range(sec.n3d())]
+                pts = [
+                    (sec.x3d(i), sec.y3d(i), sec.z3d(i), sec.diam3d(i))
+                    for i in range(sec.n3d())
+                ]
                 sec.pt3dclear()
                 for (x, y, z, diam) in pts:
                     sec.pt3dadd(x + sx, y + sy, z + sz, diam)
 
     yield (h, rxd, data, save_path, Cell)
+
 
 def test_soma_outlines(cell_model):
     """Import a Neurolucida model and use the soma outline for voxelization."""
@@ -39,8 +44,10 @@ def test_soma_outlines(cell_model):
     h, rxd, data, save_path, Cell = cell_model
     c = Cell()
     rxd.set_solve_type(c.all, dimension=3)
-    cyt = rxd.Region(c.all, name='cyt', nrn_region='i', dx=1.0)
-    ca = rxd.Species(cyt, initial=lambda node: 1 if node in c.soma[0] else 0, d=0.5, charge=2)
+    cyt = rxd.Region(c.all, name="cyt", nrn_region="i", dx=1.0)
+    ca = rxd.Species(
+        cyt, initial=lambda node: 1 if node in c.soma[0] else 0, d=0.5, charge=2
+    )
 
     h.finitialize(-65)
     h.continuerun(10)
@@ -51,16 +58,18 @@ def test_soma_outlines(cell_model):
 
 def test_soma_move(cell_model):
     """Import a Neurolucida model and move it."""
-    
+
     h, rxd, data, save_path, Cell = cell_model
-    c = Cell(shift=(20,10,20))
+    c = Cell(shift=(20, 10, 20))
     rxd.set_solve_type(c.all, dimension=3)
-    cyt = rxd.Region(c.all, name='cyt', nrn_region='i', dx=1.0)
-    ca = rxd.Species(cyt, initial=lambda node: 1 if node in c.soma[0] else 0, d=0.5, charge=2)
+    cyt = rxd.Region(c.all, name="cyt", nrn_region="i", dx=1.0)
+    ca = rxd.Species(
+        cyt, initial=lambda node: 1 if node in c.soma[0] else 0, d=0.5, charge=2
+    )
 
     h.finitialize(-65)
     h.continuerun(10)
-    assert(min([nd.x3d for nd in ca.nodes(c.soma[0])]) > 29)
+    assert min([nd.x3d for nd in ca.nodes(c.soma[0])]) > 29
     if not save_path:
         max_err = compare_data(data)
         assert max_err < tol
@@ -71,11 +80,15 @@ def test_multiple_soma(cell_model):
 
     h, rxd, data, save_path, Cell = cell_model
     c = Cell()
-    c2 = Cell(shift=(20,10,20))
+    c2 = Cell(shift=(20, 10, 20))
     rxd.set_solve_type(c.all, dimension=3)
-    cyt = rxd.Region(c.all, name='cyt', nrn_region='i', dx=1.0)
-    ca = rxd.Species(cyt, initial=lambda node: 1 if node in c.soma[0] or node in c2.soma[0] 
-                                  else 0, d=0.5, charge=2)
+    cyt = rxd.Region(c.all, name="cyt", nrn_region="i", dx=1.0)
+    ca = rxd.Species(
+        cyt,
+        initial=lambda node: 1 if node in c.soma[0] or node in c2.soma[0] else 0,
+        d=0.5,
+        charge=2,
+    )
 
     h.finitialize(-65)
     h.continuerun(10)
@@ -87,16 +100,18 @@ def test_multiple_soma(cell_model):
 def test_import_into_HOC_template(cell_model):
     """test that we can import at the top level without error"""
     h, rxd, data, save_path, Cell = cell_model
-    h('''begintemplate HocTemplateTest
+    h(
+        """begintemplate HocTemplateTest
     proc init() {localobj nl
         nl = new Import3d_Neurolucida3()
         nl.input("%s")
         import = new Import3d_GUI(nl, 0)
         import.instantiate(this)
     }
-    endtemplate HocTemplateTest''' % os.path.join(os.path.abspath(__file__), "simple.asc"))
+    endtemplate HocTemplateTest"""
+        % os.path.join(os.path.abspath(__file__), "simple.asc")
+    )
     cell = h.HocTemplateTest()
-
 
 
 def test_toplevel_import(cell_model):
@@ -106,5 +121,4 @@ def test_toplevel_import(cell_model):
     path = os.path.dirname(os.path.abspath(__file__))
     cell.input(os.path.join(path, "simple.asc"))
     i3d = h.Import3d_GUI(cell, False)
-    i3d.instantiate()
-
+    i3d.instantiate(None)
