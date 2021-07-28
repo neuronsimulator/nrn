47763a01baea5604c4a4a4fcd2a29f662507ae17
Fernando Pereira <fernando.pereira@epfl.ch>
2021-07-29 01:08:15 +0200

    Fixes for build script

--- packaging/python/gpu_wheel/build_wheel.sh
+++ packaging/python/gpu_wheel/build_wheel.sh
@@ -1,8 +1,9 @@
 #build_wheel.sh
 
 module load nvhpc
+unset CC CXX  # wheel extensions must not use nv pgi
 
-git clone https://github.com/neuronsimulator/nrn.git --depth=1
+git clone https://github.com/neuronsimulator/nrn.git --depth=1 --branch=epic/gpu_wheel
 
 cd nrn
 
