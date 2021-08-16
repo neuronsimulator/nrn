b955149675e4aaef68deae093aa586cf4ccd3ea4
Olli Lupton <oliver.lupton@epfl.ch>
2021-08-16 13:59:17 +0200

    Suppress spammy warnings in NVHPC 20.7+ (#716)
    Make sure these suppressions do not apply to, for example, CoreNEURON if NMODL is being built as a submodule.

--- cmake/CompilerHelper.cmake
+++ cmake/CompilerHelper.cmake
@@ -18,5 +18,26 @@ if(CMAKE_CXX_COMPILER_ID MATCHES "PGI" OR CMAKE_CXX_COMPILER_ID MATCHES "NVHPC")
   # messages specifically for AST. Disable these verbose warnings for now.
   # TODO : fix these warnings from template modification (#272)
   # ~~~
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --diag_suppress 1,82,111,115,177,186,611,997,1097,1625")
+  if(${CMAKE_CXX_COMPILER_VERSION} VERSION_LESS 20.7)
+    set(NMODL_COMPILER_WARNING_SUPPRESSIONS --diag_suppress=1,82,111,115,177,186,611,997,1097,1625)
+  else()
+    # https://forums.developer.nvidia.com/t/many-all-diagnostic-numbers-increased-by-1-from-previous-values/146268/3
+    # changed the numbering scheme in newer versions. The following list is from a clean start 13
+    # August 2021. It would clearly be nicer to apply these suppressions only to relevant files.
+    # Examples of the suppressed warnings are given below.
+    # ~~~
+    # "ext/spdlog/include/spdlog/fmt/fmt.h", warning #1-D: last line of file ends without a newline
+    # "ext/fmt/include/fmt/format.h", warning #111-D: statement is unreachable
+    # "ext/json/json.hpp", warning #186-D: pointless comparison of unsigned integer with zero
+    # "src/ast/all.hpp", warning #998-D: function "..." is hidden by "..." -- virtual function override intended?
+    # "ext/spdlog/include/spdlog/fmt/bundled/format.h", warning #1098-D: unknown attribute "fallthrough"
+    # "ext/pybind11/include/pybind11/detail/common.h", warning #1626-D: routine is both "inline" and "noinline"
+    # ~~~
+    # The following warnings do not seem to be suppressible with --diag_suppress:
+    # ~~~
+    # "src/codegen/codegen_cuda_visitor.cpp", NVC++-W-0277-Cannot inline function - data type mismatch
+    # "nvc++IkWUbMugiSgNH.s: Warning: stand-alone `data16' prefix
+    # ~~~
+    set(NMODL_COMPILER_WARNING_SUPPRESSIONS --diag_suppress=1,111,186,998,1098,1626)
+  endif()
 endif()
