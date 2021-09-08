aab9135494e5ce35c25b4dac588b3f5ad46b3327
Pramod Kumbhar <pramod.kumbhar@epfl.ch>
2021-09-08 22:24:14 +0200

    Compile CUDA/OpenACC code for CC 80 as well (Ampere & Turing) (BlueBrain/CoreNeuron#633)
    * boost libraries are not rpath'd (?), load module to avoid link errors at runtime
    
    CoreNEURON Repo SHA: BlueBrain/CoreNeuron@b28a00f8e16434c1a221c4166b86c36cdc2397f7

--- tests/jenkins/ringtest.sh
+++ tests/jenkins/ringtest.sh
@@ -7,6 +7,12 @@ set -x
 CORENRN_TYPE="$1"
 export MPI_UNBUFFERED_STDIO=1
 
+# load boost to avoid runtime error
+module load unstable boost
+
 cd $WORKSPACE/build_${CORENRN_TYPE}
 echo "Testing ${CORENRN_TYPE}"
 ctest --output-on-failure -T test --no-compress-output -E cppcheck_test
+
+# unload
+module unload unstable boost
