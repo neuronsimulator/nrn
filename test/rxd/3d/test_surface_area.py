741b62ed89cd090d43e1e14b0afa103015018ef5
adam newton <adam.newton@yale.edu>
2021-09-23 14:50:09 -0400

    bugfix: rxd missing surface area in voxels on grid boundaries.

---
+++ test/rxd/3d/test_surface_area.py
@@ -0,0 +1,31 @@
+import pytest
+import numpy
+from math import pi
+
+
+def test_surface_area(neuron_instance):
+    """Test that surface node all have surface area"""
+
+    h, rxd, data, save_path = neuron_instance
+
+    soma = h.Section(name="soma")
+    soma.L = 1
+    soma.diam = 1
+    soma.nseg = 1
+
+    rxd.set_solve_type(dimension=3)
+    allsecs = soma.wholetree()
+    r = rxd.Region(allsecs, dx=0.1)
+    ca = rxd.Species(r)
+    h.finitialize(-65)
+
+    # true surface area
+    sa = 1.5*pi
+    
+    areas = numpy.array(ca.nodes.surface_area)
+    # surface area 0.9 to 1.1 of true value
+    assert(abs(1-sum(areas)/sa) <= 0.1) 
+
+    # not surface nodes have zero surface area
+    assert(not any(areas[r._surface_nodes_by_seg[0]]==0))
+
