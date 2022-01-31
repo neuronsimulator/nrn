230ccdc11e2df1de4fece9c6fda9837fa2f8c7d7
Olli Lupton <oliver.lupton@epfl.ch>
2022-01-31 13:54:10 +0100

    whitespace

--- src/oc/mech_api.h
+++ src/oc/mech_api.h
@@ -1,6 +1,6 @@
 #pragma once
 /** @brief Header file included in translated mechanism.
- *  
+ *
  *  This ensures that all functions that are tacitly part of the NEURON API
  *  usable from .mod files have definitions that are visible when the translated
  *  versions of those .mod files are compiled.
