3871f82a46b060a020dbb158a3d4324b00f708bb
Olli Lupton <oliver.lupton@epfl.ch>
2022-02-09 15:42:16 +0100

    Prefer ctypes.PyDLL (#1621)
    * Consistently use nrnwrap_Python.h.
    
    Do not duplicate a windows workaround, define PY_SSIZE_T_CLEAN before
    including Python.h as recommended in the Python documentation.
    
    * nrn_dll(): cdll -> pydll.
    
    This means we use ctypes.PyDLL, which does not release the GIL when
    calling a foreign function, instead of ctypes.CDLL, which does release
    the GIL. This means that the Python C API can be used in functions being
    called without explicitly locking the GIL.
    
    * docs: build with Python 3.10.

--- src/nrnpython/nrnpython.h
+++ src/nrnpython/nrnpython.h
@@ -17,10 +17,6 @@
 #if defined(USE_PYTHON)
 #undef _POSIX_C_SOURCE
 #undef _XOPEN_SOURCE
-#if defined(__MINGW32__)
-//at least a problem with g++6.3.0
-#define _hypot hypot
-#endif
 #include <nrnwrap_Python.h>
 
 #endif /*USE_PYTHON*/
