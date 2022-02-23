7731d8977c3965f3512930a55654e40f8b99ee77
Ioannis Magkanaris <iomagkanaris@gmail.com>
2022-02-23 13:55:41 +0100

    Fix pdata calculation (BlueBrain/CoreNeuron#775)
    * Read iontype for every instance because it might be different for different instances
    
    CoreNEURON Repo SHA: BlueBrain/CoreNeuron@43e881eb55f9191dba3c3db797b498bf60ccce96

--- src/coreneuron/mechanism/eion.cpp
+++ src/coreneuron/mechanism/eion.cpp
@@ -233,7 +233,7 @@ ion_style("name_ion", [c_style, e_style, einit, eadvance, cinit])
  and models.
 */
 
-#define iontype ppd[0] /* how _AMBIGUOUS is to be handled */
+#define iontype ppd[_iml] /* how _AMBIGUOUS is to be handled */
 /*the bitmap is
 03	concentration unused, nrnocCONST, DEP, STATE
 04	initialize concentrations
@@ -265,6 +265,7 @@ void nrn_cur_ion(NrnThread* nt, Memb_list* ml, int type) {
     ppd = ml->pdata;
     // clang-format off
     nrn_pragma_acc(parallel loop present(pd[0:_cntml_padded * 5],
+                                         ppd[0:_cntml_actual],
                                          nrn_ion_global_map[0:nrn_ion_global_map_size]
                                                            [0:ion_global_map_member_size])
                                  if (nt->compute_gpu)
@@ -304,7 +305,7 @@ void nrn_init_ion(NrnThread* nt, Memb_list* ml, int type) {
     // needs to be like this.
     // clang-format off
     nrn_pragma_acc(parallel loop present(pd[0:_cntml_padded * 5],
-                                         ppd[0:1],
+                                         ppd[0:_cntml_actual],
                                          nrn_ion_global_map[0:nrn_ion_global_map_size]
                                                            [0:ion_global_map_member_size])
                                  if (nt->compute_gpu))
