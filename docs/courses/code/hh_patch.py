e1808e8dfe072e1426fef5bb341cc4cb2a07b1da
ramcdougal <robert.mcdougal@yale.edu>
2022-03-25 13:21:58 -0400

    python style change to match black

--- docs/courses/code/hh_patch.py
+++ docs/courses/code/hh_patch.py
@@ -1,6 +1,6 @@
 from neuron import h, gui
 
-soma = h.Section(name='soma')
+soma = h.Section(name="soma")
 soma.insert(h.hh)
 soma.L = 5
 soma.diam = 5
@@ -13,7 +13,7 @@ iclamp.amp = 0.3
 g = h.Graph(False)
 g.view(0, -80, 5, 120, 624, 40, 300, 200)
 h.graphList[0].append(g)
-g.addvar('v', soma(0.5)._ref_v)
+g.addvar("v", soma(0.5)._ref_v)
 
 # popup the variable step and run controls
 h.numericalmethodpanel.map()
