a3d34aee8f073d1410089ff3656f105e6f9e3709
Michael Hines <michael.hines@yale.edu>
2022-03-29 11:04:46 -0400

    clang-format when -DNRN_ENABLE_INTERVIEWS=ON

--- src/ivoc/idraw.h
+++ src/ivoc/idraw.h
@@ -12,53 +12,99 @@ class Color;
 class Brush;
 
 class OcIdraw {
-public:
-	static void prologue();
-	static void epilog();
-	static void pict();
-	static void pict(const Transformer&);
-	static void end();
-	static void text(Canvas*, const char*, const Transformer&,
-		const Font* f=NULL, const Color* c=NULL);
-	static void mline(Canvas*, int count,  const Coord* x, const Coord* y,
-		const Color* c=NULL, const Brush* b=NULL);
-	static void polygon(Canvas*, int count,  const Coord* x, const Coord* y,
-		const Color* c=NULL, const Brush* b=NULL, bool fill = false);
-	static void rect(Canvas*, Coord x1, Coord y1, Coord x2, Coord y2,
-		const Color* c=NULL, const Brush* b=NULL, bool fill = false);
-	static void line(Canvas*, Coord x1, Coord y1, Coord x2, Coord y2,
-		const Color* c=NULL, const Brush* b=NULL);
-	static void ellipse(Canvas*, Coord x1, Coord y1,
-		Coord width, Coord height,
-		const Color* c=NULL, const Brush* b= NULL, bool fill = false);
+  public:
+    static void prologue();
+    static void epilog();
+    static void pict();
+    static void pict(const Transformer&);
+    static void end();
+    static void text(Canvas*,
+                     const char*,
+                     const Transformer&,
+                     const Font* f = NULL,
+                     const Color* c = NULL);
+    static void mline(Canvas*,
+                      int count,
+                      const Coord* x,
+                      const Coord* y,
+                      const Color* c = NULL,
+                      const Brush* b = NULL);
+    static void polygon(Canvas*,
+                        int count,
+                        const Coord* x,
+                        const Coord* y,
+                        const Color* c = NULL,
+                        const Brush* b = NULL,
+                        bool fill = false);
+    static void rect(Canvas*,
+                     Coord x1,
+                     Coord y1,
+                     Coord x2,
+                     Coord y2,
+                     const Color* c = NULL,
+                     const Brush* b = NULL,
+                     bool fill = false);
+    static void line(Canvas*,
+                     Coord x1,
+                     Coord y1,
+                     Coord x2,
+                     Coord y2,
+                     const Color* c = NULL,
+                     const Brush* b = NULL);
+    static void ellipse(Canvas*,
+                        Coord x1,
+                        Coord y1,
+                        Coord width,
+                        Coord height,
+                        const Color* c = NULL,
+                        const Brush* b = NULL,
+                        bool fill = false);
 
-	static void new_path();
-	static void move_to(Coord x, Coord y);
-	static void line_to(Coord x, Coord y);
-	static void curve_to(Coord x, Coord y, Coord x1, Coord y1, Coord x2, Coord y2);
-	static void close_path();
-	static void stroke(Canvas*, const Color*, const Brush*);
-	static void fill(Canvas*, const Color*);
-	static void bspl(Canvas*, int count,  const Coord* x, const Coord* y,
-		const Color* c=NULL, const Brush* b=NULL);
-	static void cbspl(Canvas*, int count,  const Coord* x, const Coord* y,
-		const Color* c=NULL, const Brush* b=NULL, bool fill = false);
-public:
-	static ostream* idraw_stream;
-private:
-	static void rcurve(int level, Coord x, Coord y, Coord x1, Coord y1, Coord x2, Coord y2);
-	static void poly(int count,  const Coord* x, const Coord* y,
-		const Color* c=NULL, const Brush* b=NULL, bool fill = false);
-	static void add(Coord, Coord);
-	static void brush(const Brush*);
-	static void ifill(const Color*, bool);
-	static bool closed_;
-	static bool curved_;
-	static Coord *xpath_, *ypath_;
-	static int ipath_, capacity_;
+    static void new_path();
+    static void move_to(Coord x, Coord y);
+    static void line_to(Coord x, Coord y);
+    static void curve_to(Coord x, Coord y, Coord x1, Coord y1, Coord x2, Coord y2);
+    static void close_path();
+    static void stroke(Canvas*, const Color*, const Brush*);
+    static void fill(Canvas*, const Color*);
+    static void bspl(Canvas*,
+                     int count,
+                     const Coord* x,
+                     const Coord* y,
+                     const Color* c = NULL,
+                     const Brush* b = NULL);
+    static void cbspl(Canvas*,
+                      int count,
+                      const Coord* x,
+                      const Coord* y,
+                      const Color* c = NULL,
+                      const Brush* b = NULL,
+                      bool fill = false);
+
+  public:
+    static ostream* idraw_stream;
+
+  private:
+    static void rcurve(int level, Coord x, Coord y, Coord x1, Coord y1, Coord x2, Coord y2);
+    static void poly(int count,
+                     const Coord* x,
+                     const Coord* y,
+                     const Color* c = NULL,
+                     const Brush* b = NULL,
+                     bool fill = false);
+    static void add(Coord, Coord);
+    static void brush(const Brush*);
+    static void ifill(const Color*, bool);
+    static bool closed_;
+    static bool curved_;
+    static Coord *xpath_, *ypath_;
+    static int ipath_, capacity_;
 };
-	
-#define IfIdraw(arg)		if (OcIdraw::idraw_stream) {OcIdraw::arg;}
+
+#define IfIdraw(arg)             \
+    if (OcIdraw::idraw_stream) { \
+        OcIdraw::arg;            \
+    }
 
 
 #endif
--- src/ivoc/rect.h
+++ src/ivoc/rect.h
@@ -13,21 +13,27 @@ class Brush;
 class Color;
 
 class Appear: public Glyph {
-protected:
-	Appear(const Color* color=NULL, const Brush* brush=NULL);
-public:
-	virtual ~Appear();
-	const Color* color() const {return color_;}
-	void color(const Color*);
-	const Brush* brush() const {return brush_;}
-	void brush(const Brush*);
-	static const Color* default_color();
-	static const Brush* default_brush();
-private:
-	const Color* color_;
-	const Brush* brush_;
-	static const Color* dc_;
-	static const Brush* db_;
+  protected:
+    Appear(const Color* color = NULL, const Brush* brush = NULL);
+
+  public:
+    virtual ~Appear();
+    const Color* color() const {
+        return color_;
+    }
+    void color(const Color*);
+    const Brush* brush() const {
+        return brush_;
+    }
+    void brush(const Brush*);
+    static const Color* default_color();
+    static const Brush* default_brush();
+
+  private:
+    const Color* color_;
+    const Brush* brush_;
+    static const Color* dc_;
+    static const Brush* db_;
 };
 
 #if defined(__MWERKS__)
@@ -35,21 +41,26 @@ private:
 #define Rect ivoc_Rect
 #endif
 
-class Rect : public Appear {
-public:
-	Rect(Coord left, Coord bottom, Coord width, Coord height,
-		const Color* c = NULL, const Brush* b = NULL);
-	virtual void request(Requisition&) const;
-	virtual void allocate(Canvas*, const Allocation&, Extension&);
-	virtual void draw(Canvas*, const Allocation&) const;
-	virtual void pick(Canvas*, const Allocation&, int depth, Hit&);
+class Rect: public Appear {
+  public:
+    Rect(Coord left,
+         Coord bottom,
+         Coord width,
+         Coord height,
+         const Color* c = NULL,
+         const Brush* b = NULL);
+    virtual void request(Requisition&) const;
+    virtual void allocate(Canvas*, const Allocation&, Extension&);
+    virtual void draw(Canvas*, const Allocation&) const;
+    virtual void pick(Canvas*, const Allocation&, int depth, Hit&);
 
-	Coord left() const, right() const, top() const, bottom() const;
-	Coord width() const, height() const;
-	void left(Coord), bottom(Coord);
-	void width(Coord), height(Coord);
-private:
-	Coord l_, b_, w_, h_;
+    Coord left() const, right() const, top() const, bottom() const;
+    Coord width() const, height() const;
+    void left(Coord), bottom(Coord);
+    void width(Coord), height(Coord);
+
+  private:
+    Coord l_, b_, w_, h_;
 };
 
 #if defined(__MWERKS__)
@@ -57,46 +68,53 @@ private:
 #define Line ivoc_Line
 #endif
 
-class Line : public Appear  {
-public:
-	Line(Coord dx, Coord dy, const Color* color=NULL, const Brush* brush=NULL);
-	Line(Coord dx, Coord dy, float x_align, float y_align,
-		 const Color* color=NULL, const Brush* brush=NULL);
-	virtual ~Line();
-	
-	virtual void request(Requisition&) const;
-	virtual void allocate(Canvas*, const Allocation&, Extension&);
-	virtual void draw(Canvas*, const Allocation&) const;
-	virtual void pick(Canvas*, const Allocation&, int depth, Hit&);
-private:
-	Coord dx_, dy_;
-	float x_, y_;
+class Line: public Appear {
+  public:
+    Line(Coord dx, Coord dy, const Color* color = NULL, const Brush* brush = NULL);
+    Line(Coord dx,
+         Coord dy,
+         float x_align,
+         float y_align,
+         const Color* color = NULL,
+         const Brush* brush = NULL);
+    virtual ~Line();
+
+    virtual void request(Requisition&) const;
+    virtual void allocate(Canvas*, const Allocation&, Extension&);
+    virtual void draw(Canvas*, const Allocation&) const;
+    virtual void pick(Canvas*, const Allocation&, int depth, Hit&);
+
+  private:
+    Coord dx_, dy_;
+    float x_, y_;
 };
 
-class Circle : public Appear {
-public:
-	Circle(float radius, bool filled=false, const Color* color=NULL, const Brush* brush=NULL);
-	virtual ~Circle();
-	
-	virtual void request(Requisition&) const;
-	virtual void allocate(Canvas*, const Allocation&, Extension&);
-	virtual void draw(Canvas*, const Allocation&) const;
-private:
-	float radius_;
-	bool filled_;
+class Circle: public Appear {
+  public:
+    Circle(float radius, bool filled = false, const Color* color = NULL, const Brush* brush = NULL);
+    virtual ~Circle();
+
+    virtual void request(Requisition&) const;
+    virtual void allocate(Canvas*, const Allocation&, Extension&);
+    virtual void draw(Canvas*, const Allocation&) const;
+
+  private:
+    float radius_;
+    bool filled_;
 };
 
-class Triangle : public Appear {
-public:
-	Triangle(float side, bool filled=false, const Color* color=NULL, const Brush* brush=NULL);
-	virtual ~Triangle();
-	
-	virtual void request(Requisition&) const;
-	virtual void allocate(Canvas*, const Allocation&, Extension&);
-	virtual void draw(Canvas*, const Allocation&) const;
-private:
-	float side_;
-	bool filled_;
+class Triangle: public Appear {
+  public:
+    Triangle(float side, bool filled = false, const Color* color = NULL, const Brush* brush = NULL);
+    virtual ~Triangle();
+
+    virtual void request(Requisition&) const;
+    virtual void allocate(Canvas*, const Allocation&, Extension&);
+    virtual void draw(Canvas*, const Allocation&) const;
+
+  private:
+    float side_;
+    bool filled_;
 };
 
 #if defined(__MWERKS__)
@@ -104,30 +122,55 @@ private:
 #define Rectangle ivoc_Rectangle
 #endif
 
-class Rectangle : public Appear {
-public:
-	Rectangle(float height, float width, bool filled=false, const Color* color=NULL, const Brush* brush=NULL);
-	virtual ~Rectangle();
-	
-	virtual void request(Requisition&) const;
-	virtual void allocate(Canvas*, const Allocation&, Extension&);
-	virtual void draw(Canvas*, const Allocation&) const;
-      private:
-        float height_;
-	float width_;
-	bool filled_;
+class Rectangle: public Appear {
+  public:
+    Rectangle(float height,
+              float width,
+              bool filled = false,
+              const Color* color = NULL,
+              const Brush* brush = NULL);
+    virtual ~Rectangle();
+
+    virtual void request(Requisition&) const;
+    virtual void allocate(Canvas*, const Allocation&, Extension&);
+    virtual void draw(Canvas*, const Allocation&) const;
+
+  private:
+    float height_;
+    float width_;
+    bool filled_;
 };
 
-inline Coord Rect::left() const { return l_; }
-inline Coord Rect::right() const { return l_ + w_; }
-inline Coord Rect::bottom() const { return b_; }
-inline Coord Rect::top() const { return b_ + h_; }
-inline Coord Rect::width() const { return w_; }
-inline Coord Rect::height() const { return h_; }
+inline Coord Rect::left() const {
+    return l_;
+}
+inline Coord Rect::right() const {
+    return l_ + w_;
+}
+inline Coord Rect::bottom() const {
+    return b_;
+}
+inline Coord Rect::top() const {
+    return b_ + h_;
+}
+inline Coord Rect::width() const {
+    return w_;
+}
+inline Coord Rect::height() const {
+    return h_;
+}
 
-inline void Rect::left(Coord x) { l_ = x; }
-inline void Rect::bottom(Coord x) { b_ = x;}
-inline void Rect::width(Coord x) { w_ = (x > 0) ?  x : 1.;}
-inline void Rect::height(Coord x) { h_ = (x > 0) ? x : 1.;}
+inline void Rect::left(Coord x) {
+    l_ = x;
+}
+inline void Rect::bottom(Coord x) {
+    b_ = x;
+}
+inline void Rect::width(Coord x) {
+    w_ = (x > 0) ? x : 1.;
+}
+inline void Rect::height(Coord x) {
+    h_ = (x > 0) ? x : 1.;
+}
 
 #endif
