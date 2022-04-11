9f78b39cf2093bc6aeff5157c725a0fb8383ede2
Olli Lupton <oliver.lupton@epfl.ch>
2022-04-11 17:13:50 +0200

    Add *NRN_VERSION_* macros, new version 8.2.0 (BlueBrain/CoreNeuron#796)
    * CoreNEURON version bumped to 8.2.0 to match NEURON; from
      now on CoreNEURON versions will track NEURON ones.
    - Checks are added at the CMake level to make sure these do not
      drift out of sync.
    
    * Add *NRN_VERSION_* macros and variables.
    - Based on CoreNEURON version when standalone, NEURON version
      when built as a submodule.
    - These are implicitly available in all translated mechanisms.
    
    CoreNEURON Repo SHA: BlueBrain/CoreNeuron@0b3b9e943c214e4803648222582e7dffbce13be6

--- src/coreneuron/nrnconf.h
+++ src/coreneuron/nrnconf.h
@@ -1,13 +1,13 @@
 /*
 # =============================================================================
-# Copyright (c) 2016 - 2021 Blue Brain Project/EPFL
+# Copyright (c) 2016 - 2022 Blue Brain Project/EPFL
 #
 # See top-level LICENSE file for details.
 # =============================================================================.
 */
-
 #pragma once
 
+#include "coreneuron/config/version_macros.hpp"
 #include "coreneuron/utils/offload.hpp"
 
 #include <cstdio>
