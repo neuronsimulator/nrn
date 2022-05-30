54cb7040bb50c4d36c1d41ce84735fc1fe4e87d7
Olli Lupton <oliver.lupton@epfl.ch>
2022-05-30 16:28:14 +0200

    Ban sphinx 5.0.0 (BlueBrain/nmodl#877)
    This is not supported in other documentation dependencies.
    
    NMODL Repo SHA: BlueBrain/nmodl@5675849012843499fd08d43eab818a45284d16ac

--- setup.py
+++ setup.py
@@ -138,7 +138,7 @@ setup(
         "nbconvert",
         "nbsphinx>=0.3.2",
         "pytest>=3.7.2",
-        "sphinx",
+        "sphinx<5", # myst_parser requires <5 and pip falls over
         "sphinx-rtd-theme",
     ]
     + install_requirements,
