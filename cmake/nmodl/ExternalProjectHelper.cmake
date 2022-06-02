f481b1e5f005de4bb8e4872d7aff09d64c316234
Olli Lupton <oliver.lupton@epfl.ch>
2022-06-02 15:42:25 +0200

    Update GitHub Actions CI, minor CMake fixes. (BlueBrain/nmodl#879)
    - Only pin major versions of actions, update some too.
    - Drop sympy version constraints.
    - Coverage from a Debug, not Release, build.
    - Prefer ubuntu-20.04 to ubuntu-18.04, add ubuntu-22.04 too.
    - Drop add_external_project helper that was only used to
      bootstrap hpc-coding-conventions.
    - Do not add a CLI11 submodule if the relevant targets already exist,
      this fixes some combinations of NMODL-as-a-submodule build.
    - Fix NMODL_3RDPARTY_DIR to avoid the absolute path being prepended twice.
    
    NMODL Repo SHA: BlueBrain/nmodl@f3a9bd2ca835af2db81ff0b2b12462e30b7291ae

--- cmake/nmodl/ExternalProjectHelper.cmake
+++ cmake/nmodl/ExternalProjectHelper.cmake
@@ -10,16 +10,3 @@ function(initialize_submodule path)
   execute_process(COMMAND git submodule update --init -- ${path}
                   WORKING_DIRECTORY ${NMODL_PROJECT_SOURCE_DIR})
 endfunction()
-
-# check for external project and initialize submodule if it is missing
-function(add_external_project name)
-  find_path(
-    ${name}_PATH
-    NAMES CMakeLists.txt
-    PATHS "${THIRD_PARTY_DIRECTORY}/${name}")
-  if(NOT EXISTS ${${name}_PATH})
-    initialize_submodule("${THIRD_PARTY_DIRECTORY}/${name}")
-  else()
-    message(STATUS "Sub-project : using ${name} from \"${THIRD_PARTY_DIRECTORY}/${name}\"")
-  endif()
-endfunction()
