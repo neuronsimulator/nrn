bbe2a6685e20f90bff651245cb51c1eb14bf8378
Olli Lupton <oliver.lupton@epfl.ch>
2022-06-08 22:20:41 +0200

    cmake: reorganise sanitizer stuff
    - nrn-enable-sanitizer now respects NRNHOME and refers to copies of the
      suppression files in the build/install directories. It only sets
      LD_PRELOAD if --preload is passed.

--- cmake/MacroHelper.cmake
+++ cmake/MacroHelper.cmake
@@ -239,7 +239,7 @@ macro(nocmodl_mod_to_c modfile_basename)
     OUTPUT ${modfile_basename}.c
     COMMAND
       ${CMAKE_COMMAND} -E env "MODLUNIT=${PROJECT_BINARY_DIR}/share/nrn/lib/nrnunits.lib"
-      ${NRN_SANITIZER_ENABLE_ENVIRONMENT} ${PROJECT_BINARY_DIR}/bin/nocmodl ${modfile_basename}.mod
+      ${NRN_NOCMODL_SANITIZER_ENVIRONMENT} ${PROJECT_BINARY_DIR}/bin/nocmodl ${modfile_basename}.mod
     COMMAND sed "'s/_reg()/_reg_()/'" ${modfile_basename}.c > ${modfile_basename}.c.tmp
     COMMAND mv ${modfile_basename}.c.tmp ${modfile_basename}.c
     DEPENDS nocmodl ${modfile_basename}.mod
