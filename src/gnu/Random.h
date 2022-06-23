2ea965eb7de7495657999a2e50610c2113180300
Nicolas Cornu <nicolas.cornu@epfl.ch>
2022-06-23 23:51:53 +0200

    Remove useless exposition?

--- src/gnu/Random.h
+++ src/gnu/Random.h
@@ -95,26 +95,6 @@ class Binomial_random123: public Random_random123 {
         : Random_random123(gen)
         , d(t, p) {}
 
-    inline int t() {
-        return d.t();
-    }
-
-    inline int t(int xt) {
-        int tmp = d.t();
-        d.param(std::binomial_distribution<>::param_type(xt, d.p()));
-        return tmp;
-    }
-
-    inline double p() {
-        return d.p();
-    }
-
-    inline double p(int xp) {
-        double tmp = d.p();
-        d.param(std::binomial_distribution<>::param_type(d.t(), xp));
-        return tmp;
-    }
-
     inline double operator()() override {
         return (d(*(generator()->get_random_generator())));
     }
@@ -128,26 +108,6 @@ class DiscreteUniform_random123: public Random_random123 {
     inline DiscreteUniform_random123(double low, double high, RNG_random123* gen)
         : Random_random123(gen)
         , d(low, high) {}
-
-    inline double low() {
-        return d.min();
-    }
-
-    inline double low(double x) {
-        double tmp = d.min();
-        d.param(std::uniform_int_distribution<>::param_type(x, d.max()));
-        return tmp;
-    }
-
-    inline double high() {
-        return d.max();
-    }
-
-    inline double high(double x) {
-        double tmp = d.max();
-        d.param(std::uniform_int_distribution<>::param_type(d.min(), x));
-        return tmp;
-    }
 
     inline double operator()() override {
         return (d(*(generator()->get_random_generator())));
@@ -177,30 +137,6 @@ class Erlang_random123: public Random_random123 {
         d = std::gamma_distribution<>(a, b);
     }
 
-    inline double mean() {
-        return pMean;
-    }
-
-    inline double mean(double x) {
-        double tmp = pMean;
-        pMean = x;
-        setState();
-        d.param(std::gamma_distribution<>::param_type(a, b));
-        return tmp;
-    };
-
-    inline double variance() {
-        return pVariance;
-    }
-
-    inline double variance(double x) {
-        double tmp = pVariance;
-        pVariance = x;
-        setState();
-        d.param(std::gamma_distribution<>::param_type(a, b));
-        return tmp;
-    }
-
     inline double operator()() override {
         return (d(*(generator()->get_random_generator())));
     }
@@ -215,16 +151,6 @@ class Geometric_random123: public Random_random123 {
         : Random_random123(gen)
         , d(mean) {}
 
-    inline double mean() {
-        return d.p();
-    }
-
-    inline double mean(double x) {
-        double tmp = d.p();
-        d.param(std::geometric_distribution<>::param_type(x));
-        return tmp;
-    }
-
     double operator()() override {
         return (1. + d(*(generator()->get_random_generator())));
     }
@@ -233,40 +159,11 @@ class Geometric_random123: public Random_random123 {
 class Normal_random123: public Random_random123 {
   protected:
     std::normal_distribution<> d;
-    double pMean;
-    double pVariance;
-    double pStdDev;
 
   public:
     inline Normal_random123(double mean, double variance, RNG_random123* gen)
         : Random_random123(gen)
-        , d(mean, std::sqrt(variance))
-        , pMean(mean)
-        , pVariance(variance)
-        , pStdDev(std::sqrt(variance)) {}
-
-    inline double mean() {
-        return d.mean();
-    }
-
-    inline double mean(double x) {
-        pMean = x;
-        double t = d.mean();
-        d.param(std::normal_distribution<>::param_type(x, d.stddev()));
-        return t;
-    }
-
-    inline double variance() {
-        return d.stddev() * d.stddev();
-    }
-
-    inline double variance(double x) {
-        pStdDev = std::sqrt(x);
-        pVariance = x;
-        double t = d.stddev() * d.stddev();
-        d.param(std::normal_distribution<>::param_type(d.mean(), pStdDev));
-        return t;
-    }
+        , d(mean, std::sqrt(variance)) {}
 
     inline double operator()() override {
         return (d(*(generator()->get_random_generator())));
@@ -274,49 +171,11 @@ class Normal_random123: public Random_random123 {
 };
 
 class LogNormal_random123: public Normal_random123 {
-  protected:
-    double logMean;
-    double logVariance;
-    inline void setState() {
-        double m2 = logMean * logMean;
-        pMean = log(m2 / sqrt(logVariance + m2));
-        // from ch@heike.informatik.uni-dortmund.de:
-        // (was   pVariance = log((sqrt(logVariance + m2)/m2 )); )
-        pStdDev = sqrt(log((logVariance + m2) / m2));
-    }
-
   public:
     inline LogNormal_random123(double mean, double variance, RNG_random123* gen)
         : Normal_random123(std::log(mean * mean / std::sqrt(variance + (mean * mean))),
                            log((variance + (mean * mean)) / (mean * mean)),
                            gen) {
-        logMean = mean;
-        logVariance = variance;
-        setState();
-    }
-
-    inline double mean() {
-        return logMean;
-    }
-
-    inline double mean(double x) {
-        double t = logMean;
-        logMean = x;
-        setState();
-        Normal_random123::mean(pMean);
-        return t;
-    }
-
-    inline double variance() {
-        return logVariance;
-    }
-
-    inline double variance(double x) {
-        double t = logVariance;
-        logVariance = x;
-        setState();
-        Normal_random123::variance(std::sqrt(pStdDev));
-        return t;
     }
 
     double operator()() override {
@@ -326,27 +185,12 @@ class LogNormal_random123: public Normal_random123 {
 
 class NegativeExpntl_random123: public Random_random123 {
   protected:
-    double pMean;
-    double lambda;
     std::exponential_distribution<> d;
 
   public:
     inline NegativeExpntl_random123(double xmean, RNG_random123* gen)
         : Random_random123(gen)
-        , pMean(xmean)
-        , lambda(1 / pMean)
-        , d(lambda) {}
-
-    inline double mean() {
-        return pMean;
-    }
-    inline double mean(double x) {
-        double t = pMean;
-        pMean = x;
-        lambda = 1 / pMean;
-        d.param(std::exponential_distribution<>::param_type(lambda));
-        return t;
-    }
+        , d(1 / xmean) {}
 
     inline double operator()() override {
         return (d(*(generator()->get_random_generator())));
@@ -362,16 +206,6 @@ class Poisson_random123: public Random_random123 {
         : Random_random123(gen)
         , d(mean) {}
 
-    inline double mean() {
-        return d.mean();
-    }
-
-    inline double mean(double x) {
-        double t = d.mean();
-        d.param(std::poisson_distribution<>::param_type(x));
-        return t;
-    }
-
     inline double operator()() override {
         return (d(*(generator()->get_random_generator())));
     }
@@ -386,26 +220,6 @@ class Uniform_random123: public Random_random123 {
         : Random_random123(gen)
         , d(low, high) {}
 
-    inline double low() {
-        return d.min();
-    }
-
-    inline double low(double x) {
-        double tmp = d.min();
-        d.param(std::uniform_real_distribution<>::param_type(x, d.max()));
-        return tmp;
-    }
-
-    inline double high() {
-        return d.max();
-    }
-
-    inline double high(double x) {
-        double tmp = d.max();
-        d.param(std::uniform_real_distribution<>::param_type(d.min(), x));
-        return tmp;
-    }
-
     inline double operator()() override {
         return (d(*(generator()->get_random_generator())));
     }
@@ -419,26 +233,6 @@ class Weibull_random123: public Random_random123 {
     inline Weibull_random123(double alpha, double beta, RNG_random123* gen)
         : Random_random123(gen)
         , d(alpha, beta) {}
-
-    inline double alpha() {
-        return d.a();
-    }
-
-    inline double alpha(double x) {
-        double tmp = d.a();
-        d.param(std::weibull_distribution<>::param_type(x, d.b()));
-        return tmp;
-    }
-
-    inline double beta() {
-        return d.b();
-    }
-
-    inline double beta(double x) {
-        double tmp = d.b();
-        d.param(std::weibull_distribution<>::param_type(d.a(), x));
-        return tmp;
-    }
 
     inline double operator()() override {
         return (d(*(generator()->get_random_generator())));
