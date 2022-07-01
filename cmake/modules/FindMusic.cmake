aad61ef275c55a79ee1f41c2115233e065604675
Omar Awile <omar.awile@epfl.ch>
2022-07-01 17:08:39 +0200

    Fix cmake formatting

--- cmake/modules/FindMusic.cmake
+++ cmake/modules/FindMusic.cmake
@@ -6,7 +6,5 @@ find_path(MUSIC_INCDIR music.hh PATH_SUFFIXES include)
 find_path(MUSIC_LIBDIR libmusic.a PATH_SUFFIXES lib)
 find_library(MUSIC_LIBRARY libmusic.a PATHS ${MUSIC_LIBDIR})
 
-find_package_handle_standard_args(MUSIC "Failed to find MUSIC package"
-  MUSIC_INCDIR
-  MUSIC_LIBDIR
-  MUSIC_LIBRARY)
+find_package_handle_standard_args(MUSIC "Failed to find MUSIC package" MUSIC_INCDIR MUSIC_LIBDIR
+                                  MUSIC_LIBRARY)
