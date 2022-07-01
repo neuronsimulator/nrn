5b861ea9a3dacfffa03704a6c2c9cd3739b40bdd
Alexandru Săvulescu <alexandru.savulescu@epfl.ch>
2022-07-01 21:18:59 +0200

    macOS arm64 wheels: delocate workaround (#1897)
    * fix MPI testing via `DYLD_LIBRARY_PATH`

--- packaging/python/build_wheels.bash
+++ packaging/python/build_wheels.bash
@@ -161,6 +161,9 @@ build_wheel_osx() {
         echo " - Python installation is universal2 and we are on arm64, setting _PYTHON_HOST_PLATFORM to: ${_PYTHON_HOST_PLATFORM}"
         export ARCHFLAGS="-arch arm64"
         echo " - Setting ARCHFLAGS to: ${ARCHFLAGS}"
+        # This is a shortcut to have a successful delocate-wheel. See:
+        # https://github.com/matthew-brett/delocate/issues/153
+        python -c "import os,delocate; print(os.path.join(os.path.dirname(delocate.__file__), 'tools.py'));quit()"  | xargs -I{} sed -i."" "s/first, /input.pop('i386',None); first, /g" {}
       else
         export _PYTHON_HOST_PLATFORM="${py_platform/universal2/x86_64}"
         echo " - Python installation is universal2 and we are on x84_64, setting _PYTHON_HOST_PLATFORM to: ${_PYTHON_HOST_PLATFORM}"
