480d5193fcd8617f8a0da736de4885a961e05058
Olli Lupton <oliver.lupton@epfl.ch>
2022-07-21 14:40:17 +0100

    apply permutation to node voltages -- ringtest passes

--- src/nrniv/nrncore_write/utils/nrncore_utils.cpp
+++ src/nrniv/nrncore_write/utils/nrncore_utils.cpp
@@ -148,7 +148,7 @@ extern "C" int nrn_dblpntr2nrncore(double* pd, NrnThread& nt, int& type, int& in
     //     index = pd - nt._actual_v;
     // } else
     if (nt._nrn_fast_imem && pd >= nt._nrn_fast_imem->_nrn_sav_rhs &&
-               pd < (nt._nrn_fast_imem->_nrn_sav_rhs + nnode)) {
+        pd < (nt._nrn_fast_imem->_nrn_sav_rhs + nnode)) {
         type = i_membrane_;  // signifies an index into i_membrane_ array portion of _data
         index = pd - nt._nrn_fast_imem->_nrn_sav_rhs;
     } else {
