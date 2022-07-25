d64e2eb6d935fd0f15bb7a88296ae642bd581520
Olli Lupton <oliver.lupton@epfl.ch>
2022-07-25 11:58:19 +0100

    range-v3 as ExternalProject
    * min cmake 3.16.3

---
+++ cmake/modules/FindRange-v3.cmake
@@ -0,0 +1,21 @@
+include(ExternalProject)
+set(RANGE_V3_PREFIX "${THIRD_PARTY_DIRECTORY}/range-v3")
+set(RANGE_V3_INCLUDE_DIR "${RANGE_V3_PREFIX}/include")
+make_directory(${RANGE_V3_INCLUDE_DIR})
+
+ExternalProject_Add(
+  range-v3-external
+  PREFIX "${RANGE_V3_PREFIX}"
+  URL https://github.com/ericniebler/range-v3/archive/refs/tags/0.12.0.tar.gz
+  CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
+             -DCMAKE_BUILD_TYPE="Release"
+             -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
+             -DRANGES_CXX_STD=${CMAKE_CXX_STANDARD}
+             -DRANGE_V3_DOCS=OFF
+             -DRANGE_V3_TESTS=OFF
+             -DRANGE_V3_EXAMPLES=OFF)
+add_library(range-v3 INTERFACE IMPORTED)
+set_target_properties(
+  range-v3 PROPERTIES INTERFACE_SYSTEM_INCLUDE_DIRECTORIES ${RANGE_V3_INCLUDE_DIR}
+                      INTERFACE_INCLUDE_DIRECTORIES ${RANGE_V3_INCLUDE_DIR})
+add_dependencies(range-v3 range-v3-external)
