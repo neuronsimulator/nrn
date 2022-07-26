256787333a795306f98357e247fe4cb348e4668c
Olli Lupton <oliver.lupton@epfl.ch>
2022-07-26 09:53:35 +0200

    HocDataPaths: create on the fly so pointers are (probably) still valid

--- src/ivoc/datapath.cpp
+++ src/ivoc/datapath.cpp
@@ -111,19 +111,19 @@ void HocDataPaths::search() {
     }
 }
 
-String* HocDataPaths::retrieve(double* pd) {
+String* HocDataPaths::retrieve(double* pd) const {
     assert(impl_->pathstyle_ != 2);
     //	printf("HocDataPaths::retrieve\n");
-    const auto& it = impl_->table_.find((void*) pd);
+    auto const it = impl_->table_.find(pd);
     if (it != impl_->table_.end()) {
         return it->second->path;
     }
     return nullptr;
 }
 
-Symbol* HocDataPaths::retrieve_sym(double* pd) {
+Symbol* HocDataPaths::retrieve_sym(double* pd) const {
     //	printf("HocDataPaths::retrieve\n");
-    const auto& it = impl_->table_.find((void*) pd);
+    auto const it = impl_->table_.find(pd);
     if (it != impl_->table_.end()) {
         return it->second->sym;
     }
@@ -140,9 +140,9 @@ void HocDataPaths::append(char** pd) {
     }
 }
 
-String* HocDataPaths::retrieve(char** pd) {
+String* HocDataPaths::retrieve(char** pd) const {
     //	printf("HocDataPaths::retrieve\n");
-    const auto& it = impl_->table_.find((void*) pd);
+    auto const it = impl_->table_.find(pd);
     if (it != impl_->table_.end()) {
         return it->second->path;
     }
@@ -424,7 +424,6 @@ void HocDataPathImpl::search(Node* nd, double x) {
         sprintf(buf, "v(%g)", x);
         // the conversion below yields a pointer that is potentially invalidated
         // by almost any Node operation
-        assert(false);
         found(static_cast<double*>(nd->v_handle()), buf, sym_v);
     }
 
