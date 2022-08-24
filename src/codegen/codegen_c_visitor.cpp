d996a004b717af54d83996d36619698d613bd179
Nicolas Cornu <nicolas.cornu@epfl.ch>
2022-08-24 09:42:50 +0200

    Not prepend argument with t+ in net_move (#915)
    in mod2c call from net_send are prepend with t+ but not net_move

--- src/codegen/codegen_c_visitor.cpp
+++ src/codegen/codegen_c_visitor.cpp
@@ -3827,7 +3827,7 @@ void CodegenCVisitor::print_net_move_call(const FunctionCall& node) {
     // artificial cells don't use spike buffering
     // clang-format off
     if (info.artificial_cell) {
-        printer->add_text(fmt::format("artcell_net_move(&{}, {}, nt->_t+", tqitem, pnt));
+        printer->add_text(fmt::format("artcell_net_move(&{}, {}, ", tqitem, pnt));
         print_vector_elements(arguments, ", ");
         printer->add_text(")");
     } else {
