62b5d9068d3587167abf53e855041b9aa0b54451
Olli Lupton <oliver.lupton@epfl.ch>
2022-08-30 10:44:52 +0200

    use HOC stack data handle support

--- src/nrncvode/netcvode.cpp
+++ src/nrncvode/netcvode.cpp
@@ -823,11 +823,7 @@ static void steer_val(void* v) {
         static double dummy = 0.;
         d->chksrc();
         if (d->src_->thvar_) {
-            // Danger! This is returning the current address of the variable
-            // referred to by thvar_, but this may be invalidated by future
-            // permutations and modifications of the model -- even allocating or
-            // deleting a new Node object might invalidate it.
-            hoc_pushpx(static_cast<double*>(d->src_->thvar_));
+            hoc_push(d->src_->thvar_);
         } else {
             dummy = 0.;
             hoc_pushpx(&dummy);
