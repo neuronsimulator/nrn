3f2de4c4a2a56aecd66d1792e7f527425b794a24
Olli Lupton <oliver.lupton@epfl.ch>
2022-08-31 12:20:54 +0200

    more tests

--- test/unit_tests/container/node.cpp
+++ test/unit_tests/container/node.cpp
@@ -3,23 +3,47 @@
 
 #include <catch2/catch.hpp>
 
+#include <iostream>
 #include <optional>
 #include <sstream>
 #include <vector>
 
+using namespace neuron::container;
+
 TEST_CASE("data_handle<double>", "[Neuron][data_structures][data_handle]") {
     GIVEN("A null handle") {
-        neuron::container::data_handle<double> handle{};
+        // Generate a null handle both directly and via round trip to generic_data_handle
+        auto const handle =
+            GENERATE(data_handle<double>{},
+                     static_cast<data_handle<double>>(generic_data_handle{data_handle<double>{}}));
         THEN("Check it is really null") {
-            REQUIRE_FALSE(bool{handle});
+            REQUIRE_FALSE(handle);
+        }
+        THEN("Check it compares equal to a different null pointer") {
+            data_handle<double> const other_handle{};
+            REQUIRE(handle == other_handle);
         }
     }
     constexpr double magic_voltage_value = 42.;
     GIVEN("A handle wrapping a raw pointer (compatibility mode)") {
-        double foo{magic_voltage_value};
-        neuron::container::data_handle<double> handle{&foo};
+        static double foo{42};
+        // Generate a compatibility-mode handle both directly and via round trip
+        // to generic_data_handle
+        auto round_trip = GENERATE(false, true);
+        data_handle<double> handle{&foo};
+        if (round_trip) {
+            handle = static_cast<data_handle<double>>(generic_data_handle{handle});
+        }
         THEN("Check it is not null") {
-            REQUIRE(bool{handle});
+            REQUIRE(handle);
+        }
+        THEN("Check it does not compare equal to a null handle") {
+            data_handle<double> null_handle{};
+            REQUIRE(handle != null_handle);
+        }
+        THEN("Check it compares equal to a different handle wrapping the same raw pointer") {
+            data_handle<double> other_handle{&foo};
+            REQUIRE(handle == other_handle);
         }
         THEN("Check it yields the right value") {
             REQUIRE(*handle == foo);
@@ -39,11 +63,25 @@ TEST_CASE("data_handle<double>", "[Neuron][data_structures][data_handle]") {
         }
     }
     GIVEN("A handle referring to an entry in an SOA container") {
-        std::optional<Node> node{std::in_place};
+        std::optional<::Node> node{std::in_place};
         node->set_v(magic_voltage_value);
+        // Generate a handle both directly and via round trip to generic_data_handle
         auto handle = node->v_handle();
+        auto round_trip = GENERATE(false, true);
+        if (round_trip) {
+            handle = static_cast<data_handle<double>>(generic_data_handle{handle});
+        }
         THEN("Check it is not null") {
-            REQUIRE(bool{handle});
+            REQUIRE(handle);
+        }
+        THEN("Check it does not compare equal to a null handle") {
+            data_handle<double> null_handle{};
+            REQUIRE(handle != null_handle);
+        }
+        THEN("Check it does not compare equal to a handle in legacy mode") {
+            double foo{};
+            data_handle<double> foo_handle{&foo};
+            REQUIRE(handle != foo_handle);
         }
         THEN("Check it yields the right value") {
             REQUIRE(*handle == magic_voltage_value);
@@ -57,11 +95,41 @@ TEST_CASE("data_handle<double>", "[Neuron][data_structures][data_handle]") {
             REQUIRE(actual.str() == "data_handle<double>{cont=Node::field::Voltage row=0/1}");
         }
         THEN("Check that deleting the (Node) object it refers to invalidates the handle") {
-            node.reset();
-            REQUIRE_FALSE(bool{handle});
+            node.reset();  // delete the underlying Node object
+            REQUIRE_FALSE(handle);
+            REQUIRE(handle.refers_to_a_modern_data_structure());
             std::ostringstream actual;
             actual << handle;
             REQUIRE(actual.str() == "data_handle<double>{cont=Node::field::Voltage died/0}");
+        }
+    }
+}
+
+TEST_CASE("generic_data_handle", "[Neuron][data_structures][generic_data_handle]") {
+    GIVEN("A null generic handle") {
+        // no default constructor, have to go via a data_handle<T>
+        generic_data_handle null_handle{data_handle<double>{}};
+        THEN("Check it remembered the double type") {
+            REQUIRE(null_handle.type_name() == "double");
+        }
+        THEN("Check it can be converted back to data_handle<double>") {
+            auto const round_trip = static_cast<data_handle<double>>(null_handle);
+            REQUIRE_FALSE(round_trip);
+            REQUIRE(round_trip == data_handle<double>{});
+        }
+        THEN("Check it cannot be converted to data_handle<int>") {
+            REQUIRE_THROWS(static_cast<data_handle<int>>(null_handle));
+        }
+    }
+    GIVEN("A handle wrapping a raw pointer (compatibility mode)") {
+        double foo{};
+        data_handle<double> typed_handle{&foo};
+        generic_data_handle handle{typed_handle};
+        THEN("Check it remembered the double type") {
+            REQUIRE(handle.type_name() == "double");
+        }
+        THEN("Check it can be converted back to data_handle<double>") {
+            REQUIRE_NOTHROW(static_cast<data_handle<double>>(handle));
         }
     }
 }
@@ -82,12 +150,12 @@ TEST_CASE("SOA-backed Node structure", "[Neuron][data_structures][node]") {
             auto x = i++;
             return x * x;
         });
-        std::vector<Node> nodes{};
+        std::vector<::Node> nodes{};
         std::transform(reference_voltages.begin(),
                        reference_voltages.end(),
                        std::back_inserter(nodes),
                        [](auto v) {
-                           Node node{};
+                           ::Node node{};
                            node.set_v(v);
                            return node;
                        });
