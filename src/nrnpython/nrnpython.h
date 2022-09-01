d0953dc09865c4476a57aa3555c51bc6bdec91a1
Michael Hines <michael.hines@yale.edu>
2022-09-01 12:00:06 -0400

    Python error for invalid data_handle

--- src/nrnpython/nrnpython.h
+++ src/nrnpython/nrnpython.h
@@ -88,5 +88,5 @@ Object* nrnpy_pyobject_in_obj(PyObject*);
 PyObject* nrn_hocobj_handle(neuron::container::data_handle<double> d);
 extern "C" PyObject* nrn_hocobj_ptr(double*);
 int nrn_is_hocobj_ptr(PyObject*, neuron::container::data_handle<double>&);
-
+bool nrn_chk_data_handle(neuron::container::data_handle<double>&);
 #endif
