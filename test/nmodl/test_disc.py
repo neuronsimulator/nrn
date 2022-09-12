170c0e173eb34b8f3d062b1330691440957547e8
Ioannis Magkanaris <ioannis.magkanaris@epfl.ch>
2022-09-12 18:17:01 +0300

    Added meaningful test for DISCRETE

--- test/nmodl/test_disc.py
+++ test/nmodl/test_disc.py
@@ -1,14 +1,15 @@
+import numpy as np
+
 from neuron.expect_hocerr import set_quiet
 
 from disc import DiscCell
 
 def test_disc():
-    table_cell = DiscCell()
-    table_cell.record()
-    table_cell.simulate(1, 0.1)
+    disc_cell = DiscCell()
+    disc_cell.record()
+    disc_cell.simulate(1, 0.1)
 
-    print(table_cell.record_vectors)
-    assert len(table_cell.record_vectors) != 0
+    assert np.isclose([disc_cell.record_vectors["a"][2]], [8.1])
 
 if __name__ == "__main__":
     set_quiet(False)
