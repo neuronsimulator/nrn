ad7d4c889bfb35ec6d4294ab0fbe2af1f9c402a5
Olli Lupton <oliver.lupton@epfl.ch>
2022-09-13 18:09:44 +0200

    backwards-compatibility hack for a common VERBATIM pattern

--- src/neuron/container/generic_data_handle.hpp
+++ src/neuron/container/generic_data_handle.hpp
@@ -11,6 +11,26 @@ namespace neuron::container {
 template <typename>
 struct data_handle;
 
+struct generic_data_handle;
+
+/** @brief Helper type to support backwards-compatibility hacks.
+ *
+ *  The (possibly misguided) goal here is to make (T**)(&_p_foo) work in
+ *  translated MOD file code when _p_foo is a generic_data_handle. This pattern
+ *  is (regrettably) common in VERBATIM blocks. It can only realistically be
+ *  made to work if the generic_data_handle is holding a raw pointer. Using this
+ *  pattern will leave _p_foo thinking that it refers to a raw pointer of type T.
+ */
+struct generic_data_handle_proxy {
+    generic_data_handle_proxy(generic_data_handle& handle)
+        : m_handle{handle} {}
+    template <typename T>
+    explicit operator T**() const;
+
+  private:
+    generic_data_handle& m_handle;
+};
+
 /** @brief Non-template stable handle to a generic value.
  *
  *  This is a type-erased version of data_handle<T>.
@@ -23,6 +43,10 @@ struct generic_data_handle {
         : m_container{handle.m_raw_ptr}
         , m_type{typeid(T)} {
         static_assert(std::is_same_v<T, void>);
+        // data_handle<void> cannot refer to a modern data structure, it can
+        // only wrap a void*
+        assert(!m_offset);
+        assert(!m_offset.m_ptr);
     }
 
     // Avoid trying to access members of the data_handle<void> specialisation.
@@ -50,6 +74,7 @@ struct generic_data_handle {
                     "generic_data_handle referring to void should never be in 'modern' mode.");
             } else {
                 // A real and still-valid data handle
+                assert(m_container);
                 return {m_offset, *static_cast<std::vector<T>*>(m_container)};
             }
         } else if (m_offset.m_ptr) {
@@ -74,6 +99,8 @@ struct generic_data_handle {
         return static_cast<T*>(static_cast<data_handle<T>>(*this));
     }
 
+    /** @brief Reset the generic_data_handle to typeless null state.
+     */
     generic_data_handle& operator=(std::nullptr_t) {
         return (*this = {});
     }
@@ -85,7 +112,18 @@ struct generic_data_handle {
 
     template <typename T>
     generic_data_handle& operator=(T* ptr) {
-        return (*this = data_handle<T>{ptr});
+        if (ptr) {
+            return (*this = data_handle<T>{ptr});
+        } else {
+            return (*this = nullptr);
+        }
+    }
+
+    [[deprecated(
+        "this is only intended to support legacy patterns in VERBATIM "
+        "blocks")]] generic_data_handle_proxy
+    operator&() {
+        return {*this};
     }
 
     friend std::ostream& operator<<(std::ostream& os, generic_data_handle const& dh) {
@@ -110,6 +148,7 @@ struct generic_data_handle {
     }
 
   private:
+    friend struct generic_data_handle_proxy;
     // Offset into the underlying storage container. If this generic_data_handle
     // refers to a data_handle<T*> in backwards-compatibility mode, where it
     // just wraps a plain T*, then m_offset is null.
@@ -126,5 +165,30 @@ struct generic_data_handle {
     // Reference to typeid(T) for the wrapped type
     std::type_index m_type{typeid(typeless_null)};
 };
+
+template <typename T>
+generic_data_handle_proxy::operator T**() const {
+    if (m_handle.m_offset || m_handle.m_offset.m_ptr) {
+        throw std::runtime_error(
+            "Cannot convert (T**)(&generic_data_handle) pattern only supported for "
+            "generic_data_handle(" +
+            cxx_demangle(m_handle.m_type.name()) + ") in legacy mode");
+    }
+    bool const is_typeless_null{m_handle.m_type ==
+                                std::type_index{typeid(generic_data_handle::typeless_null)}};
+    if (!is_typeless_null && std::type_index{typeid(T)} != m_handle.m_type) {
+        throw std::runtime_error("Cannot convert &generic_data_handle(" +
+                                 cxx_demangle(m_handle.m_type.name()) + ") to " +
+                                 cxx_demangle(typeid(T).name()) + "**");
+    }
+    if (is_typeless_null) {
+        // double** foo = (double**)(&some_handle)
+        // leaves the handle referring to double
+        m_handle.m_type = std::type_index{typeid(T)};
+    }
+
+    return reinterpret_cast<T**>(&(m_handle.m_container));
+}
+
 
 }  // namespace neuron::container
