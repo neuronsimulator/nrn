497221993f74923a6d9010302dec0ff135511624
Olli Lupton <oliver.lupton@epfl.ch>
2022-09-20 06:58:02 +0200

    neuron::container::soa<...>::erase implementation without range::v3

--- src/nrnoc/container.cpp
+++ src/nrnoc/container.cpp
@@ -1,11 +1,6 @@
 #include "neuron/model_data.hpp"
-#include "neuron/container/node_data.hpp"
-#include "neuron/container/soa_container_impl.hpp"
 
 // see model_data.hpp
 namespace neuron::detail {
 Model model_data{};
 }
-// see soa_container.hpp, this needs to be explicitly instantiated for soa<...>
-// types that we use
-template void neuron::container::Node::storage::erase(std::size_t);
