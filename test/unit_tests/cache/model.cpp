28fe60b4f2598896880f70577a335e77ddf176f5
Olli Lupton <oliver.lupton@epfl.ch>
2022-09-23 11:02:43 +0200

    missing pieces of automatic cache invalidation

--- test/unit_tests/cache/model.cpp
+++ test/unit_tests/cache/model.cpp
@@ -5,6 +5,20 @@
 
 #include <catch2/catch.hpp>
 
+namespace {
+std::vector<double const*> get_reference_voltage_pointers(
+    neuron::container::Node::storage const& node_data) {
+    std::vector<double const*> reference_ptrs{};
+    auto const& voltage_storage =
+        std::as_const(node_data).get<neuron::container::Node::field::Voltage>();
+    std::transform(voltage_storage.begin(),
+                   voltage_storage.end(),
+                   std::back_inserter(reference_ptrs),
+                   [](auto& v) { return &v; });
+    return reference_ptrs;
+}
+}  // namespace
+
 TEST_CASE("Cached model data", "[Neuron][data_structures][cache]") {
     GIVEN("A list of nodes with integer voltages and permuted storage") {
         auto nodes_and_reference_voltages = neuron::test::get_nodes_and_reference_voltages();
@@ -12,15 +26,20 @@ TEST_CASE("Cached model data", "[Neuron][data_structures][cache]") {
         auto& reference_voltages = std::get<1>(nodes_and_reference_voltages);
         auto& node_data = neuron::model().node_data();
         node_data.rotate(node_data.size() / 2);
-        REQUIRE_FALSE(node_data.is_sorted());
+        THEN("Check the data are initially not sorted") {
+            REQUIRE_FALSE(node_data.is_sorted());
+        }
         {
-            // Mark the data as sorted
             auto sorted_token = node_data.get_sorted_token();
-            REQUIRE(node_data.is_sorted());
+            AND_THEN("Check the data have been marked sorted") {
+                REQUIRE(node_data.is_sorted());
+            }
         }
         // sorted_token went out of scope, but in this unit test we know that
         // nothing has changed since then -- so the data are still sorted
-        REQUIRE(node_data.is_sorted());
+        AND_THEN("Check the data are still sorted after the token has died") {
+            REQUIRE(node_data.is_sorted());
+        }
         WHEN("Cached data is requested for the model") {
             {
                 // This is not a toy implementation and would call NEURON's real
@@ -36,19 +55,13 @@ TEST_CASE("Cached model data", "[Neuron][data_structures][cache]") {
                 }
                 AND_THEN("The cache data should match the model data") {
                     // These should point to the entries of the voltage storage
-                    auto const& voltage_ptrs = model_cache->node_data.voltage_ptrs;
+                    auto const& cache_ptrs = model_cache->node_data.voltage_ptrs;
                     // Re-calculate the reference values
-                    std::vector<double const*> reference_ptrs{};
-                    auto const& voltage_storage =
-                        std::as_const(node_data).get<neuron::container::Node::field::Voltage>();
-                    std::transform(voltage_storage.begin(),
-                                   voltage_storage.end(),
-                                   std::back_inserter(reference_ptrs),
-                                   [](auto& v) { return &v; });
+                    auto const reference_ptrs = get_reference_voltage_pointers(node_data);
                     // Check that the vector we created on the fly contains the
                     // same pointers as the vector in the model cache
-                    REQUIRE(std::equal(voltage_ptrs.begin(),
-                                       voltage_ptrs.end(),
+                    REQUIRE(std::equal(cache_ptrs.begin(),
+                                       cache_ptrs.end(),
                                        reference_ptrs.begin(),
                                        reference_ptrs.end()));
                 }
@@ -60,6 +73,41 @@ TEST_CASE("Cached model data", "[Neuron][data_structures][cache]") {
                 AND_THEN("The data structures should no longer be frozen") {
                     REQUIRE_FALSE(node_data.is_frozen());
                 }
+            }
+        }
+    }
+    GIVEN("Some nodes with integer voltages") {
+        auto& node_data = neuron::model().node_data();
+        auto [nodes, reference_voltages] = neuron::test::get_nodes_and_reference_voltages();
+        WHEN("Some nodes are added and deleted") {
+            auto const i = GENERATE(3, 5, 7);
+            // add some new nodes to the end
+            for (auto j = 0; j < i * i; ++j) {
+                nodes.emplace_back();
+            }
+            // erase some near the middle
+            for (auto j = 0; j < i; ++j) {
+                nodes.erase(std::next(nodes.begin(), nodes.size() / 2));
+            }
+            THEN("The storage should not be sorted") {
+                REQUIRE_FALSE(node_data.is_sorted());
+            }
+            // permute the underlying storage
+            node_data.rotate(node_data.size() / 2);
+            AND_THEN("After permutation the storage should still not be sorted") {
+                REQUIRE_FALSE(node_data.is_sorted());
+            }
+            // mark it as sorted to avoid calling the "real" sort algorithm
+            { auto const token = node_data.get_sorted_token(); }
+            AND_THEN("The cache data should be correct") {
+                // get cache data for the newly permuted model
+                auto const model_cache = neuron::acquire_valid_model_cache();
+                auto const& cache_ptrs = model_cache->node_data.voltage_ptrs;
+                auto const reference_ptrs = get_reference_voltage_pointers(node_data);
+                REQUIRE(std::equal(cache_ptrs.begin(),
+                                   cache_ptrs.end(),
+                                   reference_ptrs.begin(),
+                                   reference_ptrs.end()));
             }
         }
     }
