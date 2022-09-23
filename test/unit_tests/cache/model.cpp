d0ae9d9f4a9bc818c242641ebbb8453fa97cf04f
Olli Lupton <oliver.lupton@epfl.ch>
2022-09-23 09:17:27 +0200

    toy version of cached model data and tests

--- test/unit_tests/cache/model.cpp
+++ test/unit_tests/cache/model.cpp
@@ -23,6 +23,10 @@ TEST_CASE("Cached model data", "[Neuron][data_structures][cache]") {
         REQUIRE(node_data.is_sorted());
         WHEN("Cached data is requested for the model") {
             {
+                // This is not a toy implementation and would call NEURON's real
+                // sorting algorithm if the data were not already "sorted"
+                // above. The real algorithm depends on other global state that
+                // is not set up in the unit tests.
                 auto const model_cache = neuron::acquire_valid_model_cache();
                 THEN("The data structures should be frozen") {
                     REQUIRE(node_data.is_frozen());
@@ -30,10 +34,27 @@ TEST_CASE("Cached model data", "[Neuron][data_structures][cache]") {
                         REQUIRE(node_data.is_sorted());
                     }
                 }
-                THEN("") {}
+                AND_THEN("The cache data should match the model data") {
+                    // These should point to the entries of the voltage storage
+                    auto const& voltage_ptrs = model_cache->node_data.voltage_ptrs;
+                    // Re-calculate the reference values
+                    std::vector<double const*> reference_ptrs{};
+                    auto const& voltage_storage =
+                        std::as_const(node_data).get<neuron::container::Node::field::Voltage>();
+                    std::transform(voltage_storage.begin(),
+                                   voltage_storage.end(),
+                                   std::back_inserter(reference_ptrs),
+                                   [](auto& v) { return &v; });
+                    // Check that the vector we created on the fly contains the
+                    // same pointers as the vector in the model cache
+                    REQUIRE(std::equal(voltage_ptrs.begin(),
+                                       voltage_ptrs.end(),
+                                       reference_ptrs.begin(),
+                                       reference_ptrs.end()));
+                }
             }
-            AND_THEN("When the cache token goes out of scope") {
-                AND_THEN("The data structures should still be sorted") {
+            AND_WHEN("The cache token goes out of scope") {
+                THEN("The data structures should still be sorted") {
                     REQUIRE(node_data.is_sorted());
                 }
                 AND_THEN("The data structures should no longer be frozen") {
