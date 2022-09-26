adf7175bbd19a5c3be35a0bf09aa85942c0ebdee
Olli Lupton <oliver.lupton@epfl.ch>
2022-09-26 20:32:46 +0200

    partially revert

--- src/nrnoc/membfunc.h
+++ src/nrnoc/membfunc.h
@@ -25,9 +25,13 @@ struct Memb_func {
     Pvmi current;
     Pvmi jacob;
     Pvmi state;
-    bool has_initialize() const { return m_initialize; }
+    bool has_initialize() const {
+        return m_initialize;
+    }
     void invoke_initialize(NrnThread* nt, Memb_list* ml, int type) const;
-    void set_initialize(Pvmi init) { m_initialize = init; }
+    void set_initialize(Pvmi init) {
+        m_initialize = init;
+    }
     Pvmp destructor; /* only for point processes */
     Symbol* sym;
     nrn_ode_count_t ode_count;
@@ -46,7 +50,7 @@ struct Memb_func {
     void* hoc_mech;
     void (*setdata_)(struct Prop*);
     int* dparam_semantics;  // for nrncore writing.
-private:
+  private:
     Pvmi m_initialize{};
 };
 
