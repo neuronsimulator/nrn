24e1189d5d2c2dde7532a91ecedf60d39d4e4999
Olli Lupton <oliver.lupton@epfl.ch>
2022-10-03 10:05:15 +0200

    add test case

--- test/unit_tests/oc/hoc_interpreter.cpp
+++ test/unit_tests/oc/hoc_interpreter.cpp
@@ -11,3 +11,25 @@ TEST_CASE("Test hoc interpreter", "[Neuron][hoc_interpreter]") {
     hoc_add();
     REQUIRE(hoc_xpop() == 9.0);
 }
+
+SCENARIO("Test small HOC functions", "[Neuron][hoc_interpreter]") {
+    GIVEN("A HOC function returning a local object") {
+        REQUIRE(hoc_oc("obfunc foo() {localobj lv1\n"
+                       "  lv1 = new Vector(5)\n"
+                       "  lv1.indgen()\n"
+                       "  return lv1\n"
+                       "}\n") == 0);
+        WHEN("It is called") {
+            auto const code = hoc_oc(
+                "objref v1\n"
+                "v1 = foo()\n");
+            THEN("There should be no error") {
+                REQUIRE(code == 0);
+                AND_THEN("Printing the result should give the expected result") {
+                    REQUIRE(hoc_oc("print v1\n") == 0);
+                    REQUIRE(hoc_oc("v1.printf()\n") == 0);
+                }
+            }
+        }
+    }
+}
