731c223376a42d574c1afe5b9c79923bdaff7d91
Olli Lupton <oliver.lupton@epfl.ch>
2022-10-11 13:40:30 +0200

    compiling draft with mechanism data changes

---
+++ src/neuron/container/mechanism_data.hpp
@@ -0,0 +1,37 @@
+#pragma once
+#include "neuron/container/mechanism_identifier.hpp"
+#include "neuron/container/soa_container.hpp"
+
+namespace neuron::container::Mechanism {
+namespace field {
+/** @brief Catch-all for floating point per-instance variables in the MOD file.
+ * 
+ *  @todo Update the code generation so we get some hh_data = soa<hh_identifier,
+ *  hh_a, hh_b, ...> type instead of fudging things this way.
+ */
+struct PerInstanceFloatingPointField {
+    constexpr PerInstanceFloatingPointField(std::size_t num_copies) : m_num_copies{num_copies} {}
+    /** @brief How many copes of this column should be created?
+     */
+    constexpr std::size_t num_instances() const { return m_num_copies; }
+    using type = double;
+private:
+    std::size_t m_num_copies{};
+};
+}  // namespace field
+
+/** @brief Underlying storage for all instances of a particular Mechanism.
+ */
+struct storage: soa<storage, identifier, field::PerInstanceFloatingPointField> {
+  using base_type = soa<storage, struct identifier, field::PerInstanceFloatingPointField>;
+  storage(std::size_t num_floating_point_fields)
+  : base_type{field::PerInstanceFloatingPointField{num_floating_point_fields}} {}
+  [[nodiscard]] constexpr std::size_t num_floating_point_fields() const {
+      return get_tag<field::PerInstanceFloatingPointField>().num_instances();
+  }
+};
+
+/** @brief Owning identifier for a row in the Mechanism storage;
+ */
+using owning_identifier = owning_identifier_base<storage, identifier>;
+}  // namespace neuron::container::Mechanism
