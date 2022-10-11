fc579bd4ec7cc16030e153b94c407fdc4782f0d8
Olli Lupton <oliver.lupton@epfl.ch>
2022-10-11 16:49:31 +0200

    more tests and permute support for mech data

--- test/unit_tests/container/mechanism.cpp
+++ test/unit_tests/container/mechanism.cpp
@@ -1,4 +1,5 @@
 #include "neuron/container/mechanism.hpp"
+#include "neuron/container/soa_container_impl.hpp"
 
 #include <catch2/catch.hpp>
 
@@ -25,6 +26,59 @@ TEST_CASE("SOA-backed Mechanism data structure", "[Neuron][data_structures][mech
                 REQUIRE(mech_instance.fpfield(0) == field0_value);
                 REQUIRE(mech_instance.fpfield(1) == field1_value);
                 REQUIRE_THROWS(mech_instance.fpfield(num_fields));
+            }
+        }
+        WHEN("Many rows are added") {
+            std::vector<double> reference_field0, reference_field1;
+            constexpr auto num_instances = 10;
+            std::generate_n(std::back_inserter(reference_field0), num_instances, [i = 0]() mutable {
+                auto const x = i++;
+                return x * x;
+            });
+            std::generate_n(std::back_inserter(reference_field1), num_instances, [i = 0]() mutable {
+                auto const x = i++;
+                return x * x * x;
+            });
+            std::vector<owning_handle> mech_instances{};
+            std::transform(reference_field0.begin(),
+                           reference_field0.end(),
+                           reference_field1.begin(),
+                           std::back_inserter(mech_instances),
+                           [&mech_data](auto field0, auto field1) {
+                               owning_handle mech{mech_data};
+                               mech.set_fpfield(0, field0);
+                               mech.set_fpfield(1, field1);
+                               return mech;
+                           });
+            const auto check_field_values = [&]() {
+                THEN("The correct values can be read back") {
+                    std::vector<double> current_field0, current_field1;
+                    std::transform(mech_instances.begin(),
+                                   mech_instances.end(),
+                                   std::back_inserter(current_field0),
+                                   [](auto const& mech) { return mech.fpfield(0); });
+                    std::transform(mech_instances.begin(),
+                                   mech_instances.end(),
+                                   std::back_inserter(current_field1),
+                                   [](auto const& mech) { return mech.fpfield(1); });
+                    REQUIRE(current_field0 == reference_field0);
+                    REQUIRE(current_field1 == reference_field1);
+                }
+            };
+            check_field_values();
+            THEN("The underlying storage matches the reference values") {
+                REQUIRE(reference_field0 ==
+                        mech_data.get_field_instance<field::PerInstanceFloatingPointField>(0));
+                REQUIRE(reference_field1 ==
+                        mech_data.get_field_instance<field::PerInstanceFloatingPointField>(1));
+            }
+            AND_WHEN("The underlying storage is reversed") {
+                mech_data.reverse();
+                check_field_values();
+            }
+            AND_WHEN("The underlying storage is rotated") {
+                mech_data.rotate(num_instances / 2);
+                check_field_values();
             }
         }
     }
