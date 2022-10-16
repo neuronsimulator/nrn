242f16c60f6b1b950bd43dff501c2c26c47392c6
Michael Hines <michael.hines@yale.edu>
2022-10-16 20:54:57 +0200

    added dimension changed tests

---
+++ test/hoctests/vardimtests/test2.py
@@ -0,0 +1,25 @@
+from neuron import h
+from neuron.expect_hocerr import expect_err
+import neuron.expect_hocerr
+neuron.expect_hocerr.quiet = False
+
+h('''
+begintemplate Foo
+public o
+objref o[3]
+proc init() {local i
+  objref o[$1]
+  for i = 0, $1-1 {
+    o[i] = new Vector(i)
+    o[i].fill(i)
+  }
+}
+endtemplate Foo
+''')
+
+h('''
+objref foo
+foo = new Foo(3)
+
+Foo[0]
+''')
