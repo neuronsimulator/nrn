19792ca4900acc5db59e7138d100193fa7075c50
Olli Lupton <oliver.lupton@epfl.ch>
2022-11-02 11:25:10 +0100

    Use std::recursive_mutex instead of std::mutex.

--- src/ivoc/nrnmutdec.h
+++ src/ivoc/nrnmutdec.h
@@ -4,15 +4,15 @@
 #include <memory>
 #include <mutex>
 
-#define MUTDEC         std::unique_ptr<std::mutex> mut_;
+#define MUTDEC         std::unique_ptr<std::recursive_mutex> mut_;
 #define MUTCONSTRUCTED static_cast<bool>(mut_)
-#define MUTCONSTRUCT(mkmut)                        \
-    {                                              \
-        if (mkmut) {                               \
-            mut_ = std::make_unique<std::mutex>(); \
-        } else {                                   \
-            mut_.reset();                          \
-        }                                          \
+#define MUTCONSTRUCT(mkmut)                                  \
+    {                                                        \
+        if (mkmut) {                                         \
+            mut_ = std::make_unique<std::recursive_mutex>(); \
+        } else {                                             \
+            mut_.reset();                                    \
+        }                                                    \
     }
 #define MUTDESTRUCT mut_.reset();
 #define MUTLOCK           \
