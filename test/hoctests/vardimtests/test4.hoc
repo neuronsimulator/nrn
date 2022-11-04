neuronsimulator/nrn

commit a8362dc5b980cb58ea4122ebaf3a74a838022b94

Meaningful error messages when number of subscripts differs between compile and runtime (#2024)
* stack_ndim_datum type for error when compile time and runtime nsub differs.
* Disallow use of [][]... syntax for function arg lists.
* Error messages when python accesses a hoc var with changed dimensions.
* added dimension changed tests
* Allow legacy a.b(i) syntax for 1d arrays (should be written a.b[i])
* SectionRef.child[index] always needs index.
* Allow legacy Matrix.x(i,j) as well as recommended Matrix.x[i][j]
* Allow Section.child(i) syntax as well as recommended Section.child[i]
* Replace Vector.x(i) syntax with recommended Vector.x[i]

Co-authored-by: Alexandru Săvulescu <alexandru.savulescu@epfl.ch>
Co-authored-by: Olli Lupton <oliver.lupton@epfl.ch>

src/oc/hoc_oop.cpp
@@ -896,6 +896,14 @@ static void range_suffix(Symbol* sym, int nindex, int narg) {
         if (nindex != sym->arayinfo->nsub) {
             bdim = 1;
         }
+        /*
+         It is a bit more difficult to push ndim here since the arc length, if
+         specified, is top of stack before the index. However, waiting to fix up
+         the stack til just before range_vec_indx calls hoc_araypt seems ill
+         advised since range_vec_indx is called 6 places. And who knows what
+         kinds of user errors are possible that we want to catch.
+         So fix up below.
+        */
     } else {
         if (nindex != 0) {
             bdim = 1;
@@ -906,6 +914,22 @@ static void range_suffix(Symbol* sym, int nindex, int narg) {
     }
 
     if (sym->type == RANGEVAR) {
+        if (ISARRAY(sym)) {  // fixup ndim here
+            double x = -1.0;
+            if (narg) {  // need to pop the arc length to push ndim
+                if (narg > 1) {
+                    hoc_execerr_ext("%s range variable can have only one arc length parameter",
+                                    sym->name);
+                }
+                x = xpop();
+            }
+            if (!hoc_stack_type_is_ndim()) {
+                hoc_push_ndim(nindex);
+            }
+            if (narg) {  // push back the arc length
+                pushx(x);
+            }
+        }
         hoc_pushi(narg);
         hoc_pushs(sym);
     } else if (sym->subtype == USERPROPERTY) {
@@ -953,15 +977,19 @@ void hoc_object_component() {
         narg += nindex;
         nindex = 0;
     }
+    int expect_stack_nsub{0};
     if (nindex) {
         if (narg) {
             hoc_execerror("[...](...) syntax only allowed for array range variables:", sym0->name);
         }
+        if (!hoc_stack_type_is_ndim()) {
+            hoc_push_ndim(nindex);
+        }
+        expect_stack_nsub = 1;
     } else {
         nindex = narg;
     }
-
-    obp = hoc_obj_look_inside_stack(nindex);
+    obp = hoc_obj_look_inside_stack(nindex + expect_stack_nsub);
     if (obp) {
 #if USE_PYTHON
         if (obp->ctemplate->sym == nrnpy_pyobj_sym_) {
@@ -1033,6 +1061,24 @@ void hoc_object_component() {
                 if (!ISARRAY(sym) || sym->arayinfo->nsub != nindex) {
                     hoc_execerror(sym->name, ":not right number of subscripts");
                 }
+                if (narg) {
+                    // there are 25 modeldb examples that use (index) instead
+                    // of [index] syntax for an array in this context. So we
+                    // have decided to keep allowing this legacy syntax for one
+                    // dimensional arrays.
+                    extern Symbol* nrn_matrix_sym;
+                    if (narg == 1) {
+                        hoc_push_ndim(1);
+                    } else if (narg == 2 && obp->ctemplate->sym == nrn_matrix_sym) {
+                        // Allow legacy syntax Matrix.x(i, j)
+                        hoc_push_ndim(2);
+                    } else {
+                        hoc_execerr_ext("%s.%s is array not function. Use %s[...] syntax",
+                                        hoc_object_name(obp),
+                                        sym->name,
+                                        sym->name);
+                    }
+                }
             }
             hoc_pushs(sym);
             (*obp->ctemplate->steer)(obp->u.this_pointer);
@@ -1062,6 +1108,10 @@ void hoc_object_component() {
         break;
     case PROCEDURE:
     case FUNCTION: {
+        if (expect_stack_nsub) {
+            hoc_pop_ndim();
+            hoc_execerr_ext("%s is a function not a %ddim array", sym->name, nindex);
+        }
         double d = 0.;
         call_ob_proc(obp, sym, nindex);
         if (hoc_returning) {
@@ -1077,6 +1127,14 @@ void hoc_object_component() {
     case HOCOBJFUNCTION:
     case OBFUNCTION: {
         Object** d;
+        if (expect_stack_nsub) {
+            hoc_pop_ndim();
+            // for legacy reasons allow single arg [] format.
+            // E.g. occasionally seen for List.object[index]
+            if (nindex > 1) {
+                hoc_execerr_ext("%s is a function not a %ddim array", sym->name, nindex);
+            }
+        }
         call_ob_proc(obp, sym, nindex);
         if (hoc_returning) {
             break;

test/hoctests/vardimtests/test4.hoc
@@ -1,0 +1,42 @@
+load_file("expect_err.hoc")
+double a[1][1]
+func foo() {local i
+  double a[1][1]
+  a = 3
+  return a == 3
+}
+print foo()
+
+double a[1]
+print foo()
+double a[1][1][1]
+print foo()
+
+create soma, dend[3]
+for i=0,2 {
+  connect dend[i](0), soma(1)
+}
+objref sref
+soma { sref = new SectionRef() }
+expect_err("sref.child { print secname() }")
+sref.child[1] { print secname() }
+sref.child(1) { print secname() }
+expect_err("sref.child(1,2) { print secname() }")
+expect_err("sref.child[1][2] { print secname() }")
+
+begintemplate Cell
+public soma, dend
+create soma, dend[3]
+proc init() { local i
+  for i=0, 2 {
+    connect dend[i](0), soma(1)
+  }
+}
+endtemplate Cell
+objref cell
+cell = new Cell()
+cell.dend[1] { print secname() }
+cell.dend { print secname() }
+expect_err("cell.dend[5] { print secname() }")
+expect_err("cell.dend[1][2] {print secname() }")
+expect_err("cell.soma[1] { print secname() }")

share/lib/hoc/celbild/celtopol.hoc
@@ -22,13 +22,13 @@ proc sesstr1() {
 	sprint($s2, "pt3dadd(%g, %g, %g, %g)", x.x[$1], y.x[$1], z.x[$1], d.x[$1])
 }
 func id() {
-	return idvec.x($1)
+	return idvec.x[$1]
 }
 proc setid() {
-	idvec.x($1) = $2
+	idvec.x[$1] = $2
 }
 proc xmlwrite() {
-	sprint($s2, "<Point id=\"%d\"><x value=\"%g\"/><y value=\"%g\"/><z value=\"%g\"/><diam value=\"%g\"/></Point>", idvec.x($1), x.x[$1], y.x[$1], z.x[$1], d.x[$1])
+	sprint($s2, "<Point id=\"%d\"><x value=\"%g\"/><y value=\"%g\"/><z value=\"%g\"/><diam value=\"%g\"/></Point>", idvec.x[$1], x.x[$1], y.x[$1], z.x[$1], d.x[$1])
 }
 endtemplate P3D
 

share/lib/hoc/import3d/import3d_sec.hoc
@@ -197,7 +197,7 @@ obfunc contourcenter() {local i localobj mean, pts, perim, d
 	perim = new Vector(pts.ncol)
 	for i=1, pts.ncol-1 { perim.x[i] = perim.x[i-1] + pts.getcol(i-1).mag }
 	d = new Vector(101)
-	d.indgen(perim.x(perim.size-1)/100)
+	d.indgen(perim.x[perim.size-1]/100)
 	for i=1,3 $oi.interpolate(d, perim)
 	mean = new Vector(3)
 	for i=1, 3 { mean.x[i-1] = $oi.mean }

share/lib/hoc/lincir/lincir1.hoc
@@ -634,7 +634,7 @@ proc statemap() {local i, j, in
 		k = -1
 		for j=0, tobj.elements.count-1 {
 			sel = tobj.elements.object(j)
-			k = tobj.ends.x(j) + 1
+			k = tobj.ends.x[j] + 1
 			if (sel.names[k] == nil) {
 				k = -1
 			}else{
@@ -1151,7 +1151,7 @@ proc init_out() {local n, nc, i, j, k  localobj o, sel, s
     k = -1
     for j=0, o.elements.count-1 {
       sel = o.elements.o(j)
-      k = o.ends.x(j) + 1
+      k = o.ends.x[j] + 1
       if (sel.names[k] == nil) {
         k = -1
       }else{

share/lib/hoc/mkcurve.hoc
@@ -234,7 +234,7 @@ proc remove() {
 		if ($2 < vx.x[0] - hres_ || $2 > vx.x[vx.size-1] + hres_ ) {
 			ipt = -1
 		}else{
-			g.mark(vx.x(ipt), vy.x(ipt), "S", 8, 2, 1)
+			g.mark(vx.x[ipt], vy.x[ipt], "S", 8, 2, 1)
 		}
 		sval(ipt)
 	}
@@ -321,7 +321,7 @@ proc enter() { local i, x, y
 		if ($2 < vx.x[0] - hres_ || $2 > vx.x[vx.size-1] + hres_ ) {
 			ipt = -1
 		}else{
-			g.mark(vx.x(ipt), vy.x(ipt), "S", 8, 2, 1)
+			g.mark(vx.x[ipt], vy.x[ipt], "S", 8, 2, 1)
 		}
 		sval(ipt)
 	}

share/lib/hoc/mview/mviewxml.hoc
@@ -388,7 +388,7 @@ proc xmlwrite() {local proximalid, pointcount, segmentcount, sectioncount, i, j,
                 $o1.printf("      <cablegroup name=\"%s\"> %s\n", groupName, comment)
 
                 for j=0, tobj1.subset.size - 1 {
-                    secref = cellref.secanal.all.object(tobj1.subset.x(j))
+                    secref = cellref.secanal.all.object(tobj1.subset.x[j])
 
                     $o1.printf("        <cable id = \"%d\"/>\n",secref.sec.cm(.00001))
                 }
@@ -499,7 +499,7 @@ proc xmlwrite() {local proximalid, pointcount, segmentcount, sectioncount, i, j,
                 }
                 $o1.printf("      <bio:mechanism name=\"%s\" type=\"Channel Mechanism\"%s>\n", dmech.name, passiveAttr)
                 for j=0, dmech.location.subset.size()-1 {
-                    secref = cellref.secanal.all.object(dmech.location.subset.x(j))
+                    secref = cellref.secanal.all.object(dmech.location.subset.x[j])
         //              printsec($o1, secref)
                 }
                 for dmech.parmiter(dparm) {
@@ -722,7 +722,7 @@ proc printparmval() {
 	    if ($o4.location.type() == 8) {
 
 //	        for m=0, $o4.location.subset.size()-1 {
-//		        secref = $o2.object($o4.location.subset.x(m))
+//		        secref = $o2.object($o4.location.subset.x[m])
 //	            printsec($o1, secref)
 //	        }
 
@@ -792,8 +792,8 @@ proc printparmval() {
         infoVals = ""
 
         for i=0, $o4.vec.size - 1 {
-             sprint(infoVals, "%s %g, ", infoVals, $o4.vec.x(i))
-             avgVal = avgVal + $o4.vec.x(i)
+             sprint(infoVals, "%s %g, ", infoVals, $o4.vec.x[i])
+             avgVal = avgVal + $o4.vec.x[i]
         }
         avgVal = avgVal / $o4.vec.size
 
@@ -825,8 +825,8 @@ proc printvecvals() {local i
         ///$o1.printf("        <section name=\"%s\">\n", secname())
     }
     for i=0, $o2.vec.size - 1 {
-        //$o1.printf("        <value>%g</value>\n", $o2.vec.x(i))
-        $o1.printf("            <!-- Value %g: %g -->\n", i, $o2.vec.x(i))
+        //$o1.printf("        <value>%g</value>\n", $o2.vec.x[i])
+        $o1.printf("            <!-- Value %g: %g -->\n", i, $o2.vec.x[i])
     }
     //$o1.printf("      </section>\n")
 }

src/ivoc/matrix.cpp
@@ -9,7 +9,7 @@
 #include "ivocvect.h"
 
 #define EPS hoc_epsilon
-static Symbol* smat_;
+Symbol* nrn_matrix_sym;  // also used in oc/hoc_oop.cpp
 
 extern int hoc_return_type_code;
 
@@ -43,7 +43,7 @@ static void check_capac(int i, int j) {
 
 Matrix* matrix_arg(int i) {
     Object* ob = *hoc_objgetarg(i);
-    if (!ob || ob->ctemplate != smat_->u.ctemplate) {
+    if (!ob || ob->ctemplate != nrn_matrix_sym->u.ctemplate) {
         check_obj_type(ob, "Matrix");
     }
     return (Matrix*) (ob->u.this_pointer);
@@ -55,7 +55,7 @@ Object** Matrix::temp_objvar() {
     if (m->obj_) {
         po = hoc_temp_objptr(m->obj_);
     } else {
-        po = hoc_temp_objvar(smat_, (void*) m);
+        po = hoc_temp_objvar(nrn_matrix_sym, (void*) m);
         obj_ = *po;
     }
     return po;
@@ -730,6 +730,10 @@ static void steer_x(void* v) {
     Matrix* m = (Matrix*) v;
     int i1, i2;
     Symbol* s = hoc_spop();
+    if (!hoc_stack_type_is_ndim()) {
+        hoc_execerr_ext("Array dimension of Matrix.x is 2");
+    }
+    hoc_pop_ndim();
     i2 = (int) (hoc_xpop() + EPS);
     i1 = (int) (hoc_xpop() + EPS);
     check_domain(i1, m->nrow() - 1);
@@ -744,9 +748,9 @@ void Matrix_reg();
 
 void Matrix_reg() {
     class2oc("Matrix", m_cons, m_destruct, m_members, NULL, m_retobj_members, NULL);
-    smat_ = hoc_lookup("Matrix");
+    nrn_matrix_sym = hoc_lookup("Matrix");
     // now make the x variable an actual double
-    Symbol* sx = hoc_table_lookup("x", smat_->u.ctemplate->symtable);
+    Symbol* sx = hoc_table_lookup("x", nrn_matrix_sym->u.ctemplate->symtable);
     sx->type = VAR;
     sx->arayinfo = (Arrayinfo*) hoc_Emalloc(sizeof(Arrayinfo) + 2 * sizeof(int));
     sx->arayinfo->refcount = 1;
@@ -754,5 +758,5 @@ void Matrix_reg() {
     sx->arayinfo->nsub = 2;
     sx->arayinfo->sub[0] = 1;
     sx->arayinfo->sub[1] = 1;
-    smat_->u.ctemplate->steer = steer_x;
+    nrn_matrix_sym->u.ctemplate->steer = steer_x;
 }

commit 67e56141ab5ea42ec2606ebf7a39043a1f21fd9c

section connect obj.secarray[i](0), 1 is valid syntax (#2066)
Fixes an error introduced by #2024 in some models.

src/oc/hoc_oop.cpp
@@ -1213,6 +1213,9 @@ void hoc_object_component() {
             if (!ISARRAY(sym) || OPARINFO(sym)->nsub != nindex) {
                 hoc_execerror(sym->name, ":not right number of subscripts");
             }
+            if (!hoc_stack_type_is_ndim()) {
+                hoc_push_ndim(nindex);
+            }
             nindex = araypt(sym, OBJECTVAR);
         }
         hoc_pop_defer();

test/hoctests/vardimtests/test4.hoc
@@ -40,3 +40,8 @@ cell.dend { print secname() }
 expect_err("cell.dend[5] { print secname() }")
 expect_err("cell.dend[1][2] {print secname() }")
 expect_err("cell.soma[1] { print secname() }")
+
+forall disconnect()
+expect_err("connect cell.dend[1](0), cell.dend[0](1)") // limitation of parser
+cell.dend[1] connect cell.dend[2](0), 1
+