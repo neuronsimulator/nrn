5a08b981581edca3678d1f04d40f4e0f43cd1185
Olli Lupton <oliver.lupton@epfl.ch>
2022-11-29 23:19:18 +0100

    small improvements

--- src/nrncvode/netcvode.cpp
+++ src/nrncvode/netcvode.cpp
@@ -1719,8 +1719,8 @@ bool NetCvode::init_global() {
             // statement that needs to be handled.
             std::unordered_set<int> ba_candidate;
             {
-                std::vector<int> batypes = {BEFORE_STEP, BEFORE_BREAKPOINT, AFTER_SOLVE};
-                for (const auto& bat: batypes) {
+                constexpr std::array batypes{BEFORE_STEP, BEFORE_BREAKPOINT, AFTER_SOLVE};
+                for (auto const bat: batypes) {
                     for (BAMech* bam = bamech_[bat]; bam; bam = bam->next) {
                         ba_candidate.insert(bam->type);
                     }
@@ -1771,7 +1771,8 @@ bool NetCvode::init_global() {
                     // non-contiguous mode, so we're going to create a lot of 1-element Memb_list
                     // inside cml->ml
                     cml->ml.reserve(cml->ml[0].nodecount);
-                    cml->ml.resize(0);  // remove the single entry from contiguous mode
+                    // remove the single entry from contiguous mode
+                    cml->ml.clear();
                 }
             }
             // fill pointers (and nodecount)
@@ -1794,7 +1795,6 @@ bool NetCvode::init_global() {
                         newml.nodecount = 1;
                         newml.nodelist = new Node*[1];
                         newml.nodelist[0] = ml->nodelist[j];
-                        assert(newml.nodelist[0] == ml->nodelist[j]);
 #if CACHEVEC
                         newml.nodeindices = new int[1]{ml->nodeindices[j]};
 #endif
@@ -2087,14 +2087,15 @@ int NetCvode::solve(double tout) {
             }
         }
     } else if (!gcv_) {  // lvardt
+        auto const cache_token = nrn_ensure_model_data_are_sorted();
         if (tout >= 0.) {
-            time_t rt = time(nil);
+            time_t rt = time(nullptr);
             //			int cnt = 0;
             TQueue* tq = p[0].tq_;
             TQueue* tqe = p[0].tqe_;
             NrnThread* nt = nrn_threads;
             while (tq->least_t() < tout || tqe->least_t() <= tout) {
-                err = local_microstep(nrn_ensure_model_data_are_sorted(), *nt);
+                err = local_microstep(cache_token, *nt);
                 if (nrn_allthread_handle) {
                     (*nrn_allthread_handle)();
                 }
@@ -2103,13 +2104,13 @@ int NetCvode::solve(double tout) {
                 }
 #if HAVE_IV
                 IFGUI
-                if (rt < time(nil)) {
+                if (rt < time(nullptr)) {
                     //				if (++cnt > 10000) {
                     //					cnt = 0;
                     Oc oc;
                     oc.notify();
                     single_event_run();
-                    rt = time(nil);
+                    rt = time(nullptr);
                 }
                 ENDGUI
 #endif
@@ -2128,7 +2129,7 @@ int NetCvode::solve(double tout) {
             double tc = tq->least_t();
             double te = p[0].tqe_->least_t();
             while (tq->least_t() <= tc && p[0].tqe_->least_t() <= te) {
-                err = local_microstep(nrn_ensure_model_data_are_sorted(), *nrn_threads);
+                err = local_microstep(cache_token, *nrn_threads);
                 if (nrn_allthread_handle) {
                     (*nrn_allthread_handle)();
                 }
