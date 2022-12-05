d8b514fbea20df476d5468ef1d9dd8bae756e52a
Olli Lupton <oliver.lupton@epfl.ch>
2022-12-05 10:41:14 +0100

    format

--- src/coreneuron/io/phase2.cpp
+++ src/coreneuron/io/phase2.cpp
@@ -1036,7 +1036,7 @@ void Phase2::populate(NrnThread& nt, const UserParams& userParams) {
             num_point_process += n;
         }
     }
-    nt.pntprocs = new Point_process[num_point_process]{}; // includes acell with and without gid
+    nt.pntprocs = new Point_process[num_point_process]{};  // includes acell with and without gid
     nt.n_pntproc = num_point_process;
     nt._ndata = offset;
 
