2e174d7a19a98a864b4557a70f9f39e8ff5e5ab0
Nicolas Cornu <nicolas.cornu@epfl.ch>
2022-12-08 14:54:37 +0100

    fix test

--- test/nmodl/test_function_table.py
+++ test/nmodl/test_function_table.py
@@ -9,7 +9,7 @@ def test_function_table():
     k3st_cell = k3stCell()
     k3st_cell.record()
     k3st_cell.simulate(1, 0.1)
-    assert np.isclose([k3st_cell.record_vectors["tau1_rec"][2]], [8.1])
+    assert np.isclose([k3st_cell.record_vectors["tau1_rec"][2]], [0.097022])
     assert np.isclose([k3st_cell.record_vectors["tau2_rec"][2]], [100])
 
 
