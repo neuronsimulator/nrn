936be55328239343b743a0ea4e1d372e9bc9ef0c
Olli Lupton <oliver.lupton@epfl.ch>
2022-12-13 09:00:38 +0100

    NRN_MECHANISM_DATA_IS_SOA and neuron::legacy::set_globals_from_prop

--- src/nrnoc/nrnversionmacros.h
+++ src/nrnoc/nrnversionmacros.h
@@ -18,3 +18,13 @@
 #if NRN_VERSION_GTEQ(8, 2, 0)
 #define NRN_VERSION_GTEQ_8_2_0
 #endif
+
+// Around NEURON 9.0.0 there are significant changes to data structures, which in some cases require
+// that VERBATIM code is adapted.
+#if NRN_VERSION_LT(9, 0, 0)
+#error "Changes from NEURON 9+ may have been erroneously backported to an older branch"
+#else
+// Used to test if floating point mechanism data are accessed via a double* (_p) or via a Memb_list*
+// and std::size_t pair (_ml and _iml)
+#define NRN_MECHANISM_DATA_IS_SOA  // _ml and _iml are used
+#endif
