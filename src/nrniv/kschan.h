f952e9740b8b2c6d967aa53fbc7886ee500d4bd6
Olli Lupton <oliver.lupton@epfl.ch>
2022-12-21 13:32:13 +0100

    format and cleanup

--- src/nrniv/kschan.h
+++ src/nrniv/kschan.h
@@ -355,7 +355,11 @@ class KSChan {
     void add_channel(const char**);
     // for cvode
     virtual int count();
-    virtual void map(Prop*, int, neuron::container::data_handle<double>*, neuron::container::data_handle<double>*, double*);
+    virtual void map(Prop*,
+                     int,
+                     neuron::container::data_handle<double>*,
+                     neuron::container::data_handle<double>*,
+                     double*);
     virtual void spec(Memb_list*);
     virtual void matsol(NrnThread*, Memb_list*);
     virtual void cv_sc_update(NrnThread*, Memb_list*);
