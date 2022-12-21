6475bf3487ebe85c8b134a0019d4ee02a37dcca1
Nicolas Cornu <nicolas.cornu@epfl.ch>
2022-12-21 13:36:47 +0100

    Run nmodl tests with coreneuron (#2127)

--- test/nmodl/test_function_table.py
+++ test/nmodl/test_function_table.py
@@ -1,3 +1,6 @@
+import os
+import distutils.util
+
 from neuron.expect_hocerr import set_quiet
 
 import numpy as np
@@ -8,7 +11,13 @@ from function_table import k3stCell
 def test_function_table():
     k3st_cell = k3stCell()
     k3st_cell.record()
-    k3st_cell.simulate(1, 0.1)
+    coreneuron_enable = bool(
+        distutils.util.strtobool(os.environ.get("NRN_CORENEURON_ENABLE", "false"))
+    )
+    coreneuron_gpu = bool(
+        distutils.util.strtobool(os.environ.get("CORENRN_ENABLE_GPU", "false"))
+    )
+    k3st_cell.simulate(1, 0.1, coreneuron_enable, coreneuron_gpu)
     assert np.isclose([k3st_cell.record_vectors["tau1_rec"][2]], [0.097022])
     assert np.isclose([k3st_cell.record_vectors["tau2_rec"][2]], [100])
 
