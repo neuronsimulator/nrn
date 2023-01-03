7d4f9749c18434c8efe17240f397f261b2b8fb2f
Olli Lupton <oliver.lupton@epfl.ch>
2023-01-03 15:26:21 +0100

    scopmath: template more

---
+++ src/scopmath/row_view.hpp
@@ -0,0 +1,16 @@
+#pragma once
+namespace neuron::scopmath {
+template <typename MechRange>
+struct row_view {
+    row_view(MechRange* ml, std::size_t iml) : m_iml{iml}, m_ml{ml} {}
+    double& operator[](std::size_t col) {
+        return m_ml->data(m_iml, col);
+    }
+    double const& operator[](std::size_t col) const {
+        return m_ml->data(m_iml, col);
+    }
+private:
+    std::size_t m_iml{};
+    MechRange* m_ml{};
+};
+}
