97d3bdf67780cd4d348e1265538ccd9e31e5f98b
Olli Lupton <oliver.lupton@epfl.ch>
2023-01-11 16:14:09 +0100

    Callbacks for raw pointer updating
    Tentative re-implementation of callbacks for keeping raw pointer values
    up to date in conjunction with the neuron::container::soa<...> based
    data structures. Reinstate nrn_recalc_ptr. Aiming to support reporting
    use-cases for BBP models.

--- src/nrnoc/container.cpp
+++ src/nrnoc/container.cpp
@@ -1,6 +1,10 @@
 #include "membfunc.h"
+#include "neuron/container/callbacks.hpp"
 #include "neuron/container/generic_data_handle.hpp"
 #include "neuron/model_data.hpp"
+
+#include <cstddef>
+#include <optional>
 
 namespace {
 void invalidate_cache() {
@@ -44,6 +48,64 @@ namespace neuron::cache {
 std::optional<Model> model{};
 }
 namespace neuron::container {
+void ptr_update_data::prepare() {
+    // Make sure the containers are sorted by the first member of the tuple
+    std::sort(m_permutations.begin(), m_permutations.end(), [](auto const& lhs, auto const& rhs) {
+        return std::get<0>(lhs) < std::get<0>(rhs);
+    });
+    std::sort(m_reallocations.begin(), m_reallocations.end(), [](auto const& lhs, auto const& rhs) {
+        return std::get<0>(lhs) < std::get<0>(rhs);
+    });
+}
+void* ptr_update_data::recalculate_ptr(void* ptr, std::size_t sizeof_ptr_type) const {
+    auto* const byte_ptr = static_cast<std::byte*>(ptr);
+    // the first element of each row in container is the starting address of the range
+    // represented by that row
+    auto lookup = [byte_ptr](auto const& container) {
+        if (container.empty()) {
+            // the container is empty => no match
+            return container.end();
+        }
+        // first iterator greater than byte_ptr, or last if not found
+        auto const iter = std::upper_bound(container.begin(),
+                                           container.end(),
+                                           byte_ptr,
+                                           [](std::byte* ptr, auto const& entry) {
+                                               return ptr < std::get<0>(entry);
+                                           });
+        if (iter == container.begin()) {
+            // the first range starts after byte_ptr => no match
+            return container.end();
+        } else {
+            // this should be the iterator to the range byte_ptr might fall in
+            return std::prev(iter);
+        }
+    };
+    if (auto const iter = lookup(m_permutations); iter != m_permutations.end()) {
+        auto const& [data, perm, size_of_type, size_of_data] = *iter;
+        assert(byte_ptr >= data);
+        if (byte_ptr < data + size_of_type * size_of_data) {
+            assert(sizeof_ptr_type == size_of_type);
+            assert((byte_ptr - data) % size_of_type == 0);
+            auto const old_index = (byte_ptr - data) / size_of_type;
+            assert(old_index < size_of_data);
+            auto const new_index = perm[old_index];
+            return data + new_index * size_of_type;
+        }
+    }
+    if (auto const iter = lookup(m_reallocations); iter != m_reallocations.end()) {
+        auto const& [old_data, new_data, size_of_type, old_size] = *iter;
+        assert(byte_ptr >= old_data);
+        if (byte_ptr < old_data + size_of_type * old_size) {
+            assert(sizeof_ptr_type == size_of_type);
+            assert((byte_ptr - old_data) % size_of_type == 0);
+            auto const index = (byte_ptr - old_data) / size_of_type;
+            return new_data + index * size_of_type;
+        }
+    }
+    return ptr;
+}
+
 std::ostream& operator<<(std::ostream& os, generic_data_handle const& dh) {
     os << "generic_data_handle{";
     if (!dh.m_offset.has_always_been_null()) {
@@ -73,6 +135,9 @@ std::ostream& operator<<(std::ostream& os, generic_data_handle const& dh) {
 }
 }  // namespace neuron::container
 namespace neuron::container::detail {
+// See neuron/container/callbacks.hpp
+ptr_update_data ptr_transformations;
+std::vector<ptr_update_callback_t> ptr_update_callbacks;
 // See neuron/container/soa_identifier.hpp
 std::vector<std::unique_ptr<std::size_t>> garbage;
 }  // namespace neuron::container::detail
