078d306790b8b4771c57abda113a5014767632dd
Ioannis Magkanaris <iomagkanaris@gmail.com>
2023-01-16 17:47:37 +0100

    Replacing actual_rhs() with _sp13_rhs in nrndae

--- src/nrniv/nrndae.cpp
+++ src/nrniv/nrndae.cpp
@@ -213,7 +213,7 @@ void NrnDAE::update() {
     // note that the following is correct also for states that refer
     // to the internal potential of a segment. i.e rhs is v + vext[0]
     for (int i = 0; i < size_; ++i) {
-        y_[i] += _nt->actual_rhs(bmap_[i]);
+        y_[i] += _nt->_sp13_rhs[bmap_[i]];
     }
     // for (int i=0; i < size_; ++i) printf(" i=%d bmap_[i]=%d y_[i]=%g\n", i, bmap_[i],
     // y_->elem(i));
@@ -293,7 +293,7 @@ void NrnDAE::rhs() {
     v2y();
     f_(y_, yptmp_, size_);
     for (int i = 0; i < size_; ++i) {
-        _nt->actual_rhs(bmap_[i]) += yptmp_[i];
+        _nt->_sp13_rhs[bmap_[i]] += yptmp_[i];
     }
 }
 
