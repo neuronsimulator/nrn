d40c56c3d6e04deb6362b1a18fa51f4590eee13b
Olli Lupton <oliver.lupton@epfl.ch>
2023-03-14 17:59:17 +0100

    fixes for mpi + pytest-cov

--- test/pynrn/run_pytest.py
+++ test/pynrn/run_pytest.py
@@ -1,9 +1,27 @@
+import coverage
 import os
 import pytest
 import sys
+import warnings
 
 if __name__ == "__main__":
     args = os.environ.get("NRN_PYTEST_ARGS", "").split()
-    code = pytest.main(args)
+    # pytest-cov does not play nicely with MPI. Each MPI rank will try to
+    # collect coverage files from all ranks and write them to the same file,
+    # with predictable race conditions and errors. Try and avoid that by
+    # telling each rank to write to a different output file.
+    from neuron import h
+
+    pc = h.ParallelContext()
+    os.environ["COVERAGE_FILE"] = ".coverage.pynrn." + str(pc.id())
+    # Importing neuron just above causes a warning, which seems hard to avoid
+    with warnings.catch_warnings():
+        warnings.filterwarnings(
+            "ignore",
+            category=coverage.exceptions.CoverageWarning,
+            message=r"Module neuron was previously imported, but not measured \(module-not-measured\)",
+        )
+        code = pytest.main(args)
+    pc.barrier()
     print("run_pytest: exiting with code", code, int(code))
     sys.exit(int(code))
