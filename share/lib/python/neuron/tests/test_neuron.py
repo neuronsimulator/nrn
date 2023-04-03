6dd297335f3f4382b5d3afcbcea7b2ff4ec2fac2
Olli Lupton <oliver.lupton@epfl.ch>
2023-04-03 21:50:42 +0200

    missing import

--- share/lib/python/neuron/tests/test_neuron.py
+++ share/lib/python/neuron/tests/test_neuron.py
@@ -286,7 +286,7 @@ def basicRxD3D():
 
 
 def suite():
-    import multiprocessing, os
+    import multiprocessing, os, sys
 
     # For sanitizer runtimes that need to be preloaded on macOS, we need to
     # propagate this manually so multiprocessing.Process works
