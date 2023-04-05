0b6e2fdba19e3a0d56f60cde788bf2d6792dbe4b
Olli Lupton <oliver.lupton@epfl.ch>
2023-04-05 18:59:30 +0200

    more macro cleanup

--- cmake/SanitizerHelper.cmake
+++ cmake/SanitizerHelper.cmake
@@ -59,7 +59,6 @@ if(NRN_SANITIZERS)
   endif()
   # Needed for using sanitizers on macOS
   cpp_cc_strip_python_shims(EXECUTABLE "${PYTHON_EXECUTABLE}" OUTPUT PYTHON_EXECUTABLE)
-  set(NRN_DEFAULT_PYTHON_EXECUTABLE "${PYTHON_EXECUTABLE}")
   configure_file(bin/nrn-enable-sanitizer.in bin/nrn-enable-sanitizer @ONLY)
   install(PROGRAMS ${PROJECT_BINARY_DIR}/bin/nrn-enable-sanitizer
           DESTINATION ${CMAKE_INSTALL_PREFIX}/bin)
