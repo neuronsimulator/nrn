d046c0db8784f0de32c644dd5cf53cea821767b2
Pramod S Kumbhar <pramod.s.kumbhar@gmail.com>
2023-04-20 05:44:35 +0200

    make formatters happy

--- test/pynrn/test_loadbal.py
+++ test/pynrn/test_loadbal.py
@@ -1,6 +1,7 @@
 from neuron import h
 import sys
 
+
 def test_is_ion():
     mt = h.MechanismType(0)
 
@@ -11,6 +12,7 @@ def test_is_ion():
     # check for ion type
     mt.select("k_ion")
     assert mt.is_ion() == True
+
 
 if __name__ == "__main__":
     test_is_ion()
