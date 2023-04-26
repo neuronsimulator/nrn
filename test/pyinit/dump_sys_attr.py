3f2d93c84ef6ff080fc4062c3751476d7b713d7f
Olli Lupton <oliver.lupton@epfl.ch>
2023-04-26 14:40:57 +0200

    teach pyinit about this backwards-compatibility exception

--- test/pyinit/dump_sys_attr.py
+++ test/pyinit/dump_sys_attr.py
@@ -4,6 +4,8 @@ import sys
 
 fname, attr = sys.argv[-2:]
 data = reduce(getattr, [sys] + attr.split("."))
+if attr == "path" and sys.argv[-3] == "override_sys_path_0_to_be_empty":
+    data[0] = ""
 print("dumping sys.{} = {} to {}".format(attr, data, fname))
 with open(fname, "w") as ofile:
     json.dump(data, ofile)
