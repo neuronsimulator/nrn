4883f1f79b8190c92585f314a646be422b8feb9d
Olli Lupton <oliver.lupton@epfl.ch>
2023-05-04 23:15:30 +0200

    Drop fast_math and move CI from %intel to %oneapi (BlueBrain/nmodl#1042)
    NMODL Repo SHA: BlueBrain/nmodl@f8a1ee69028f189847de8f1af332262561948db3

--- src/nmodl/codegen/codegen_cpp_visitor.cpp
+++ src/nmodl/codegen/codegen_cpp_visitor.cpp
@@ -2450,7 +2450,6 @@ void CodegenCVisitor::print_backend_info() {
 void CodegenCVisitor::print_standard_includes() {
     printer->add_newline();
     printer->add_line("#include <math.h>");
-    printer->add_line("#include \"nmodl/fast_math.hpp\" // extend math with some useful functions");
     printer->add_line("#include <stdio.h>");
     printer->add_line("#include <stdlib.h>");
     printer->add_line("#include <string.h>");
