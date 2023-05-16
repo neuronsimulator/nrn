bdaaff2c95cdd357ddeff54b9e6d7373703892b4
Robert A. McDougal <robert.mcdougal@yale.edu>
2023-05-16 06:16:27 -0400

    clang-format

--- test/api/hh_sim.cpp
+++ test/api/hh_sim.cpp
@@ -11,37 +11,48 @@ using std::ofstream;
 
 static const char* argv[] = {"hh_sim", "-nogui", "-nopython", nullptr};
 
-extern "C" void modl_reg() {};
+extern "C" void modl_reg(){};
 
 void setup_neuron_api(void) {
-    void* handle = dlopen("libnrniv.dylib", RTLD_NOW | RTLD_LOCAL); 
+    void* handle = dlopen("libnrniv.dylib", RTLD_NOW | RTLD_LOCAL);
     if (!handle) {
         cout << "Couldn't open dylib." << endl << dlerror() << endl;
         exit(-1);
-    }    
-    nrn_init = reinterpret_cast<decltype(nrn_init)>(dlsym(handle, "nrn_init")); 
+    }
+    nrn_init = reinterpret_cast<decltype(nrn_init)>(dlsym(handle, "nrn_init"));
     assert(nrn_init);
-    nrn_push_str = reinterpret_cast<decltype(nrn_push_str)>(dlsym(handle, "nrn_push_str")); 
-    nrn_call_function = reinterpret_cast<decltype(nrn_call_function)>(dlsym(handle, "nrn_call_function")); 
-    nrn_get_symbol = reinterpret_cast<decltype(nrn_get_symbol)>(dlsym(handle, "nrn_get_symbol")); 
-    nrn_pop_double = reinterpret_cast<decltype(nrn_pop_double)>(dlsym(handle, "nrn_pop_double")); 
-    nrn_push_section = reinterpret_cast<decltype(nrn_push_section)>(dlsym(handle, "nrn_push_section")); 
-    nrn_new_section = reinterpret_cast<decltype(nrn_new_section)>(dlsym(handle, "nrn_new_section")); 
-    nrn_set_nseg = reinterpret_cast<decltype(nrn_set_nseg)>(dlsym(handle, "nrn_set_nseg")); 
-    nrn_insert_mechanism = reinterpret_cast<decltype(nrn_insert_mechanism)>(dlsym(handle, "nrn_insert_mechanism")); 
-    nrn_push_double = reinterpret_cast<decltype(nrn_push_double)>(dlsym(handle, "nrn_push_double")); 
-    nrn_new_object = reinterpret_cast<decltype(nrn_new_object)>(dlsym(handle, "nrn_new_object")); 
-    nrn_get_pp_property_ptr = reinterpret_cast<decltype(nrn_get_pp_property_ptr)>(dlsym(handle, "nrn_get_pp_property_ptr"));     
-    nrn_push_double_ptr = reinterpret_cast<decltype(nrn_push_double_ptr)>(dlsym(handle, "nrn_push_double_ptr")); 
-    nrn_get_rangevar_ptr = reinterpret_cast<decltype(nrn_get_rangevar_ptr)>(dlsym(handle, "nrn_get_rangevar_ptr")); 
-    nrn_call_method = reinterpret_cast<decltype(nrn_call_method)>(dlsym(handle, "nrn_call_method")); 
-    nrn_unref_object = reinterpret_cast<decltype(nrn_unref_object)>(dlsym(handle, "nrn_unref_object")); 
-    nrn_vector_capacity = reinterpret_cast<decltype(nrn_vector_capacity)>(dlsym(handle, "nrn_vector_capacity"));     
-    nrn_vector_data_ptr = reinterpret_cast<decltype(nrn_vector_data_ptr)>(dlsym(handle, "nrn_vector_data_ptr"));
-    nrn_get_method_symbol = reinterpret_cast<decltype(nrn_get_method_symbol)>(dlsym(handle, "nrn_get_method_symbol")); 
-    nrn_call_method = reinterpret_cast<decltype(nrn_call_method)>(dlsym(handle, "nrn_call_method")); 
+    nrn_push_str = reinterpret_cast<decltype(nrn_push_str)>(dlsym(handle, "nrn_push_str"));
+    nrn_call_function = reinterpret_cast<decltype(nrn_call_function)>(
+        dlsym(handle, "nrn_call_function"));
+    nrn_get_symbol = reinterpret_cast<decltype(nrn_get_symbol)>(dlsym(handle, "nrn_get_symbol"));
+    nrn_pop_double = reinterpret_cast<decltype(nrn_pop_double)>(dlsym(handle, "nrn_pop_double"));
+    nrn_push_section = reinterpret_cast<decltype(nrn_push_section)>(
+        dlsym(handle, "nrn_push_section"));
+    nrn_new_section = reinterpret_cast<decltype(nrn_new_section)>(dlsym(handle, "nrn_new_section"));
+    nrn_set_nseg = reinterpret_cast<decltype(nrn_set_nseg)>(dlsym(handle, "nrn_set_nseg"));
+    nrn_insert_mechanism = reinterpret_cast<decltype(nrn_insert_mechanism)>(
+        dlsym(handle, "nrn_insert_mechanism"));
+    nrn_push_double = reinterpret_cast<decltype(nrn_push_double)>(dlsym(handle, "nrn_push_double"));
+    nrn_new_object = reinterpret_cast<decltype(nrn_new_object)>(dlsym(handle, "nrn_new_object"));
+    nrn_get_pp_property_ptr = reinterpret_cast<decltype(nrn_get_pp_property_ptr)>(
+        dlsym(handle, "nrn_get_pp_property_ptr"));
+    nrn_push_double_ptr = reinterpret_cast<decltype(nrn_push_double_ptr)>(
+        dlsym(handle, "nrn_push_double_ptr"));
+    nrn_get_rangevar_ptr = reinterpret_cast<decltype(nrn_get_rangevar_ptr)>(
+        dlsym(handle, "nrn_get_rangevar_ptr"));
+    nrn_call_method = reinterpret_cast<decltype(nrn_call_method)>(dlsym(handle, "nrn_call_method"));
+    nrn_unref_object = reinterpret_cast<decltype(nrn_unref_object)>(
+        dlsym(handle, "nrn_unref_object"));
+    nrn_vector_capacity = reinterpret_cast<decltype(nrn_vector_capacity)>(
+        dlsym(handle, "nrn_vector_capacity"));
+    nrn_vector_data_ptr = reinterpret_cast<decltype(nrn_vector_data_ptr)>(
+        dlsym(handle, "nrn_vector_data_ptr"));
+    nrn_get_method_symbol = reinterpret_cast<decltype(nrn_get_method_symbol)>(
+        dlsym(handle, "nrn_get_method_symbol"));
+    nrn_call_method = reinterpret_cast<decltype(nrn_call_method)>(dlsym(handle, "nrn_call_method"));
     nrn_pop_object = reinterpret_cast<decltype(nrn_pop_object)>(dlsym(handle, "nrn_pop_object"));
-    nrn_get_symbol_ptr = reinterpret_cast<decltype(nrn_get_symbol_ptr)>(dlsym(handle, "nrn_get_symbol_ptr")); 
+    nrn_get_symbol_ptr = reinterpret_cast<decltype(nrn_get_symbol_ptr)>(
+        dlsym(handle, "nrn_get_symbol_ptr"));
 }
 
 int main(void) {
@@ -66,7 +77,7 @@ int main(void) {
     // topology
     soma = nrn_new_section("soma");
     nrn_set_nseg(soma, 3);
-    
+
     // define soma morphology with two 3d points
     nrn_push_section(soma);
     // (0, 0, 0, 10)
--- test/api/sections.c
+++ test/api/sections.c
@@ -7,34 +7,41 @@
 
 static const char* argv[] = {"sections", "-nogui", "-nopython", NULL};
 
-void modl_reg() {};
+void modl_reg(){};
 
 void setup_neuron_api(void) {
-    void* handle = dlopen("libnrniv.dylib", RTLD_NOW | RTLD_LOCAL); 
+    void* handle = dlopen("libnrniv.dylib", RTLD_NOW | RTLD_LOCAL);
     if (!handle) {
         printf("Couldn't open dylib.\n%s\n", dlerror());
         exit(-1);
-    }    
-    nrn_init = (int (*)(int, const char **))(dlsym(handle, "nrn_init")); 
-    assert(nrn_init); /* NOTE: this function only exists in versions of NEURON with the API defined */
-    nrn_new_section = (Section *(*)(char const *))(dlsym(handle, "nrn_new_section"));
-    nrn_connect_sections = (void (*)(Section*, double, Section*, double))(dlsym(handle, "nrn_connect_sections")); 
-    nrn_set_nseg = (void (*)(Section*, int))(dlsym(handle, "nrn_set_nseg")); 
-    nrn_call_function = (void (*)(Symbol*, int))(dlsym(handle, "nrn_call_function")); 
-    nrn_get_symbol = (Symbol* (*)(char const *))(dlsym(handle, "nrn_get_symbol"));
-    nrn_new_object = (Object* (*)(Symbol *, int))(dlsym(handle, "nrn_new_object"));
-    nrn_push_section = (void (*)(Section *))(dlsym(handle, "nrn_push_section"));
+    }
+    nrn_init = (int (*)(int, const char**))(dlsym(handle, "nrn_init"));
+    assert(nrn_init); /* NOTE: this function only exists in versions of NEURON with the API defined
+                       */
+    nrn_new_section = (Section * (*) (char const*) )(dlsym(handle, "nrn_new_section"));
+    nrn_connect_sections = (void (*)(Section*, double, Section*, double))(
+        dlsym(handle, "nrn_connect_sections"));
+    nrn_set_nseg = (void (*)(Section*, int))(dlsym(handle, "nrn_set_nseg"));
+    nrn_call_function = (void (*)(Symbol*, int))(dlsym(handle, "nrn_call_function"));
+    nrn_get_symbol = (Symbol * (*) (char const*) )(dlsym(handle, "nrn_get_symbol"));
+    nrn_new_object = (Object * (*) (Symbol*, int) )(dlsym(handle, "nrn_new_object"));
+    nrn_push_section = (void (*)(Section*))(dlsym(handle, "nrn_push_section"));
     nrn_pop_section = (void (*)(void))(dlsym(handle, "nrn_pop_section"));
-    nrn_get_method_symbol = (Symbol* (*)(Object*, char const*))(dlsym(handle, "nrn_get_method_symbol"));
+    nrn_get_method_symbol = (Symbol *
+                             (*) (Object*, char const*) )(dlsym(handle, "nrn_get_method_symbol"));
     nrn_call_method = (void (*)(Object*, Symbol*, int))(dlsym(handle, "nrn_call_method"));
-    nrn_new_sectionlist_iterator = (SectionListIterator* (*)(hoc_Item*))(dlsym(handle, "nrn_new_sectionlist_iterator"));
-    nrn_get_allsec = (hoc_Item* (*)(void))(dlsym(handle, "nrn_get_allsec"));
-    nrn_sectionlist_iterator_done = (int (*)(SectionListIterator*))(dlsym(handle, "nrn_sectionlist_iterator_done"));
-    nrn_sectionlist_iterator_next = (Section* (*)(SectionListIterator*))(dlsym(handle, "nrn_sectionlist_iterator_next"));
-    nrn_free_sectionlist_iterator = (void (*)(SectionListIterator*))(dlsym(handle, "nrn_free_sectionlist_iterator"));
-    nrn_secname = (char const* (*)(Section*))(dlsym(handle, "nrn_secname"));
-    nrn_get_sectionlist_data = (hoc_Item* (*)(Object*))(dlsym(handle, "nrn_get_sectionlist_data"));
-
+    nrn_new_sectionlist_iterator = (SectionListIterator * (*) (hoc_Item*) )(
+        dlsym(handle, "nrn_new_sectionlist_iterator"));
+    nrn_get_allsec = (hoc_Item * (*) (void) )(dlsym(handle, "nrn_get_allsec"));
+    nrn_sectionlist_iterator_done = (int (*)(SectionListIterator*))(
+        dlsym(handle, "nrn_sectionlist_iterator_done"));
+    nrn_sectionlist_iterator_next = (Section * (*) (SectionListIterator*) )(
+        dlsym(handle, "nrn_sectionlist_iterator_next"));
+    nrn_free_sectionlist_iterator = (void (*)(SectionListIterator*))(
+        dlsym(handle, "nrn_free_sectionlist_iterator"));
+    nrn_secname = (char const* (*) (Section*) )(dlsym(handle, "nrn_secname"));
+    nrn_get_sectionlist_data = (hoc_Item *
+                                (*) (Object*) )(dlsym(handle, "nrn_get_sectionlist_data"));
 }
 
 int main(void) {
@@ -52,7 +59,7 @@ int main(void) {
     nrn_connect_sections(dend3, 0, dend1, 1);
     nrn_connect_sections(axon, 0, soma, 0);
     nrn_set_nseg(axon, 5);
-    
+
     // print out the morphology
     nrn_call_function(nrn_get_symbol("topology"), 0);
 
@@ -65,17 +72,17 @@ int main(void) {
     /* loop over allsec, print out */
     printf("allsec:\n");
     SectionListIterator* sli = nrn_new_sectionlist_iterator(nrn_get_allsec());
-    for (;!nrn_sectionlist_iterator_done(sli);) { 
-        Section* sec=nrn_sectionlist_iterator_next(sli);
+    for (; !nrn_sectionlist_iterator_done(sli);) {
+        Section* sec = nrn_sectionlist_iterator_next(sli);
         printf("    %s\n", nrn_secname(sec));
     }
     nrn_free_sectionlist_iterator(sli);
 
     printf("\ndend1's subtree:\n");
     sli = nrn_new_sectionlist_iterator(nrn_get_sectionlist_data(seclist));
-    for (;!nrn_sectionlist_iterator_done(sli);) { 
-        Section* sec=nrn_sectionlist_iterator_next(sli);
+    for (; !nrn_sectionlist_iterator_done(sli);) {
+        Section* sec = nrn_sectionlist_iterator_next(sli);
         printf("    %s\n", nrn_secname(sec));
     }
-    nrn_free_sectionlist_iterator(sli);    
+    nrn_free_sectionlist_iterator(sli);
 }
