d22c043c3d118ca4acf9844cb5403d853139467e
Siyan Guo <amygdk@Siyans-MBP.wireless.yale.internal>
2023-06-02 12:18:04 -0400

    For adding only nodes to nodelist. Please check before merging.

---
+++ test/rxd/test_nodelist.py
@@ -0,0 +1,85 @@
+def test_only_nodes(neuron_instance):
+    """Test to make sure node lists only contain nodes"""
+
+    h, rxd, data, save_path = neuron_instance
+
+    dend = h.Section(name="dend")
+    r = rxd.Region(h.allsec())
+    hydrogen = rxd.Species(r, initial=1)
+    water = rxd.Species(r, initial=1)
+
+    h.finitialize(-65)
+
+    nodelist = hydrogen.nodes
+
+    # test that should not work, so an append that succeeds is an error
+
+    try:
+        nodelist.append(water.nodes) #append nodelist
+        raise Exception("should not get here")
+    except TypeError:
+        ...
+
+    try:
+        nodelist.extend([1,2,3,water.nodes[0]]) #extend with non-nodes
+        raise Exception("should not get here")
+    except TypeError:
+        ...
+
+    try:
+        nodelist[0] = 17 
+        raise Exception("should not get here")
+    except TypeError:
+        ...
+        
+    try:
+        nl = rxd.nodelist.NodeList([1,2,3,water.nodes[0]])   #create NodeList with non-nodes
+        raise Exception("should not get here")
+    except TypeError:
+        ...
+
+    try:
+        nodelist.insert(1, "llama") #insert non-node into nodelist
+        raise Exception("should not get here")
+    except TypeError:
+        ...
+        
+    # test that should work, so getting in the except is an error
+    try:
+        nodelist.append(water.nodes[0]) #append node
+    except TypeError:
+        print("The appended item is not a node.")
+        raise Exception("should not get here")
+
+    try:
+        original_length = len(nodelist) #extend nodes
+        nodelist.extend(item for item in water.nodes)
+        assert(len(nodelist) == original_length + len(water.nodes))
+    except TypeError:
+        print("The extended list are not all nodes.")
+        raise Exception("should not get here")
+
+    try:
+        nodelist[0] = water.nodes[0] 
+    except TypeError:
+        print("Nodelist should only contain nodes.")
+        raise Exception("should not get here")
+
+    try:
+        nl = rxd.nodelist.NodeList([water.nodes[0], water.nodes[0]]) #create nodelist with nodes
+    except TypeError:
+        print("Nodelist should only contain nodes.")
+        raise Exception("should not get here")
+
+
+    try:
+        nodelist.insert(1, water.nodes[0]) #insert node into nodelist
+    except TypeError:
+        print("Only nodes can be inserted into the nodelist.")
+        raise Exception("should not get here")
+
+    try:
+        nl = rxd.nodelist.NodeList([]) #create empty nodelist
+    except TypeError:
+        print("Only nodes can be inserted into the nodelist.")
+        raise Exception("should not get here")
