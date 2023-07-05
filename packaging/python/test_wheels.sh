11a67b1a2a5c3eb10c10c2a0bbfc5755146b2a2a
Alexandru Săvulescu <alexandru.savulescu@epfl.ch>
2023-07-05 13:59:22 +0200

    retrofit: macOS arm64 wheels: delocate workaround (#1897) (#2413)
    * retrofit: macOS arm64 wheels: delocate workaround (#1897)
    * fix MPI testing via `DYLD_LIBRARY_PATH`
      * mpich as well

--- packaging/python/test_wheels.sh
+++ packaging/python/test_wheels.sh
@@ -203,16 +203,12 @@ run_parallel_test() {
     # this is for MacOS system
     if [[ "$OSTYPE" == "darwin"* ]]; then
       # assume both MPIs are installed via brew.
+      BREW_PREFIX=$(brew --prefix)
 
       brew unlink openmpi
       brew link mpich
-      BREW_PREFIX=$(brew --prefix)
-
-      # TODO : latest mpich has issuee on Azure OSX
-      if [[ "$CI_OS_NAME" == "osx" ]]; then
-          export DYLD_LIBRARY_PATH=${BREW_PREFIX}/opt/mpich/lib:$DYLD_LIBRARY_PATH
-          run_mpi_test "${BREW_PREFIX}/opt/mpich/bin/mpirun" "MPICH" ""
-      fi
+      export DYLD_LIBRARY_PATH=${BREW_PREFIX}/opt/mpich/lib:$DYLD_LIBRARY_PATH
+      run_mpi_test "${BREW_PREFIX}/opt/mpich/bin/mpirun" "MPICH" ""
 
       brew unlink mpich
       brew link openmpi
