3407b1c5a54b9a9f4fe1d6cb276745e5f7a38a46
Nicolas Cornu <nicolas.cornu@epfl.ch>
2023-09-02 11:08:12 +0200

    Format

--- test/unit_tests/matrix.cpp
+++ test/unit_tests/matrix.cpp
@@ -303,7 +303,7 @@ SCENARIO("A Matrix", "[neuron_ivoc][OcMatrix]") {
             c.vec() = {0., 0., v(2, 0), v(2, 1), v(2, 2)};
             CHECK_THAT(c.vec(), ApproxOrOpposite({0., 0., 0.66666, -0.66666, -0.3333333}));
         }
-        { // Try with vectors too short
+        {  // Try with vectors too short
             IvocVect s(2);
             s.vec() = {1., 2.};
             m.setrow(0, &s);
