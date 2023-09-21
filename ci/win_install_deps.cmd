8362391b24c1e8220833fc400b5abe453c23fbd9
Erik Heeren <erik.heeren@epfl.ch>
2023-09-21 17:02:31 +0200

    Attempt 2

--- ci/win_install_deps.cmd
+++ ci/win_install_deps.cmd
@@ -71,7 +71,7 @@ mingw-w64-x86_64-python3-pip ^
 mingw64/mingw-w64-x86_64-dlfcn ^
 mingw-w64-x86_64-toolchain || goto :error
 
-%MSYS2_ROOT%\usr\bin\pip install py-build-cmake
+%MSYS2_ROOT%\usr\bin\bash -lc "/mingw64/bin/pip install py-build-cmake"
 
 :: if all goes well, go to end
 goto :EOF
