6b217e0117a917b4219361a5bee3f947a571bbaf
Pramod S Kumbhar <pramod.s.kumbhar@gmail.com>
2023-09-26 19:02:29 -0400

    Remove chocolatey install as its no useful?

--- ci/win_install_deps.cmd
+++ ci/win_install_deps.cmd
@@ -71,6 +71,9 @@ mingw-w64-x86_64-python3-pip ^
 mingw64/mingw-w64-x86_64-dlfcn ^
 mingw-w64-x86_64-toolchain || goto :error
 
+:: Remove Choco artifacts
+rmdir /s /q C:\ProgramData\chocolatey
+
 :: if all goes well, go to end
 goto :EOF
 
