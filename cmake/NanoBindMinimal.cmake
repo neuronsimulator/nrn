9e2e0e71c1a3d8adf08fbc2f541e0cef48d6003e
Fernando Pereira <fernando.pereira@epfl.ch>
2023-09-28 16:10:50 -0400

    Create nanobind target with better flags. No default visibility

--- cmake/NanoBindMinimal.cmake
+++ cmake/NanoBindMinimal.cmake
@@ -2,17 +2,21 @@
 # minimal nanobind interface
 # =============================================================================
 
-set(NB_DIR "external/nanobind")
+set(NB_DIR ${PROJECT_SOURCE_DIR}/external/nanobind)
 
-add_library(nanobind INTERFACE)
-target_sources(
-  nanobind
-  INTERFACE ${NB_DIR}/src/common.cpp
-            ${NB_DIR}/src/error.cpp
-            ${NB_DIR}/src/nb_internals.cpp
-            ${NB_DIR}/src/nb_static_property.cpp
-            ${NB_DIR}/src/nb_type.cpp
-            ${NB_DIR}/src/nb_func.cpp
-            ${NB_DIR}/src/implicit.cpp)
+set(NB_SOURCE_FILES
+    ${NB_DIR}/src/common.cpp
+    ${NB_DIR}/src/error.cpp
+    ${NB_DIR}/src/nb_internals.cpp
+    ${NB_DIR}/src/nb_static_property.cpp
+    ${NB_DIR}/src/nb_type.cpp
+    ${NB_DIR}/src/nb_func.cpp
+    ${NB_DIR}/src/implicit.cpp)
 
-target_include_directories(nanobind INTERFACE ${NB_DIR}/include ${NB_DIR}/ext/robin_map/include)
+function(make_nanobind_target target_name pyinc)
+  add_library(${target_name} OBJECT ${NB_SOURCE_FILES})
+  target_include_directories(${target_name} PUBLIC ${NB_DIR}/include)
+  target_include_directories(${target_name} PRIVATE ${NB_DIR}/ext/robin_map/include ${pyinc})
+  set_property(TARGET ${target_name} PROPERTY C_VISIBILITY_PRESET hidden)
+  set_property(TARGET ${target_name} PROPERTY POSITION_INDEPENDENT_CODE True)
+endfunction()
