1e8dfaaf936767454f585d77a27f47df2f6172d3
Matthias Wolf <matthias.wolf@epfl.ch>
2023-10-06 10:28:09 +0200

    ❄️

--- src/nmodl/modl.cpp
+++ src/nmodl/modl.cpp
@@ -280,7 +280,8 @@ static void openfiles(char* given_filename, char* output_dir) {
     if (output_dir) {
         try {
             fs::create_directories(output_dir);
-        } except (...) {
+        }
+        except(...) {
             fprintf(stderr, "Can't create output directory %s\n", output_dir);
             exit(1);
         }
