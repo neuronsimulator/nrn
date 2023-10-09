eac056efc1c5e2dec4c92b3001e2bb92a168557f
Pramod S Kumbhar <pramod.s.kumbhar@gmail.com>
2023-10-09 22:41:25 +0200

    put back single quotes for sed

--- cmake/MacroHelper.cmake
+++ cmake/MacroHelper.cmake
@@ -213,7 +213,7 @@ macro(nocmodl_mod_to_cpp modfile_basename)
       ${CMAKE_COMMAND} -E env "MODLUNIT=${PROJECT_BINARY_DIR}/share/nrn/lib/nrnunits.lib"
       ${NRN_NOCMODL_SANITIZER_ENVIRONMENT} $<TARGET_FILE:nocmodl>
       ${PROJECT_SOURCE_DIR}/${modfile_basename}.mod
-    COMMAND sed "s/_reg()/_reg_()/" ${PROJECT_SOURCE_DIR}/${modfile_basename}.cpp >
+    COMMAND sed "'s/_reg()/_reg_()/'" ${PROJECT_SOURCE_DIR}/${modfile_basename}.cpp >
             ${PROJECT_BINARY_DIR}/${modfile_basename}.cpp
     COMMAND ${CMAKE_COMMAND} -E rm ${PROJECT_SOURCE_DIR}/${modfile_basename}.cpp
     DEPENDS nocmodl ${PROJECT_SOURCE_DIR}/${modfile_basename}.mod
