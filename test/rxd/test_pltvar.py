95c9af47cc5206a7a248a5d62f94b5235a22a650
Rickeim Gourdine <rickeim.gourdine@yale.edu>
2023-10-20 15:18:04 -0400

    resolve conversations

--- test/rxd/test_pltvar.py
+++ test/rxd/test_pltvar.py
@@ -1,4 +1,5 @@
 import pytest
+import plotly
 from neuron import units
 from matplotlib import pyplot
 
@@ -6,10 +7,7 @@ from matplotlib import pyplot
 def test_plt_variable(neuron_instance):
     """Test to make sure species with multiple regions is not plotted"""
 
-    h, rxd, data, save_path = neuron_instance
-    import plotly
-
-    h.load_file("stdrun.hoc")
+    h, rxd, _, _ = neuron_instance
 
     dend1 = h.Section("dend1")
     dend2 = h.Section("dend2")
@@ -32,7 +30,7 @@ def test_plt_variable(neuron_instance):
     ca.nodes(dend1(0.5))[0].include_flux(1e-13, units="mmol/ms")
 
     h.finitialize(-65 * units.mV)
-    h.continuerun(50 * units.ms)
+    h.fadvance()
 
     ps = h.PlotShape(False)
 
@@ -46,9 +44,24 @@ def test_plt_variable(neuron_instance):
         ps.variable(ca)
         ps.plot(plotly)
 
+    cb = rxd.Species(
+        [cyt],
+        name="cb",
+        charge=2,
+        initial=0 * units.mM,
+        d=1 * units.µm**2 / units.ms,
+    )
+
     # Scenarios that should work
     ps.variable(ca[cyt])
     ps.plot(plotly)  # No error expected here
 
     ps.variable(ca[cyt])
     ps.plot(pyplot)  # No error expected here
+
+    # Test plotting with only one region
+    ps.variable(cb)
+    ps.plot(plotly)  # No Error expected here
+
+    ps.variable(cb)
+    ps.plot(pyplot)  # No Error expected here
