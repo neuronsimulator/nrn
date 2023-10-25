07c32b62dd8526d252ae6032a01c25d004bfa0af
Rickeim Gourdine <rickeim.gourdine@yale.edu>
2023-10-25 18:30:42 -0400

    removed units

--- test/rxd/test_nodelist_include_flux.py
+++ test/rxd/test_nodelist_include_flux.py
@@ -1,20 +1,17 @@
-from neuron.units import mM, mV
-
-
 def test_nodelist_include_flux(neuron_instance):
-    h, rxd, data, save_path = neuron_instance
+    h, rxd, _, _ = neuron_instance
     dend1 = h.Section("dend1")
     diff = 1e-15
     cyt = rxd.Region(dend1.wholetree(), nrn_region="i")
-    ca1 = rxd.Species(cyt, name="ca1", charge=2, initial=0 * mM)
-    ca2 = rxd.Species(cyt, name="ca2", charge=2, initial=0 * mM)
+    ca1 = rxd.Species(cyt, name="ca1", charge=2, initial=0)
+    ca2 = rxd.Species(cyt, name="ca2", charge=2, initial=0)
 
     ca1.nodes(dend1(0.5)).include_flux(1e-16, units="mmol/ms")
     ca2.nodes(dend1(0.5))[0].include_flux(1e-16, units="mmol/ms")
     node1 = ca1.nodes(dend1(0.5))[0]
     node2 = ca2.nodes(dend1(0.5))[0]
 
-    h.finitialize(-65 * mV)
+    h.finitialize(-65)
     h.fadvance()
 
     assert abs(node1.concentration - 1.2732395447351626e-10) < diff
