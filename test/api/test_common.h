ab1b15231fdfc3ea9d281a968f90679339bb9f8c
Fernando Pereira <fernando.pereira@epfl.ch>
2023-12-19 00:56:54 +0100

    api: Make tests resilient to accumulated errors

--- test/api/test_common.h
+++ test/api/test_common.h
@@ -6,16 +6,15 @@
 
 constexpr double EPSILON = 1.0 / 1024;
 
-bool almost_equal(double a, double b) {
-    return std::fabs(a - b) < EPSILON;
+/// Compare two doubles. Mitigate accumulated errors by updating a drift
+inline bool almost_equal(double a, double b, double* drift) {
+    double diff = b - a;
+    bool is_ok = std::fabs(diff - *drift) < EPSILON;
+    *drift = diff;
+    return is_ok;
 }
 
 /// @brief Compared volatges from a csv file against raw vectors of tvec and voltage
-/// @param ref_csv
-/// @param tvec
-/// @param vvec
-/// @param n_spikes
-/// @return
 bool compare_spikes(const char* ref_csv, double* tvec, double* vvec, long n_voltages) {
     std::ifstream ref_file(ref_csv);
     if (!ref_file.is_open()) {
@@ -25,6 +24,8 @@ bool compare_spikes(const char* ref_csv, double* tvec, double* vvec, long n_volt
 
     std::string line;
     long cur_line = 0;
+    double t_drift = 0.;
+    double v_drift = 0.;
 
     while (std::getline(ref_file, line) && cur_line < n_voltages) {
         size_t end;
@@ -32,21 +33,21 @@ bool compare_spikes(const char* ref_csv, double* tvec, double* vvec, long n_volt
         auto vref = std::strtof(line.c_str() + end + 1, NULL);
         // ref values have exponent around 0, so we compare absolute values
         // and our epsilon is relatively large
-        if (!almost_equal(tvec[cur_line], tref)) {
+        if (!almost_equal(tvec[cur_line], tref, &t_drift)) {
             std::cerr << "DIFF at line " << cur_line << ": " << tvec[cur_line] << "!=" << tref
                       << std::endl;
             return false;
         }
-        if (!almost_equal(vvec[cur_line], vref)) {
+        if (!almost_equal(vvec[cur_line], vref, &v_drift)) {
             std::cerr << "DIFF at line " << cur_line << ": " << vvec[cur_line] << "!=" << vref
-                      << std::endl;
+                      << " (Drift=" << v_drift << ')' << std::endl;
             return false;
         }
         ++cur_line;
     }
 
     if (cur_line != n_voltages) {
-        std::cerr << "Unexpected array length: " << cur_line << " (Ref: " << cur_line << ')'
+        std::cerr << "Unexpected array length: " << cur_line << " (Ref: " << n_voltages << ')'
                   << std::endl;
         return false;
     }
