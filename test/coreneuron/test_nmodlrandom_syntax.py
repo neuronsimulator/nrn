b29c2b8389be3c7b87cbec68ad41a0d3236e3dd5
Michael Hines <michael.hines@yale.edu>
2023-12-27 19:17:09 -0500

    unlink issue sometimes also for temp.mod

--- test/coreneuron/test_nmodlrandom_syntax.py
+++ test/coreneuron/test_nmodlrandom_syntax.py
@@ -63,7 +63,7 @@ def chk_nmodl(txt, program="nocmodl", rcode=False):
     result = run([program, "temp.mod"])
     ret = (result.returncode == 0) == rcode
     if ret:
-        pathlib.Path.unlink("temp.mod")
+        pathlib.Path.unlink("temp.mod", missing_ok=True)
         pathlib.Path.unlink("temp.cpp", missing_ok=True)
     else:
         print("chk_nmodl ", program, " return code ", result.returncode)
