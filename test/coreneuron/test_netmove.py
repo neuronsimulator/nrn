1b5c860c2a341e5da6c924ba16128ccc5158f193
Goran Jelic-Cizmek <goran.jelic-cizmek@epfl.ch>
2024-01-09 23:14:45 +0100

    Remove distutils usage
    Backported using af37b3f28d61b4a0d04c64f87d170e6ddf3b2a8c and
    799a104f42c88dd19d54ada4b5f8d26bff0169b1
    
    * remove distutils usage
      * follow PEP632
      * add own `strtobool`
      * use `new_compiler` from `setuptools.build_ext` for now
    
    * drop `SETUPTOOLS_USE_DISTUTILS=stdlib`
    * allow Cython 3

--- test/coreneuron/test_netmove.py
+++ test/coreneuron/test_netmove.py
@@ -1,6 +1,6 @@
 # Basically want to test that net_move statement doesn't get
 # mixed up with other instances.
-import distutils.util
+from neuron.tests.utils.strtobool import strtobool
 import os
 
 from neuron import h
@@ -79,9 +79,7 @@ def test_netmove():
     h.CVode().cache_efficient(1)
     coreneuron.enable = True
     coreneuron.verbose = 0
-    coreneuron.gpu = bool(
-        distutils.util.strtobool(os.environ.get("CORENRN_ENABLE_GPU", "false"))
-    )
+    coreneuron.gpu = bool(strtobool(os.environ.get("CORENRN_ENABLE_GPU", "false")))
 
     def runassert(mode):
         run(tstop, mode)
