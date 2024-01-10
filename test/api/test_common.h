d23f8918c0d11c2e08a44561cc571229614983c0
Fernando Pereira <fernando.pereira@epfl.ch>
2024-01-10 12:31:27 +0100

    Creating catch unit tests for API

--- test/api/test_common.h
+++ test/api/test_common.h
@@ -1,59 +1,12 @@
-#ifdef __cplusplus
+#include <string.h>
 
+#ifdef __cplusplus
+#include <iostream>
 #include <fstream>
 #include <string>
 #include <cmath>
 
 constexpr double EPSILON = 0x1p-16;  // 1>>16 for double
-
-/// Compare two doubles. Mitigate accumulated errors by updating a drift
-inline bool almost_equal(double a, double b, double* drift) {
-    double diff = b - a;
-    bool is_ok = std::fabs(diff - *drift) < EPSILON;
-    *drift = diff;
-    return is_ok;
-}
-
-/// @brief Compared volatges from a csv file against raw vectors of tvec and voltage
-inline bool compare_spikes(const char* ref_csv, double* tvec, double* vvec, long n_voltages) {
-    std::ifstream ref_file(ref_csv);
-    if (!ref_file.is_open()) {
-        std::cerr << "Bad ref file: " << ref_csv << std::endl;
-        return false;
-    }
-
-    std::string line;
-    long cur_line = 0;
-    double t_drift = 0.;
-    double v_drift = 0.;
-
-    while (std::getline(ref_file, line) && cur_line < n_voltages) {
-        size_t end;
-        auto tref = std::stod(line, &end);
-        auto vref = std::strtof(line.c_str() + end + 1, NULL);
-        // ref values have exponent around 0, so we compare absolute values
-        // and our epsilon is relatively large
-        if (!almost_equal(tvec[cur_line], tref, &t_drift)) {
-            std::cerr << "DIFF at line " << cur_line << ": " << tvec[cur_line] << "!=" << tref
-                      << std::endl;
-            return false;
-        }
-        if (!almost_equal(vvec[cur_line], vref, &v_drift)) {
-            std::cerr << "DIFF at line " << cur_line << ": " << vvec[cur_line] << "!=" << vref
-                      << " (Drift=" << v_drift << ')' << std::endl;
-            return false;
-        }
-        ++cur_line;
-    }
-
-    if (cur_line != n_voltages) {
-        std::cerr << "Unexpected array length: " << cur_line << " (Ref: " << n_voltages << ')'
-                  << std::endl;
-        return false;
-    }
-
-    return true;
-}
 
 inline bool approximate(const std::initializer_list<double>& reference, Object* v) {
     long v_size = nrn_vector_size(v);
@@ -79,21 +32,21 @@ inline bool approximate(const std::initializer_list<double>& reference, Object* 
 
 inline double nrn_function_call_s(const char* func_name, const char* v) {
     char* temp_str = strdup(v);
-    auto x = nrn_function_call(func_name, NRN_ARG_STR_PTR, &temp_str);
+    double x = nrn_function_call(func_name, NRN_ARG_STR_PTR, &temp_str);
     free(temp_str);
     return x;
 }
 
 inline Object* nrn_object_new_s(const char* cls_name, const char* v) {
     char* temp_str = strdup(v);
-    auto x = nrn_object_new(cls_name, NRN_ARG_STR_PTR, &temp_str);
+    Object* x = nrn_object_new(cls_name, NRN_ARG_STR_PTR, &temp_str);
     free(temp_str);
     return x;
 }
 
-inline decltype(auto) nrn_method_call_s(Object* obj, const char* method_name, const char* v) {
+inline NrnResult nrn_method_call_s(Object* obj, const char* method_name, const char* v) {
     char* temp_str = strdup(v);
-    auto x = nrn_method_call(obj, method_name, NRN_ARG_STR_PTR, &temp_str);
+    NrnResult x = nrn_method_call(obj, method_name, NRN_ARG_STR_PTR, &temp_str);
     free(temp_str);
     return x;
 }
