92bf3fe43fe67bb9ff5b36ba6580aec5428c40c9
Luc Grosheintz <luc.grosheintz@gmail.com>
2024-01-11 11:14:47 +0100

    Cross-check against NRN+nocmodl. (BlueBrain/nmodl#1136)
    NMODL Repo SHA: BlueBrain/nmodl@72af0a3d0eeca44d86da3a82f34ba5a99b70dd6f

--- test/nmodl/transpiler/usecases/run_test.sh
+++ test/nmodl/transpiler/usecases/run_test.sh
@@ -7,8 +7,13 @@ usecase_dir="$2"
 
 pushd "${usecase_dir}"
 
+# NRN + nocmodl
 rm -r "${output_dir}" tmp || true
+nrnivmodl
+"$(uname -m)/special" -nogui simulate.py
 
+# NRN + NMODL
+rm -r "${output_dir}" tmp || true
 nrnivmodl -nmodl "${nmodl}"
 "$(uname -m)/special" -nogui simulate.py
 
