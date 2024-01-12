1c198c5c8b79e869ddc6e2afb4c49a3e11b10d7a
Weina Ji <weina.ji@epfl.ch>
2024-01-12 16:24:53 +0100

    review comment, split read_phase3

--- src/coreneuron/io/nrn_setup.cpp
+++ src/coreneuron/io/nrn_setup.cpp
@@ -34,6 +34,7 @@
 #include "coreneuron/utils/nrnoc_aux.hpp"
 #include "coreneuron/io/phase1.hpp"
 #include "coreneuron/io/phase2.hpp"
+#include "coreneuron/io/phase3.hpp"
 #include "coreneuron/io/mech_report.h"
 #include "coreneuron/io/reports/nrnreport.hpp"
 
@@ -73,18 +74,6 @@ int (*nrn2core_all_spike_vectors_return_)(std::vector<double>& spikevec, std::ve
 
 void (*nrn2core_all_weights_return_)(std::vector<double*>& weights);
 
-void (*nrn2core_get_dat3_cell_count)(int& cell_count);
-void (*nrn2core_get_dat3_cellmapping)(int i, int& gid, int& nsec, int& nseg, int& n_seclist);
-void (*nrn2core_get_dat3_secmapping)(int i_c,
-                                     int i_sec,
-                                     std::string& segname,
-                                     int& nsec,
-                                     int& nseg,
-                                     size_t& total_lfp_factors,
-                                     int& n_electrode,
-                                     std::vector<int>& data_sec,
-                                     std::vector<int>& data_seg,
-                                     std::vector<double>& data_lfp);
 // file format defined in cooperation with nrncore/src/nrniv/nrnbbcore_write.cpp
 // single integers are ascii one per line. arrays are binary int or double
 // Note that regardless of the gid contents of a group, since all gids are
@@ -940,90 +929,18 @@ void read_phase2(NrnThread& nt, UserParams& userParams) {
 
 /** read mapping information for neurons */
 void read_phase3(NrnThread& nt, UserParams& userParams) {
-    /** restore checkpoint state (before restoring queue items */
-    auto& F = userParams.file_reader[nt.id];
-    F.restore_checkpoint();
-
     /** mapping information for all neurons in single NrnThread */
     NrnThreadMappingInfo* ntmapping = new NrnThreadMappingInfo();
+
+    Phase3 p3;
     if (corenrn_embedded) {
-        int count;
-        nrn2core_get_dat3_cell_count(count);
-        /** for every neuron */
-        for (int i = 0; i < count; i++) {
-            int gid;
-            int t_sec;
-            int t_seg;
-            int nseclist;
-            nrn2core_get_dat3_cellmapping(i, gid, t_sec, t_seg, nseclist);
-            auto cmap = new CellMapping(gid);
-            for (int j = 0; j < nseclist; j++) {
-                std::string sclname;
-                int n_sec;
-                int n_seg;
-                int n_electrodes;
-                size_t total_lfp_factors;
-                std::vector<int> data_sec;
-                std::vector<int> data_seg;
-                std::vector<double> data_lfp;
-                nrn2core_get_dat3_secmapping(i,
-                                             j,
-                                             sclname,
-                                             n_sec,
-                                             n_seg,
-                                             total_lfp_factors,
-                                             n_electrodes,
-                                             data_sec,
-                                             data_seg,
-                                             data_lfp);
-                auto smap = new SecMapping();
-                smap->name = sclname;
-                for (int i_seg = 0; i_seg < n_seg; i_seg++) {
-                    smap->add_segment(data_sec[i_seg], data_seg[i_seg]);
-                    ntmapping->add_segment_id(data_seg[i_seg]);
-                    int factor_offset = i_seg * n_electrodes;
-                    if (total_lfp_factors > 0) {
-                        // Abort if the factors contains a NaN
-                        nrn_assert(count_if(data_lfp.begin(), data_lfp.end(), [](double d) {
-                                       return std::isnan(d);
-                                   }) == 0);
-                        std::vector<double> segment_factors(data_lfp.begin() + factor_offset,
-                                                            data_lfp.begin() + factor_offset +
-                                                                n_electrodes);
-                        cmap->add_segment_lfp_factor(data_seg[i], segment_factors);
-                    }
-                }
-                cmap->add_sec_map(smap);
-            }
-            ntmapping->add_cell_mapping(cmap);
-        }
+        p3.read_direct(ntmapping);
     } else {
-        int count = 0;
-
-        F.read_mapping_cell_count(&count);
-
-        /** number of cells in mapping file should equal to cells in NrnThread */
-        nrn_assert(count == nt.ncell);
-
-        /** for every neuron */
-        for (int i = 0; i < nt.ncell; i++) {
-            int gid, nsec, nseg, nseclist;
-
-            // read counts
-            F.read_mapping_count(&gid, &nsec, &nseg, &nseclist);
-
-            CellMapping* cmap = new CellMapping(gid);
-
-            // read section-segment mapping for every section list
-            for (int j = 0; j < nseclist; j++) {
-                SecMapping* smap = new SecMapping();
-                F.read_mapping_info(smap, ntmapping, cmap);
-                cmap->add_sec_map(smap);
-            }
-
-            ntmapping->add_cell_mapping(cmap);
-        }
+        auto& F = userParams.file_reader[nt.id];
+        F.restore_checkpoint();
+        p3.read_file(F, ntmapping);
     }
+
     // make number #cells match with mapping size
     nrn_assert((int) ntmapping->size() == nt.ncell);
 
---
+++ src/coreneuron/io/phase3.hpp
@@ -0,0 +1,22 @@
+/*
+# =============================================================================
+# Copyright (c) 2016 - 2024 Blue Brain Project/EPFL
+#
+# See top-level LICENSE file for details.
+# =============================================================================
+*/
+
+#pragma once
+
+#include "coreneuron/io/nrn_filehandler.hpp"
+#include "coreneuron/io/user_params.hpp"
+
+namespace coreneuron {
+struct NrnThread;
+
+class Phase3 {
+  public:
+    void read_file(FileHandler& F, NrnThreadMappingInfo* ntmapping);
+    void read_direct(NrnThreadMappingInfo* ntmapping);
+};
+}  // namespace coreneuron
