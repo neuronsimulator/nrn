c1f44bcddb54852cbdb14be6088c4948d60a3843
Fernando Pereira <fernando.pereira@epfl.ch>
2024-01-18 18:33:21 +0100

    Switch dparam_semantics to unique_ptr (no raw!)

--- src/nrnoc/init.cpp
+++ src/nrnoc/init.cpp
@@ -814,9 +814,10 @@ void register_data_fields(int mechtype,
                           std::vector<std::pair<const char*, const char*>> const& dparam_info) {
     nrn_prop_param_size_[mechtype] = param_info.size();
     nrn_prop_dparam_size_[mechtype] = dparam_info.size();
-    delete[] std::exchange(memb_func[mechtype].dparam_semantics, nullptr);
-    if (!dparam_info.empty()) {
-        memb_func[mechtype].dparam_semantics = new int[dparam_info.size()];
+    if (dparam_info.empty()) {
+        memb_func[mechtype].dparam_semantics.reset(nullptr);
+    } else {
+        memb_func[mechtype].dparam_semantics.reset(new int[dparam_info.size()]);
         for (auto i = 0; i < dparam_info.size(); ++i) {
             // dparam_info[i].first is the name of the variable, currently unused...
             memb_func[mechtype].dparam_semantics[i] = dparam_semantics_to_int(
