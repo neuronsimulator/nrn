1920ed9af9e9f9de8f62809321c22304607d46a4
Michael Hines <michael.hines@yale.edu>
2024-02-04 12:41:22 -0500

    96% line coverage for hocmech.cpp

--- test/hoctests/tests/test_hocmech.py
+++ test/hoctests/tests/test_hocmech.py
@@ -1,4 +1,7 @@
 from neuron import h, gui
+from neuron.expect_hocerr import expect_err, set_quiet
+
+set_quiet(False)
 import math
 
 soma = h.Section(name="soma")
@@ -13,7 +16,9 @@ stim.amp = 0.3
 h(
     """
     begintemplate Max
-        public V
+        public V, a, b
+        a = 0
+        double b[2]
 
         proc initial() {
             V = v($1)
@@ -27,13 +32,15 @@ h(
     endtemplate Max
 """
 )
-h.make_mechanism("max", "Max")
+h.make_mechanism("max", "Max", "a b")
 
 # declare a POINT_PROCESS using HOC
 h(
     """
     begintemplate PMax
-        public V
+        public V, c, d
+        c = 0
+        double d[2]
 
         proc initial() {
             V = v($1)
@@ -47,7 +54,10 @@ h(
     endtemplate PMax
 """
 )
-h.make_pointprocess("PMax")
+pm = h.PMax()
+expect_err('h.make_pointprocess("PMax", "c d")')
+del pm
+h.make_pointprocess("PMax", "c d")
 
 soma.insert("max")
 pm = h.PMax(0.5, sec=soma)
@@ -58,3 +68,19 @@ h.run()
 print("V_max = %g" % soma(0.5).V_max)
 print("max.V = %g" % soma(0.5).max.V)
 print("pm.V = %g" % pm.V)
+
+expect_err('h.make_mechanism("max", "Max")')
+assert pm.has_loc() == True
+expect_err("pm.loc()")
+pm.loc(soma(1.0))
+print(pm.get_loc(), h.secname())
+
+mt = h.MechanismType(1)
+mt.select("PMax")
+pmaxref = h.ref(None)
+mt.make(pmaxref)  # eventually calls hoc_new_opoint
+print(pmaxref[0])
+expect_err("pmaxref[0].get_loc()")
+del mt, pmaxref
+
+expect_err('h.make_mechanism("xmax", "Max", "a 3")')
