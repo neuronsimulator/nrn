c67b182e2d22ad0a593187b87f41298bc5825dfb
Omar Awile <omar.awile@epfl.ch>
2024-02-05 16:27:33 +0100

    Add a bit more of code documentation

--- src/gnu/nrnran123.h
+++ src/gnu/nrnran123.h
@@ -37,25 +37,60 @@ struct nrnran123_array4x32 {
 extern void nrnran123_set_globalindex(std::uint32_t gix);
 extern std::uint32_t nrnran123_get_globalindex();
 
+/** Construct a new Random123 stream based on the philox4x32 generator.
+ *  
+ *  @param id1 stream ID
+ *  @param id2 optional defaults to 0
+ *  @param id3 optional defaults to 0
+ *  @return an nrnran123_State object representing this stream
+ */
 extern nrnran123_State* nrnran123_newstream(std::uint32_t id1,
                                             std::uint32_t id2 = 0,
                                             std::uint32_t id3 = 0);
-/** @brief. Deprecated, use nrnran123_newstream **/
+
+/** @deprecated use nrnran123_newstream instead **/
 extern nrnran123_State* nrnran123_newstream3(std::uint32_t id1,
                                              std::uint32_t id2,
                                              std::uint32_t id3);
-/** @brief with no args, each instance is an independent stream
+
+/** Construct a new Random123 stream based on the philox4x32 generator.
+ *
+ *  @note This overload constructs each stream instance as an independent stream.
  *  Independence is derived by using id1=1, id2=nrnmpi_myid,
  *  id3 = ++internal_static_uint32_initialized_to_0
  */
 extern nrnran123_State* nrnran123_newstream();
 
-extern void nrnran123_deletestream(nrnran123_State*);
-extern void nrnran123_getseq(nrnran123_State*, std::uint32_t* seq, char* which);
-extern void nrnran123_setseq(nrnran123_State*, std::uint32_t seq, char which);
-extern void nrnran123_setseq(nrnran123_State*, double seq4which);  // seq*4+which);
-extern void nrnran123_getids(nrnran123_State*, std::uint32_t* id1, std::uint32_t* id2);
-extern void nrnran123_getids(nrnran123_State*,
+/** Destroys the given Random123 stream. */
+extern void nrnran123_deletestream(nrnran123_State* s);
+
+/** Get sequence number and selector from an nrnran123_State object */
+extern void nrnran123_getseq(nrnran123_State* s, std::uint32_t* seq, char* which);
+
+/** Set a Random123 sequence for a sequnece ID and which selector.
+ *
+ * @param s an Random123 state object
+ * @param seq the sequence ID for which to initialize the random number sequence
+ * @param which the selector (0 <= which < 4) of the sequence
+ */
+extern void nrnran123_setseq(nrnran123_State* s, std::uint32_t seq, char which);
+
+/** Set a Random123 sequence for a sequnece ID and which selector.
+ *
+ * This overload encodes the sequence ID and which in one double. This is done specifically to be
+ * able to expose the Random123 API in HOC, which only supports real numbers.
+ *
+ * @param s an Random123 state object
+ * @param seq4which encodes both seq and which as seq*4+which
+ */
+extern void nrnran123_setseq(nrnran123_State* s, double seq4which);  // seq*4+which);
+
+/** Get stream IDs from Random123 State object */
+extern void nrnran123_getids(nrnran123_State* s, std::uint32_t* id1, std::uint32_t* id2);
+
+
+/** Get stream IDs from Random123 State object */
+extern void nrnran123_getids(nrnran123_State* s,
                               std::uint32_t* id1,
                               std::uint32_t* id2,
                               std::uint32_t* id3);
