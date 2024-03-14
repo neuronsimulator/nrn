47879cb15d32c43d51a94eaf2248fbead6ec516b
Weina Ji <weina.ji@epfl.ch>
2024-03-14 22:51:26 +0100

    keep coreneuron init values for other tests

--- test/pytest_coreneuron/test_coreneuron_configuration.py
+++ test/pytest_coreneuron/test_coreneuron_configuration.py
@@ -2,20 +2,20 @@ from neuron import coreneuron
 
 
 def test_coreneuron_configuration():
+    with coreneuron(restore_path="restore",
+                    save_path="save",
+                    model_path="coredat",
+                    skip_write_model_to_disk=True):
+        assert coreneuron.restore_path == "restore"
+        assert coreneuron.save_path == "save"
+        assert coreneuron.model_path == "coredat"
+        assert coreneuron.skip_write_model_to_disk == True
+
+    #  back to the old values out of the "with" scope
     assert coreneuron.restore_path is None
     assert coreneuron.save_path is None
     assert coreneuron.model_path is None
     assert coreneuron.skip_write_model_to_disk == False
-
-    coreneuron.restore_path = "restore"
-    coreneuron.save_path = "save"
-    coreneuron.model_path = "coredat"
-    coreneuron.skip_write_model_to_disk = True
-
-    assert coreneuron.restore_path == "restore"
-    assert coreneuron.save_path == "save"
-    assert coreneuron.model_path == "coredat"
-    assert coreneuron.skip_write_model_to_disk == True
 
 
 if __name__ == "__main__":
