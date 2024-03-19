aed536f39b1428f4737dccd6a986f557137f335b
Pramod Kumbhar <pramod.s.kumbhar@gmail.com>
2024-03-19 16:43:08 +0100

    fix the merge with master

--- src/nrniv/nrncore_write/data/cell_group.cpp
+++ src/nrniv/nrncore_write/data/cell_group.cpp
@@ -257,8 +257,7 @@ void CellGroup::datumindex_fill(int ith, CellGroup& cg, DatumIndices& di, Memb_l
     // what is the size of the nt._vdata portion needed for a single ml->dparam[i]
     int vdata_size = 0;
     for (int i = 0; i < dsize; ++i) {
-        int* ds = memb_func[di.type].dparam_semantics;
-        if (ds[i] == -4 || ds[i] == -6 || ds[i] == -7 || ds[i] == -11 || ds[i] == 0) {
+        if (dmap[i] == -4 || dmap[i] == -6 || dmap[i] == -7 || dmap[i] == -11 || dmap[i] == 0) {
             ++vdata_size;
         }
     }
