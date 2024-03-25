5c6379dfbd4e1ae17056bf50e412d8e89e95324f
Luc Grosheintz <luc.grosheintz@gmail.com>
2024-03-25 10:50:33 +0100

    Refactor `DatumIndices`.
    The names of the members `ion_{type,index}` were confusing and have
    been renamed.

--- src/nrniv/nrncore_write/data/datum_indices.cpp
+++ src/nrniv/nrncore_write/data/datum_indices.cpp
@@ -1,13 +1,6 @@
 #include "datum_indices.h"
 
-DatumIndices::DatumIndices() {
-    type = -1;
-    ion_type = ion_index = 0;
-}
-
 DatumIndices::~DatumIndices() {
-    if (ion_type)
-        delete[] ion_type;
-    if (ion_index)
-        delete[] ion_index;
+    delete[] datum_type;
+    delete[] datum_index;
 }
