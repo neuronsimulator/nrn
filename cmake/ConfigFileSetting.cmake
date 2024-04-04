826e0aba536a6782b418ffb2bf701e9060c7c74f
Goran Jelic-Cizmek <goran.jelic-cizmek@epfl.ch>
2024-04-04 17:42:16 +0200

    Remove all references to `STDC_HEADERS`
    They are defined by the standard, and any compiler worth anything should
    implement these, so we should not need them anymore

--- cmake/ConfigFileSetting.cmake
+++ cmake/ConfigFileSetting.cmake
@@ -124,11 +124,6 @@ nrn_check_include_files(sys/types.h HAVE_SYS_TYPES_H)
 nrn_check_include_files(unistd.h HAVE_UNISTD_H)
 
 # =============================================================================
-# Check for standard headers
-# =============================================================================
-check_include_files("dlfcn.h;stdint.h;stddef.h;inttypes.h;stdlib.h;string.h;float.h" STDC_HEADERS)
-
-# =============================================================================
 # Check symbol using check_cxx_symbol_exists but use ${NRN_HEADERS_INCLUDE_LIST}
 # =============================================================================
 # note that this must be called after all *check_include_files because we use
