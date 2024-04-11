d7023072bebc0432ede072a77e519a2f12c776ec
Alexander Temerev <alexander.temerev@epfl.ch>
2024-04-11 13:06:49 +0200

    explicit sequencing in random123 test - v2

--- test/coreneuron/unit/random/test_random.cpp
+++ test/coreneuron/unit/random/test_random.cpp
@@ -32,7 +32,10 @@ TEST_CASE("random123 smoke test") {
     nrn_pragma_omp(target teams distribute parallel for map(tofrom: res[0:NUM_TRIES]) is_device_ptr(s))
     nrn_pragma_acc(parallel loop copy(res [0:NUM_TRIES]) deviceptr(s))
     for (int i = 0; i < NUM_TRIES; i++) {
-        nrnran123_setseq(s, i);
+        uint32_t seq;
+        char which;
+        nrnran123_getseq(s, &seq, &which);
+        nrnran123_setseq(s, i, which);
         res[i] = nrnran123_dblpick(s);
     }
 
