b61bf63550bd180cffd20dd5fef3f17271944ec8
Nicolas Cornu <me@alkino.fr>
2024-04-22 15:05:15 +0200

    handle return

--- src/nrnoc/init.cpp
+++ src/nrnoc/init.cpp
@@ -227,16 +227,17 @@ int nrn_is_cable(void) {
 }
 
 void* nrn_realpath_dlopen(const char* relpath, int flags) {
+    void* handle = nullptr;
     try { // Try with an absolute path otherwise relpath
         auto abspath = fs::absolute(relpath);
-        void* handle = dlopen(abspath.string().c_str(), flags);
+        handle = dlopen(abspath.string().c_str(), flags);
 #if DARWIN
         if (!handle) {
             nrn_possible_mismatched_arch(abspath.c_str());
         }
 #endif
     } catch (const std::filesystem::filesystem_error& e) {
-        void *handle = dlopen(relpath, flags);
+        handle = dlopen(relpath, flags);
         if (!handle) {
             std::cerr << "std::filesystem::absolute failed (" << e.what() << ") and dlopen failed with '" << relpath << "'" << std::endl;
 #if DARWIN
