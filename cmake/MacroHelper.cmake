ed95786b5001098dcadc69900b7916e787c7113a
Nicolas Cornu <nicolas.cornu@epfl.ch>
2024-04-23 13:42:42 +0200

    More relocatibility

--- cmake/MacroHelper.cmake
+++ cmake/MacroHelper.cmake
@@ -161,27 +161,20 @@ endmacro()
 # =============================================================================
 # Run nocmodl to convert NMODL to C
 # =============================================================================
-macro(nocmodl_mod_to_cpp modfile_basename)
+macro(nocmodl_mod_to_cpp modfile_basename source_dir working_dir)
   set(NOCMODL_SED_EXPR "s/_reg()/_reg_()/")
   if(NOT MSVC)
     set(NOCMODL_SED_EXPR "'${NOCMODL_SED_EXPR}'")
   endif()
-  set(REMOVE_CMAKE_COMMAND "rm")
-  if(CMAKE_VERSION VERSION_LESS "3.17")
-    set(REMOVE_CMAKE_COMMAND "remove")
-  endif()
   add_custom_command(
-    OUTPUT ${PROJECT_BINARY_DIR}/${modfile_basename}.cpp
+    OUTPUT ${working_dir}/${modfile_basename}.cpp
     COMMAND
       ${CMAKE_COMMAND} -E env "MODLUNIT=${PROJECT_BINARY_DIR}/share/nrn/lib/nrnunits.lib"
-      ${NRN_NOCMODL_SANITIZER_ENVIRONMENT} $<TARGET_FILE:nocmodl>
-      ${PROJECT_SOURCE_DIR}/${modfile_basename}.mod
-    COMMAND sed ${NOCMODL_SED_EXPR} ${PROJECT_SOURCE_DIR}/${modfile_basename}.cpp >
-            ${PROJECT_BINARY_DIR}/${modfile_basename}.cpp
-    COMMAND ${CMAKE_COMMAND} -E ${REMOVE_CMAKE_COMMAND}
-            ${PROJECT_SOURCE_DIR}/${modfile_basename}.cpp
-    DEPENDS nocmodl ${PROJECT_SOURCE_DIR}/${modfile_basename}.mod
-    WORKING_DIRECTORY ${PROJECT_BINARY_DIR}/src/nrniv)
+      ${NRN_NOCMODL_SANITIZER_ENVIRONMENT} $<TARGET_FILE:nocmodl> -o "${working_dir}"
+      "${source_dir}/${modfile_basename}.mod"
+    COMMAND sed -i ${NOCMODL_SED_EXPR} "${working_dir}/${modfile_basename}.cpp"
+    DEPENDS nocmodl "${source_dir}/${modfile_basename}.mod" "${PROJECT_BINARY_DIR}/share/nrn/lib/nrnunits.lib"
+    WORKING_DIRECTORY ${working_dir})
 endmacro()
 
 # =============================================================================
