d8353bd7e9772d3e6a2e887f95df328fe53ebe36
Nicolas Cornu <nicolas.cornu@epfl.ch>
2024-05-28 14:02:51 +0200

    Fix shared / object / static

--- cmake/NanoBindMinimal.cmake
+++ cmake/NanoBindMinimal.cmake
@@ -5,7 +5,7 @@
 set(NB_DIR ${PROJECT_SOURCE_DIR}/external/nanobind)
 
 function(make_nanobind_target TARGET_NAME PYINC)
-  add_library(${TARGET_NAME} OBJECT ${NB_DIR}/src/nb_combined.cpp)
+  add_library(${TARGET_NAME} STATIC ${NB_DIR}/src/nb_combined.cpp)
   target_include_directories(${TARGET_NAME} SYSTEM PUBLIC ${NB_DIR}/include)
   target_include_directories(${TARGET_NAME} SYSTEM PRIVATE ${NB_DIR}/ext/robin_map/include ${PYINC})
   target_compile_options(${TARGET_NAME} PRIVATE -fno-strict-aliasing)
