2a4b45f15dbbda0d470fdf152fa63fd3cd6c7db4
Nicolas Cornu <me@alkino.fr>
2024-05-29 14:56:34 +0200

    Revert "Simplify CI"
    This reverts commit 6fe9eaa4083281ca04ede06918ad28f2547ac563.

--- ci/win_test_installer.cmd
+++ ci/win_test_installer.cmd
@@ -18,6 +18,9 @@ if not exist association.hoc.out (start /wait /REALTIME %cd%\ci\association.hoc)
 
 :: test all pythons
 C:\Python38\python -c "import neuron; neuron.test(); quit()" || set "errorfound=y"
+C:\Python39\python -c "import neuron; neuron.test(); quit()" || set "errorfound=y"
+C:\Python310\python -c "import neuron; neuron.test(); quit()" || set "errorfound=y"
+C:\Python311\python -c "import neuron; neuron.test(); quit()" || set "errorfound=y"
 :: install numpy dependency
 python -m pip install numpy
 :: run also using whatever is system python
