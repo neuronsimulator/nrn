8b7200ac8c0485c98e87b4fe261d2a46c4dfceb0
Nicolas Cornu <nicolas.cornu@epfl.ch>
2024-05-30 14:22:08 +0200

    shortened nb_defs.h

--- src/nrnpython/nb_defs.h
+++ src/nrnpython/nb_defs.h
@@ -2,13 +2,6 @@
 
 #if defined(_WIN32)
 #define NB_EXPORT   __declspec(dllexport)
-#define NB_IMPORT   __declspec(dllimport)
-#define NB_INLINE   __forceinline
-#define NB_NOINLINE __declspec(noinline)
-#define NB_INLINE_LAMBDA
 #else
 #define NB_EXPORT   __attribute__((visibility("default")))
-#define NB_IMPORT   NB_EXPORT
-#define NB_INLINE   inline __attribute__((always_inline))
-#define NB_NOINLINE __attribute__((noinline))
 #endif
