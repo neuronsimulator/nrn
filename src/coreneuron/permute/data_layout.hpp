363ad4c47fae73b8fe7fef2b022568bc53bf9045
Luc Grosheintz <luc.grosheintz@gmail.com>
2024-06-05 14:53:19 +0200

    Remove `{SOA,AOS}_LAYOUT`.

--- src/coreneuron/permute/data_layout.hpp
+++ src/coreneuron/permute/data_layout.hpp
@@ -8,8 +8,6 @@
 
 #pragma once
 
-#define SOA_LAYOUT 0
-#define AOS_LAYOUT 1
 namespace coreneuron {
 struct Memb_list;
 int get_data_index(int node_index, int variable_index, int mtype, Memb_list* ml);
