355229af56e0023d85b459f1696c4f33ab4a27c8
Luc Grosheintz <luc.grosheintz@gmail.com>
2024-06-05 09:43:10 +0200

    Check `nrn_inverse_i_layout` is safe.

--- src/coreneuron/io/nrn_setup.cpp
+++ src/coreneuron/io/nrn_setup.cpp
@@ -673,8 +673,7 @@ double* legacy_index2pointer(int mtype, int index, NrnThread& nt) {
 // from i to (icnt, isz)
 void nrn_inverse_i_layout(int i, int& icnt, int cnt, int& isz, int sz, int layout) {
     if (layout == Layout::AoS) {
-        icnt = i / sz;
-        isz = i % sz;
+        throw std::runtime_error("find me. dweui");
     } else if (layout == Layout::SoA) {
         int padded_cnt = nrn_soa_padded_size(cnt);
         icnt = i % padded_cnt;
