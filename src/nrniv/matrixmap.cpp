e7de10f3c4a9baffbc5f3ce40122866b0408ef93
Nicolas Cornu <nicolas.cornu@epfl.ch>
2024-06-05 14:29:47 +0200

    Clean MatrixMap class (#2899)

--- src/nrniv/matrixmap.cpp
+++ src/nrniv/matrixmap.cpp
@@ -1,37 +1,24 @@
 #include <../../nrnconf.h>
 #include "matrixmap.h"
 #include <vector>
-using std::vector;
 
 #include "spmatrix.h"
 
 MatrixMap::MatrixMap(Matrix& mat)
-    : m_(mat)
-    , plen_(0)
-    , ptree_(NULL)
-    , pm_(NULL) {}
+    : m_(mat) {}
 
 MatrixMap::MatrixMap(Matrix* mat)
-    : m_(*mat)
-    , plen_(0)
-    , ptree_(NULL)
-    , pm_(NULL) {}
-
-MatrixMap::~MatrixMap() {
-    mmfree();
-}
+    : m_(*mat) {}
 
 void MatrixMap::mmfree() {
-    // safe to delete NULL pointers
-    delete[] ptree_;
-    delete[] pm_;
-    ptree_ = NULL;
-    pm_ = NULL;
+    pm_.clear();
+    pm_.shrink_to_fit();
+    ptree_.clear();
+    ptree_.shrink_to_fit();
 }
 
 void MatrixMap::add(double fac) {
     for (int i = 0; i < plen_; ++i) {
-        // printf("i=%d %g += %g * %g\n", i, *ptree_[i], fac, *pm_[i]);
         *ptree_[i] += fac * (*pm_[i]);
     }
 }
@@ -39,23 +26,18 @@ void MatrixMap::add(double fac) {
 void MatrixMap::alloc(int start, int nnode, Node** nodes, int* layer) {
     NrnThread* _nt = nrn_threads;
     mmfree();
-    // how many elements
-    int nrow = m_.nrow();
-    int ncol = m_.ncol();
-    // printf("MatrixMap::alloc nrow=%d ncol=%d\n", nrow, ncol);
 
     plen_ = 0;
-    vector<int> nonzero_i, nonzero_j;
+    std::vector<int> nonzero_i, nonzero_j;
     m_.nonzeros(nonzero_i, nonzero_j);
-    pm_ = new double*[nonzero_i.size()];
-    ptree_ = new double*[nonzero_i.size()];
+    pm_.resize(nonzero_i.size());
+    ptree_.resize(nonzero_i.size());
     for (int k = 0; k < nonzero_i.size(); k++) {
         const int i = nonzero_i[k];
         const int j = nonzero_j[k];
         int it;
         if (i < nnode) {
             it = nodes[i]->eqn_index_ + layer[i];
-            // printf("i=%d it=%d area=%g\n", i, it, nodes[i]->area);
             if (layer[i] > 0 && !nodes[i]->extnode) {
                 it = 0;
             }
@@ -72,7 +54,6 @@ void MatrixMap::alloc(int start, int nnode, Node** nodes, int* layer) {
         } else {
             jt = start + j - nnode;
         }
-        // printf("MatrixMap::alloc getElement(%d,%d)\n", it, jt);
         ptree_[plen_] = spGetElement(_nt->_sp13mat, it, jt);
         ++plen_;
     }
