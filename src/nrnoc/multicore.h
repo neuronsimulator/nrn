cbd699507393110608b53d4303935a10682c88e7
Nicolas Cornu <me@alkino.fr>
2024-06-06 01:01:39 +0200

    Avoid including eigen there

--- src/nrnoc/multicore.h
+++ src/nrnoc/multicore.h
@@ -27,7 +27,10 @@ actual_v, etc.
 */
 
 #include "membfunc.h"
-#include <Eigen/Sparse>
+namespace Eigen {
+    template <typename _Scalar, int _Options, typename _StorageIndex>
+    class SparseMatrix;
+}
 
 #include <cstddef>
 
@@ -92,7 +95,7 @@ struct NrnThread {
     Node** _v_parent;
     double* _sparse_rhs;           /* rhs matrix for sparse13 solver. updates to and from this vector
                                     need to be transfered to actual_rhs */
-    Eigen::SparseMatrix<double, Eigen::RowMajor>* _sparseMat{}; /* handle to general sparse matrix */
+    Eigen::SparseMatrix<double, 1, int>* _sparseMat{}; /* handle to general sparse matrix */
     Memb_list* _ecell_memb_list; /* normally nullptr */
     Node** _ecell_children;      /* nodes with no extcell but parent has it */
     void* _vcv;                  /* replaces old cvode_instance and nrn_cvode_ */
