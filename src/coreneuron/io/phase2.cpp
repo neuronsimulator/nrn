86055ffb42b632861f1373a125298e201961a022
Luc Grosheintz <luc.grosheintz@gmail.com>
2024-06-05 14:00:07 +0200

    Fix `Phase2::populate`.
    Delete dead branches.

--- src/coreneuron/io/phase2.cpp
+++ src/coreneuron/io/phase2.cpp
@@ -1117,15 +1117,11 @@ void Phase2::populate(NrnThread& nt, const UserParams& userParams) {
 #if CHKPNTDEBUG  // Not substantive. Only for debugging.
             Memb_list_chkpnt* mlc = ntc.mlmap[type];
             mlc->pdata_not_permuted = (int*) coreneuron::ecalloc_align(n * szdp, sizeof(int));
-            if (layout == Layout::SoA) {  // transpose and unpad
-                int align_cnt = nrn_soa_padded_size(n);
-                for (int i = 0; i < n; ++i) {
-                    for (int j = 0; j < szdp; ++j) {
-                        mlc->pdata_not_permuted[i * szdp + j] = ml->pdata[i + j * align_cnt];
-                    }
+            int align_cnt = nrn_soa_padded_size(n);
+            for (int i = 0; i < n; ++i) {
+                for (int j = 0; j < szdp; ++j) {
+                    mlc->pdata_not_permuted[i * szdp + j] = ml->pdata[i + j * align_cnt];
                 }
-            } else {
-                throw std::runtime_error("Critically buggy code!");
             }
 #endif
         } else {
