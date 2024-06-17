ad5cd5c152e38cc982878a31c24919385ae503e8
Luc Grosheintz <luc.grosheintz@gmail.com>
2024-06-17 13:30:22 +0200

    Improve code comments (#2915)

--- src/neuron/cache/mechanism_range.hpp
+++ src/neuron/cache/mechanism_range.hpp
@@ -204,7 +204,7 @@ struct MechanismInstance: MechanismRange<NumFloatingPointFields, NumDatumFields>
     MechanismInstance(Prop* prop)
         : base_type{_nrn_mechanism_get_type(prop), mechanism::_get::_current_row(prop)} {
         if (!prop) {
-            // grrr...see cagkftab test where setdata is not called(?) and extcall_prop is null(?)
+            // see cagkftab test where setdata is not called(?) and extcall_prop is null(?)
             return;
         }
         indices_to_cache(_nrn_mechanism_get_type(prop), [this, prop](auto field) {
