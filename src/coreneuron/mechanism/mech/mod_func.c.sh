5f0d135cc256cb054566f0a949a7a8fd071c0f31
Matthias Wolf <matthias.wolf@epfl.ch>
2024-07-05 13:39:13 +0200

    Remove Perl dependency.
    No real Perl functionality is required in NEURON, but it adds complexity
    to the build, and introduces yet another language.
    
    For MacOS, starting with 12.3, `readlink -f` is supported.  MacOS 11 aka
    Big Sur has reached the end of official Apple support in 2023.

--- src/coreneuron/mechanism/mech/mod_func.c.sh
+++ src/coreneuron/mechanism/mech/mod_func.c.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/perl
+#!/bin/bash
 #
 # =============================================================================
 # Copyright (c) 2016 - 2021 Blue Brain Project/EPFL
@@ -9,39 +9,48 @@
 #Construct the modl_reg() function from a provided list
 #of modules.
 
-#Usage : mod_func.c.pl[MECH1.mod MECH2.mod...]
+#Usage: mod_func.c.sh [MECH1.mod MECH2.mod...]
 
-@mods = @ARGV;
-s/\.mod$// foreach @mods;
-
-@mods=sort @mods;
-
-if(!@mods) {
-    print STDERR "mod_func.c.pl: No mod files provided";
-    print "// No mod files provided
+if [[ "$#" -eq 0 ]]; then
+    >&2 echo "mod_func.c.sh: No mod files provided";
+    cat <<EOF
+// No mod files provided
 namespace coreneuron {
   void modl_reg() {}
 }
-";
-    exit 0;
-}
+EOF
+    exit 0
+fi
 
-print << "__eof";
+readarray -td '' mods < <(printf '%s\0' "${BASH_ARGV[@]%%.mod}" | env LC_ALL=C sort -z)
+
+cat <<EOF
 #include <cstdio>
 namespace coreneuron {
 extern int nrnmpi_myid;
 extern int nrn_nobanner_;
-extern int @{[join ",\n  ", map{"_${_}_reg(void)"} @mods]};
+$(
+  for mod in "${mods[@]}"; do
+    printf "extern int _%s_reg(void);\n" "$mod"
+  done
+)
 
 void modl_reg() {
     if (!nrn_nobanner_ && nrnmpi_myid < 1) {
         fprintf(stderr, " Additional mechanisms from files\\n");
-        @{[join "\n        ",
-           map{"fprintf(stderr, \" $_.mod\");"} @mods] }
+$(
+  for mod in "${mods[@]}"; do
+    printf "        fprintf(stderr, \" %s.mod\");\n" "$mod"
+  done
+)
         fprintf(stderr, "\\n\\n");
     }
 
-    @{[join "\n    ", map{"_${_}_reg();"} @mods] }
+$(
+  for mod in "${mods[@]}"; do
+    printf "    _%s_reg();\n" "$mod"
+  done
+)
 }
 } //namespace coreneuron
-__eof
+EOF
