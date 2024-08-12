a0cb308b4c8d31da74b3fa30929c0cda0fd63cc0
Goran Jelic-Cizmek <goran.jelic-cizmek@epfl.ch>
2024-08-12 15:01:36 +0200

    Improve installation experience on multi-arch MacOS

--- packaging/python/build_wheels.bash
+++ packaging/python/build_wheels.bash
@@ -160,7 +160,7 @@ build_wheel_osx() {
       fi
     fi
 
-    python setup.py build_ext --cmake-prefix="/opt/nrnwheel/ncurses;/opt/nrnwheel/readline;/usr/x11" --cmake-defs="$CMAKE_DEFS" $setup_args bdist_wheel
+    python setup.py build_ext --cmake-prefix="/opt/nrnwheel/$(uname -m)/ncurses;/opt/nrnwheel/$(uname -m)/readline;/usr/x11" --cmake-defs="$CMAKE_DEFS" $setup_args bdist_wheel
 
     echo " - Calling delocate-listdeps"
     delocate-listdeps dist/*.whl
