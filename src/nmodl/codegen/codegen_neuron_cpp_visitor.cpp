f0d9f649991ed6be4b241bd155c3dd221e64f7a0
Luc Grosheintz <luc.grosheintz@gmail.com>
2024-08-29 13:36:22 +0200

    Remove broken code. (BlueBrain/nmodl#1411)
    For code that has `info.vectorize == false` the removed
    lines generated code, that fails to compile.
    
    NMODL Repo SHA: BlueBrain/nmodl@e4cb3d8e79991dddd420f87ab69629145f2aefae

--- src/nmodl/codegen/codegen_neuron_cpp_visitor.cpp
+++ src/nmodl/codegen/codegen_neuron_cpp_visitor.cpp
@@ -192,12 +192,6 @@ void CodegenNeuronCppVisitor::print_setdata_functions() {
             _prop_id = _nrn_get_prop_id(_prop);
         )CODE");
     }
-    if (!info.vectorize) {
-        printer->add_multi_line(R"CODE(
-            neuron::legacy::set_globals_from_prop(_prop, _lmc, _ml, _iml);
-            _ppvar = _nrn_mechanism_access_dparam(_prop);
-        )CODE");
-    }
     printer->pop_block();
 
     if (info.point_process) {
