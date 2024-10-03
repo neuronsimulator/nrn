2f777291362bc2a9418755f31ede0a26cdc332c8
Luc Grosheintz <luc.grosheintz@gmail.com>
2024-10-03 14:47:45 +0200

    Remove empty visitor from `main.cpp`. (BlueBrain/nmodl#1496)
    NMODL Repo SHA: BlueBrain/nmodl@65a70941f852d3b428b9ea396d363b7916cc51a7

--- src/nmodl/main.cpp
+++ src/nmodl/main.cpp
@@ -330,9 +330,6 @@ int run_nmodl(int argc, const char* argv[]) {
         /// one whenever we run symtab visitor.
         bool update_symtab = false;
 
-        /// just visit the ast
-        AstVisitor().visit_program(*ast);
-
         {
             logger->info("Running argument renaming visitor");
             RenameFunctionArgumentsVisitor().visit_program(*ast);
