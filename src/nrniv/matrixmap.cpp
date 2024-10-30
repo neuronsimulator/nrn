829adaa7ca76515f837cfaecb03dcb179a72fa4f
Nicolas Cornu <me@alkino.fr>
2024-10-30 16:46:41 +0100

    Fix merge

--- src/nrniv/matrixmap.cpp
+++ src/nrniv/matrixmap.cpp
@@ -19,7 +19,8 @@ void MatrixMap::mmfree() {
 
 void MatrixMap::add(double fac) {
     for (int i = 0; i < plen_; ++i) {
-        *ptree_[i] += fac * (*pm_[i]);
+        auto [it, jt] = pm_[i];
+        *ptree_[i] += fac * m_(it, jt);
     }
 }
 
@@ -28,13 +29,10 @@ void MatrixMap::alloc(int start, int nnode, Node** nodes, int* layer) {
     mmfree();
 
     plen_ = 0;
-    std::vector<int> nonzero_i, nonzero_j;
-    m_.nonzeros(nonzero_i, nonzero_j);
-    pm_.resize(nonzero_i.size());
-    ptree_.resize(nonzero_i.size());
-    for (int k = 0; k < nonzero_i.size(); k++) {
-        const int i = nonzero_i[k];
-        const int j = nonzero_j[k];
+    std::vector<std::pair<int, int>> nzs = m_.nonzeros();
+    pm_.resize(nzs.size());
+    ptree_.resize(nzs.size());
+    for (const auto [i, j]: nzs) {
         int it;
         if (i < nnode) {
             it = nodes[i]->eqn_index_ + layer[i];
@@ -45,7 +43,7 @@ void MatrixMap::alloc(int start, int nnode, Node** nodes, int* layer) {
             it = start + i - nnode;
         }
         int jt;
-        pm_[plen_] = m_.mep(i, j);
+        pm_[plen_] = std::make_pair(i, j);
         if (j < nnode) {
             jt = nodes[j]->eqn_index_ + layer[j];
             if (layer[j] > 0 && !nodes[j]->extnode) {
