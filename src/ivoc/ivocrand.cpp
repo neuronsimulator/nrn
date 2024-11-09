4426b495da114d44f4333ca70b32c09e4694d236
Nicolas Cornu <nicolas.cornu@epfl.ch>
2024-11-09 11:41:02 +0100

    Remove deprecated MLCG

--- src/ivoc/ivocrand.cpp
+++ src/ivoc/ivocrand.cpp
@@ -18,7 +18,6 @@
 #include <nrnran123.h>
 
 #include <ACG.h>
-#include <MLCG.h>
 #include <Random.h>
 #include <Poisson.h>
 #include <Normal.h>
@@ -130,29 +129,6 @@ static double r_ACG(void* r) {
     x->type_ = 0;
     delete x->gen;
     x->gen = x->rand->generator();
-    return 1.;
-}
-
-// Use a Multiplicative Linear Congruential Generator.  Not as high
-// quality as the ACG, but uses only 8 bytes
-// syntax:
-// r.MLCG([seed1],[seed2])
-
-static double r_MLCG(void* r) {
-    Rand* x = (Rand*) r;
-
-    unsigned long seed1 = 0;
-    unsigned long seed2 = 0;
-
-    if (ifarg(1))
-        seed1 = long(*getarg(1));
-    if (ifarg(2))
-        seed2 = long(*getarg(2));
-
-    x->rand->generator(new MLCG(seed1, seed2));
-    delete x->gen;
-    x->gen = x->rand->generator();
-    x->type_ = 1;
     return 1.;
 }
 
@@ -489,7 +465,6 @@ extern "C" void nrn_random_play() {
 
 
 static Member_func r_members[] = {{"ACG", r_ACG},
-                                  {"MLCG", r_MLCG},
                                   {"Isaac64", r_Isaac64},
                                   {"MCellRan4", r_MCellRan4},
                                   {"Random123", r_nrnran123},
