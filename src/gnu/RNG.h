88b3a9ee25003a3d077a346e1e28b0a9ead0f92e
Nicolas Cornu <nicolas.cornu@epfl.ch>
2024-11-19 11:41:38 +0100

    Nuke RNG.cpp

--- src/gnu/RNG.h
+++ src/gnu/RNG.h
@@ -1,40 +1,12 @@
-// This may look like C code, but it is really -*- C++ -*-
-/* 
-Copyright (C) 1988 Free Software Foundation
-    written by Dirk Grunwald (grunwald@cs.uiuc.edu)
-
-This file is part of the GNU C++ Library.  This library is free
-software; you can redistribute it and/or modify it under the terms of
-the GNU Library General Public License as published by the Free
-Software Foundation; either version 2 of the License, or (at your
-option) any later version.  This library is distributed in the hope
-that it will be useful, but WITHOUT ANY WARRANTY; without even the
-implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-PURPOSE.  See the GNU Library General Public License for more details.
-You should have received a copy of the GNU Library General Public
-License along with this library; if not, write to the Free Software
-Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
-*/
 #pragma once
 #include <cstdint>
-#include <cassert>
-#include <cmath>
 #include <limits>
 
-union PrivateRNGDoubleType {		   	// used to access doubles as unsigneds
-    double d;
-    uint32_t u[2];
-};
-
-//
-// Base class for Random Number Generators.
-//
 class RNG {
-    static PrivateRNGDoubleType doubleMantissa;	// mantissa bit vector
 public:
     using result_type = std::uint32_t;
 
-    RNG();
+    RNG() = default;
     virtual ~RNG() = default;
 
     // Return a long-words word of random bits
@@ -42,7 +14,7 @@ public:
     virtual void reset() = 0;
 
     // Return random bits converted to a double
-    virtual double asDouble();
+    virtual double asDouble() = 0;
 
     static constexpr result_type min() {
         return std::numeric_limits<result_type>::min();
