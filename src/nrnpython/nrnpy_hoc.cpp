85584f4e9419bb3a52a12ade47adf055110ccd70
Nicolas Cornu <nicolas.cornu@epfl.ch>
2024-11-21 16:28:17 +0100

    Update src/nrnpython/nrnpy_hoc.cpp
    Co-authored-by: Luc Grosheintz <luc.grosheintz@gmail.com>

--- src/nrnpython/nrnpy_hoc.cpp
+++ src/nrnpython/nrnpy_hoc.cpp
@@ -2922,6 +2922,8 @@ static PyObject* hocpickle_reduce(PyObject* self, PyObject* args) {
     // Fill object's state. Tuple with 4 args:
     // pickle version, endianness sentinel,
     // vector size, string data
+    //
+    // To be able to read data on a system with different endianness, a sentinel is added, the convention is that the value of the sentinel is `2.0` (when cast to a double). Therefore, if the machine reads the sentinel and it's not `2.0` it know that it needs to swap the bytes of all doubles in the payload.
     double x = 2.0;
     nb::bytes byte_order((const void*) (&x), sizeof(double));
     nb::bytes vec_data(vec->data(), vec->size() * sizeof(double));
