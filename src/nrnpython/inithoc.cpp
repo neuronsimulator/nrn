0438b753bcba562cdc578b2802487ef1e9607bc5
nrnhines <michael.hines@yale.edu>
2024-12-02 06:54:14 -0800

    Fix thread sanitizer leak when launching Python 3.13 and using from neuron import h, gui. (#3243)
    * nrnpython_finalize can only cleanup and Py_Finalize from MainThread
    
    * save stdin terminal settings on import hoc and restore on h.quit()

--- src/nrnpython/inithoc.cpp
+++ src/nrnpython/inithoc.cpp
@@ -215,6 +215,33 @@ static int have_opt(const char* arg) {
     return 0;
 }
 
+#if defined(__linux__) || defined(DARWIN)
+
+/* we do this because thread sanitizer does not allow system calls.
+   In particular
+      system("stty sane")
+   returns an error code of 139
+*/
+
+#include <iostream>
+#include <termios.h>
+#include <unistd.h>
+
+static struct termios original_termios;
+
+static void save_original_terminal_settings() {
+    if (tcgetattr(STDIN_FILENO, &original_termios) == -1) {
+        std::cerr << "Error getting original terminal attributes\n";
+    }
+}
+
+static void restore_original_terminal_settings() {
+    if (tcsetattr(STDIN_FILENO, TCSANOW, &original_termios) == -1) {
+        std::cerr << "Error restoring terminal attributes\n";
+    }
+}
+#endif  // __linux__
+
 void nrnpython_finalize() {
 #if USE_PTHREAD
     pthread_t now = pthread_self();
@@ -222,11 +249,18 @@ void nrnpython_finalize() {
 #else
     {
 #endif
+        // Call python_gui_cleanup() if defined in Python
+        PyRun_SimpleString(
+            "try:\n"
+            "    gui.cleanup()\n"
+            "except NameError:\n"
+            "    pass\n");
+
+        // Finalize Python
         Py_Finalize();
     }
-#if linux
-    if (system("stty sane > /dev/null 2>&1")) {
-    }  // 'if' to avoid ignoring return value warning
+#if defined(__linux__) || defined(DARWIN)
+    restore_original_terminal_settings();
 #endif
 }
 
@@ -238,6 +272,10 @@ extern "C" PyObject* PyInit_hoc() {
 #if USE_PTHREAD
     main_thread_ = pthread_self();
 #endif
+
+#if defined(__linux__) || defined(DARWIN)
+    save_original_terminal_settings();
+#endif  // __linux__
 
     if (nrn_global_argv) {  // ivocmain was already called so already loaded
         return nrnpy_hoc();
