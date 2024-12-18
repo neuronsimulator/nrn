39f05526d7d047830de4de5560ff348296dfbce1
Goran Jelic-Cizmek <goran.jelic-cizmek@epfl.ch>
2024-12-18 12:45:25 +0100

    Add compat with C++20

--- src/utils/formatting.hpp
+++ src/utils/formatting.hpp
@@ -6,6 +6,6 @@
 
 template <typename... T>
 [[noreturn]] void hoc_execerror_fmt(const std::string& fmt, T&&... args) {
-    auto s = fmt::format(fmt, std::forward<T>(args)...);
+    auto s = fmt::format(fmt::runtime(fmt), std::forward<T>(args)...);
     hoc_execerror(s.c_str(), nullptr);
 }
