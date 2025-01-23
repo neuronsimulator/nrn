d23569daeecb2724483217bdae72235b4f108dde
Michael Hines <michael.hines@yale.edu>
2025-01-23 14:43:16 -0500

    everywhere python 3.11 is mentioned, possibly allow 3.13

--- ci/win_test_installer_wo_rxd.cmd
+++ ci/win_test_installer_wo_rxd.cmd
@@ -26,6 +26,7 @@ C:\Python39\python -c "import neuron; neuron.test(); quit()" || set "errorfound=
 C:\Python310\python -c "import neuron; neuron.test(); quit()" || set "errorfound=y"
 C:\Python311\python -c "import neuron; neuron.test(); quit()" || set "errorfound=y"
 C:\Python312\python -c "import neuron; neuron.test(); quit()" || set "errorfound=y"
+C:\Python313\python -c "import neuron; neuron.test(); quit()" || set "errorfound=y"
 
 :: install numpy dependency
 python -m pip install "numpy<2"
