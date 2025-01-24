d0b56602a0c18ebac115697bf18511d51669a228
Goran Jelic-Cizmek <goran.jelic-cizmek@epfl.ch>
2025-01-24 13:00:39 +0100

    Add missing setuptools on Windows

--- ci/win_build_cmake.sh
+++ ci/win_build_cmake.sh
@@ -9,8 +9,9 @@ export MINGW_CHOST=x86_64-w64-mingw32
 export MSYSTEM_PREFIX=/mingw64
 export PATH=/mingw64/bin:$PATH
 
-# have compatible cython
-python3 -m pip install cython
+
+# have compatible cython and setuptools
+python3 -m pip install cython 'setuptools;python_version>=3.12'
 
 # if BUILD_SOURCESDIRECTORY not available, use te root of the repo
 if [ -z "$BUILD_SOURCESDIRECTORY" ]; then
