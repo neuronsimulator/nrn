7af8ad0db007ed9d2a49b660cb5c95ecfd1da14a
Goran Jelic-Cizmek <goran.jelic-cizmek@epfl.ch>
2025-04-01 15:08:17 +0200

    Fix local errors

--- cmake/PythonNotebookHelper.cmake
+++ cmake/PythonNotebookHelper.cmake
@@ -1,14 +1,15 @@
 # execute a single notebook from `INPUT_PATH` inplace
 function(add_notebook target)
-  cmake_parse_arguments(opt "" "INPUT_PATH" "ENV" ${ARGN})
+  cmake_parse_arguments(opt "" "INPUT_PATH;WORKING_DIRECTORY" "ENV" ${ARGN})
 
   # name of the file
   get_filename_component(filename "${opt_INPUT_PATH}" NAME)
 
   add_custom_target(
     "${target}"
-    COMMAND ${CMAKE_COMMAND} -E env ${opt_ENV} jupyter nbconvert --debug --to notebook --execute
-            --inplace "${opt_INPUT_PATH}"
+    COMMAND ${CMAKE_COMMAND} -E env ${opt_ENV} jupyter nbconvert --to notebook --execute --inplace
+            "${opt_INPUT_PATH}"
+    WORKING_DIRECTORY "${opt_WORKING_DIRECTORY}"
     VERBATIM)
 
   # the one used for cleanup
