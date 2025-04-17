a500ae2082e3c0f25674e0599dba95a99938cce5
Michael Hines <michael.hines@yale.edu>
2025-04-17 09:38:43 -0400

    When crosscompiling ncurses, don't run ncurses build products.

--- packaging/python/build_static_readline_osx.bash
+++ packaging/python/build_static_readline_osx.bash
@@ -37,24 +37,28 @@ curl -L -o readline-7.0.tar.gz https://ftp.gnu.org/gnu/readline/readline-7.0.tar
 export CFLAGS="$ARCHFLAGS -fPIC"
 export CXXFLAGS="$ARCHFLAGS -fPIC"
 hostarg=""
+ncursearg=""
 if [ "$ARCHTYPE" == "universal2" ]; then
     export MACOSX_DEPLOYMENT_TARGET=11.0  # Required for universal2?
 elif [ "$ARCHTYPE" == "x86_64" ]; then
     export MACOSX_DEPLOYMENT_TARGET=10.15
     if [ "$ARCHTYPE" != $(uname -m) ]; then
         hostarg="--host=x86_64-apple-darwin"
+        ncursearg="--without-progs --disable-db-install"
     fi
 elif [ "$ARCHTYPE" == "arm64" ]; then
     export MACOSX_DEPLOYMENT_TARGET=10.15
     if [ "$ARCHTYPE" != $(uname -m) ]; then
         hostarg="--host=aarch64-apple-darwin"
+        ncursearg="--without-progs --disable-db-install"
     fi
 fi
 echo "hostarg=$hostarg"
+echo "ncursearg=$ncursearg"
 
 # Build ncurses (static only, no executables)
 (cd ncurses-6.4 \
-    && ./configure --prefix="${ULOC}/ncurses" --without-shared --without-tests $hostarg \
+    && ./configure --prefix="${ULOC}/ncurses" --without-shared --without-tests $hostarg $ncursearg \
     && make clean \
     && make -j \
     && make install)
