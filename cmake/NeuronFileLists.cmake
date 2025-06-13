a97927dcbc5c00144648e1c6a746c0b39306de68
Goran Jelic-Cizmek <goran.jelic-cizmek@epfl.ch>
2025-06-13 15:40:28 +0200

    Missing file

--- cmake/NeuronFileLists.cmake
+++ cmake/NeuronFileLists.cmake
@@ -446,7 +446,7 @@ if(NRN_ENABLE_MPI)
   nrn_create_file_list(NRN_PARALLEL_SRC_FILES ${PROJECT_SOURCE_DIR}/src/nrniv
                        nvector_nrnparallel_ld.cpp)
   nrn_create_file_list(NRN_PARALLEL_SRC_FILES ${PROJECT_SOURCE_DIR}/src/sundials/shared
-                       nvector_parallel.c)
+                       nvector_parallel.cpp)
 endif()
 nrn_create_file_list(NRN_SPARSE_SRC_FILES ${PROJECT_SOURCE_DIR}/src/sparse ${SPARSE_FILES_LIST})
 nrn_create_file_list(NRN_SCOPMATH_SRC_FILES ${PROJECT_SOURCE_DIR}/src/scopmath
