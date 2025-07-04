5f189fc232c003f5f461ca403e95575c1ef5167e
Alessandro Cattabiani <alessandro.cattabiani@gmail.com>
2025-07-04 12:06:07 +0200

    for Jim

--- test/coreneuron/test_report.py
+++ test/coreneuron/test_report.py
@@ -1,4 +1,7 @@
-from neuron import h, gui
+from neuron import h, gui, coreneuron
+from pathlib import Path
+from typing import List
+
 
 def inspect(v):
     print(v, type(v), id(v))
@@ -31,79 +34,139 @@ class Cell:
 
 
 # function to register section-segment mapping with bbcore write
-def setup_nrnbbcore_register_mapping(rings):
+def setup_nrnbbcore_register_mapping(gid):
 
     #for recording
     recordlist = []
 
     pc = h.ParallelContext()
 
-    #all rings in the simulation
-    for ring in rings:
+    #vector for soma sections and segment
+    somasec = h.Vector()
+    somaseg = h.Vector()
 
-        #every gid in the ring
-        for gid in ring.gids:
+    #vector for dendrite sections and segment
+    densec = h.Vector()
+    denseg = h.Vector()
 
-            #vector for soma sections and segment
-            somasec = h.Vector()
-            somaseg = h.Vector()
+    #if gid exist on rank
+    if (pc.gid_exists(gid)):
 
-            #vector for dendrite sections and segment
-            densec = h.Vector()
-            denseg = h.Vector()
+        #get cell instance
+        cell = pc.gid2cell(gid)
+        isec = 0
 
-            #if gid exist on rank
-            if (pc.gid_exists(gid)):
+        #soma section, only pne
+        for sec in [cell.soma]:
+            for seg in sec:
+                #get section and segment index
+                somasec.append(isec)
+                somaseg.append(seg.node_index())
 
-                #get cell instance
-                cell = pc.gid2cell(gid)
-                isec = 0
+                #vector for recording
+                v = h.Vector()
+                v.record(seg._ref_v)
+                v.label("soma %d %d" % (isec, seg.node_index()))
+                recordlist.append(v)
+        isec += 1
 
-                #soma section, only pne
-                for sec in [cell.soma]:
-                    for seg in sec:
-                        #get section and segment index
-                        somasec.append(isec)
-                        somaseg.append(seg.node_index())
-
-                        #vector for recording
-                        v = h.Vector()
-                        v.record(seg._ref_v)
-                        v.label("soma %d %d" % (isec, seg.node_index()))
-                        recordlist.append(v)
-                isec += 1
-
-                #for sections in dendrite
-                for sec in cell.den:
-                    for seg in sec:
-                        densec.append(isec)
-                        denseg.append(seg.node_index())
-
-                        #for recordings
-                        v = h.Vector()
-                        v.record(seg._ref_v)
-                        v.label("dend %d %d" % (isec, seg.node_index()))
-                        recordlist.append(v)
-                    isec += 1
-
-                #register soma section list
-                pc.nrnbbcore_register_mapping(gid, "soma", somasec, somaseg)
-
-                #register dend section list
-                pc.nrnbbcore_register_mapping(gid, "dend", densec, denseg)
+        #register soma section list
+        pc.nrnbbcore_register_mapping(gid, "soma", somasec, somaseg)
 
     return recordlist
 
+def write_report_config(output_file, report_name, target_name, report_type, report_variable,
+                        unit, report_format, target_type, dt, start_time, end_time, gids,
+                        buffer_size=8):
+    import struct
+    num_gids = len(gids)
+    report_conf = Path(output_file)
+    report_conf.parent.mkdir(parents=True, exist_ok=True)
+    with report_conf.open("wb") as fp:
+        # Write the formatted string to the file
+        fp.write(b"1\n")
+        fp.write(("%s %s %s %s %s %s %d %lf %lf %lf %d %d\n" % (
+            report_name,
+            target_name,
+            report_type,
+            report_variable,
+            unit,
+            report_format,
+            target_type,
+            dt,
+            start_time,
+            end_time,
+            num_gids,
+            buffer_size
+        )).encode())
+        # Write the array of integers to the file in binary format
+        fp.write(struct.pack(f'{num_gids}i', *gids))
+        fp.write(b'\n')
+
+def write_spike_config(output_file: str, spike_filename: str,
+                       population_names: List[str], population_offsets: List[int]):
+    report_conf = Path(output_file)
+    num_population = len(population_names)
+    with report_conf.open("a") as fp:
+        fp.write(f"{num_population}\n")
+        for pop_name, offset in zip(population_names, population_offsets):
+            fp.write(f"{pop_name} {offset}\n")
+        fp.write(f"{spike_filename}\n")
+
+def write_sim_config(output_file, coredata_dir, report_conf, tstop):
+    sim_conf = Path(output_file)
+    sim_conf.parent.mkdir(parents=True, exist_ok=True)
+    Path(coredata_dir).mkdir(parents=True, exist_ok=True)
+    with sim_conf.open("w") as fp:
+        fp.write("outpath=./\n")
+        fp.write(f"datpath=./{coredata_dir}\n")
+        fp.write(f"tstop={tstop}\n")
+        fp.write(f"report-conf='{report_conf}'\n")
+        fp.write("mpi=true\n")
+
+def spike_record():
+    global tvec, idvec
+    pc = h.ParallelContext()
+    tvec = h.Vector(1000000)
+    idvec = h.Vector(1000000)
+    pc.spike_record(-1, tvec, idvec)
 
 def test_coreneuron_report():
-    gid = 0
+    # model setup
     pc = h.ParallelContext()
 
-    c = Cell(gid)
+    c = Cell(0)
     h.cvode.use_fast_imem(1)
 
-    inspect(pc.gid2cell(0))
-    inspect(c)
+    # activate coreneuron
+    coreneuron.enable = True
+    coreneuron.file_mode = False
+    coreneuron.gpu = False
+    coreneuron.cell_permute = False
+
+    # register reports
+    if pc.id() == 0:
+        setup_nrnbbcore_register_mapping(c.gid)
+        report_conf_file = "report.conf"
+        sim_conf_file = "sim.conf"
+        write_report_config(report_conf_file, "soma_v.h5", "Mosaic", "compartment", "v",
+                    "mV", "SONATA", 2, 1, 0, h.tstop, [c.gid])
+        write_spike_config(report_conf_file, "spikes.h5", ["default"], [0])
+        write_sim_config(sim_conf_file, "corenrn_data", report_conf_file, h.tstop)
+    coreneuron.sim_config=sim_conf_file
+
+    spike_record()
+
+    # run
+    pc.set_maxstep(10)
+    h.stdinit()
+    pc.psolve(h.tstop)
+
+
+
+    # assert False
+
+
     
 
 
