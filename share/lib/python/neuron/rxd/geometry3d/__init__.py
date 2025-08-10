8bba79fd03870817c8e53a55eaa7e00759683442
Robert A. McDougal <robert.mcdougal@yale.edu>
2025-08-10 12:07:07 -0400

    more legacy code removal

--- share/lib/python/neuron/rxd/geometry3d/__init__.py
+++ share/lib/python/neuron/rxd/geometry3d/__init__.py
@@ -1,7 +1,5 @@
 from .surface import surface
 from .triangularMesh import TriangularMesh
-from .voxelize import voxelize
 
-# from .voxelize2 import voxelize2
 from .scalarField import ScalarField
 from .FullJoinMorph import fullmorph as voxelize2
