c7275515cd70735ce72c71ad4f229f08d0fb55cd
Goran Jelic-Cizmek <goran.jelic-cizmek@epfl.ch>
2025-09-12 10:55:53 +0200

    Fix tests

--- test/nmodl/transpiler/unit/visitor/merge_top_level_blocks.cpp
+++ test/nmodl/transpiler/unit/visitor/merge_top_level_blocks.cpp
@@ -130,7 +130,7 @@ SCENARIO("Check multiple INITIAL blocks are handled properly",
 
 SCENARIO("Check multiple BREAKPOINT blocks are handled properly",
          "[visitor][merge_top_level_blocks]") {
-    GIVEN("A mod file with multiple INITIAL blocks") {
+    GIVEN("A mod file with multiple BREAKPOINT blocks") {
         const auto nmodl_text_before = R"(
             NEURON {
                 SUFFIX BreakpointBlockTest
@@ -164,30 +164,6 @@ SCENARIO("Check multiple BREAKPOINT blocks are handled properly",
             // TODO the AST class lacks an overload for `operator==` so here we compare it at the
             // string level
             REQUIRE(reindent_text(program_actual) == reindent_text(program_expected));
-        }
-    }
-    GIVEN("A mod file with an INITIAL block, and one inside of a NET_RECEIVE block") {
-        // Note that the visitor actually modifies the AST (since there is > 1 INITIAL block in the
-        // entire file: one top-level, and one in NET_RECEIVE). If we place the top-level INITIAL
-        // block before NET_RECEIVE in the below, the top-level INITIAL block will be deleted and
-        // appended. However, since the position of the INITIAL block in the mod file has no impact
-        // on the semantics, the visitor works as expected
-        const auto nmodl_text_before = R"(
-            NEURON {
-                SUFFIX test
-                RANGE foo, bar
-            }
-
-            BREAKPOINT {
-                bar = 2
-            }
-        )";
-        const auto program_actual =
-            generate_mod_after_merge_top_level_blocks_visitor<ast::InitialBlock,
-                                                              ast::AstNodeType::INITIAL_BLOCK>(
-                nmodl_text_before);
-        THEN("leave the mod file as-is") {
-            REQUIRE(reindent_text(program_actual) == reindent_text(nmodl_text_before));
         }
     }
 }
