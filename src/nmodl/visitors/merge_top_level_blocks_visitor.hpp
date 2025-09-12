0cb03d315b90fe78d249116a783c5b82a5412aca
Goran Jelic-Cizmek <goran.jelic-cizmek@epfl.ch>
2025-09-12 15:40:04 +0200

    Add other part that works

--- src/nmodl/visitors/merge_top_level_blocks_visitor.hpp
+++ src/nmodl/visitors/merge_top_level_blocks_visitor.hpp
@@ -46,9 +46,17 @@ class MergeTopLevelBlocksVisitor: public AstVisitor {
         // collect all statements from all blocks (including nested includes)
         ast::StatementVector statements;
         std::unordered_set<ast::Node*> blocks_to_delete;
+        // since ast::Program::erase_node can only delete top-level nodes, we
+        // need to keep track of the found includes, as well as all of the
+        // _other_ statements (since ast::Include does not provide an
+        // erase_node function, but only set_blocks)
+        std::unordered_map<std::shared_ptr<ast::Include>, ast::NodeVector> include_blocks_to_keep;
 
         const auto& blocks = node.get_blocks();
-        collect_statements_from_vector(blocks, statements, blocks_to_delete);
+        collect_statements_from_vector(blocks,
+                                       statements,
+                                       blocks_to_delete,
+                                       include_blocks_to_keep);
 
         // insert new top-level block which has all the collected statements
         auto new_block = ast::StatementBlock(statements);
@@ -57,24 +65,47 @@ class MergeTopLevelBlocksVisitor: public AstVisitor {
 
         // delete all of the previously-found top-level blocks
         node.erase_node(blocks_to_delete);
+
+        // also delete all of the blocks from INCLUDE blocks
+        for (const auto& [include_block, blocks_to_keep]: include_blocks_to_keep) {
+            include_block->set_blocks(blocks_to_keep);
+        }
     }
 
   private:
+    // Helper function to collect all top-level blocks in an INCLUDE except `ast_class` ones
+    ast::NodeVector collect_include_except(const ast::Include& node) const {
+        ast::NodeVector result;
+        for (const auto& block: node.get_blocks()) {
+            // only insert if it's not an instance of `ast_class`
+            if (std::dynamic_pointer_cast<ast_class>(block) == nullptr) {
+                result.push_back(block);
+            }
+        }
+        return result;
+    }
+
     // Helper function to collect statements from a NodeVector (including nested includes)
-    void collect_statements_from_vector(const ast::NodeVector& blocks,
-                                        ast::StatementVector& statements,
-                                        std::unordered_set<ast::Node*>& blocks_to_delete) {
+    void collect_statements_from_vector(
+        const ast::NodeVector& blocks,
+        ast::StatementVector& statements,
+        std::unordered_set<ast::Node*>& blocks_to_delete,
+        std::unordered_map<std::shared_ptr<ast::Include>, ast::NodeVector>&
+            include_blocks_to_keep) {
         for (auto& block: blocks) {
             auto include_block = std::dynamic_pointer_cast<ast::Include>(block);
             if (include_block) {
                 // Recursively process nested includes
                 const auto& included_blocks = include_block->get_blocks();
-                collect_statements_from_vector(included_blocks, statements, blocks_to_delete);
+                include_blocks_to_keep[include_block] = collect_include_except(*include_block);
+                collect_statements_from_vector(included_blocks,
+                                               statements,
+                                               blocks_to_delete,
+                                               include_blocks_to_keep);
             } else {
                 auto temp_block = std::dynamic_pointer_cast<ast_class>(block);
                 // check if it's the correct type
                 if (temp_block) {
-                    logger->info("Found block {}", typeid(ast_class).name());
                     auto statement_block = temp_block->get_statement_block();
                     // if block is not empty, copy statements into vector
                     if (statement_block) {
