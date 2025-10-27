f856642961617d37d64a6b77db12ab876ddc2639
Mike Gevaert <mgevaert@openbraininstitute.org>
2025-10-27 16:44:05 +0100

    use `[:blank:]` character class for SWC parser
    * seems to work more reliably on libc++; ie: the default C++ library for macOS
    * fixes #3647

---
+++ test/unit_tests/hoc_python/test_swc.py
@@ -0,0 +1,22 @@
+from neuron import h
+
+def test_swc_loader(tmp_path, capsys):
+    """Test for regression noted in:
+    https://github.com/neuronsimulator/nrn/issues/%20#issuecomment-3451674614
+    """
+    swc = """\
+# index     type         X            Y            Z       radius       parent
+1           1  1.049119830 -8.248093605  0.500801444  1.359369397          -1
+2           1  0.945353746 -7.414265633  0.450333387  2.455268145           1
+"""
+    path = tmp_path / "test.swc"
+    path.write_text(swc)
+
+    h.load_file("import3d.hoc")
+    reader = h.Import3d_SWC_read()
+    reader.quiet = 1
+    reader.input(str(path))
+
+    captured = capsys.readouterr()
+    assert not captured.out
+    assert not captured.err
