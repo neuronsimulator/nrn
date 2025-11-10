fc25cb2c232c314b8cda305052c3c9fa8b53a520
Goran Jelic-Cizmek <goran.jelic-cizmek@epfl.ch>
2025-11-10 15:20:32 +0100

    Use pwsh instead of curl

--- ci/win_download_deps.cmd
+++ ci/win_download_deps.cmd
@@ -15,7 +15,7 @@ pwsh -command Invoke-WebRequest -MaximumRetryCount 4 -OutFile msmpisetup.exe htt
 pwsh -command Invoke-WebRequest -MaximumRetryCount 4 -OutFile msmpisdk.msi https://download.microsoft.com/download/a/5/2/a5207ca5-1203-491a-8fb8-906fd68ae623/msmpisdk.msi || goto :error
 
 :: nsis + plugin
-curl -LO --retry 4 http://prdownloads.sourceforge.net/nsis/nsis-3.05-setup.exe || goto :error
+pwsh -command Invoke-WebRequest -MaximumRetryCount 4 -OutFile nsis-3.05-setup.exe https://prdownloads.sourceforge.net/nsis/nsis-3.05-setup.exe || goto :error
 pwsh -command "Get-FileHash nsis-3.05-setup.exe -Algorithm SHA256 | Format-List" || goto :error
 pwsh -command Invoke-WebRequest -MaximumRetryCount 4 -OutFile EnVar_pugin.zip https://nsis.sourceforge.io/mediawiki/images/7/7f/EnVar_plugin.zip || goto :error
 
