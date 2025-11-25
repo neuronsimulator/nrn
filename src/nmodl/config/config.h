3efeb709cf4f066ddcdc825f1651fd5541187f98
Goran Jelic-Cizmek <goran.jelic-cizmek@epfl.ch>
2025-11-25 12:05:58 +0100

    Revert "Fix issue with embedded nrnunits.lib"
    This reverts commit aabe405ed7708cb0c94910ae3dacf6e91c58bc9b.

--- src/nmodl/config/config.h
+++ src/nmodl/config/config.h
@@ -15,19 +15,15 @@
  * \brief Version information and units file path
  */
 
-#include <chrono>
 #include <cstdlib>
-#include <filesystem>
 #include <fstream>
 #include <sstream>
 #include <string>
-#include <thread>
 #include <vector>
 
 #include "utils/common_utils.hpp"
 
 namespace nmodl {
-namespace fs = std::filesystem;
 
 /**
  * \brief Project version information
@@ -52,8 +48,6 @@ struct NrnUnitsLib {
     /// paths where nrnunits.lib can be found
     static std::vector<std::string> NRNUNITSLIB_PATH;
 
-    static const std::string_view embedded_nrnunits;
-
     /**
      * Return path of units database file
      */
@@ -76,21 +70,7 @@ struct NrnUnitsLib {
         for (const auto& path: NRNUNITSLIB_PATH) {
             err_msg << path << "\n";
         }
-        err_msg << "Falling back to embedded nrnunits.lib\n";
-        const auto& temp_dir = fs::temp_directory_path();
-
-        const auto& embedded_path = temp_dir / "nrnunits.lib";
-        const auto& lock_dir = embedded_path.string() + ".lock";
-        while (not fs::create_directory(lock_dir)) {
-            // Someone else has the lock, wait
-            std::this_thread::sleep_for(std::chrono::milliseconds(10));
-        }
-        if (not fs::exists(embedded_path)) {
-            auto file = std::ofstream(embedded_path);
-            file << embedded_nrnunits;
-        }
-        fs::remove(lock_dir);
-        return embedded_path;
+        throw std::runtime_error(err_msg.str());
     }
 };
 
