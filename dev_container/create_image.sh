b9b5b2fc075d2d7ac80f891f22099339afc4767f
Goran Jelic-Cizmek <goran.jelic-cizmek@epfl.ch>
2025-12-05 15:38:31 +0100

    Add various convenience scripts

---
+++ dev_container/create_image.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# create the base Podman/Docker image
+# default image name: neuron_container
+
+set -eu
+
+current_dir="$(cd "$(dirname "$0")"; pwd -P)"
+
+if command -v docker >& /dev/null; then
+    container_engine=docker
+elif command -v podman >& /dev/null; then
+    container_engine=podman
+else
+    echo "ERROR: docker or podman not found, you need to install one of those first and make them available in PATH"
+    exit 2
+fi
+
+IMAGE_NAME="${1:-neuron_container}"
+
+${container_engine} build -t "${IMAGE_NAME}" -f "${current_dir}/Dockerfile" "${current_dir}"
+
+echo "Created ${container_engine} image: ${IMAGE_NAME}"
---
+++ dev_container/create_volume_cache.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# one time script for creating a docker volume that hosts the Python and compiler cache
+# default volume name: cache_volume
+
+
+set -eu
+
+if command -v docker >& /dev/null; then
+    container_engine=docker
+elif command -v podman >& /dev/null; then
+    container_engine=podman
+else
+    echo "ERROR: docker or podman not found, you need to install one of those first and make them available in PATH"
+    exit 2
+fi
+
+VOLUME_NAME="${1:-cache_volume}"
+
+# Create image volume if it doesn't exist
+${container_engine} volume inspect "${VOLUME_NAME}" >/dev/null 2>&1 || docker volume create "${VOLUME_NAME}"
+
+echo "Created ${container_engine} volume: ${VOLUME_NAME}"
